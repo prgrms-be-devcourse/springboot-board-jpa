1f4444f5fd7d7219d5c97f544b01771a6da5087d
Kwonkyu <park2348190@naver.com>
2021-10-05 04:23:36 +0900

    project init.

---
+++ src/test/java/com/devcourse/bbs/BbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcourse.bbs;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
