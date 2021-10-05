0f051d79b9648d142eeae966878ee9069a0f020d
Taehyun Lee <taehyun16@naver.com>
2021-10-05 22:34:58 +0900

    [test] 게시글 CRUD 테스트 코드 작성

--- src/test/java/com/programmers/jpaboard/board/service/BoardServiceTest.java
+++ src/test/java/com/programmers/jpaboard/board/service/BoardServiceTest.java
@@ -1,34 +1,40 @@
 package com.programmers.jpaboard.board.service;
 
 import com.programmers.jpaboard.board.domian.Board;
+import com.programmers.jpaboard.board.repository.BoardRepository;
 import com.programmers.jpaboard.member.domain.Member;
+import com.programmers.jpaboard.member.repository.MemberRepository;
 import com.programmers.jpaboard.member.service.MemberService;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
-@TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
 class BoardServiceTest {
 
     @Autowired
     private BoardService boardService;
 
     @Autowired
+    private BoardRepository boardRepository;
+
+    @Autowired
     private MemberService memberService;
 
-    Member member;
+    @Autowired
+    private MemberRepository memberRepository;
 
-    @BeforeAll
-    void setUp(){
+    Member member;
+    Board board;
+
+    @BeforeEach
+    void setUp() {
         member = Member.builder()
                 .age(10)
                 .name("name")
@@ -36,6 +42,21 @@ class BoardServiceTest {
                 .build();
 
         memberService.saveMember(member);
+
+
+        // Given
+        board = Board.builder()
+                .title("SetupTitle")
+                .content("SetupContent")
+                .build();
+
+        boardService.saveBoard(board, member);
+    }
+
+    @AfterEach
+    void tearDown(){
+        boardRepository.deleteAll();
+        memberRepository.deleteAll();
     }
 
     @Test
@@ -50,6 +71,50 @@ class BoardServiceTest {
 
         // When
         Board actual = boardService.saveBoard(board, member);
+
+        // Then
+        assertThat(actual).isEqualTo(board);
+    }
+
+    @Test
+    @DisplayName("게시글 한 개를 조회한다.")
+    public void findOneTest() {
+        // When
+        Board actual = boardService.findOne(board.getId());
+
+        // Then
+        assertThat(actual).isEqualTo(board);
+    }
+
+    @Test
+    @DisplayName("모든 게시글을 조회한다")
+    public void findAllTest() {
+        // Given
+        Board newBoard = Board.builder()
+                .title("newTitle")
+                .content("newContent")
+                .build();
+        boardService.saveBoard(newBoard, member);
+
+        // When
+        List<Board> all = boardService.findAll();
+
+        // Then
+        assertThat(all).containsExactlyInAnyOrder(newBoard, board);
+    }
+
+    @Test
+    @DisplayName("게시글을 수정한다")
+    public void updateTest() {
+        Board newBoard = Board.builder()
+                .title("updatedTitle")
+                .content("updatedContent")
+                .build();
+
+        boardService.saveBoard(board, member);
+
+        // When
+        Board actual = boardService.updateBoard(board.getId(), newBoard);
 
         // Then
         assertThat(actual).isEqualTo(board);
