154bd2d6451ebe6ff7e335b2395349bda41e7c8c
samkimuel <samkim2626@gmail.com>
2021-10-06 01:50:56 +0900

    Initial commit

---
+++ src/main/java/com/devcourse/springbootboard/DevcousreSpringbootBoardApplication.java
@@ -0,0 +1,13 @@
+package com.devcourse.springbootboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcousreSpringbootBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcousreSpringbootBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devcourse/springbootboard/DevcousreSpringbootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcourse.springbootboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevcousreSpringbootBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
