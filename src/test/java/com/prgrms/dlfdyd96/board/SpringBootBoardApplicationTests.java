a385810bfb27658db1d63f322f6ef4c1ba8ebaed
dlfdyd96 <1yongs_@naver.com>
2021-10-07 01:48:42 +0900

    FEAT: initialize project

---
+++ src/test/java/com/prgrms/dlfdyd96/board/SpringBootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.dlfdyd96.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
