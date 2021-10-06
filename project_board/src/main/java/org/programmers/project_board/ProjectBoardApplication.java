2465850c2c7ce0a768ae6064183b5530af847b04
yekyeong <kwonliz6.6@gmail.com>
2021-10-06 22:52:17 +0900

    feat : init project

---
+++ project_board/src/main/java/org/programmers/project_board/ProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package org.programmers.project_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBoardApplication.class, args);
+    }
+
+}
---
+++ project_board/src/test/java/org/programmers/project_board/ProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.programmers.project_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
