15234a6e163721e8df5a58b5e015b3894eace068
yekyeong <kwonliz6.6@gmail.com>
2021-10-08 02:11:17 +0900

    feat : entity - dto 변환역할의 converter 추가

---
+++ project_board/src/main/java/org/programmers/project_board/converter/PostConverter.java
@@ -0,0 +1,38 @@
+package org.programmers.project_board.converter;
+
+import org.programmers.project_board.dto.PostDto;
+import org.programmers.project_board.entity.Post;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostConverter {
+
+    private final UserConverter userConverter;
+
+    public PostConverter(UserConverter userConverter) {
+        this.userConverter = userConverter;
+    }
+
+    // dto -> entity
+    public Post convertPost(PostDto postDto) {
+        Post post = new Post();
+        post.setId(postDto.getId());
+        post.setTitle(postDto.getTitle());
+        post.setContent(postDto.getContent());
+
+        post.setUser(userConverter.convertUser(postDto.getUserDto()));
+
+        return post;
+    }
+
+    // entity -> dto
+    public PostDto convertPostDto(Post post) {
+        return PostDto.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .userDto(userConverter.convertUserDto(post.getUser()))
+                .build();
+    }
+
+}
---
+++ project_board/src/main/java/org/programmers/project_board/converter/UserConverter.java
@@ -0,0 +1,31 @@
+package org.programmers.project_board.converter;
+
+import org.programmers.project_board.dto.UserDto;
+import org.programmers.project_board.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserConverter {
+
+    // dto -> entity
+    public User convertUser(UserDto userDto) {
+        User user = new User();
+        user.setId(userDto.getId());
+        user.setName(userDto.getName());
+        user.setAge(userDto.getAge());
+        user.setHobby(userDto.getHobby());
+
+        return user;
+    }
+
+    // entity -> dto
+    public UserDto convertUserDto(User user) {
+        return UserDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .age(user.getAge())
+                .hobby(user.getHobby())
+                .build();
+    }
+
+}
