99b30be6453ce01d5276639797ddbe5f4ee94766
dlfdyd96 <1yongs_@naver.com>
2021-10-08 01:07:11 +0900

    FEAT: Post, User Repository 구현

---
+++ src/main/java/com/prgrms/dlfdyd96/board/user/post/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package com.prgrms.dlfdyd96.board.user.post.repository;
+
+import com.prgrms.dlfdyd96.board.user.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
---
+++ src/main/java/com/prgrms/dlfdyd96/board/user/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.prgrms.dlfdyd96.board.user.user.repository;
+
+import com.prgrms.dlfdyd96.board.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
