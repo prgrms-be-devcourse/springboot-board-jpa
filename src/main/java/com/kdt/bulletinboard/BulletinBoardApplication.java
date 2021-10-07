efb15aefc26d53a15abee37eaaefe7f3b3846269
GetMapping <eonju.dev@gmail.com>
2021-10-07 23:08:34 +0900

    프로젝트 생성

---
+++ src/main/java/com/kdt/bulletinboard/BulletinBoardApplication.java
@@ -0,0 +1,13 @@
+package com.kdt.bulletinboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BulletinBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kdt/bulletinboard/BulletinBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdt.bulletinboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
