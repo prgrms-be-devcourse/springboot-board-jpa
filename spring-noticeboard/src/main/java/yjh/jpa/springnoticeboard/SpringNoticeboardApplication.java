728c47b74d1a188b9aea56ef5862617bd0794038
dbwlgna98@naver.com <dbwlgna98@naver.com>
2021-10-09 00:51:14 +0900

    Feat: Project 세팅 작업

---
+++ spring-noticeboard/src/main/java/yjh/jpa/springnoticeboard/SpringNoticeboardApplication.java
@@ -0,0 +1,13 @@
+package yjh.jpa.springnoticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNoticeboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNoticeboardApplication.class, args);
+    }
+
+}
---
+++ spring-noticeboard/src/test/java/yjh/jpa/springnoticeboard/SpringNoticeboardApplicationTests.java
@@ -0,0 +1,13 @@
+package yjh.jpa.springnoticeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNoticeboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
