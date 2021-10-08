a5ae24c56a703c87d4b2608379e7c5204edaf2f6
Taehyun Lee <taehyun16@naver.com>
2021-10-08 15:52:06 +0900

    [feat] add ExceptionHandler to handle Exception

--- src/main/java/com/programmers/jpaboard/board/exhandler/ErrorStatus.java
+++ src/main/java/com/programmers/jpaboard/board/exhandler/ErrorStatus.java
@@ -1,12 +1,14 @@
 package com.programmers.jpaboard.board.exhandler;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @Getter
 public enum ErrorStatus {
-    BOARD_NOT_FOUND(400, "Board Not Found"),
-    METHOD_ARGUMENT_NOT_VALID(400, "MethodArgument Not Valid");
+    BOARD_NOT_FOUND(HttpStatus.BAD_REQUEST.value(), "Board Not Found"),
+    METHOD_ARGUMENT_NOT_VALID(HttpStatus.BAD_REQUEST.value(), "MethodArgument Not Valid"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
 
     private final int code;
     private final String message;
