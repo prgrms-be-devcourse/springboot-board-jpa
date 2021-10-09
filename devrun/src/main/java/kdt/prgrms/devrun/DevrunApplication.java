4285174aab28dc3f9315a308d6d1cb761e1d8a44
devrunner21 <devrunner21@gmail.com>
2021-10-09 21:54:07 +0900

    [init] 프로젝트를 생성하라.

---
+++ devrun/src/main/java/kdt/prgrms/devrun/DevrunApplication.java
@@ -0,0 +1,13 @@
+package kdt.prgrms.devrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevrunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevrunApplication.class, args);
+    }
+
+}
---
+++ devrun/src/test/java/kdt/prgrms/devrun/DevrunApplicationTests.java
@@ -0,0 +1,13 @@
+package kdt.prgrms.devrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevrunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
