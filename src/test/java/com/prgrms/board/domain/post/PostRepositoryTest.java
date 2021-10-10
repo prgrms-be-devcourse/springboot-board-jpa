9e797155f37da37658ea1a1712125100d667c759
venzym <venzym@naver.com>
2021-10-10 23:29:46 +0900

    Feat: Repository 테스트 코드 작성

--- src/test/java/com/prgrms/board/domain/post/PostRepositoryTest.java
+++ src/test/java/com/prgrms/board/domain/post/PostRepositoryTest.java
@@ -1,4 +1,103 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.prgrms.board.domain.post;
+
+import com.prgrms.board.domain.user.User;
+import com.prgrms.board.domain.user.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
 class PostRepositoryTest {
-  
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = User.builder()
+                .name("이범진")
+                .age(27)
+                .hobby("축구")
+                .build();
+    }
+
+    @AfterEach
+    void tearDown() {
+        postRepository.deleteAll();
+    }
+
+    @DisplayName("게시물 생성 테스트")
+    @Test
+    void createPost() {
+        //given
+        Post post = Post.builder()
+                .title("첫번째 게시글")
+                .content("게시글을 작성했습니다.")
+                .user(user)
+                .build();
+
+        //when
+        postRepository.save(post);
+
+        //then
+        Post findPost = postRepository.findById(post.getId()).get();
+
+        assertThat(findPost.getTitle()).isEqualTo(post.getTitle());
+        assertThat(findPost.getContent()).isEqualTo(post.getContent());
+    }
+
+    @DisplayName("게시물 조회 테스트")
+    @Test
+    void selectPost() {
+        //given
+        Post post = Post.builder()
+                .title("첫번째 게시글")
+                .content("게시글을 작성했습니다.")
+                .user(user)
+                .build();
+
+        //when
+        postRepository.save(post);
+
+        //then
+        List<Post> postList = postRepository.findAll();
+
+        assertThat(postList).hasSize(1);
+    }
+
+    @DisplayName("게시물 수정 테스트")
+    @Test
+    @Transactional
+    void updatePost() {
+        //given
+        Post post = Post.builder()
+                .title("첫번째 게시글")
+                .content("게시글을 작성했습니다.")
+                .user(user)
+                .build();
+        postRepository.save(post);
+
+        //when
+        post.changeTitle("수정된 게시글");
+        post.changeContent("게시글을 수정했습니다.");
+
+        //then
+        Post updated = postRepository.getById(post.getId());
+
+        assertThat(updated.getTitle()).isEqualTo(post.getTitle());
+        assertThat(updated.getContent()).isEqualTo(post.getContent());
+    }
 }
--- src/test/java/com/prgrms/board/domain/user/UserRepositoryTest.java
+++ src/test/java/com/prgrms/board/domain/user/UserRepositoryTest.java
@@ -1,4 +1,101 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.prgrms.board.domain.user;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
 class UserRepositoryTest {
-  
+
+    @Autowired
+    UserRepository userRepository;
+
+    @AfterEach
+    void tearDown() {
+        userRepository.deleteAll();
+    }
+
+    @DisplayName("회원 생성 테스트")
+    @Test
+    void createUser() {
+        //given
+        User user = User.builder()
+                .name("이범진")
+                .age(27)
+                .hobby("게임")
+                .build();
+
+        //when
+        userRepository.save(user);
+
+        //then
+        User findUser = userRepository.findById(user.getId()).get();
+        assertThat(findUser.getName()).isEqualTo(user.getName());
+        assertThat(findUser.getAge()).isEqualTo(user.getAge());
+        assertThat(findUser.getHobby()).isEqualTo(user.getHobby());
+    }
+
+    @DisplayName("회원 조회 테스트")
+    @Test
+    void selectUser() {
+        //given
+        User user = User.builder()
+                .name("이범진")
+                .age(27)
+                .hobby("게임")
+                .build();
+
+        //when
+        userRepository.save(user);
+
+        //then
+        List<User> userList = userRepository.findAll();
+        assertThat(userList).hasSize(1);
+    }
+
+    @DisplayName("회원 수정 테스트")
+    @Test
+    @Transactional
+    void updateUser() {
+        //given
+        User user = User.builder()
+                .name("이범진")
+                .age(27)
+                .hobby("게임")
+                .build();
+        userRepository.save(user);
+
+        //when
+        User findUser = userRepository.findById(user.getId()).get();
+        findUser.changeHobby("눕기");
+
+        //then
+        User updateUser = userRepository.findById(user.getId()).get();
+        assertThat(updateUser.getHobby()).isEqualTo(findUser.getHobby());
+    }
+
+    @DisplayName("회원 삭제 테스트")
+    @Test
+    void deleteUser() {
+        //given
+        User user = User.builder()
+                .name("이범진")
+                .age(27)
+                .hobby("게임")
+                .build();
+        userRepository.delete(user);
+
+        //when
+        List<User> userList = userRepository.findAll();
+
+        //then
+        assertThat(userList).hasSize(0);
+    }
 }
