d29114e3c1f6951473de68bacdf2bf7310a33a53
devrunner21 <devrunner21@gmail.com>
2021-10-10 21:38:55 +0900

    [fix] PostForm 직렬화를 위해 생성자를 추가하라.

--- devrun/src/main/java/kdt/prgrms/devrun/post/dto/PostForm.java
+++ devrun/src/main/java/kdt/prgrms/devrun/post/dto/PostForm.java
@@ -2,13 +2,17 @@ package kdt.prgrms.devrun.post.dto;
 
 import kdt.prgrms.devrun.domain.Post;
 import kdt.prgrms.devrun.domain.User;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostForm {
 
     @NotBlank
