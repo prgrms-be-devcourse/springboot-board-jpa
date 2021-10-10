29d4f70e0ae6c3a0faaf0f444012f38f6b8dfc0f
eunseo2 <20180972@sungshin.ac.kr>
2021-10-10 11:24:10 +0900

    Feat: Dto -> Entity 변경 Converter 구현

---
+++ springBoard/src/main/java/com/programmers/springboard/converter/PostConverter.java
@@ -0,0 +1,33 @@
+package com.programmers.springboard.converter;
+
+import com.programmers.springboard.dto.PostDto;
+import com.programmers.springboard.dto.UserDto;
+import com.programmers.springboard.model.Post;
+import com.programmers.springboard.model.User;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class PostConverter {
+    public Post convertPost(PostDto postDto) {
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setContent(postDto.getContent());
+        post.setUser(this.convertUser(postDto.getUserDto()));
+        post.setCreatedBy(postDto.getUserDto().getName());
+        post.setCreatedAt(LocalDateTime.now());
+        return post;
+    }
+
+    public User convertUser(UserDto userDto) {
+        User user = new User();
+        user.setName(userDto.getName());
+        user.setAge(userDto.getAge());
+        user.setHobby(userDto.getHobby());
+        user.setCreatedBy(userDto.getName());
+        user.setCreatedAt(LocalDateTime.now());
+        return user;
+    }
+
+}
