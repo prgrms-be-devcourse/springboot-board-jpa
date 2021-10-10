78b2d54cfb96810eccf78c4784a94296267179e6
eunseo2 <20180972@sungshin.ac.kr>
2021-10-10 11:07:17 +0900

    Feat: Post 엔티티 추가

---
+++ springBoard/src/main/java/com/programmers/springboard/model/Post.java
@@ -0,0 +1,36 @@
+package com.programmers.springboard.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "posts")
+@Getter
+@Setter
+public class Post extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    public void setUser(User user) {
+        if (Objects.nonNull(this.user)) {
+            this.user.getPosts().remove(this);
+        }
+        this.user = user;
+        user.getPosts().add(this);
+    }
+
+}
