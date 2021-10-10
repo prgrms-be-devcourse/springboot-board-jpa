cb8860aaf21cec941a607be587fc243eb7c436f8
devrunner21 <devrunner21@gmail.com>
2021-10-10 18:45:34 +0900

    [feat] UserRepository를 생성하고 "LoginId로 조회" 메서드를 추가하라.

---
+++ devrun/src/main/java/kdt/prgrms/devrun/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package kdt.prgrms.devrun.user.repository;
+
+import kdt.prgrms.devrun.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUsersByLoginId(String loginId);
+
+}
