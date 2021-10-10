c29ae9ce2f0a0a2a7111049d6dad2e63b6ccd3e4
sunH0 <sunho6824@daum.net>
2021-10-10 23:35:38 +0900

    Feat: project init

---
+++ src/test/java/com/kdt/devboard/DevBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdt.devboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
