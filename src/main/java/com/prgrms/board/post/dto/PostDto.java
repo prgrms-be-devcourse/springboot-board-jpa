d8975b2a7cc7b87959c1e825466b3258c789e8ac
venzym <venzym@naver.com>
2021-10-12 03:54:29 +0900

    Feat: Dto 추가

--- src/main/java/com/prgrms/board/post/dto/PostDto.java
+++ src/main/java/com/prgrms/board/post/dto/PostDto.java
@@ -1,2 +1,33 @@
-package com.prgrms.board.post.dto;public class PostDto {
+package com.prgrms.board.post.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PostDto {
+    private Long id;
+    private String title;
+    private String content;
+
+    private UserDto userDto;
+
+    protected PostDto() {
+    }
+
+    @Builder
+    public PostDto(Long id, String title, String content, UserDto userDto) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.userDto = userDto;
+    }
+
+    public void changeTitle(String title) {
+        this.title = title;
+    }
+
+    public void changeContent(String content) {
+        this.content = content;
+    }
 }
--- src/main/java/com/prgrms/board/post/dto/UserDto.java
+++ src/main/java/com/prgrms/board/post/dto/UserDto.java
@@ -1,2 +1,23 @@
-package com.prgrms.board.post.dto;public class UserDto {
+package com.prgrms.board.post.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class UserDto {
+    private Long id;
+    private String name;
+    private int age;
+    private String hobby;
+
+    protected UserDto() {
+    }
+
+    @Builder
+    public UserDto(Long id, String name, int age, String hobby) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.hobby = hobby;
+    }
 }
