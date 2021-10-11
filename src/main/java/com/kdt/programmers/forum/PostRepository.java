de8d0f5ad5f5eb9b7d81bf79346d245fb59a0e07
Sangmin Lee <sangminlee7648@gmail.com>
2021-10-11 17:54:58 +0900

    Style : 사용하지 않는 객체 import문 삭제

--- src/main/java/com/kdt/programmers/forum/PostRepository.java
+++ src/main/java/com/kdt/programmers/forum/PostRepository.java
@@ -2,9 +2,6 @@ package com.kdt.programmers.forum;
 
 import com.kdt.programmers.forum.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
