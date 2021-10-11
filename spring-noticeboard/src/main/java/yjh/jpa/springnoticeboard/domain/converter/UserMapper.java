8d39e63703f4f4469d88d297017e289cd3011e38
dbwlgna98@naver.com <dbwlgna98@naver.com>
2021-10-12 03:05:00 +0900

    Fixed: UserMapper 오류 수정

--- spring-noticeboard/src/main/java/yjh/jpa/springnoticeboard/domain/converter/UserMapper.java
+++ spring-noticeboard/src/main/java/yjh/jpa/springnoticeboard/domain/converter/UserMapper.java
@@ -4,7 +4,9 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
+import yjh.jpa.springnoticeboard.domain.dto.PostDto;
 import yjh.jpa.springnoticeboard.domain.dto.UserDto;
+import yjh.jpa.springnoticeboard.domain.entity.Post;
 import yjh.jpa.springnoticeboard.domain.entity.User;
 
 import java.util.Collections;
@@ -15,8 +17,14 @@ public interface UserMapper {
 
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
+//    @Mapping(target = "posts", expression = "java(new ArrayList<Post>())")
     User userDtoToEntity(UserDto userDto);
 
+//    @Mapping(target = "user.posts", expression = "java(UserMapper.INSTANCE.toDtoList(user.posts))")
     UserDto userEntityToDto(User user);
+
+    List<Post> toDtoList(List<PostDto> posts);
+
+    List<PostDto> toEntityList(List<Post> postDtos);
 
 }
