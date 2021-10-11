29cf98b1c7d03c10f61f749ff49c769696cdc0d9
eunseo2 <20180972@sungshin.ac.kr>
2021-10-11 14:28:16 +0900

    Feat: PostService 추가

--- springBoard/src/main/java/com/programmers/springboard/dto/PostDto.java
+++ springBoard/src/main/java/com/programmers/springboard/dto/PostDto.java
@@ -2,10 +2,15 @@ package com.programmers.springboard.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Builder
+@ToString
+@Setter
 public class PostDto {
+    private Long id;
     private String title;
     private String content;
     private UserDto userDto;
---
+++ springBoard/src/main/java/com/programmers/springboard/service/PostService.java
@@ -0,0 +1,51 @@
+package com.programmers.springboard.service;
+
+import com.programmers.springboard.config.ErrorCode;
+import com.programmers.springboard.converter.PostConverter;
+import com.programmers.springboard.dto.PostDto;
+import com.programmers.springboard.model.Post;
+import com.programmers.springboard.repository.PostRepository;
+import javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private PostConverter postConverter;
+
+    @Transactional
+    public Long save(PostDto dto) {
+        Post post = postConverter.convertPost(dto);
+        Post entity = postRepository.save(post);
+        return entity.getId();
+    }
+
+    @Transactional
+    public PostDto findOne(Long id) throws NotFoundException {
+        return postRepository.findById(id)
+                .map(postConverter::convertPostDto)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.POSTS_NOT_FOUND.getMessage()));
+    }
+
+    @Transactional
+    public Page<PostDto> findAll(Pageable pageable) {
+        return postRepository.findAll(pageable)
+                .map(postConverter::convertPostDto);
+    }
+
+    @Transactional
+    public Long update(PostDto dto, Long id) throws NotFoundException {
+        var post = postRepository.findById(id).orElseThrow(() -> new NotFoundException(ErrorCode.POSTS_NOT_FOUND.getMessage()));
+        post.update(dto.getTitle(), dto.getContent(), postConverter.convertUser(dto.getUserDto()));
+        return id;
+    }
+
+
+}
