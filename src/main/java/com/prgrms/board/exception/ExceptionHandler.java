7edc9009007bf6136c595790b7a00ad30e28d143
venzym <venzym@naver.com>
2021-10-12 04:01:18 +0900

    Feat: 예외 처리 및 오류 응답 추가

--- src/main/java/com/prgrms/board/exception/ExceptionHandler.java
+++ src/main/java/com/prgrms/board/exception/ExceptionHandler.java
@@ -4,28 +4,27 @@ import com.prgrms.board.post.api.ErrorResponse;
 import javassist.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
-public class ServerExceptionHandler {
-    @ExceptionHandler(IllegalArgumentException.class)
+public class ExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse illegalArgumentHandler(IllegalArgumentException e) {
         log.error("IllegalArgumentHandler : {}", e.getMessage());
         return new ErrorResponse(HttpStatus.BAD_REQUEST, "400", e.getMessage());
     }
 
-    @ExceptionHandler(NotFoundException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse notFoundExceptionHandler(NotFoundException e) {
         log.error("NotFoundExceptionHandler : {}", e.getMessage());
         return new ErrorResponse(HttpStatus.NOT_FOUND, "404", e.getMessage());
     }
 
-    @ExceptionHandler(Exception.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse exceptionHandler(Exception e) {
         log.error("exceptionHandler : {}", e.getMessage());
--- src/main/java/com/prgrms/board/post/api/ErrorResponse.java
+++ src/main/java/com/prgrms/board/post/api/ErrorResponse.java
@@ -1,2 +1,23 @@
-package com.prgrms.board.post.api;public class ErrorResponse {
+package com.prgrms.board.post.api;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ErrorResponse {
+    private final LocalDateTime timestamp;
+    private final int status;
+    private final String error;
+    private final String message;
+
+    @Builder
+    public ErrorResponse(HttpStatus httpStatus, String errCode, String message) {
+        this.timestamp = LocalDateTime.now();
+        this.status = httpStatus.value();
+        this.error = errCode;
+        this.message = message;
+    }
 }
