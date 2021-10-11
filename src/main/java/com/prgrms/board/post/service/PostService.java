130de096fb8de81bdca88ab2b06fb91ff69ed9e4
venzym <venzym@naver.com>
2021-10-12 03:55:51 +0900

    Feat: PostService 추가

--- src/main/java/com/prgrms/board/post/service/PostService.java
+++ src/main/java/com/prgrms/board/post/service/PostService.java
@@ -1,7 +1,13 @@
 package com.prgrms.board.post.service;
 
+import com.prgrms.board.converter.PostConverter;
+import com.prgrms.board.domain.post.Post;
 import com.prgrms.board.domain.post.PostRepository;
+import com.prgrms.board.post.dto.PostDto;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,25 +15,35 @@ import org.springframework.transaction.annotation.Transactional;
 public class PostService {
 
     @Autowired
-    PostRepository postRepository;
+    private PostRepository postRepository;
+
+    @Autowired
+    private PostConverter postConverter;
 
     @Transactional
-    public void save() {
-
+    public Long save(PostDto postDto) {
+        Post post = postConverter.convertPost(postDto);
+        return postRepository.save(post).getId();
     }
 
     @Transactional
-    public void findAll() {
-
+    public Long update(PostDto postDto) throws NotFoundException {
+        Post findPost = postRepository.findById(postDto.getId())
+                .map(post -> postConverter.convertPost(postDto))
+                .orElseThrow(() -> new NotFoundException("게시물을 찾을 수 없습니다."));
+        return findPost.getId();
     }
 
     @Transactional
-    public void findOne() {
-
+    public Page<PostDto> findAll(Pageable pageable) {
+        return postRepository.findAll(pageable)
+                .map(postConverter::convertPostDto);
     }
 
     @Transactional
-    public void update() {
-
+    public PostDto findOne(Long id) throws NotFoundException {
+        return postRepository.findById(id)
+                .map(postConverter::convertPostDto)
+                .orElseThrow(() -> new NotFoundException("게시물을 찾을 수 없습니다."));
     }
 }
