fb38e0a29f7db23dedfab9e19ef50787906fa71b
LSH0809 <skyey94@naver.com>
2021-10-11 10:38:05 +0900

    [feat] : Dto @Valid 추가

--- SpringBoot-Board/src/main/java/com/homework/springbootboard/dto/PostDto.java
+++ SpringBoot-Board/src/main/java/com/homework/springbootboard/dto/PostDto.java
@@ -1,16 +1,28 @@
 package com.homework.springbootboard.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostDto {
     private Long id;
+
+    @NotNull(message = "Title is mandatory")
+    @Size(max = 100)
     private String title;
+
+    @NotNull(message = "Content is mandatory")
     private String content;
+
+    @NotNull
+    @JsonProperty(value = "user")
     private UserDto userDto;
 
     @Builder
--- SpringBoot-Board/src/main/java/com/homework/springbootboard/dto/UserDto.java
+++ SpringBoot-Board/src/main/java/com/homework/springbootboard/dto/UserDto.java
@@ -1,19 +1,31 @@
 package com.homework.springbootboard.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
+import javax.validation.constraints.*;
 import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserDto {
     private Long id;
+
+    @NotNull(message = "Name is mandatory")
     private String name;
+
+    @Range(min = 1, max = 100, message = "Age is between 0 and 100")
     private int age;
+
+    @Pattern(regexp = "^[a-zA-Z]*$", message = "Hobby should be written by English")
     private String hobby;
+
+    @JsonProperty(value = "post")
     private List<PostDto> postDtos;
 
     @Builder
