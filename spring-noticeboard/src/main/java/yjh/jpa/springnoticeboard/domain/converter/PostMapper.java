55a25e4d9138ee58ec5994a52d7c4eaf7877c845
dbwlgna98@naver.com <dbwlgna98@naver.com>
2021-10-11 22:02:53 +0900

    Feat: PostMapper 추가

---
+++ spring-noticeboard/src/main/java/yjh/jpa/springnoticeboard/domain/converter/PostMapper.java
@@ -0,0 +1,18 @@
+package yjh.jpa.springnoticeboard.domain.converter;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
+import yjh.jpa.springnoticeboard.domain.dto.PostDto;
+import yjh.jpa.springnoticeboard.domain.entity.Post;
+
+@Mapper
+public interface PostMapper {
+
+    PostMapper INSTANCE = Mappers.getMapper(PostMapper.class);
+
+    Post postDtoToEntity(PostDto postDto);
+
+    PostDto postToDto(Post post);
+}
