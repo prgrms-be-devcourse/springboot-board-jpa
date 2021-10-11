ff34b5365dc7d26f4460c4f7381b473f17cca6d9
Sangmin Lee <sangminlee7648@gmail.com>
2021-10-11 16:41:30 +0900

    Feat : User 엔티티 추가

---
+++ src/main/java/com/kdt/programmers/forum/domain/User.java
@@ -0,0 +1,27 @@
+package com.kdt.programmers.forum.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User extends CreationBaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @Column(name = "hobby")
+    private String hobby;
+
+}
