e6bcf63016f626f4389fd1e66e471e5b0586dbd8
dbwlgna98@naver.com <dbwlgna98@naver.com>
2021-10-11 21:57:57 +0900

    Feat: PostDto 추가

---
+++ spring-noticeboard/src/main/java/yjh/jpa/springnoticeboard/domain/dto/PostDto.java
@@ -0,0 +1,20 @@
+package yjh.jpa.springnoticeboard.domain.dto;
+
+import lombok.*;
+import yjh.jpa.springnoticeboard.domain.entity.User;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+public class PostDto {
+
+    private Long id;
+    private String title;
+    private String content;
+
+    private User user;
+}
