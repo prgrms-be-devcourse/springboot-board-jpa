e905893d15128d446332f5be3e64b77cd3abb29b
venzym <venzym@naver.com>
2021-10-12 04:00:21 +0900

    Feat: ApiResponse 추가

--- src/main/java/com/prgrms/board/post/api/ApiResponse.java
+++ src/main/java/com/prgrms/board/post/api/ApiResponse.java
@@ -1,2 +1,29 @@
-package com.prgrms.board.post.api;public class ApiResponse {
+package com.prgrms.board.post.api;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class ApiResponse<T> {
+    private HttpStatus httpStatus;
+
+    private T data;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime serverDatetime;
+
+    public ApiResponse(HttpStatus httpStatus, T data) {
+        this.httpStatus = httpStatus;
+        this.data = data;
+        this.serverDatetime = LocalDateTime.now();
+    }
+
+    public static <T> ApiResponse<T> ok(T data) {
+        return new ApiResponse<T>(HttpStatus.OK, data);
+    }
 }
