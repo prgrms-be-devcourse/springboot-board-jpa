53c31ff1444d1733d1dd3909be3a38323f585c3e
dbwlgna98@naver.com <dbwlgna98@naver.com>
2021-10-12 19:14:43 +0900

    Fixed: MapStruct mapper 사용 중지

--- spring-noticeboard/src/main/java/yjh/jpa/springnoticeboard/domain/converter/PostMapper.java
+++ spring-noticeboard/src/main/java/yjh/jpa/springnoticeboard/domain/converter/PostMapper.java
@@ -7,15 +7,12 @@ import org.mapstruct.factory.Mappers;
 import yjh.jpa.springnoticeboard.domain.dto.PostDto;
 import yjh.jpa.springnoticeboard.domain.entity.Post;
 
-@Mapper
 public interface PostMapper {
 
     PostMapper INSTANCE = Mappers.getMapper(PostMapper.class);
 
-//    @Mapping(target = "postDto.user", expression = "java(UserMapper.INSTANCE.userDtoToEntity(postDto.user))")
     Post postDtoToEntity(PostDto postDto);
 
-//    @Mapping(target = "post.user", expression = "java(UserMapper.INSTANCE.userEntityToDto(post.user))")
     PostDto postToDto(Post post);
 
 }
--- spring-noticeboard/src/main/java/yjh/jpa/springnoticeboard/domain/converter/UserMapper.java
+++ spring-noticeboard/src/main/java/yjh/jpa/springnoticeboard/domain/converter/UserMapper.java
@@ -12,15 +12,12 @@ import yjh.jpa.springnoticeboard.domain.entity.User;
 import java.util.Collections;
 import java.util.List;
 
-@Mapper
 public interface UserMapper {
 
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
-//    @Mapping(target = "posts", expression = "java(new ArrayList<Post>())")
     User userDtoToEntity(UserDto userDto);
 
-//    @Mapping(target = "user.posts", expression = "java(UserMapper.INSTANCE.toDtoList(user.posts))")
     UserDto userEntityToDto(User user);
 
     List<Post> toDtoList(List<PostDto> posts);
