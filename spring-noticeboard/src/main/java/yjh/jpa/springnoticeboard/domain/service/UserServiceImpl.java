519c8db051f25b99b2a973c43207c8f02f6a69c1
dbwlgna98@naver.com <dbwlgna98@naver.com>
2021-10-12 19:09:32 +0900

    Fixed: UserService 구현체 최종 오류 수정

--- spring-noticeboard/src/main/java/yjh/jpa/springnoticeboard/domain/service/UserServiceImpl.java
+++ spring-noticeboard/src/main/java/yjh/jpa/springnoticeboard/domain/service/UserServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import yjh.jpa.springnoticeboard.domain.converter.Converter;
 import yjh.jpa.springnoticeboard.domain.converter.UserMapper;
 import yjh.jpa.springnoticeboard.domain.dto.PostDto;
 import yjh.jpa.springnoticeboard.domain.dto.UserDto;
@@ -20,16 +21,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService{
 
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    Converter converter;
+
     @Transactional
     @Override
     public Long createUser(UserDto userDto) {
-        User user = UserMapper.INSTANCE.userDtoToEntity(userDto);
+        User user = converter.userDtoToEntity(userDto);
         user.setCreatedBy("admin");
         user.setCratedAt(LocalDateTime.now());
         User entity = userRepository.save(user);
@@ -40,7 +45,7 @@ public class UserServiceImpl implements UserService{
     @Override
     public Long updateUser(Long userId, UserDto userDto) throws NotFoundException {
         UserDto update = userRepository.findById(userId)
-                .map(user -> UserMapper.INSTANCE.userEntityToDto(user))
+                .map(user -> converter.userToUserDto(user))
                 .orElseThrow(()->new NotFoundException("해당 유저를 찾을 수 없습니다."));
         update.setAge(userDto.getAge());
         update.setHobby(userDto.getHobby());
@@ -52,14 +57,14 @@ public class UserServiceImpl implements UserService{
     @Override
     public Page<Object> findAll(Pageable pageable) {
         return userRepository.findAll(pageable)
-                .map(user -> UserMapper.INSTANCE.userEntityToDto(user));
+                .map(user -> converter.userToUserDto(user));
     }
 
     @Transactional(readOnly = true)
     @Override
     public UserDto findUser(Long userId) throws NotFoundException {
         UserDto userDto = userRepository.findById(userId)
-                .map(user -> UserMapper.INSTANCE.userEntityToDto(user))
+                .map(user -> converter.userToUserDto(user))
                 .orElseThrow(()->new NotFoundException("해당 유저를 찾을 수 없습니다."));
         return userDto;
     }
