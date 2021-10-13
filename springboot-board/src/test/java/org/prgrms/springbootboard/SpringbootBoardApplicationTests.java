c042a50b4a8f07bdffe8272abfd401750eca3e1a
jummi <98qkrwjdal@naver.com>
2021-10-14 05:28:37 +0900

    Initial Commit

---
+++ springboot-board/src/test/java/org/prgrms/springbootboard/SpringbootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prgrms.springbootboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
