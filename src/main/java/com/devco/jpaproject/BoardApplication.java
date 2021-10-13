ebc551137efd28cfb047cd586300abe3724de700
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-10-13 15:41:10 +0900

    feat: EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/devco/jpaproject/BoardApplication.java
+++ src/main/java/com/devco/jpaproject/BoardApplication.java
@@ -2,8 +2,10 @@ package com.devco.jpaproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BoardApplication {
 
 	public static void main(String[] args) {
