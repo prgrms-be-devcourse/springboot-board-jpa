b4a48d78fbe33b1376bad582aa58eb65753f8d64
wooko5 <wooko5@naver.com>
2021-10-13 17:40:01 +0900

    Create Post Entity

---
+++ SpringJPA-Board/src/main/java/com/programmers/board/domain/Post.java
@@ -0,0 +1,17 @@
+package com.programmers.board.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "post")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "title", nullable = false, length = 50)
+    private String title;
+
+    @Column(name = "content", nullable = false, length = 200)
+    private String content;
+}
