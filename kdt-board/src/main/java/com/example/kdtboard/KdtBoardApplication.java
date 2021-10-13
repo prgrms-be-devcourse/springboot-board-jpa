16cff82aad854bf059cefc7aaa480dc1d507207b
kwhyo <rkgmlgygml@naver.com>
2021-10-13 16:18:59 +0900

    default 세팅

---
+++ kdt-board/src/main/java/com/example/kdtboard/KdtBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.kdtboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdtBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdtBoardApplication.class, args);
+    }
+
+}
