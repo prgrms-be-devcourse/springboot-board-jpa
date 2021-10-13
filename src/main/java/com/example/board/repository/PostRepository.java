d26651a281901a926f976e849c71fca483c00e40
BeautterLife <pgh9801@gmail.com>
2021-10-13 18:06:28 +0900

    feat/@Repository:Spring Data JPA 사용

---
+++ src/main/java/com/example/board/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package com.example.board.repository;
+
+import com.example.board.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Integer> {
+
+}
