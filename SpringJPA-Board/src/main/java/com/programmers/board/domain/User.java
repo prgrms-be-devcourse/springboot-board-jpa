06fc0b2f281dca63b98a9ff63cfb35f33a05cbae
wooko5 <wooko5@naver.com>
2021-10-13 17:32:03 +0900

    create User Entity

---
+++ SpringJPA-Board/src/main/java/com/programmers/board/domain/User.java
@@ -0,0 +1,25 @@
+package com.programmers.board.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // Auto 전략으로 기본키를 맵핑할 것이다
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 30)
+    private String name;
+
+    @Column(name = "age", nullable = false)
+    private int age;
+
+    @Column(name = "hobby")
+    private String hobby;
+}
