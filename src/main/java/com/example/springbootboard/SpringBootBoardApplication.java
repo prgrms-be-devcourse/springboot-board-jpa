848ce85748604a159d9073e2fa229b56c927cb88
hyunjunkim <kdghj1002@gmail.com>
2021-10-13 17:52:25 +0900

    프로젝트 세팅

---
+++ src/main/java/com/example/springbootboard/SpringBootBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBoardApplication.class, args);
+    }
+
+}
