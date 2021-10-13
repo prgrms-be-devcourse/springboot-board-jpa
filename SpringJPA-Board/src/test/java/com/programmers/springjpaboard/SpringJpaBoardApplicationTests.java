bfaf0d37722f865997ef4b808601b55d502a1d4e
wooko5 <wooko5@naver.com>
2021-10-13 17:05:29 +0900

    init project.

---
+++ SpringJPA-Board/src/test/java/com/programmers/springjpaboard/SpringJpaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.springjpaboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
