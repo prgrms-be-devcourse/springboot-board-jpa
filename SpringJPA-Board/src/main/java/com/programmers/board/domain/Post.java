93144ee223446ce144bc4a49c9d1c728b535b8aa
wooko5 <wooko5@naver.com>
2021-10-13 17:41:17 +0900

    Update Post, Getter/Setter

--- SpringJPA-Board/src/main/java/com/programmers/board/domain/Post.java
+++ SpringJPA-Board/src/main/java/com/programmers/board/domain/Post.java
@@ -1,9 +1,14 @@
 package com.programmers.board.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "post")
+@Getter
+@Setter
 public class Post extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
