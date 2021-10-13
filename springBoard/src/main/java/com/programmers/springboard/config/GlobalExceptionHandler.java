2f36e90935a8290aa4d040e326d6dd8f30d3bc9f
eunseo2 <20180972@sungshin.ac.kr>
2021-10-13 18:38:01 +0900

    Style: 불필요한 주석 및 롬복 삭제

--- springBoard/src/main/java/com/programmers/springboard/config/GlobalExceptionHandler.java
+++ springBoard/src/main/java/com/programmers/springboard/config/GlobalExceptionHandler.java
@@ -11,27 +11,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 public class GlobalExceptionHandler {
 
-    /*
-     * 400 BAD_REQUEST: 잘못된 요청
-     */
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity IllegalArgumentHandler(IllegalArgumentException e) {
         log.error("illegalArgumentHandler : {}", e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
-    /*
-     * 404 NOT_FOUND: 리소스를 찾을 수 없음
-     */
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity notFoundHandler(NotFoundException e) {
         log.error("notFoundHandler : {}", e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
-    /*
-     * 500 INTERNAL_SERVER_ERROR: 내부 서버 오류
-     */
     @ExceptionHandler(Exception.class)
     public ResponseEntity internalServerErrorHandler(Exception e) {
         log.error("internalServerErrorHandler : {}", e.getMessage());
--- springBoard/src/main/java/com/programmers/springboard/dto/PostResponseDto.java
+++ springBoard/src/main/java/com/programmers/springboard/dto/PostResponseDto.java
@@ -2,13 +2,11 @@ package com.programmers.springboard.dto;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Builder
-@Setter
 public class PostResponseDto {
     private Long id;
     private String title;
