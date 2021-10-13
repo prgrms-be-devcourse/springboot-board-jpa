f48777e9b9f71c6dff6ad2d996c2b6209bb60cca
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-10-13 15:37:44 +0900

    feat: converter 수정

---
+++ src/main/java/com/devco/jpaproject/service/converter/Converter.java
@@ -0,0 +1,54 @@
+package com.devco.jpaproject.service.converter;
+
+import com.devco.jpaproject.controller.dto.PostRequestDto;
+import com.devco.jpaproject.controller.dto.PostResponseDto;
+import com.devco.jpaproject.controller.dto.UserResponseDto;
+import com.devco.jpaproject.domain.Post;
+import com.devco.jpaproject.domain.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Converter {
+
+    /**
+     * dto를 entity로 변환
+     */
+    public Post toPostEntity(PostRequestDto dto, User writer) {
+        var post = Post.builder()
+                .content(dto.getContent())
+                .title(dto.getTitle())
+                .build();
+
+        post.setWriter(writer);
+
+        return post;
+    }
+
+    /**
+     * 영속 상태 entity를 dto 객체로 변환
+     */
+    public PostResponseDto toPostResponseDto(Post post) {
+        return PostResponseDto.builder()
+                .content(post.getContent())
+                .title(post.getTitle())
+                .postId(post.getId())
+                .writer(this.toUserResponseDto(post.getWriter()))
+                .build();
+    }
+
+    public UserResponseDto toUserResponseDto(User user) {
+        return UserResponseDto.builder()
+                .age(user.getAge())
+                .hobby(user.getHobby())
+                .name(user.getName())
+                .build();
+    }
+
+    public User toUserEntity(UserResponseDto userDto) {
+        return User.builder()
+                .age(userDto.getAge())
+                .name(userDto.getName())
+                .hobby(userDto.getHobby())
+                .build();
+    }
+}
