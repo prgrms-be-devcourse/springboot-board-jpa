2bdfe454e7d3b40209ebb9b6ff59fbd47ddfa135
Sangmin Lee <sangminlee7648@gmail.com>
2021-10-13 18:12:18 +0900

    Refactor : 요청 DTO 이외 DTO 내부의 검증 로직 삭제

--- src/main/java/com/kdt/programmers/forum/transfer/PostDto.java
+++ src/main/java/com/kdt/programmers/forum/transfer/PostDto.java
@@ -4,36 +4,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 public class PostDto {
 
-    private Long id;
-    private String title;
-    private String content;
+    private final Long id;
+    private final String title;
+    private final String content;
 
     public PostDto(String title, String content) {
-        validateTitle(title);
+        this.id = null;
         this.title = title;
         this.content = content;
     }
 
     public PostDto(Long id, String title, String content) {
-        validateId(id);
         this.id = id;
-
-        validateTitle(title);
         this.title = title;
         this.content = content;
-    }
-
-    private void validateId(Long id) {
-        if (id <= 0)
-            throw new IllegalArgumentException("id must be larger than 0");
-    }
-
-    private void validateTitle(String title) {
-        final int TITLE_LENGTH_LIMIT = 60;
-        if (title.length() > TITLE_LENGTH_LIMIT)
-            throw new IllegalArgumentException("length of title must be less than " + TITLE_LENGTH_LIMIT);
     }
 }
