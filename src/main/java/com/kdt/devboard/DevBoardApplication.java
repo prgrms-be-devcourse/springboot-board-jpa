8cb226cd8fa3c242c8b729621c000c0effa92fd3
sunH0 <sunho6824@daum.net>
2021-10-14 02:41:03 +0900

    Feat: project init

---
+++ src/main/java/com/kdt/devboard/DevBoardApplication.java
@@ -0,0 +1,13 @@
+package com.kdt.devboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevBoardApplication.class, args);
+    }
+
+}
