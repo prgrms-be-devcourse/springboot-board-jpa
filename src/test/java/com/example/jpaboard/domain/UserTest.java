e0e59c164b68018e90a27907a84532d472bd0982
awesomeo184 <63030569+awesomeo184@users.noreply.github.com>
2021-10-13 23:54:07 +0900

    test: p6spy 의존성 추가, User, Post 기능 테스트 작성

---
+++ src/test/java/com/example/jpaboard/domain/UserTest.java
@@ -0,0 +1,51 @@
+package com.example.jpaboard.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class UserTest {
+
+    @Autowired EntityManager em;
+
+    @Test
+    @DisplayName("유저에 포스트를 추가하는 경우 포스트의 FK에 잘 등록이 되는가")
+    void test_add_post() {
+        // given
+        User user = new User("awesomeo", "coding");
+        Post post = new Post("test title", "test content");
+        user.addPost(post);
+        em.persist(user);
+
+        // when
+        Post findPost = em.find(Post.class, post.getId());
+        User author = findPost.getAuthor();
+
+        // then
+        assertThat(author).isEqualTo(user);
+    }
+
+    @Test
+    @DisplayName("유저를 삭제할 경우 해당 유저가 작성한 글이 잘 삭제 되는가")
+    void test_delete_post_when_user_removed() {
+        // given
+        User user = new User("awesomeo", "coding");
+        Post post = new Post("test title", "test content");
+        user.addPost(post);
+        em.persist(user);
+
+        // when
+        em.remove(user);
+
+        // then
+        Post shouldBeRemoved = em.find(Post.class, post.getId());
+        assertThat(shouldBeRemoved).isNull();
+    }
+}
