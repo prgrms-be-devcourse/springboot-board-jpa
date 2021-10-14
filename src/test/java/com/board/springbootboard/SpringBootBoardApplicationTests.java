c72e3c1e3717c6af451132e8b87ee5edf7ba39a4
sds1vrk <sdsvrk@gmail.com>
2021-10-15 01:32:57 +0900

    init start

---
+++ src/test/java/com/board/springbootboard/SpringBootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.springbootboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
