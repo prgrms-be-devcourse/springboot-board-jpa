c3b73c1d87b3c6673534347d6d78a2b7f6c20321
김부희 <kimbuhui@naver.com>
2021-10-15 02:45:02 +0900

    refactor : ResponseExceptionHandler 수정

--- springboot-board/src/main/java/org/prgms/board/common/response/ResponseExceptionHandler.java
+++ springboot-board/src/main/java/org/prgms/board/common/response/ResponseExceptionHandler.java
@@ -2,38 +2,28 @@ package org.prgms.board.common.response;
 
 import org.prgms.board.common.exception.NotFoundException;
 import org.prgms.board.common.exception.NotMatchException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 @ControllerAdvice
-public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
+public class ResponseExceptionHandler {
 
     @ExceptionHandler({NotFoundException.class})
-    public ResponseEntity<ErrorResponse> handleNotFound(Exception ex) {
+    public ResponseEntity<ErrorResponse> handleNotFound(NotFoundException ex) {
         return ErrorResponse.toResponseEntity(HttpStatus.NOT_FOUND, ex.getMessage());
     }
 
     @ExceptionHandler({NotMatchException.class})
-    public ResponseEntity<ErrorResponse> handleNotMatch(HttpServletRequest request, Exception ex) {
+    public ResponseEntity<ErrorResponse> handleNotMatch(NotMatchException ex) {
         return ErrorResponse.toResponseEntity(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("status", status.value());
-        body.put("error", status.name());
-        body.put("message", ex.getBindingResult().getAllErrors().get(0).getDefaultMessage());
-        return new ResponseEntity<>(body, headers, status);
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        return ErrorResponse.toResponseEntity(HttpStatus.BAD_REQUEST,
+            ex.getBindingResult().getAllErrors().get(0).getDefaultMessage());
     }
 }
