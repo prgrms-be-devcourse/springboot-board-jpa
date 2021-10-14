fa092b92a672662428b113384e38d7e15d5d1d3d
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-10-14 19:50:54 +0900

    fix: 모든 게시물 조회 uri 수정

--- src/main/java/com/devco/jpaproject/controller/PostController.java
+++ src/main/java/com/devco/jpaproject/controller/PostController.java
@@ -16,7 +16,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,7 +23,7 @@ import java.util.List;
 public class PostController {
     private final PostService postService;
 
-    @GetMapping("/posts/pages")
+    @GetMapping("/posts")
     public ApiResponse<Page<PostResponseDto>> findAllByPages(Pageable pageable){
         var allPosts = postService.findAllByPages(pageable);
         return ApiResponse.ok(allPosts);
