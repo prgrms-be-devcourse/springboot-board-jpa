82e8ff19f6b8d595cde1665a7f40dfd97f6be82d
jungeu1509 <jungeu1509@gmail.com>
2021-10-14 18:35:46 +0900

    add UserService

---
+++ src/main/java/com/eunu/springbootboard/domain/user/UserService.java
@@ -0,0 +1,22 @@
+package com.eunu.springbootboard.domain.user;
+
+import com.eunu.springbootboard.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserConverter userConverter;
+
+    @Transactional
+    public String save(UserDto userDto) {
+        User user = userConverter.convertUser(userDto);
+        User entity = userRepository.save(user);
+        return entity.getId();
+    }
+}
