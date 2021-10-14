c2cfc20ca18634a339effa63338f52f7aabc493f
suebeen <1003jamie@naver.com>
2021-10-15 03:18:33 +0900

    Initial Commit

---
+++ src/test/java/com/kdt/springbootboard/SpringBootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdt.springbootboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
