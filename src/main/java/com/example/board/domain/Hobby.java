dd706874692c515036036ab99feba8788b60f167
BeautterLife <pgh9801@gmail.com>
2021-10-15 03:26:02 +0900

    fix/DB에 저장될 문자열 설정중

--- src/main/java/com/example/board/domain/Hobby.java
+++ src/main/java/com/example/board/domain/Hobby.java
@@ -1,6 +1,16 @@
 package com.example.board.domain;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public enum Hobby {
-    SPORTS,
-    ARTS
+    SPORTS("운동"),
+    ARTS("예술");
+
+    private final String value;
+
+    @Override
+    public String toString(){
+        return this.value;
+    }
 }
