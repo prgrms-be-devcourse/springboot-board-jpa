d04dfcec4d45d1b686a9af5eff8d0e0abb97d4e1
iyj6707 <iyj6707@naver.com>
2021-10-14 16:39:14 +0900

    Init SpringBoot-Board project

---
+++ src/test/java/com/programmers/iyj/springbootboard/SpringBootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.iyj.springbootboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
