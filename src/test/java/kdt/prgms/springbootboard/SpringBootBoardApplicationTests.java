adc46c02741315224a568ef1f9d0904322bf7a2b
LeeJuOh <>
2021-10-14 17:27:47 +0900

    initial commit

---
+++ src/test/java/kdt/prgms/springbootboard/SpringBootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package kdt.prgms.springbootboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
