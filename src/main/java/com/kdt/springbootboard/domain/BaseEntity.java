47f90dd52c816e085ff09f44a0f9e8169cda16c9
suebeen <1003jamie@naver.com>
2021-10-16 00:18:12 +0900

    Chore: BaseEntity 수정

--- src/main/java/com/kdt/springbootboard/domain/BaseEntity.java
+++ src/main/java/com/kdt/springbootboard/domain/BaseEntity.java
@@ -2,17 +2,27 @@ package com.kdt.springbootboard.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     @Column(name = "created_by")
     private String createdBy;
+
+    @CreatedDate
     @Column(name = "created_at", columnDefinition = "TIMESTAMP")
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
