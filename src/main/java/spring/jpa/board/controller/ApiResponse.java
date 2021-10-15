0a63339f33cce5032b15e2ffa543efafc89a98e4
Heejeong01110 <123rkdrkd@naver.com>
2021-10-16 04:08:15 +0900

    Feat: ApiResponse 생성

---
+++ src/main/java/spring/jpa/board/controller/ApiResponse.java
@@ -0,0 +1,33 @@
+package spring.jpa.board.controller;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SelectBeforeUpdate;
+
+@NoArgsConstructor
+@Getter
+@SelectBeforeUpdate
+public class ApiResponse<T> {
+
+  private int statusCode;
+  private T data;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+  private LocalDateTime serverDateTime;
+
+  public ApiResponse(int statusCode, T data) {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.serverDateTime = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS);
+  }
+
+  public static <T> ApiResponse<T> ok(T data) {
+    return new ApiResponse<>(200, data);
+  }
+
+  public static <T> ApiResponse<T> fail(int statusCode, T data) {
+    return new ApiResponse<>(statusCode, data);
+  }
+}
