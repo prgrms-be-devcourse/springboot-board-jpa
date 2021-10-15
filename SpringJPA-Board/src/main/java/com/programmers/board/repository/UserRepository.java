594612f2d3aa7a00c1ee06f2bc2f80a74da456e0
wooko5 <wooko5@naver.com>
2021-10-16 00:56:32 +0900

    Delete UserRepository.java

--- SpringJPA-Board/src/main/java/com/programmers/board/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.programmers.board.repository;
-
-import com.programmers.board.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
