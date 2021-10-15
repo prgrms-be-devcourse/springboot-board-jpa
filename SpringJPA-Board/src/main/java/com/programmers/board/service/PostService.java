d423cbde276b801defe3d8807a3a639ad276c733
wooko5 <wooko5@naver.com>
2021-10-15 17:14:21 +0900

    Create PostService.java

---
+++ SpringJPA-Board/src/main/java/com/programmers/board/service/PostService.java
@@ -0,0 +1,24 @@
+package com.programmers.board.service;
+
+import com.programmers.board.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public void save(){
+
+    }
+
+    public void findAll(){
+
+    }
+
+    public void findById(){
+
+    }
+}
