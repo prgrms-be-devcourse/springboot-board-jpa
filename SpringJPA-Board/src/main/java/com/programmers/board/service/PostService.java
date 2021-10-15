846f8a03f99056f50f5e4a33dd99490f11ac4343
wooko5 <wooko5@naver.com>
2021-10-15 23:28:57 +0900

    PostService에 수정, 삭제 추가

--- SpringJPA-Board/src/main/java/com/programmers/board/service/PostService.java
+++ SpringJPA-Board/src/main/java/com/programmers/board/service/PostService.java
@@ -11,30 +11,43 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Service
 public class PostService {
-
-//    @Autowired // 필드 주입
-//    private PostRepository postRepository;
-//
-//    @Autowired
-//    private PostConverter postConverter;
 
     /*생성자 주입 방식을 쓴 이유: 필드를 @Autowired로 주입하는 것에 비해 순환 참조를 막을 수 있기 때문*/
     private final PostRepository postRepository;
     private final PostConverter postConverter;
-    public PostService(PostRepository postRepository, PostConverter postConverter){
+
+    public PostService(PostRepository postRepository, PostConverter postConverter) {
         this.postRepository = postRepository;
         this.postConverter = postConverter;
     }
 
     @Transactional
-    public Long save(PostDto postDto){
-        // 1. dto -> entity 변환 (준영속)
-        Post post = postConverter.convertPost(postDto);
-        // 2. postRepository.save(post) -> 영속화
-        Post entity = postRepository.save(post);
-        return entity.getId(); // 3. 결과 반환
+    public Long save(PostDto postDto) {
+//        // 1. dto -> entity 변환 (준영속)
+//        Post post = postConverter.convertPost(postDto);
+//        // 2. postRepository.save(post) -> 영속화
+//        Post entity = postRepository.save(post);
+//        // 3. 결과 반환의 과정을 밑 코드로 요약
+//        return entity.getId();
+        return postRepository.save(postConverter.convertPost(postDto)).getId(); // 위의 3줄짜리 코드를 요약한 코드
+    }
+
+    @Transactional
+    public PostDto update(PostDto postDto) throws NotFoundException {
+        Optional<Post> post = postRepository.findById(postDto.getId());
+        if (post.isEmpty()) {
+            throw new NotFoundException("해당 게시물을 찾을 수 없습니다");
+        }
+        post.get().setTitle(postDto.getTitle());
+        post.get().setContent(postDto.getContent());
+        post.get().setUpdatedAt(LocalDateTime.now());
+        Post updatedPost = postRepository.save(post.get());
+        return postConverter.convertPostDto(updatedPost);
     }
 
     @Transactional
@@ -47,8 +60,13 @@ public class PostService {
     }
 
     @Transactional
-    public Page<PostDto> findAll(Pageable pageable){
+    public Page<PostDto> findAll(Pageable pageable) {
         return postRepository.findAll(pageable)
                 .map(postConverter::convertPostDto);
+    }
+
+    @Transactional
+    public void deleteOneById(Long id) {
+        postRepository.deleteById(id);
     }
 }
