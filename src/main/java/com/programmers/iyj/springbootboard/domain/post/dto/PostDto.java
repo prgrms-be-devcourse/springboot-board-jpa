e02c5d762a585a089ca61a8a28e4dc6a22d60401
iyj6707 <iyj6707@naver.com>
2021-10-15 22:13:26 +0900

    feat: Add postdto and userdto

--- src/main/java/com/programmers/iyj/springbootboard/domain/post/dto/PostDto.java
+++ src/main/java/com/programmers/iyj/springbootboard/domain/post/dto/PostDto.java
@@ -1,4 +1,7 @@
 package com.programmers.iyj.springbootboard.domain.post.dto;
 
 public class PostDto {
+    private Long id;
+    private String title;
+    private String content;
 }
--- src/main/java/com/programmers/iyj/springbootboard/domain/user/dto/UserDto.java
+++ src/main/java/com/programmers/iyj/springbootboard/domain/user/dto/UserDto.java
@@ -1,4 +1,10 @@
 package com.programmers.iyj.springbootboard.domain.user.dto;
 
+import com.programmers.iyj.springbootboard.domain.user.domain.Hobby;
+
 public class UserDto {
+    private Long id;
+    private String name;
+    private Integer age;
+    private Hobby hobby;
 }
