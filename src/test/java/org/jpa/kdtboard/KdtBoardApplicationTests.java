96aaeee3aa16ae706ac18a9883bc08fb335bc585
yun <locodingve@gmail.com>
2021-10-15 16:59:10 +0900

    init_1

---
+++ src/test/java/org/jpa/kdtboard/KdtBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jpa.kdtboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdtBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
