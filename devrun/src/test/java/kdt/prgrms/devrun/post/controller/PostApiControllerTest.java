56282074f56324071250fb5783ec218ab4374b03
devrunner21 <devrunner21@gmail.com>
2021-10-15 23:37:34 +0900

    [feat] ControllerTest에 Rest-Docs 작성 코드를 추가하라.

--- devrun/src/test/java/kdt/prgrms/devrun/post/controller/PostApiControllerTest.java
+++ devrun/src/test/java/kdt/prgrms/devrun/post/controller/PostApiControllerTest.java
@@ -27,6 +27,7 @@ import java.util.ArrayList;
 import java.util.stream.IntStream;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -84,7 +85,26 @@ class PostApiControllerTest {
             .param("size", String.valueOf(10))
             .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
-            .andDo(print());
+            .andDo(print())
+            .andDo(document("post-list",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                responseFields(
+                    fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("성공여부"),
+                    fieldWithPath("serverDatetime").type(JsonFieldType.STRING).description("서버 응답 시간"),
+                    fieldWithPath("payload").type(JsonFieldType.OBJECT).description("응답 데이터").optional(),
+                    fieldWithPath("payload.totalCount").type(JsonFieldType.NUMBER).description("전체 데이터 수"),
+                    fieldWithPath("payload.pageNo").type(JsonFieldType.NUMBER).description("현재 페이지 번호"),
+                    fieldWithPath("payload.pageSize").type(JsonFieldType.NUMBER).description("한 페이지 크기"),
+                    fieldWithPath("payload.list").type(JsonFieldType.ARRAY).description("페이징 리스트"),
+                    fieldWithPath("payload.list[].id").type(JsonFieldType.NUMBER).description("게시글 Id"),
+                    fieldWithPath("payload.list[].title").type(JsonFieldType.STRING).description("게시글 제목"),
+                    fieldWithPath("payload.list[].createdBy").type(JsonFieldType.STRING).description("작성자 Id"),
+                    fieldWithPath("payload.list[].createdAt").type(JsonFieldType.STRING).description("작성일자"),
+                    fieldWithPath("error").type(JsonFieldType.OBJECT).description("에러 데이터").optional(),
+                    fieldWithPath("error.code").type(JsonFieldType.STRING).description("에러 코드"),
+                    fieldWithPath("error.message").type(JsonFieldType.STRING).description("에러 메시지")
+                )));
 
     }
 
@@ -95,7 +115,23 @@ class PostApiControllerTest {
         mockMvc.perform(get(BASE_URI + "/{id}", VALID_POST_ID)
             .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
-            .andDo(print());
+            .andDo(print())
+            .andDo(document("post-detail",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                responseFields(
+                    fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("성공여부"),
+                    fieldWithPath("serverDatetime").type(JsonFieldType.STRING).description("서버 응답 시간"),
+                    fieldWithPath("payload").type(JsonFieldType.OBJECT).description("응답 데이터").optional(),
+                    fieldWithPath("payload.id").type(JsonFieldType.NUMBER).description("게시글 Id"),
+                    fieldWithPath("payload.title").type(JsonFieldType.STRING).description("게시글 제목"),
+                    fieldWithPath("payload.content").type(JsonFieldType.STRING).description("게시글 내용"),
+                    fieldWithPath("payload.createdBy").type(JsonFieldType.STRING).description("작성자 Id"),
+                    fieldWithPath("payload.createdAt").type(JsonFieldType.STRING).description("작성일자"),
+                    fieldWithPath("error").type(JsonFieldType.OBJECT).description("에러 데이터").optional(),
+                    fieldWithPath("error.code").type(JsonFieldType.STRING).description("에러 코드"),
+                    fieldWithPath("error.message").type(JsonFieldType.STRING).description("에러 메시지")
+                )));
 
     }
 
@@ -113,7 +149,24 @@ class PostApiControllerTest {
             .contentType(MediaType.APPLICATION_JSON)
             .content(objectMapper.writeValueAsString(postForm)))
             .andExpect(status().isCreated())
-            .andDo(print());
+            .andDo(print())
+            .andDo(document("post-save",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestFields(
+                    fieldWithPath("title").type(JsonFieldType.STRING).description("생성할 게시글 제목"),
+                    fieldWithPath("content").type(JsonFieldType.STRING).description("생성할 게시글 내용"),
+                    fieldWithPath("createdBy").type(JsonFieldType.STRING).description("생성자 Id")
+                ),
+                responseFields(
+                    fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("성공여부"),
+                    fieldWithPath("serverDatetime").type(JsonFieldType.STRING).description("서버 응답 시간"),
+//                    fieldWithPath("payload").type(JsonFieldType.OBJECT).description("응답 데이터").optional(),
+                    fieldWithPath("payload").type(JsonFieldType.NUMBER).description("생성된 게시글 Id"),
+                    fieldWithPath("error").type(JsonFieldType.OBJECT).description("에러 데이터").optional(),
+                    fieldWithPath("error.code").type(JsonFieldType.STRING).description("에러 코드"),
+                    fieldWithPath("error.message").type(JsonFieldType.STRING).description("에러 메시지")
+                )));
 
     }
 
@@ -131,7 +184,24 @@ class PostApiControllerTest {
             .contentType(MediaType.APPLICATION_JSON)
             .content(objectMapper.writeValueAsString(postForm)))
             .andExpect(status().isOk())
-            .andDo(print());
+            .andDo(print())
+            .andDo(document("post-update",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestFields(
+                    fieldWithPath("title").type(JsonFieldType.STRING).description("생성할 게시글 제목"),
+                    fieldWithPath("content").type(JsonFieldType.STRING).description("생성할 게시글 내용"),
+                    fieldWithPath("createdBy").type(JsonFieldType.STRING).description("생성자 Id")
+                ),
+                responseFields(
+                    fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("성공여부"),
+                    fieldWithPath("serverDatetime").type(JsonFieldType.STRING).description("서버 응답 시간"),
+//                    fieldWithPath("payload").type(JsonFieldType.OBJECT).description("응답 데이터").optional(),
+                    fieldWithPath("payload").type(JsonFieldType.NUMBER).description("생성된 게시글 Id"),
+                    fieldWithPath("error").type(JsonFieldType.OBJECT).description("에러 데이터").optional(),
+                    fieldWithPath("error.code").type(JsonFieldType.STRING).description("에러 코드"),
+                    fieldWithPath("error.message").type(JsonFieldType.STRING).description("에러 메시지")
+                )));
     }
 
     @Test
@@ -143,7 +213,18 @@ class PostApiControllerTest {
         mockMvc.perform(delete(BASE_URI + "/{id}", savedPostId)
             .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isNoContent())
-            .andDo(print());
+            .andDo(print())
+            .andDo(document("post-delete",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                responseFields(
+                    fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("성공여부"),
+                    fieldWithPath("serverDatetime").type(JsonFieldType.STRING).description("서버 응답 시간"),
+                    fieldWithPath("payload").type(JsonFieldType.NULL).description("응답 데이터").optional(),
+                    fieldWithPath("error").type(JsonFieldType.OBJECT).description("에러 데이터").optional(),
+                    fieldWithPath("error.code").type(JsonFieldType.STRING).description("에러 코드"),
+                    fieldWithPath("error.message").type(JsonFieldType.STRING).description("에러 메시지")
+                )));
 
     }
 
