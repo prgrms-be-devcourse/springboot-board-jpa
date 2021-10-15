47931bb64cef1a16e3d14207324483ea0c4a1de3
Heejeong01110 <123rkdrkd@naver.com>
2021-10-16 04:05:40 +0900

    feat: 공통 MappedSuperClass 생성

---
+++ src/main/java/spring/jpa/board/domain/BaseEntity.java
@@ -0,0 +1,32 @@
+package spring.jpa.board.domain;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+  @CreatedDate
+  @Column(name = "created_at")
+  private LocalDateTime createdAt;
+
+  @Column(name = "created_by")
+  private Long createdBy;
+
+  @PrePersist
+  public void prePersist() {
+    this.createdAt = this.createdAt.truncatedTo(ChronoUnit.MILLIS);
+  }
+
+}
