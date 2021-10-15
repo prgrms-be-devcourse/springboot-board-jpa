c6c02d4b8aa1c149d51975b1d2d31b6743533835
wooko5 <wooko5@naver.com>
2021-10-16 00:56:26 +0900

    Update PostDto.java

--- SpringJPA-Board/src/main/java/com/programmers/board/dto/PostDto.java
+++ SpringJPA-Board/src/main/java/com/programmers/board/dto/PostDto.java
@@ -1,5 +1,6 @@
 package com.programmers.board.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 @Builder
@@ -7,6 +8,7 @@ import lombok.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PostDto {
     private Long id;
     private String title;
