57c5a82fec2df136829abd90bda3cffccd0a30c7
wooko5 <wooko5@naver.com>
2021-10-16 00:56:37 +0900

    Update PostControllerTest.java

--- SpringJPA-Board/src/test/java/com/programmers/board/controller/PostControllerTest.java
+++ SpringJPA-Board/src/test/java/com/programmers/board/controller/PostControllerTest.java
@@ -7,6 +7,7 @@ import com.programmers.board.repository.PostRepository;
 import com.programmers.board.service.PostService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
@@ -41,13 +42,13 @@ class PostControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    private Long id = 1L;
+    private final Long id = 1L;
 
     @BeforeEach
     void save() {
         // Given
         PostDto postDto = PostDto.builder()
-                .id(id)
+                //.id(id)
                 .title("여기에는 게시물 제목")
                 .content("여기에는 게시물 내용")
                 .userDto(
@@ -71,25 +72,8 @@ class PostControllerTest {
     }
 
     @Test
-    void getOne() throws Exception {
-        mockMvc.perform(get("/posts/{id}", id)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(print());
-    }
-
-    @Test
-    void getAll() throws Exception {
-        mockMvc.perform(get("/posts")
-                        .param("page", String.valueOf(0))
-                        .param("size", String.valueOf(10))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(print());
-    }
-
-    @Test
-    void saveCallTest() throws Exception {
+    @DisplayName("저장 작업을 테스트합니다")
+    void saveTest() throws Exception {
         // Given
         PostDto postDto = PostDto.builder()
                 .id(id)
@@ -97,7 +81,7 @@ class PostControllerTest {
                 .content("여기에는 게시물 내용")
                 .userDto(
                         UserDto.builder()
-                                .id(id) // 없다면 JsonFieldType.NULL 처리를 해줘야함
+                                .id(id)
                                 .name("오재욱")
                                 .age(28)
                                 .hobby("영화 감상")
@@ -124,6 +108,87 @@ class PostControllerTest {
                         responseFields(
                                 fieldWithPath("statusCode").type(JsonFieldType.NUMBER).description("상태코드"),
                                 fieldWithPath("data").type(JsonFieldType.NUMBER).description("데이터"),
+                                fieldWithPath("serverDatetime").type(JsonFieldType.STRING).description("응답시간")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("단일 조회를 테스트합니다")
+    void getOneTest() throws Exception {
+        mockMvc.perform(get("/posts/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andDo(document("post-getOne",
+                        responseFields(
+                                fieldWithPath("statusCode").type(JsonFieldType.NUMBER).description("상태코드"),
+                                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("id"),
+                                fieldWithPath("data.title").type(JsonFieldType.STRING).description("title"),
+                                fieldWithPath("data.content").type(JsonFieldType.STRING).description("content"),
+                                fieldWithPath("data.userDto.id").type(JsonFieldType.NUMBER).description("userDto.id"),
+                                fieldWithPath("data.userDto.name").type(JsonFieldType.STRING).description("userDto.name"),
+                                fieldWithPath("data.userDto.age").type(JsonFieldType.NUMBER).description("userDto.age"),
+                                fieldWithPath("data.userDto.hobby").type(JsonFieldType.STRING).description("userDto.hobby"),
+                                fieldWithPath("serverDatetime").type(JsonFieldType.STRING).description("서버 응답시간")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("전체 조회를 테스트합니다")
+    void getAllTest() throws Exception {
+        mockMvc.perform(get("/posts")
+                        .param("page", String.valueOf(0))
+                        .param("size", String.valueOf(10))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andDo(document("post-getAll"));
+    }
+
+    @Test
+    @DisplayName("수정 작업을 테스트합니다")
+    void updateTest() throws Exception{
+        // Given
+        PostDto updatedPostDto = PostDto.builder()
+                .id(id)
+                .title("여기에는 게시물 제목")
+                .content("여기에는 게시물 내용")
+                .userDto(
+                        UserDto.builder()
+                                .id(id)
+                                .name("오재욱")
+                                .age(28)
+                                .hobby("영화 감상")
+                                .build()
+                ).build();
+        //When //Then
+        mockMvc.perform(post("/posts/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updatedPostDto)))
+                .andExpect(status().isOk())//200
+                .andDo(print())
+                .andDo(document("post-update",
+                        requestFields(
+                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("id"),
+                                fieldWithPath("title").type(JsonFieldType.STRING).description("title"),
+                                fieldWithPath("content").type(JsonFieldType.STRING).description("content"),
+                                fieldWithPath("userDto").type(JsonFieldType.OBJECT).description("userDto"),
+                                fieldWithPath("userDto.id").type(JsonFieldType.NUMBER).description("userDto.id"),
+                                fieldWithPath("userDto.name").type(JsonFieldType.STRING).description("userDto.name"),
+                                fieldWithPath("userDto.age").type(JsonFieldType.NUMBER).description("userDto.age"),
+                                fieldWithPath("userDto.hobby").type(JsonFieldType.STRING).description("userDto.hobby")
+                        ),
+                        responseFields(
+                                fieldWithPath("statusCode").type(JsonFieldType.NUMBER).description("상태코드"),
+                                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("데이터.id"),
+                                fieldWithPath("data.title").type(JsonFieldType.STRING).description("데이터.title"),
+                                fieldWithPath("data.content").type(JsonFieldType.STRING).description("데이터.content"),
+                                fieldWithPath("data.userDto.id").type(JsonFieldType.NUMBER).description("데이터.userDto.id"),
+                                fieldWithPath("data.userDto.name").type(JsonFieldType.STRING).description("데이터.userDto.name"),
+                                fieldWithPath("data.userDto.age").type(JsonFieldType.NUMBER).description("데이터.userDto.age"),
+                                fieldWithPath("data.userDto.hobby").type(JsonFieldType.STRING).description("데이터.userDto.hobby"),
                                 fieldWithPath("serverDatetime").type(JsonFieldType.STRING).description("응답시간")
                         )
                 ));
