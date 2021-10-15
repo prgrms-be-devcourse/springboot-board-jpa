89cb6b460365110e7cd15866844d16fe274a334f
wooko5 <wooko5@naver.com>
2021-10-15 16:37:06 +0900

    Create UserDto.java

---
+++ SpringJPA-Board/src/main/java/com/programmers/board/dto/UserDto.java
@@ -0,0 +1,17 @@
+package com.programmers.board.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDto {
+    private Long userId;
+    private String name;
+    private int age;
+    private String hobby;
+}
