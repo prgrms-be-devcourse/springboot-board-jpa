a3d0660fca69d61de4f7e8c5800e2faa8fccba2c
thkthk97@naver.com <thkthk97@naver.com>
2021-10-15 11:15:51 +0900

    Initial Commit

---
+++ src/test/java/com/kdt/Board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdt.Board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
