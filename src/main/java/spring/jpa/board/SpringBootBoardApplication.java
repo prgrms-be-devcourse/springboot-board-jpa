289d66f519f3063c5af5850f7be1b1b6ccb211c6
Heejeong01110 <123rkdrkd@naver.com>
2021-10-16 03:49:51 +0900

    Style: 탭 간격 변경

--- src/main/java/spring/jpa/board/SpringBootBoardApplication.java
+++ src/main/java/spring/jpa/board/SpringBootBoardApplication.java
@@ -2,12 +2,13 @@ package spring.jpa.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class SpringBootBoardApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootBoardApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootBoardApplication.class, args);
+  }
 
 }
