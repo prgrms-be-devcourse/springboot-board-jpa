ea697a5831c1c442cabac381a88f842a8ce0f519
iyj6707 <iyj6707@naver.com>
2021-10-16 18:58:06 +0900

    refactor: Remove @Repository annotation from Repository
    Repository interface doesn't need @Repository annotation because of JpaRepository interface.

--- src/main/java/com/programmers/iyj/springbootboard/domain/post/repository/PostRepository.java
+++ src/main/java/com/programmers/iyj/springbootboard/domain/post/repository/PostRepository.java
@@ -2,8 +2,6 @@ package com.programmers.iyj.springbootboard.domain.post.repository;
 
 import com.programmers.iyj.springbootboard.domain.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
