a2af3bec0b948340d7e357faad4736302631f618
iyj6707 <iyj6707@naver.com>
2021-10-16 10:36:30 +0900

    fix: Add getter, builder to Dto

--- src/main/java/com/programmers/iyj/springbootboard/domain/post/dto/PostDto.java
+++ src/main/java/com/programmers/iyj/springbootboard/domain/post/dto/PostDto.java
@@ -1,5 +1,10 @@
 package com.programmers.iyj.springbootboard.domain.post.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
 public class PostDto {
     private Long id;
     private String title;
--- src/main/java/com/programmers/iyj/springbootboard/domain/user/dto/UserDto.java
+++ src/main/java/com/programmers/iyj/springbootboard/domain/user/dto/UserDto.java
@@ -1,7 +1,11 @@
 package com.programmers.iyj.springbootboard.domain.user.dto;
 
 import com.programmers.iyj.springbootboard.domain.user.domain.Hobby;
+import lombok.Builder;
+import lombok.Getter;
 
+@Builder
+@Getter
 public class UserDto {
     private Long id;
     private String name;
