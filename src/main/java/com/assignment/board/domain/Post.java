24b8e98b1b54c9ff7f766d7ef652dae52f978e41
Chaeonida <46310555+Chaeonida@users.noreply.github.com>
2021-10-18 03:32:22 +0900

    Post 변경

--- src/main/java/com/assignment/board/domain/Post.java
+++ src/main/java/com/assignment/board/domain/Post.java
@@ -3,12 +3,17 @@ package com.assignment.board.domain;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "post")
 @Getter
+@NoArgsConstructor
+@Setter
 public class Post extends BaseEntity {
 
     @Id
@@ -20,18 +25,29 @@ public class Post extends BaseEntity {
     @Lob
     private String content;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
-
-    protected Post() {
-    }
 
     @Builder
     public Post(String title, String content, User user) {
         this.title = title;
         this.content = content;
+        setUser(user);
+    }
+
+    public void setUser(User user) {
+        if(Objects.nonNull(this.user)) {
+            this.user.getPosts().remove(this);
+        }
+
         this.user = user;
+        user.getPosts().add(this);
+    }
+
+    public void postUpdate(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 
 }
