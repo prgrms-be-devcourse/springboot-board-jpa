f17bc81498bc2751a605c9c2b5ebeee4f1d8a0f8
iyj6707 <iyj6707@naver.com>
2021-10-17 14:25:44 +0900

    feat: Add equals, hashcode, tostring to dto

--- src/main/java/com/programmers/iyj/springbootboard/domain/post/dto/PostDto.java
+++ src/main/java/com/programmers/iyj/springbootboard/domain/post/dto/PostDto.java
@@ -1,10 +1,12 @@
 package com.programmers.iyj.springbootboard.domain.post.dto;
 
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Builder
 @Getter
+@EqualsAndHashCode
 public class PostDto {
     private Long id;
     private String title;
--- src/main/java/com/programmers/iyj/springbootboard/domain/user/dto/UserDto.java
+++ src/main/java/com/programmers/iyj/springbootboard/domain/user/dto/UserDto.java
@@ -2,10 +2,14 @@ package com.programmers.iyj.springbootboard.domain.user.dto;
 
 import com.programmers.iyj.springbootboard.domain.user.domain.Hobby;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 @Builder
 @Getter
+@ToString
+@EqualsAndHashCode
 public class UserDto {
     private Long id;
     private String name;
