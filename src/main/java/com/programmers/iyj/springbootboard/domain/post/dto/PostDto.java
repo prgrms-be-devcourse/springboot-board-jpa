af3b83a15415de3dd495e1c3ba8df79c5e3afd5f
iyj6707 <iyj6707@naver.com>
2021-10-18 01:15:33 +0900

    test: add api documents

--- src/main/java/com/programmers/iyj/springbootboard/domain/post/dto/PostDto.java
+++ src/main/java/com/programmers/iyj/springbootboard/domain/post/dto/PostDto.java
@@ -1,8 +1,11 @@
 package com.programmers.iyj.springbootboard.domain.post.dto;
 
+import com.programmers.iyj.springbootboard.domain.user.domain.User;
+import com.programmers.iyj.springbootboard.domain.user.dto.UserDto;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -11,6 +14,7 @@ import javax.validation.constraints.Size;
 @Builder
 @Getter
 @EqualsAndHashCode
+@ToString
 public class PostDto {
     @NotNull
     private Long id;
@@ -22,4 +26,7 @@ public class PostDto {
     @NotEmpty
     @Size(min = 1, max = 10000000)
     private String content;
+
+    @NotNull
+    private UserDto userDto;
 }
--- src/test/java/com/programmers/iyj/springbootboard/domain/post/api/PostApiTest.java
+++ src/test/java/com/programmers/iyj/springbootboard/domain/post/api/PostApiTest.java
@@ -1,32 +1,45 @@
 package com.programmers.iyj.springbootboard.domain.post.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.programmers.iyj.springbootboard.domain.post.domain.Post;
 import com.programmers.iyj.springbootboard.domain.post.dto.PostDto;
 import com.programmers.iyj.springbootboard.domain.post.service.PostService;
+import com.programmers.iyj.springbootboard.domain.user.domain.Hobby;
+import com.programmers.iyj.springbootboard.domain.user.dto.UserDto;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@AutoConfigureRestDocs
 @WebMvcTest(PostApi.class)
 class PostApiTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Autowired
     private MockMvc mockMvc;
@@ -42,6 +55,14 @@ class PostApiTest {
                 .id(1L)
                 .title("title1")
                 .content("content1")
+                .userDto(
+                        UserDto.builder()
+                                .id(1L)
+                                .name("john")
+                                .age(25)
+                                .hobby(Hobby.NETFLIX)
+                                .build()
+                )
                 .build();
     }
 
@@ -61,18 +82,31 @@ class PostApiTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("data.title").value("title1"))
                 .andExpect(jsonPath("data.content").value("content1"));
+
+        actions.andExpect(status().isOk())
+                .andDo(document("post-get",
+                        responseFields(
+                                fieldWithPath("statusCode").type(JsonFieldType.STRING).description("상태 코드"),
+                                fieldWithPath("serverDatetime").type(JsonFieldType.STRING).description("서버 시간"),
+                                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("게시물 아이디"),
+                                fieldWithPath("data.title").type(JsonFieldType.STRING).description("제목"),
+                                fieldWithPath("data.content").type(JsonFieldType.STRING).description("내용"),
+                                fieldWithPath("data.userDto.id").type(JsonFieldType.NUMBER).description("유저 아이디"),
+                                fieldWithPath("data.userDto.name").type(JsonFieldType.STRING).description("이름"),
+                                fieldWithPath("data.userDto.age").type(JsonFieldType.NUMBER).description("나이"),
+                                fieldWithPath("data.userDto.hobby").type(JsonFieldType.STRING).description("취미")
+                        )));
     }
 
     @Test
     @DisplayName("getAll api should response OK")
     void getAll() throws Exception {
         // Given
-        PageRequest page = PageRequest.of(0, 10);
         List<PostDto> postDtos = new ArrayList<>();
         postDtos.add(postDto);
         Page<PostDto> postDtoPage = new PageImpl<>(postDtos);
 
-        given(postService.findAll(page)).willReturn(postDtoPage);
+        given(postService.findAll(any())).willReturn(postDtoPage);
 
         // When
         ResultActions actions = mockMvc.perform(get("/posts")
@@ -80,6 +114,102 @@ class PostApiTest {
                         .andDo(print());
 
         // Then
-        actions.andExpect(status().isOk());
+        actions.andExpect(status().isOk())
+                .andDo(document("posts-get",
+                        responseFields(
+                                fieldWithPath("statusCode").type(JsonFieldType.STRING).description("상태 코드"),
+                                fieldWithPath("serverDatetime").type(JsonFieldType.STRING).description("서버 시간"),
+                                fieldWithPath("data.content[].id").type(JsonFieldType.NUMBER).description("게시물 아이디"),
+                                fieldWithPath("data.content[].title").type(JsonFieldType.STRING).description("제목"),
+                                fieldWithPath("data.content[].content").type(JsonFieldType.STRING).description("내용"),
+                                fieldWithPath("data.content[].userDto.id").type(JsonFieldType.NUMBER).description("유저 아이디"),
+                                fieldWithPath("data.content[].userDto.name").type(JsonFieldType.STRING).description("이름"),
+                                fieldWithPath("data.content[].userDto.age").type(JsonFieldType.NUMBER).description("나이"),
+                                fieldWithPath("data.content[].userDto.hobby").type(JsonFieldType.STRING).description("취미")
+                        )));
+    }
+
+    @Test
+    @DisplayName("save api should response OK")
+    public void save() throws Exception {
+        // Given
+        given(postService.save(any())).willReturn(postDto);
+
+        // When
+        ResultActions actions = mockMvc.perform(post("/posts")
+                        .content(objectMapper.writeValueAsString(postDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print());
+
+        // Then
+        actions.andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("data.title").value("title1"))
+                .andExpect(jsonPath("data.content").value("content1"));
+
+        actions.andExpect(status().isOk())
+                .andDo(document("post-create",
+                        requestFields(
+                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("게시물 아이디"),
+                                fieldWithPath("title").type(JsonFieldType.STRING).description("제목"),
+                                fieldWithPath("content").type(JsonFieldType.STRING).description("내용"),
+                                fieldWithPath("userDto.id").type(JsonFieldType.NUMBER).description("유저 아이디"),
+                                fieldWithPath("userDto.name").type(JsonFieldType.STRING).description("이름"),
+                                fieldWithPath("userDto.age").type(JsonFieldType.NUMBER).description("나이"),
+                                fieldWithPath("userDto.hobby").type(JsonFieldType.STRING).description("취미")
+                        ),
+                        responseFields(
+                                fieldWithPath("statusCode").type(JsonFieldType.STRING).description("상태 코드"),
+                                fieldWithPath("serverDatetime").type(JsonFieldType.STRING).description("서버 시간"),
+                                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("게시물 아이디"),
+                                fieldWithPath("data.title").type(JsonFieldType.STRING).description("제목"),
+                                fieldWithPath("data.content").type(JsonFieldType.STRING).description("내용"),
+                                fieldWithPath("data.userDto.id").type(JsonFieldType.NUMBER).description("유저 아이디"),
+                                fieldWithPath("data.userDto.name").type(JsonFieldType.STRING).description("이름"),
+                                fieldWithPath("data.userDto.age").type(JsonFieldType.NUMBER).description("나이"),
+                                fieldWithPath("data.userDto.hobby").type(JsonFieldType.STRING).description("취미")
+                        )));
+    }
+
+    @Test
+    @DisplayName("update api should response OK")
+    public void update() throws Exception {
+        // Given
+        given(postService.save(any(), anyLong())).willReturn(postDto);
+
+        // When
+        ResultActions actions = mockMvc.perform(patch("/posts/{id}", 1L)
+                        .content(objectMapper.writeValueAsString(postDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print());
+
+        // Then
+        actions.andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("data.title").value("title1"))
+                .andExpect(jsonPath("data.content").value("content1"));
+
+        actions.andExpect(status().isOk())
+                .andDo(document("post-update",
+                        requestFields(
+                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("게시물 아이디"),
+                                fieldWithPath("title").type(JsonFieldType.STRING).description("제목"),
+                                fieldWithPath("content").type(JsonFieldType.STRING).description("내용"),
+                                fieldWithPath("userDto.id").type(JsonFieldType.NUMBER).description("유저 아이디"),
+                                fieldWithPath("userDto.name").type(JsonFieldType.STRING).description("이름"),
+                                fieldWithPath("userDto.age").type(JsonFieldType.NUMBER).description("나이"),
+                                fieldWithPath("userDto.hobby").type(JsonFieldType.STRING).description("취미")
+                        ),
+                        responseFields(
+                                fieldWithPath("statusCode").type(JsonFieldType.STRING).description("상태 코드"),
+                                fieldWithPath("serverDatetime").type(JsonFieldType.STRING).description("서버 시간"),
+                                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("게시물 아이디"),
+                                fieldWithPath("data.title").type(JsonFieldType.STRING).description("제목"),
+                                fieldWithPath("data.content").type(JsonFieldType.STRING).description("내용"),
+                                fieldWithPath("data.userDto.id").type(JsonFieldType.NUMBER).description("유저 아이디"),
+                                fieldWithPath("data.userDto.name").type(JsonFieldType.STRING).description("이름"),
+                                fieldWithPath("data.userDto.age").type(JsonFieldType.NUMBER).description("나이"),
+                                fieldWithPath("data.userDto.hobby").type(JsonFieldType.STRING).description("취미")
+                        )));
     }
 }
