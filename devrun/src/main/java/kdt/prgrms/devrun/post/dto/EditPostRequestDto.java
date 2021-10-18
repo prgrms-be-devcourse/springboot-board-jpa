d48fe1051ee36abc6b78395b44e848cb505b196d
devrunner21 <devrunner21@gmail.com>
2021-10-18 19:16:27 +0900

    [refactor] DTO에도 Entity 처럼 길이 제약조건을 정의하라.

--- devrun/src/main/java/kdt/prgrms/devrun/post/dto/EditPostRequestDto.java
+++ devrun/src/main/java/kdt/prgrms/devrun/post/dto/EditPostRequestDto.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 
@@ -16,8 +17,10 @@ import javax.validation.constraints.NotBlank;
 public class EditPostRequestDto {
 
     @NotBlank(message = "제목은 필수 입력 사항입니다.")
+    @Length(max = 200, message = "200자 이하로 적어주세요.")
     private String title;
 
+    @Length(max = 1000, message = "1000자 이하로 적어주세요.")
     private String content;
 
     @NotBlank(message = "작성자 ID는 필수 입력 사항입니다.")
