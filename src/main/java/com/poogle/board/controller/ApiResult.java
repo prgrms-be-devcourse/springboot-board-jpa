7355bd87df6680111e6e4987814e27ea20f19652
suhyunsim <cnc1106@naver.com>
2021-10-19 03:24:48 +0900

    feat: 전체 응답을 위한 ApiResult 객체 구현

---
+++ src/main/java/com/poogle/board/controller/ApiResult.java
@@ -0,0 +1,48 @@
+package com.poogle.board.controller;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.springframework.http.HttpStatus;
+
+public class ApiResult<T> {
+
+    private final boolean success;
+    private final T response;
+    private final ApiError error;
+
+    public ApiResult(boolean success, T response, ApiError error) {
+        this.success = success;
+        this.response = response;
+        this.error = error;
+    }
+
+    public static <T> ApiResult<T> OK(T response) {
+        return new ApiResult<>(true, response, null);
+    }
+
+    public static ApiResult<?> ERROR(Throwable throwable, HttpStatus status) {
+        return new ApiResult<>(false, null, new ApiError(throwable, status));
+    }
+    public static ApiResult<?> ERROR(String errorMessage, HttpStatus status) {
+        return new ApiResult<>(false, null, new ApiError(errorMessage, status));
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+    public ApiError getError() {
+        return error;
+    }
+    public T getResponse() {
+        return response;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .append("success", success)
+                .append("response", response)
+                .append("error", error)
+                .toString();
+    }
+}
