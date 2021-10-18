b0cf4e0f52bd8389af2b96d27ed5d8cef65df28a
suhyunsim <cnc1106@naver.com>
2021-10-19 03:22:52 +0900

    feat: GeneralExceptionHandler를 구현해서 전역 예외 처리

---
+++ src/main/java/com/poogle/board/controller/GeneralExceptionHandler.java
@@ -0,0 +1,54 @@
+package com.poogle.board.controller;
+
+import com.poogle.board.error.NotFoundException;
+import com.poogle.board.error.ServiceRuntimeException;
+import com.poogle.board.error.UnauthorizedException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import static com.poogle.board.controller.ApiResult.ERROR;
+
+@Slf4j
+@ControllerAdvice
+public class GeneralExceptionHandler {
+
+    private ResponseEntity<ApiResult<?>> newResponse(Throwable throwable, HttpStatus status) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        return new ResponseEntity<>(ERROR(throwable, status), headers, status);
+    }
+
+    @ExceptionHandler({
+            IllegalStateException.class, IllegalArgumentException.class
+    })
+    public ResponseEntity<?> handleBadRequestException(Exception e) {
+        log.debug("[*] Bad request exception occurred: {}", e.getMessage(), e);
+        return newResponse(e, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<?> handleMethodNotAllowedException(Exception e) {
+        return newResponse(e, HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    @ExceptionHandler(ServiceRuntimeException.class)
+    public ResponseEntity<?> handleServiceRuntimeException(ServiceRuntimeException e) {
+        if (e instanceof NotFoundException) return newResponse(e, HttpStatus.NOT_FOUND);
+        if (e instanceof UnauthorizedException) return newResponse(e, HttpStatus.UNAUTHORIZED);
+        log.warn("[*] Unexpected service exception occurred: {}", e.getMessage(), e);
+        return newResponse(e, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler({
+            Exception.class, RuntimeException.class
+    })
+    public ResponseEntity<?> handleException(Exception e) {
+        log.error("[*] Unexpected exception occurred: {}", e.getMessage(), e);
+        return newResponse(e, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
