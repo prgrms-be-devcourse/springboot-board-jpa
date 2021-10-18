4db22b035e6ce6f8931e291bec5c0fd6b201b8ae
GetMapping <eonju.dev@gmail.com>
2021-10-18 18:41:05 +0900

    feat: restdocs 문서화

--- src/test/java/com/kdt/bulletinboard/controller/PostControllerTest.java
+++ src/test/java/com/kdt/bulletinboard/controller/PostControllerTest.java
@@ -9,19 +9,24 @@ import com.kdt.bulletinboard.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Transactional
+@AutoConfigureRestDocs
 @AutoConfigureMockMvc
 @SpringBootTest
 class PostControllerTest {
@@ -38,7 +43,7 @@ class PostControllerTest {
     private PostDto postDto;
 
     @BeforeEach
-    void save() {
+    void saveSampleData() {
         UserDto userDto = UserDto.builder()
                 .name("eonju")
                 .hobby(Hobby.CYCLING)
@@ -60,7 +65,23 @@ class PostControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(postDto)))
                 .andExpect(status().isOk())
-                .andDo(print());
+                .andDo(print())
+                .andDo(document("post-save"
+                        , requestFields(
+                                fieldWithPath("id").type(JsonFieldType.NULL).description("id")
+                                ,fieldWithPath("title").type(JsonFieldType.STRING).description("title")
+                                ,fieldWithPath("content").type(JsonFieldType.STRING).description("content")
+                                ,fieldWithPath("userDto").type(JsonFieldType.OBJECT).description("userDto")
+                                ,fieldWithPath("userDto.id").type(JsonFieldType.NULL).description("userDto.id")
+                                ,fieldWithPath("userDto.name").type(JsonFieldType.STRING).description("userDto.name")
+                                ,fieldWithPath("userDto.hobby").type(JsonFieldType.STRING).description("userDto.hobby")
+                        )
+                        , responseFields(
+                                fieldWithPath("statusCode").type(JsonFieldType.NUMBER).description("상태코드"),
+                                fieldWithPath("data").type(JsonFieldType.NUMBER).description("데이터"),
+                                fieldWithPath("serverDatetime").type(JsonFieldType.STRING).description("응답시간")
+                        )
+                ));
     }
 
     @Test
