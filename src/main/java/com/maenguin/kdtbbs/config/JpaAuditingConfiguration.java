5883a464c00ff1c6a44816fd124a9a0ef7dccec4
maenguin <maenguin924@gmail.com>
2021-10-18 16:32:27 +0900

    refactor : @EnableJpaAuditing을 별도의 설정 파일로 추출

---
+++ src/main/java/com/maenguin/kdtbbs/config/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.maenguin.kdtbbs.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfiguration {
+}
