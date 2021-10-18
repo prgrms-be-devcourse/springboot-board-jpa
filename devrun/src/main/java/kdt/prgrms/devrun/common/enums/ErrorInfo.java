1c49cec2459d55be98144b24f8cdde9810c66b25
devrunner21 <devrunner21@gmail.com>
2021-10-18 18:39:34 +0900

    [refactor] ErrorEnum에 소스 오타를 수정하라.
    - 생성자에 접근제한자 붙이기
    - 메시지 오타 수정

--- devrun/src/main/java/kdt/prgrms/devrun/common/enums/ErrorInfo.java
+++ devrun/src/main/java/kdt/prgrms/devrun/common/enums/ErrorInfo.java
@@ -7,9 +7,10 @@ public enum ErrorInfo {
 
     USER_NOT_FOUND("USER_NOT_FOUND","사용자를 찾을 수 없습니다."),
     POST_NOT_FOUND("POST_NOT_FOUND","게시글을 찾을 수 없습니다."),
-    UNKNOWN("UNKNOWN", "서버 에러로 인해 데이터를 로드 힐 수 없습니다.");
+    UNKNOWN("UNKNOWN", "서버 에러로 인해 데이터를 로드 할 수 없습니다.");
 
-    ErrorInfo(String code, String message) {
+
+    private ErrorInfo(String code, String message) {
         this.code = code;
         this.message = message;
     }
