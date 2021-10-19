47ec604aeea850fc59f88fc16df0a18534756473
suhyunsim <cnc1106@naver.com>
2021-10-19 13:48:00 +0900

    feat: User, Post RestController에 Create, Read, Update 요청 매핑

---
+++ src/main/java/com/poogle/board/controller/post/PostController.java
@@ -0,0 +1,50 @@
+package com.poogle.board.controller.post;
+
+import com.poogle.board.controller.ApiResult;
+import com.poogle.board.converter.Converter;
+import com.poogle.board.error.NotFoundException;
+import com.poogle.board.model.post.Post;
+import com.poogle.board.service.post.PostService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import static com.poogle.board.controller.ApiResult.OK;
+
+@RestController
+@RequestMapping("api/post")
+public class PostController {
+
+    private final PostService postService;
+    private final Converter converter;
+
+    public PostController(PostService postService, Converter converter) {
+        this.postService = postService;
+        this.converter = converter;
+    }
+
+    @PostMapping
+    public ApiResult<PostResponse> posting(@RequestBody PostRequest request) {
+        return OK(converter.convertPostDto(postService.write(request.newPost())));
+    }
+
+    @GetMapping
+    public ApiResult<Page<PostResponse>> list(Pageable pageable) {
+        return OK(postService.findPosts(pageable)
+                .map(converter::convertPostDto));
+    }
+
+    @GetMapping("/{id}")
+    public ApiResult<PostResponse> post(@PathVariable Long id) {
+        return OK(postService.findPost(id)
+                .map(converter::convertPostDto)
+                .orElseThrow(() -> new NotFoundException(Post.class, id)));
+    }
+
+    @PutMapping("/{id}")
+    public ApiResult<PostResponse> update(
+            @PathVariable Long id,
+            @RequestBody PostRequest request) {
+        return OK(converter.convertPostDto(postService.modify(id, request)));
+    }
+}
---
+++ src/main/java/com/poogle/board/controller/user/UserController.java
@@ -0,0 +1,53 @@
+package com.poogle.board.controller.user;
+
+import com.poogle.board.controller.ApiResult;
+import com.poogle.board.converter.Converter;
+import com.poogle.board.error.NotFoundException;
+import com.poogle.board.model.user.User;
+import com.poogle.board.service.user.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import static com.poogle.board.controller.ApiResult.OK;
+
+@RestController
+@RequestMapping("api/user")
+public class UserController {
+
+    private final UserService userService;
+    private final Converter converter;
+
+    public UserController(UserService userService, Converter converter) {
+        this.userService = userService;
+        this.converter = converter;
+    }
+
+    @PostMapping("/join")
+    public ApiResult<UserResponse> join(@RequestBody UserRequest request) {
+        return OK(converter.convertUserDto(userService.join(request.newUser())));
+    }
+
+    @GetMapping
+    public ApiResult<Page<UserResponse>> list(Pageable pageable) {
+        return OK(
+                userService.findUsers(pageable)
+                        .map(converter::convertUserDto)
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ApiResult<UserResponse> user(@PathVariable Long id) {
+        return OK(userService.findUser(id)
+                .map(converter::convertUserDto)
+                .orElseThrow(() -> new NotFoundException(User.class, id)));
+    }
+
+    @PutMapping("/{id}")
+    public ApiResult<UserResponse> update(
+            @PathVariable Long id,
+            @RequestBody UserRequest request) {
+        return OK(converter.convertUserDto(userService.modify(id, request)));
+    }
+
+}
