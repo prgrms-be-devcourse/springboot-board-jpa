d6705841efad5657150e227fe9c47511b4bee886
LeeJuOh <>
2021-10-19 18:58:02 +0900

    Feat: 엔티티 관련 전용 exception 클래스 추가

---
+++ src/main/java/kdt/prgms/springbootboard/global/error/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package kdt.prgms.springbootboard.global.error.exception;
+
+public class UserNotFoundException extends EntityNotFoundException{
+
+    public UserNotFoundException(String target) {
+        super(target + " is not found");
+    }
+}
