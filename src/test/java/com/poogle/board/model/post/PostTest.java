a7e23cd20b1b8200cfdb1816b65c7e30b7b9a122
suhyunsim <cnc1106@naver.com>
2021-10-19 11:32:42 +0900

    test: Post 테스트 추가

---
+++ src/test/java/com/poogle/board/model/post/PostTest.java
@@ -0,0 +1,40 @@
+package com.poogle.board.model.post;
+
+import com.poogle.board.model.user.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class PostTest {
+
+    @Test
+    @DisplayName("Post 생성 테스트")
+    void create_valid_post_test() {
+        Post post = Post.of("title", "content");
+        post.setUser(User.of("name", 27, "hobby"));
+        assertThat(post)
+                .hasFieldOrPropertyWithValue("title", "title")
+                .hasFieldOrPropertyWithValue("content", "content");
+    }
+
+    @Test
+    @DisplayName("Post 생성 실패 테스트")
+    void fail_create_post_test() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> Post.of("", "content"))
+                .withMessage("Title must be provided.");
+    }
+
+    @Test
+    @DisplayName("Post 수정 테스트")
+    void update_post_test() {
+        Post post = Post.of("title", "content");
+        post.setUser(User.of("name", 27, "hobby"));
+        post.update("new title", "new content");
+        assertThat(post)
+                .hasFieldOrPropertyWithValue("title", "new title")
+                .hasFieldOrPropertyWithValue("content", "new content");
+    }
+}
