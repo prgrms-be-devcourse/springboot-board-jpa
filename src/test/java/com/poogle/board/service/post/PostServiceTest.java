b863e940876ccdd74ca57d2eb5bf8a7707ea7d2c
suhyunsim <cnc1106@naver.com>
2021-10-19 13:47:03 +0900

    test: User, Post Service 테스트 추가

---
+++ src/test/java/com/poogle/board/service/post/PostServiceTest.java
@@ -0,0 +1,92 @@
+package com.poogle.board.service.post;
+
+import com.poogle.board.controller.post.PostRequest;
+import com.poogle.board.model.post.Post;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@Slf4j
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class PostServiceTest {
+
+    @Autowired
+    private PostService postService;
+
+    private String title;
+    private String content;
+
+    @BeforeAll
+    void setUp() {
+        title = "title";
+        content = "content";
+        postService.write(Post.of(title, content));
+    }
+
+    @Test
+    @DisplayName("포스트 추가")
+    void create_user() {
+        Post post = postService.write(Post.of(title, content));
+        assertAll(
+                () -> assertThat(post, is(notNullValue())),
+                () -> assertThat(post.getId(), is(notNullValue())),
+                () -> assertThat(post.getTitle(), is(title)),
+                () -> assertThat(post.getContent(), is(content))
+        );
+        log.info("[*] Inserted post: {}", post);
+    }
+
+    @Test
+    @DisplayName("포스트 id로 조회")
+    void find_post_by_id() {
+        Post foundPost = postService.findPost(1L).orElse(null);
+        assertAll(
+                () -> assertThat(foundPost, is(notNullValue())),
+                () -> assertThat(foundPost.getId(), is(1L))
+        );
+        log.info("[*] Found post: {}", foundPost);
+    }
+
+    @Test
+    @DisplayName("포스트 정보 수정")
+    void update_post() {
+        Post post = postService.findPost(1L).orElse(null);
+        PostRequest newPost = PostRequest.builder()
+                .title("new title")
+                .content("new content")
+                .build();
+        assertThat(post, is(notNullValue()));
+        postService.modify(1L, newPost);
+        assertAll(
+                () -> assertThat(post, is(notNullValue())),
+                () -> assertThat(post.getId(), is(1L))
+        );
+        log.info("[*] New post: {}", post);
+    }
+
+    @Test
+    @DisplayName("포스트 전체 조회")
+    void list_posts() {
+        Pageable pageable = PageRequest.of(0, 2);
+        Page<Post> posts = postService.findPosts(pageable);
+        assertAll(
+                () -> assertThat(posts, is(notNullValue())),
+                () -> assertThat(posts.getSize(), is(2))
+        );
+    }
+
+
+}
---
+++ src/test/java/com/poogle/board/service/user/UserServiceTest.java
@@ -0,0 +1,94 @@
+package com.poogle.board.service.user;
+
+import com.poogle.board.controller.user.UserRequest;
+import com.poogle.board.model.user.User;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@Slf4j
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    private String name;
+    private int age;
+    private String hobby;
+
+    @BeforeAll
+    void setUp() {
+        name = "tester";
+        age = 27;
+        hobby = "listening music";
+        userService.join(User.of(name, age, hobby));
+    }
+
+    @Test
+    @DisplayName("사용자 추가")
+    void create_user() {
+        User user = userService.join(User.of(name, age, hobby));
+        assertAll(
+                () -> assertThat(user, is(notNullValue())),
+                () -> assertThat(user.getId(), is(notNullValue())),
+                () -> assertThat(user.getAge(), is(age)),
+                () -> assertThat(user.getHobby(), is(hobby))
+        );
+        log.info("[*] Inserted User: {}", user);
+    }
+
+    @Test
+    @DisplayName("사용자 id로 조회")
+    void find_user_by_id() {
+        User foundUser = userService.findUser(1L).orElse(null);
+        assertAll(
+                () -> assertThat(foundUser, is(notNullValue())),
+                () -> assertThat(foundUser.getId(), is(1L))
+        );
+        log.info("[*] Found User: {}", foundUser);
+    }
+
+    @Test
+    @DisplayName("사용자 정보 수정")
+    void update_user() {
+        User user = userService.findUser(1L).orElse(null);
+        UserRequest newUser = UserRequest.builder()
+                .name("newTester")
+                .age(28)
+                .hobby("watching movie")
+                .build();
+        assertThat(user, is(notNullValue()));
+        userService.modify(1L, newUser);
+        assertAll(
+                () -> assertThat(user, is(notNullValue())),
+                () -> assertThat(user.getId(), is(1L))
+        );
+        log.info("[*] New User: {}", user);
+    }
+
+    @Test
+    @DisplayName("사용자 전체 조회")
+    void list_users() {
+        Pageable pageable = PageRequest.of(0, 2);
+        Page<User> users = userService.findUsers(pageable);
+        assertAll(
+                () -> assertThat(users, is(notNullValue())),
+                () -> assertThat(users.getSize(), is(2))
+        );
+    }
+
+}
