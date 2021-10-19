bbaa048c8ad289cf813499ea9c08337ba89760bd
suhyunsim <cnc1106@naver.com>
2021-10-19 11:43:27 +0900

    test: User, Post Repository 테스트 추가, BaseTimeEntity에 대한 테스트 추가

---
+++ src/test/java/com/poogle/board/repository/post/PostRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.poogle.board.repository.post;
+
+import com.poogle.board.model.post.Post;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@Slf4j
+@SpringBootTest
+class PostRepositoryTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Test
+    @DisplayName("포스트 저장 후 불러오기")
+    public void save_post_test() {
+        //given
+        String title = "title";
+        String content = "content";
+
+        postRepository.save(Post.of(title, content));
+
+        //when
+        List<Post> postList = postRepository.findAll();
+
+        //then
+        Post post = postList.get(0);
+        assertAll(
+                () -> assertThat(post.getTitle()).isEqualTo(title),
+                () -> assertThat(post.getContent()).isEqualTo(content)
+        );
+    }
+
+    @Test
+    @DisplayName("BaseTimeEntity 테스트")
+    public void base_time_entity_test() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2021, 10, 19, 0, 0, 0);
+        postRepository.save(Post.of("title", "content"));
+
+        //when
+        List<Post> postList = postRepository.findAll();
+
+        //then
+        Post post = postList.get(0);
+        log.info("[*] createdAt: {}", post.getCreatedAt());
+        log.info("[*] modifiedAt: {}", post.getModifiedAt());
+        assertAll(
+                () -> assertThat(post.getCreatedAt()).isAfter(now),
+                () -> assertThat(post.getModifiedAt()).isAfter(now)
+        );
+    }
+}
---
+++ src/test/java/com/poogle/board/repository/user/UserRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.poogle.board.repository.user;
+
+import com.poogle.board.model.user.User;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@Slf4j
+@SpringBootTest
+class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    @DisplayName("사용자 저장 후 불러오기")
+    public void save_user_test() {
+        //given
+        String name = "name";
+        int age = 27;
+        String hobby = "content";
+
+        userRepository.save(User.of(name, age, hobby));
+
+        //when
+        List<User> userList = userRepository.findAll();
+
+        //then
+        User user = userList.get(0);
+        assertAll(
+                () -> assertThat(user.getName()).isEqualTo(name),
+                () -> assertThat(user.getAge()).isEqualTo(age),
+                () -> assertThat(user.getHobby()).isEqualTo(hobby)
+        );
+    }
+
+    @Test
+    @DisplayName("BaseTimeEntity 테스트")
+    public void base_time_entity_test() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2021, 10, 19, 0, 0, 0);
+        userRepository.save(User.of("name", 27, "hobby"));
+
+        //when
+        List<User> userList = userRepository.findAll();
+
+        //then
+        User user = userList.get(0);
+        log.info("[*] createdAt: {}", user.getCreatedAt());
+        log.info("[*] modifiedAt: {}", user.getModifiedAt());
+        assertAll(
+                () -> assertThat(user.getCreatedAt()).isAfter(now),
+                () -> assertThat(user.getModifiedAt()).isAfter(now)
+        );
+    }
+
+}
