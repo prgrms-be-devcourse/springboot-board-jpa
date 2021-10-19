76455aed0bee49a98f94274f773934b677fd892c
suhyunsim <cnc1106@naver.com>
2021-10-19 16:20:03 +0900

    style: 사용하지 않는 로거 삭제

--- src/test/java/com/poogle/board/model/user/UserTest.java
+++ src/test/java/com/poogle/board/model/user/UserTest.java
@@ -1,13 +1,11 @@
 package com.poogle.board.model.user;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
-@Slf4j
 class UserTest {
 
     @Test
