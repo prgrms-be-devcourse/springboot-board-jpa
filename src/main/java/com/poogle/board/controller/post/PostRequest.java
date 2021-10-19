92f2b8e666516d9d48d24e3991a345e973434afd
suhyunsim <cnc1106@naver.com>
2021-10-19 11:59:03 +0900

    refactor: DTO에 빌더와 생성자 추가

--- src/main/java/com/poogle/board/controller/post/PostRequest.java
+++ src/main/java/com/poogle/board/controller/post/PostRequest.java
@@ -1,10 +1,14 @@
 package com.poogle.board.controller.post;
 
 import com.poogle.board.model.post.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import javax.validation.constraints.NotEmpty;
 
+@Builder
+@AllArgsConstructor
 @Getter
 public class PostRequest {
     private @NotEmpty String title;
--- src/main/java/com/poogle/board/controller/user/UserRequest.java
+++ src/main/java/com/poogle/board/controller/user/UserRequest.java
@@ -4,19 +4,20 @@ import com.poogle.board.model.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public class UserRequest {
 
     private @NotEmpty String name;
     private @NotEmpty int age;
     private @NotEmpty String hobby;
+
+    protected UserRequest() {
+    }
 
     public User newUser() {
         return User.of(name, age, hobby);
