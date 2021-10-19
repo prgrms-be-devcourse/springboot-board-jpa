9bdf72a630c28c5b1ee23659c7588aaecd24e21a
suhyunsim <cnc1106@naver.com>
2021-10-19 16:26:21 +0900

    refactor: DataJpaTest로 변경

--- src/test/java/com/poogle/board/repository/user/UserRepositoryTest.java
+++ src/test/java/com/poogle/board/repository/user/UserRepositoryTest.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @Slf4j
-@SpringBootTest
+@DataJpaTest
 class UserRepositoryTest {
 
     @Autowired
