02e17f82e67c11bc33e3378787eaa98b843fbcf6
venzym <venzym@naver.com>
2021-10-21 03:02:16 +0900

    Feat: Notblank validation 메시지 추가

--- src/main/java/com/prgrms/board/post/dto/PostDto.java
+++ src/main/java/com/prgrms/board/post/dto/PostDto.java
@@ -1,28 +1,27 @@
 package com.prgrms.board.post.dto;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostDto {
     private Long id;
 
-    @NotBlank
+    @NotBlank(message = "제목을 입력해주세요.")
     @Length(max = 50)
     private String title;
 
-    @NotBlank
+    @NotBlank(message = "내용을 입력해주세요.")
     @Length(max = 500)
     private String content;
 
     private UserDto userDto;
-
-    protected PostDto() {
-    }
 
     @Builder
     public PostDto(Long id, String title, String content, UserDto userDto) {
