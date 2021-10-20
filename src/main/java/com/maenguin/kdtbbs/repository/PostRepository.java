eb6e61de8c3032ff690ecb360e645cb9cebd47c5
maenguin <maenguin924@gmail.com>
2021-10-21 01:08:39 +0900

    feat : 비관적 락 게시글 조회 메서드 추가

--- src/main/java/com/maenguin/kdtbbs/repository/PostRepository.java
+++ src/main/java/com/maenguin/kdtbbs/repository/PostRepository.java
@@ -4,7 +4,18 @@ import com.maenguin.kdtbbs.domain.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.persistence.LockModeType;
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
     Page<Post> findAll(Pageable pageable);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select p from Post p where p.postId = :postId")
+    Optional<Post> findByIdForUpdate(@Param("postId") Long postId);
 }
