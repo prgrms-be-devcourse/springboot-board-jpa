a9581d76fb1a8b8a0fa2adf58cc9d09b013ce736
Heejeong01110 <123rkdrkd@naver.com>
2021-10-20 16:25:06 +0900

    Refactor: id 생성 전략 수정

--- src/main/java/spring/jpa/board/domain/User.java
+++ src/main/java/spring/jpa/board/domain/User.java
@@ -20,7 +20,7 @@ import lombok.Setter;
 public class User extends BaseEntity {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   @Column(name = "name", nullable = false, length = 20)
