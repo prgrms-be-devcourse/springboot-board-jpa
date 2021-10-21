94cad6cbd7fda5d4b486dbdd2bad2849c80dafdd
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-10-22 00:18:33 +0900

    fix: HttpStatus code 활용

--- src/main/java/com/devco/jpaproject/controller/advice/GlobalAdvice.java
+++ src/main/java/com/devco/jpaproject/controller/advice/GlobalAdvice.java
@@ -4,6 +4,7 @@ import com.devco.jpaproject.controller.ApiResponse;
 import com.devco.jpaproject.exception.PostNotFoundException;
 import com.devco.jpaproject.exception.UserAndPostNotMatchException;
 import com.devco.jpaproject.exception.UserNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,22 +13,22 @@ public class GlobalAdvice {
 
     @ExceptionHandler(PostNotFoundException.class)
     public ApiResponse<String> postNotFoundHandler(PostNotFoundException e){
-        return ApiResponse.fail(404, e.getMessage());
+        return ApiResponse.fail(HttpStatus.NOT_FOUND.value(), e.getMessage());
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ApiResponse<String> userNotFoundHandler(UserNotFoundException e){
-        return ApiResponse.fail(404, e.getMessage());
+        return ApiResponse.fail(HttpStatus.NOT_FOUND.value(), e.getMessage());
     }
 
     @ExceptionHandler(UserAndPostNotMatchException.class)
     public ApiResponse<String> userAndPostNotMatchHandler(UserAndPostNotMatchException e){
-        return ApiResponse.fail(404, e.getMessage());
+        return ApiResponse.fail(HttpStatus.NOT_FOUND.value(), e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public ApiResponse<String> enternalServerErrorHandler(Exception e){
-        return ApiResponse.fail(500, e.getMessage());
+        return ApiResponse.fail(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
     }
 
     //TODO: 가능한 모든 예외 처리 추가
