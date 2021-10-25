03f9eed72ae54804d340680fdb4802769de5221f
venzym <venzym@naver.com>
2021-10-25 16:46:17 +0900

    Refactor: checkedException uncheckedException으로 변경

--- src/main/java/com/prgrms/board/exception/ApiExceptionHandler.java
+++ src/main/java/com/prgrms/board/exception/ApiExceptionHandler.java
@@ -1,30 +1,29 @@
 package com.prgrms.board.exception;
 
 import com.prgrms.board.post.api.ErrorResponse;
-import javassist.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class ApiExceptionHandler {
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResponse illegalArgumentHandler(IllegalArgumentException e) {
-        log.error("IllegalArgumentHandler : {}", e.getMessage());
-        return new ErrorResponse(HttpStatus.BAD_REQUEST, "400", e.getMessage());
-    }
+    private static final String BAD_REQUEST_CODE = "400";
+    private static final String INTERNAL_SERVER_ERROR_CODE = "500";
 
-    @ExceptionHandler(NotFoundException.class)
-    public ErrorResponse notFoundExceptionHandler(NotFoundException e) {
-        log.error("NotFoundExceptionHandler : {}", e.getMessage());
-        return new ErrorResponse(HttpStatus.NOT_FOUND, "404", e.getMessage());
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse badRequestException(RuntimeException e) {
+        log.error("RuntimeExceptionHandler : {}", e.getMessage());
+        return new ErrorResponse(HttpStatus.BAD_REQUEST, BAD_REQUEST_CODE, e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse exceptionHandler(Exception e) {
         log.error("exceptionHandler : {}", e.getMessage());
-        return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "500", e.getMessage());
+        return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR_CODE, e.getMessage());
     }
 }
--- src/main/java/com/prgrms/board/post/controller/PostController.java
+++ src/main/java/com/prgrms/board/post/controller/PostController.java
@@ -1,47 +1,44 @@
 package com.prgrms.board.post.controller;
 
+import com.prgrms.board.common.dto.PageDto;
 import com.prgrms.board.post.api.ApiResponse;
 import com.prgrms.board.post.dto.PostDto;
 import com.prgrms.board.post.service.PostService;
-import javassist.NotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequestMapping("/api")
 @RestController
 public class PostController {
 
-    @Autowired
-    private PostService postService;
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
 
     @PostMapping("/posts")
-    public ApiResponse<Long> save(@RequestBody PostDto postDto) {
+    public ApiResponse<Long> save(@RequestBody @Valid PostDto postDto) {
         Long id = postService.save(postDto);
         return ApiResponse.ok(id);
     }
 
     @GetMapping("/posts/{id}")
-    public ApiResponse<PostDto> getOne(
-            @PathVariable Long id
-    ) throws NotFoundException {
+    public ApiResponse<PostDto> getOne(@PathVariable Long id) {
         PostDto findPostDto = postService.findOne(id);
         return ApiResponse.ok(findPostDto);
     }
 
     @GetMapping("/posts")
-    public ApiResponse<Page<PostDto>> getAll(
-            Pageable pageable
-    ) {
-        Page<PostDto> findPostDtos = postService.findAll(pageable);
+    public ApiResponse<PageDto<PostDto>> getAll(Pageable pageable) {
+        PageDto<PostDto> findPostDtos = postService.findAll(pageable);
         return ApiResponse.ok(findPostDtos);
     }
 
     @PostMapping("/posts/{id}")
-    public ApiResponse<Long> update(
-            @PathVariable Long id, @RequestBody PostDto postDto
-    ) throws NotFoundException {
+    public ApiResponse<Long> update(@PathVariable Long id, @RequestBody @Valid PostDto postDto) {
         Long updatedPostId = postService.update(id, postDto);
         return ApiResponse.ok(updatedPostId);
     }
