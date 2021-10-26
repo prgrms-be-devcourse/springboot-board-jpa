376cc61986a9a4257e6a0061787faeb348908221
Jihun-Hwang <goharrm@ajou.ac.kr>
2021-10-26 15:28:04 +0900

    fix: @valid에 따른 request DTO 예외처리 핸들러 추가

--- src/main/java/com/devco/jpaproject/controller/advice/GlobalAdvice.java
+++ src/main/java/com/devco/jpaproject/controller/advice/GlobalAdvice.java
@@ -5,6 +5,8 @@ import com.devco.jpaproject.exception.PostNotFoundException;
 import com.devco.jpaproject.exception.UserAndPostNotMatchException;
 import com.devco.jpaproject.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -29,6 +31,14 @@ public class GlobalAdvice {
     @ExceptionHandler(Exception.class)
     public ApiResponse<String> enternalServerErrorHandler(Exception e){
         return ApiResponse.fail(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+    }
+
+    /**
+     * @Valid 에 따른 request DTO 검증 예외처리
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ApiResponse<String> methodValidException(MethodArgumentNotValidException e){
+        return ApiResponse.fail(HttpStatus.NOT_FOUND.value(), e.getMessage());
     }
 
     //TODO: 가능한 모든 예외 처리 추가
