d3fadda17c0c147772575741bc4c85cd6c51d709
iyj6707 <iyj6707@naver.com>
2021-10-27 09:46:46 +0900

    fix: Fix mapping error

--- src/main/java/com/programmers/iyj/springbootboard/domain/post/mapper/PostMapper.java
+++ src/main/java/com/programmers/iyj/springbootboard/domain/post/mapper/PostMapper.java
@@ -5,9 +5,14 @@ import com.programmers.iyj.springbootboard.domain.post.dto.PostDto;
 import com.programmers.iyj.springbootboard.domain.user.domain.User;
 import com.programmers.iyj.springbootboard.domain.user.dto.UserDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface PostMapper {
+
+    @Mapping(source = "userDto", target = "user")
     Post dtoToEntity(PostDto postDto);
+
+    @Mapping(source = "user", target = "userDto")
     PostDto entityToDto(Post post);
 }
