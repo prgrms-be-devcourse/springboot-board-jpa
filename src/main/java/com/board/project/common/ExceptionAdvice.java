9c02283759b28e40ae0a97bc987dd12f3e78d3b4
Rudolf <stylish0631@gmail.com>
2021-11-11 15:42:53 +0900

    feat: ApiResponse 예외처리를 위한 ExceptionAdvice 생성

---
+++ src/main/java/com/board/project/common/ExceptionAdvice.java
@@ -0,0 +1,19 @@
+package com.board.project.common;
+
+import javassist.NotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionAdvice {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ApiResponse<String> notFoundHandler(NotFoundException e) {
+        return ApiResponse.fail(404, e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ApiResponse<String> exception(Exception e) {
+        return ApiResponse.fail(500, e.getMessage());
+    }
+}
