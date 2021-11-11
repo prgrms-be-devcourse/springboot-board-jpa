f3c75a590115d4448a37a5ef39be166a52ed875f
Rudolf <stylish0631@gmail.com>
2021-11-11 15:44:27 +0900

    feat: UserRepository - JpaRepository 상속

--- src/main/java/com/board/project/user/repository/UserRepository.java
+++ src/main/java/com/board/project/user/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package com.board.project.user.repository;
 
-public class UserRepository {
+import com.board.project.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
