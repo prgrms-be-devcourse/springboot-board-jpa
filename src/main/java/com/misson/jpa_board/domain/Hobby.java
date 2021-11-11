0e23bedbf064ff8f03ce2bbd3927fc542db5cf40
choiyoungkwon12 <hpwh4457@naver.com>
2021-11-11 12:57:58 +0900

    hobby 유효성 검사 메서드 대신 @Size로 대체

--- src/main/java/com/misson/jpa_board/domain/Hobby.java
+++ src/main/java/com/misson/jpa_board/domain/Hobby.java
@@ -4,23 +4,17 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.validation.constraints.Size;
 
 @Embeddable
 @NoArgsConstructor
 public class Hobby {
-
+    @Size(min = 1, max = 30)
     @Column(name = "user_hobby")
     private String hobby;
 
     public Hobby(String hobby) {
-        if (!validate(hobby)) {
-            throw new IllegalArgumentException("취미은 1글자 이상 30글자 이하여야 합니다.");
-        }
         this.hobby = hobby;
-    }
-
-    private boolean validate(String hobby) {
-        return hobby.length() > 0 && hobby.length() <= 30;
     }
 }
 
