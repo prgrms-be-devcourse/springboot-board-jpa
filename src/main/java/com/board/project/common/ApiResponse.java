8dd7ed253aeab49eb2fd3190c08b543344ab0dff
Rudolf <stylish0631@gmail.com>
2021-11-11 15:39:01 +0900

    feat: 공통 ApiResponse 생성

---
+++ src/main/java/com/board/project/common/ApiResponse.java
@@ -0,0 +1,37 @@
+package com.board.project.common;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ApiResponse<T> {
+
+    private int statusCode;
+
+    private T data;
+
+    @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime serverDatetime;
+
+    @Builder
+    public ApiResponse(int statusCode, T data) {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.serverDatetime = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS);
+    }
+
+    public static <T> ApiResponse<T> ok(T data) {
+        return new ApiResponse<>(200, data);
+    }
+
+    public static <T> ApiResponse<T> fail(int statusCode, T data) {
+        return new ApiResponse<>(statusCode, data);
+    }
+}
