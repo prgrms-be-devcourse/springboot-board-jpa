668a8c76d13135ade0ae43bfdc0871028a8dcf33
choiyoungkwon12 <hpwh4457@naver.com>
2021-11-11 12:42:00 +0900

    post 컨트롤러 공통된 경로 클래스단에서 선언

--- src/main/java/com/misson/jpa_board/controller/PostRestController.java
+++ src/main/java/com/misson/jpa_board/controller/PostRestController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
+@RequestMapping("/posts")
 @RestController
 public class PostRestController {
 
@@ -21,36 +22,34 @@ public class PostRestController {
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public ApiResponse<String> notFoundHandler(NotFoundException e){
-        return ApiResponse.fail(404,e.getMessage());
+    public ApiResponse<String> notFoundHandler(NotFoundException e) {
+        return ApiResponse.fail(404, e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
-    public ApiResponse<String> internalServerErrorHandler(Exception e){
-        return ApiResponse.fail(500,e.getMessage());
+    public ApiResponse<String> internalServerErrorHandler(Exception e) {
+        return ApiResponse.fail(500, e.getMessage());
     }
 
-    @PostMapping("/posts")
+    @PostMapping
     public ApiResponse<Long> save(@RequestBody PostCreateRequest postDto) throws NotFoundException {
         log.info("게시글 작성");
-        log.info("postDto.getTitle() :: {}", postDto.getTitle());
-        log.info("postDto.getContent() :: {}",postDto.getContent());
         return ApiResponse.ok(postService.save(postDto));
     }
 
-    @PutMapping("/posts/{id}")
-    public ApiResponse<PostDto> changePost(@PathVariable(name = "id") Long id, @RequestBody PostDto postDto){
+    @PutMapping("/{id}")
+    public ApiResponse<PostDto> changePost(@PathVariable Long id, @RequestBody PostDto postDto) {
         log.info("게시글 수정");
         return ApiResponse.ok(postService.postChange(id, postDto));
     }
 
-    @GetMapping("/posts")
-    public ApiResponse<Page<PostDto>> getPage(Pageable pageable){
+    @GetMapping
+    public ApiResponse<Page<PostDto>> getPage(Pageable pageable) {
         log.info("게시글 페이지 조회");
         return ApiResponse.ok(postService.findAll(pageable));
     }
 
-    @GetMapping("/posts/{id}")
+    @GetMapping("/{id}")
     public ApiResponse<PostDto> postFindById(@PathVariable(value = "id") Long id) throws NotFoundException {
         log.info("게시글 단건 조회");
         PostDto postDto = postService.postFindById(id);
