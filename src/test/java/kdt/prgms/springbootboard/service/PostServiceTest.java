34f3d9e0fe800c54811a50cf6a32c22dbb0c4cb2
LeeJuOh <>
2021-11-20 19:27:02 +0900

    test : Add post service test

--- src/test/java/kdt/prgms/springbootboard/service/PostServiceTest.java
+++ src/test/java/kdt/prgms/springbootboard/service/PostServiceTest.java
@@ -1,13 +1,18 @@
 package kdt.prgms.springbootboard.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.*;
 
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 import kdt.prgms.springbootboard.converter.PostConverter;
 import kdt.prgms.springbootboard.domain.Post;
 import kdt.prgms.springbootboard.domain.User;
+import kdt.prgms.springbootboard.dto.PostDetailResponseDto;
+import kdt.prgms.springbootboard.dto.PostSaveRequestDto;
+import kdt.prgms.springbootboard.dto.SimpleUserDto;
 import kdt.prgms.springbootboard.repository.PostRepository;
 import kdt.prgms.springbootboard.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -38,6 +43,50 @@ class PostServiceTest {
     private PostConverter postConvertor;
 
     @Test
+    @DisplayName("게시글 생성 테스트")
+    void savePostTest() {
+        //given
+        var user = new User("tester", 20);
+        var post = new Post("title", "content", user);
+        var simpleUserDto = mock(SimpleUserDto.class);
+        var postSaveRequestDto = mock(PostSaveRequestDto.class);
+
+        given(postSaveRequestDto.getSimpleUserDto()).willReturn(simpleUserDto);
+        given(simpleUserDto.getName()).willReturn("tester");
+        given(userRepository.findByName(any(String.class))).willReturn(Optional.of(user));
+        given(postConvertor.convertPost(any(PostSaveRequestDto.class), any(User.class))).willReturn(post);
+        given(postRepository.save(any(Post.class))).willReturn(post);
+        given(postConvertor.convertPostDetailDto(any(Post.class))).willReturn(mock(PostDetailResponseDto.class));
+
+        //when
+        postService.save(postSaveRequestDto);
+
+        //then
+        verify(userRepository, times(1)).findByName(any(String.class));
+        verify(postConvertor, times(1)).convertPost(any(PostSaveRequestDto.class), any(User.class));
+        verify(postRepository, times(1)).save(any(Post.class));
+        verify(postConvertor, times(1)).convertPostDetailDto(any(Post.class));
+    }
+
+    @Test
+    @DisplayName("게시글 수정 테스트")
+    void updatePostTest() {
+        //given
+        var post = new Post("title", "content", mock(User.class));
+        var postSaveRequestDto = mock(PostSaveRequestDto.class);
+
+        given(postRepository.findById(anyLong())).willReturn(Optional.of(post));
+        given(postConvertor.convertPostDetailDto(any(Post.class))).willReturn(mock(PostDetailResponseDto.class));
+
+        //when
+        postService.update(anyLong(), postSaveRequestDto);
+
+        //then
+        verify(postRepository, times(1)).findById(anyLong());
+        verify(postConvertor, times(1)).convertPostDetailDto(any(Post.class));
+    }
+
+    @Test
     @DisplayName("전체 게시글 조회 테스트")
     void findAllPostTest() {
         //given
@@ -53,5 +102,21 @@ class PostServiceTest {
         //then
         assertThat(result.getTotalElements()).isEqualTo(20L);
         verify(postRepository, times(1)).findAll(pageRequest);
+    }
+
+    @Test
+    @DisplayName("게시글 단건 조회 테스트")
+    void findOnePostTest() {
+        //given
+        var post = new Post("title", "content", mock(User.class));
+        given(postRepository.findById(anyLong())).willReturn(Optional.of(post));
+        given(postConvertor.convertPostDetailDto(any(Post.class))).willReturn(mock(PostDetailResponseDto.class));
+
+        //when
+        postService.findOne(anyLong());
+
+        //then
+        verify(postRepository, times(1)).findById(anyLong());
+        verify(postConvertor, times(1)).convertPostDetailDto(any(Post.class));
     }
 }
