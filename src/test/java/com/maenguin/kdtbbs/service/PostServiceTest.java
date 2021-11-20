9865016184602a18dbf8805bd6212d62e679458a
maenguin <maenguin924@gmail.com>
2021-11-20 22:27:17 +0900

    test : PostServiceTest 추가

--- src/test/java/com/maenguin/kdtbbs/service/PostServiceTest.java
+++ src/test/java/com/maenguin/kdtbbs/service/PostServiceTest.java
@@ -1,59 +1,117 @@
 package com.maenguin.kdtbbs.service;
 
-import com.maenguin.kdtbbs.controller.OptimisticLockTryer;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.maenguin.kdtbbs.config.JpaAuditingConfiguration;
+import com.maenguin.kdtbbs.converter.BBSConverter;
 import com.maenguin.kdtbbs.domain.Post;
 import com.maenguin.kdtbbs.domain.User;
+import com.maenguin.kdtbbs.dto.PostAddDto;
+import com.maenguin.kdtbbs.dto.PostAddResDto;
 import com.maenguin.kdtbbs.dto.PostDto;
+import com.maenguin.kdtbbs.exception.PostNotFoundException;
+import com.maenguin.kdtbbs.exception.UserNotFoundException;
 import com.maenguin.kdtbbs.repository.PostRepository;
 import com.maenguin.kdtbbs.repository.UserRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
-import java.util.Optional;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-
-@SpringBootTest
-@Slf4j
+@DataJpaTest
+@Import({PostService.class, BBSConverter.class, JpaAuditingConfiguration.class})
 class PostServiceTest {
 
     @Autowired
     private PostRepository postRepository;
+
     @Autowired
     private UserRepository userRepository;
-    @Autowired
-    private PostService postService;
-    @Autowired
-    private OptimisticLockTryer optimisticLockTryer;
 
-    @BeforeEach
-    void setup() {
-        User user = new User("maeng", "piano");
-        Post post = new Post("hello", "world");
-        user.addPost(post);
-        userRepository.save(user);
+    @Autowired
+    private PostService sut;
+
+    @Test
+    @DisplayName("게시글 단건 조회 - 성공 테스트")
+    void getPostByIdSuccessTest() {
+        //given
+        Post originPost = postRepository.save(new Post("title", "content"));
+        //when
+        assertThat(originPost.getView()).isZero();
+        PostDto postDto = sut.getPostById(originPost.getPostId());
+        //then
+        assertThat(postDto.getId()).isEqualTo(originPost.getPostId());
+        assertThat(postDto.getTitle()).isEqualTo(originPost.getTitle());
+        assertThat(postDto.getContent()).isEqualTo(originPost.getContent());
+        Post findPost = postRepository.getById(originPost.getPostId());
+        assertThat(findPost.getView()).isEqualTo(1);
     }
 
     @Test
-    @DisplayName("조회수 증가 테스트")
-    void test2() throws Exception {
+    @DisplayName("게시글 단건 조회 - 실패 테스트 (잘못된 게시글 Id)")
+    void getPostByIdFailureTest() {
         //given
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
-        for (int i = 0; i < 10; i++) {
-            executorService.execute(() -> {
-                PostDto attempt = optimisticLockTryer.attempt(() -> postService.getPostById(2L), 10);
-            });
-        }
-        Thread.sleep(1000);
-        Optional<Post> post = postRepository.findById(2L);
-        assertThat(post.isPresent(), is(true));
-        assertThat(post.get().getView(), is(10L));
+        Long invalidPostId = 1L;
+        //when then
+        assertThatThrownBy(() -> sut.getPostById(invalidPostId))
+            .isInstanceOf(PostNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("게시글 등록 - 성공 테스트")
+    void savePostSuccessTest() {
+        //given
+        User user = new User("user", "piano");
+        User savedUser = userRepository.save(user);
+        PostAddDto postAddDto = new PostAddDto(savedUser.getUserId(), "title", "content");
+        //when
+        PostAddResDto postAddResDto = sut.savePost(postAddDto);
+        //then
+        Optional<Post> findPost = postRepository.findById(postAddResDto.getPostId());
+        assertThat(findPost).isPresent();
+        assertThat(findPost.get().getUser().getUserId()).isEqualTo(savedUser.getUserId());
+        assertThat(findPost.get().getTitle()).isEqualTo(postAddDto.getTitle());
+        assertThat(findPost.get().getContent()).isEqualTo(postAddDto.getContent());
+    }
+
+    @Test
+    @DisplayName("게시글 등록 - 실패 테스트 (잘못된 유저 Id)")
+    void savePostFailureTest() {
+        //given
+        Long invalidUserId = 1L;
+        PostAddDto postAddDto = new PostAddDto(invalidUserId, "title", "content");
+        //when then
+        assertThatThrownBy(() -> sut.savePost(postAddDto))
+            .isInstanceOf(UserNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("게시글 수정 - 성공 테스트")
+    void editPostSuccessTest() {
+        //given
+        Post originPost = postRepository.save(new Post("title", "content"));
+        PostAddDto postAddDto = new PostAddDto(1L, "title2", "content2");
+        //when
+        PostAddResDto postAddResDto = sut.editPost(originPost.getPostId(), postAddDto);
+        //then
+        assertThat(postAddResDto.getPostId()).isEqualTo(originPost.getPostId());
+        Optional<Post> findPost = postRepository.findById(originPost.getPostId());
+        assertThat(findPost).isPresent();
+        assertThat(findPost.get().getTitle()).isEqualTo(postAddDto.getTitle());
+        assertThat(findPost.get().getContent()).isEqualTo(postAddDto.getContent());
+    }
+
+    @Test
+    @DisplayName("게시글 수정 - 실패 테스트(잘못된 게시글 Id)")
+    void editPostFailureTest() {
+        //given
+        Long invalidPostId = 1L;
+        PostAddDto postAddDto = new PostAddDto(1L, "title2", "content2");
+        //when then
+        assertThatThrownBy(() -> sut.editPost(invalidPostId, postAddDto))
+            .isInstanceOf(PostNotFoundException.class);
     }
 }
