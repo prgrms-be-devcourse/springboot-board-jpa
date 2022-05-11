62060e0a50c15ddfc93a36c5c82c0c8d0cae8d56
ndy2 <emrdbs12@gmail.com>
2022-05-11 16:38:54 +0900

    feat: Post 에 User 매핑 추가

--- src/main/java/com/study/board/domain/post/domain/Post.java
+++ src/main/java/com/study/board/domain/post/domain/Post.java
@@ -1,14 +1,15 @@
 package com.study.board.domain.post.domain;
 
 import com.study.board.domain.support.auditing.BaseEntity;
+import com.study.board.domain.user.domain.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.util.StringUtils;
 
 import javax.persistence.*;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
+import static javax.persistence.FetchType.LAZY;
 import static lombok.AccessLevel.PROTECTED;
 import static org.springframework.util.StringUtils.hasText;
 
@@ -32,17 +33,23 @@ public class Post extends BaseEntity {
     @Column(name = "content", nullable = false)
     private String content;
 
-    private Post(Long id, String title, String content) {
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User writer;
+
+    private Post(Long id, String title, String content, User writer) {
         checkArgument(hasText(title), "title - 글자를 가져야함");
         checkArgument(title.length() <= 255, "title 길이 - 255 이하 여야함");
         checkArgument(hasText(content), "content - 글자를 가져야함");
+        checkNotNull(writer, "writer - null 이 될 수 없음");
 
         this.id = id;
         this.title = title;
         this.content = content;
+        this.writer = writer;
     }
 
-    public static Post create(String title, String content){
-        return new Post(null, title, content);
+    public static Post create(String title, String content, User writer){
+        return new Post(null, title, content, writer);
     }
 }
