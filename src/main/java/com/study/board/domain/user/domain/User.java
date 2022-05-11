c0c334ca6708a62a7a4aef5c16b1c17bb75a7dc5
ndy2 <emrdbs12@gmail.com>
2022-05-11 16:10:21 +0900

    feat: User 엔티티

---
+++ src/main/java/com/study/board/domain/user/domain/User.java
@@ -0,0 +1,55 @@
+package com.study.board.domain.user.domain;
+
+import com.study.board.domain.support.auditing.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import java.util.Optional;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static lombok.AccessLevel.PROTECTED;
+import static org.springframework.util.StringUtils.hasText;
+
+/**
+ * 회원
+ */
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "post_id")
+    private Long id;
+
+    @Column(name = "name", length = 50, nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "hobby", length = 50)
+    private String hobby;
+
+    private User(Long id, String name, String hobby) {
+        checkArgument(hasText(name), "name - 글자를 가져야함");
+        checkArgument(name.length() <=50, "name 길이 - 50 이하 여야함");
+        if(hobby != null){
+            checkArgument(hobby.length() <=50, "hobby 길이 - 50 이하 여야함");
+        }
+        this.id = id;
+        this.name = name;
+        this.hobby = hobby;
+    }
+
+    public static User create(String name, Optional<String> hobby) {
+        checkNotNull(hobby, "hobby optional - null 이 될 수 없음");
+
+        return new User(null, name, hobby.orElse(null));
+    }
+
+}
