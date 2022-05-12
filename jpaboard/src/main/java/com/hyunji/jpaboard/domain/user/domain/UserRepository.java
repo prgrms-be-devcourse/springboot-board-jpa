4ae99d0be5caca79934e3a83d1600b9e7f21cd23
Hyunji <rioreo2022@gmail.com>
2022-05-12 22:53:33 +0900

    feat: UserRepository 추가

---
+++ jpaboard/src/main/java/com/hyunji/jpaboard/domain/user/domain/UserRepository.java
@@ -0,0 +1,6 @@
+package com.hyunji.jpaboard.domain.user.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
