0ad17a81854df2312826689907de2d2e5fcb5d96
Hyunji <rioreo2022@gmail.com>
2022-05-12 22:46:02 +0900

    feat: Post Builder 제거, 생성자 private로 변경, 정적 메소드 create 추가

--- jpaboard/src/main/java/com/hyunji/jpaboard/domain/post/domain/Post.java
+++ jpaboard/src/main/java/com/hyunji/jpaboard/domain/post/domain/Post.java
@@ -3,15 +3,12 @@ package com.hyunji.jpaboard.domain.post.domain;
 import com.hyunji.jpaboard.domain.user.domain.User;
 import com.hyunji.jpaboard.model.BaseEntity;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.apache.logging.log4j.util.Strings;
-import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 import java.util.Objects;
 
 import static com.google.common.base.Preconditions.checkArgument;
@@ -29,25 +26,19 @@ public class Post extends BaseEntity {
     @Column(nullable = false)
     private String content;
 
-    @CreationTimestamp
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
-
-    @Column(nullable = false, updatable = false)
-    private String createdBy;
-
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
-    @Builder
-    public Post(String title, String content, String createdBy) {
+    private Post(String title, String content) {
         checkArgument(Strings.isNotBlank(title), "title 공백 불가");
         checkArgument(Strings.isNotBlank(content), "content 공백 불가");
-        checkArgument(Strings.isNotBlank(createdBy), "createdBy 공백 불가");
 
         this.title = title;
         this.content = content;
-        this.createdBy = createdBy;
+    }
+
+    public static Post create(String title, String content) {
+        return new Post(title, content);
     }
 
     public void isWrittenBy(User user) {
