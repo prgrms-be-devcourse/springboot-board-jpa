871fc22d2ce8d8183f05818c46fc3e8f5568585e
Hyunji <rioreo2022@gmail.com>
2022-05-12 22:14:22 +0900

    feat: BaseEntity 구현

---
+++ jpaboard/src/main/java/com/hyunji/jpaboard/model/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.hyunji.jpaboard.model;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners((AuditingEntityListener.class))
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+}
