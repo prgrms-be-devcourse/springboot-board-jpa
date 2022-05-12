23ac8d18e1796ae049cfd54e8de6aab2aba555f3
Gosh95 <goshk1995@gmail.com>
2022-05-12 23:03:52 +0300

    init: 프로젝트 생성

---
+++ src/test/java/prgrms/project/post/PostApplicationTests.java
@@ -0,0 +1,13 @@
+package prgrms.project.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
