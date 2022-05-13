af3dc7723acd31f56ec7f1b509026cd74766fd64
park0503 <hooo0503@gmail.com>
2022-05-13 16:08:40 +0900

    initialize: initialize spring boot application

---
+++ src/main/java/com/example/boardjpa/BoardJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardJpaApplication.class, args);
+    }
+
+}
