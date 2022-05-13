17b1ca36fea9e85ed12ac47e6fc8880060ad8c33
Gil KeunO <rlfrmsdh@nextlab.co.kr>
2022-05-13 17:32:03 +0900

    추가  - PostRepository, 테스트코드 추가

---
+++ springboot/src/test/java/org/spring/notice/domain/post/PostRepositoryTest.java
@@ -0,0 +1,42 @@
+package org.spring.notice.domain.post;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.spring.notice.fixture.Fixture.createUser;
+
+@SpringBootTest
+class PostRepositoryTest {
+
+    private final Post post1 = Post.write("테스트1", "테스트 내용", createUser());
+    private final Post post2 = Post.write("테스트2", "테스트 내용2", createUser());
+    private final Post post3 = Post.write("테스트3", "테스트 내용3", createUser());
+
+    @Autowired
+    PostRepository postRepository;
+
+    @BeforeEach
+    void setup(){
+        postRepository.save(post1);
+        postRepository.save(post2);
+        postRepository.save(post3);
+    }
+
+    @AfterEach
+    void tearDown(){
+        postRepository.deleteAll();
+    }
+
+    @Test
+    void findByTitleTest() {
+        List<Post> posts = postRepository.findByTitle("테스트1");
+
+        assertThat(posts).containsExactly(post1);
+    }
+}
