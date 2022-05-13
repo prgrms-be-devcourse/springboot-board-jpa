20a6d829b63bc87c601110006d01c1368e3485aa
Juhan <juhan211@naver.com>
2022-05-13 22:39:44 +0900

    make springboot JPA board project

---
+++ src/test/java/com/programmers/springbootboardjpa/SpringbootBoardJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.springbootboardjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBoardJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
