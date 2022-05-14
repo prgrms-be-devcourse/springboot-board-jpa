ede2897f0369948767a2df6c039781271fbfcad2
Hyunggeun447 <bopi447@naver.com>
2022-05-15 02:12:27 +0900

    user service

--- src/main/java/com/kdt/boardMission/repository/UserRepository.java
+++ src/main/java/com/kdt/boardMission/repository/UserRepository.java
@@ -1,7 +1,18 @@
 package com.kdt.boardMission.repository;
 
 import com.kdt.boardMission.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query(value = "select u from User u where u.name like %:name%", countQuery = "select count(u) from User u")
+    Page<User> findByName(@Param("name") String name, Pageable pageable);
+
 }
--- src/main/java/com/kdt/boardMission/service/UserService.java
+++ src/main/java/com/kdt/boardMission/service/UserService.java
@@ -3,11 +3,18 @@ package com.kdt.boardMission.service;
 import com.kdt.boardMission.domain.User;
 import com.kdt.boardMission.dto.UserDto;
 import com.kdt.boardMission.repository.UserRepository;
+import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
 import static com.kdt.boardMission.dto.UserDto.convertUser;
+import static com.kdt.boardMission.dto.UserDto.convertUserDto;
 
 @Service
 @Transactional
@@ -22,4 +29,26 @@ public class UserService {
         return save.getId();
     }
 
+    public void deleteUser(UserDto userDto) {
+        User user = convertUser(userDto);
+        userRepository.delete(user);
+    }
+
+    public UserDto findUserById(UserDto userDto) throws NotFoundException {
+        Optional<User> byId = userRepository.findById(userDto.getId());
+        if (byId.isEmpty()) {
+            throw new NotFoundException("해당 아이디를 가진 유저가 없습니다.");
+        }
+        User user = byId.get();
+        return convertUserDto(user);
+    }
+
+    public Page<UserDto> findUserByName(UserDto userDto, Pageable pageable) {
+        String name = userDto.getName();
+        return userRepository.findByName(name, pageable).map(UserDto::convertUserDto);
+    }
+
+    public Page<UserDto> findAll(Pageable pageable) {
+        return userRepository.findAll(pageable).map(UserDto::convertUserDto);
+    }
 }
