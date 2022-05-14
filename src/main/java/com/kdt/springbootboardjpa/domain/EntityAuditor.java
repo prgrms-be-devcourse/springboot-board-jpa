af886eb0e46a4c2a2ceb28c9e957eaf8a1878fb4
kkj0419 <hgkimmme9@gmail.com>
2022-05-14 17:32:49 +0900

    add : auditing 기능

---
+++ src/main/java/com/kdt/springbootboardjpa/domain/EntityAuditor.java
@@ -0,0 +1,23 @@
+package com.kdt.springbootboardjpa.domain;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Optional;
+
+@Component
+public class EntityAuditor implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        var request = ((ServletRequestAttributes)
+                RequestContextHolder.currentRequestAttributes()).getRequest();
+        String uri = request.getRequestURI().toString();
+        if (uri.matches("^/api")) {
+            return Optional.of("api");
+        }
+        return Optional.empty();
+    }
+}
