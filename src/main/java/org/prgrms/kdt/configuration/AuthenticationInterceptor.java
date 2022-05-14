8e4b9fa563f6f982f7573ceea8e050bea2646158
ordilov <ordilov@gmail.com>
2022-05-14 19:16:06 +0900

    feat: 유저 인증 인터셉터를 추가하라

---
+++ src/main/java/org/prgrms/kdt/configuration/AuthenticationInterceptor.java
@@ -0,0 +1,34 @@
+package org.prgrms.kdt.configuration;
+
+import static java.text.MessageFormat.format;
+
+import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.prgrms.kdt.dto.UserDto.CurrentUser;
+import org.prgrms.kdt.mapper.UserMapper;
+import org.prgrms.kdt.repository.UserRepository;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Configuration
+@RequiredArgsConstructor
+public class AuthenticationInterceptor implements HandlerInterceptor {
+
+  private final UserMapper mapper;
+  private final UserRepository userRepository;
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+      throws Exception {
+    String userId = request.getHeader("Authorization");
+    if (userId != null) {
+      CurrentUser currentUser = userRepository.findById(Long.parseLong(userId))
+          .map(mapper::of)
+          .orElseThrow(() -> new EntityNotFoundException(format("ID: {0}의 유저를 찾을 수 없습니다", userId)));
+      request.getServletContext().setAttribute("currentUser", currentUser);
+    }
+    return HandlerInterceptor.super.preHandle(request, response, handler);
+  }
+}
---
+++ src/main/java/org/prgrms/kdt/configuration/SimpleAuditAware.java
@@ -0,0 +1,21 @@
+package org.prgrms.kdt.configuration;
+
+import java.util.Optional;
+import javax.servlet.ServletContext;
+import lombok.RequiredArgsConstructor;
+import org.prgrms.kdt.dto.UserDto.CurrentUser;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SimpleAuditAware implements AuditorAware<String> {
+
+  private final ServletContext context;
+
+  @Override
+  public Optional<String> getCurrentAuditor() {
+    CurrentUser user = (CurrentUser) context.getAttribute("currentUser");
+    return Optional.of(user.name());
+  }
+}
