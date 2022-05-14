c6fbcbf1c3f2075f231ecff8091f5ad617655b2f
unnokid <kimki0609@gmail.com>
2022-05-14 22:23:13 +0900

    Initial Setting

---
+++ src/main/java/com/example/springboardjpa/SpringBoardJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboardjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoardJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoardJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboardjpa/SpringBoardJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboardjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoardJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
