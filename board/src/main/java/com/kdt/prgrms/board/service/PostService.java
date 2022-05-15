999b1e6b00e7a50d1f9b763897facbd5f18f29f2
UJ15 <dbwogml15@naver.com>
2022-05-15 18:07:59 +0900

    :sparkles: feat : PostService 뼈대

---
+++ board/src/main/java/com/kdt/prgrms/board/service/PostService.java
@@ -0,0 +1,13 @@
+package com.kdt.prgrms.board.service;
+
+import com.kdt.prgrms.board.domain.Post;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    public Post addPost(Post post) {
+
+        return null;
+    }
+}
