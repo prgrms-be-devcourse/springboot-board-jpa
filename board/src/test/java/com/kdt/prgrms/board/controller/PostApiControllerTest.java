26dc26f2d9de4c1fb19d765844bdc31f34045b99
UJ15 <dbwogml15@naver.com>
2022-05-15 18:01:54 +0900

    :white_check_mark: test : AddPost기능 Controller test 작성

--- board/src/test/java/com/kdt/prgrms/board/controller/PostApiControllerTest.java
+++ board/src/test/java/com/kdt/prgrms/board/controller/PostApiControllerTest.java
@@ -1,23 +1,59 @@
 package com.kdt.prgrms.board.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kdt.prgrms.board.domain.Post;
+import com.kdt.prgrms.board.dto.PostAddRequest;
+import com.kdt.prgrms.board.service.PostService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = PostApiController.class)
 public class PostApiControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    PostService postService;
 
     @Nested
     @DisplayName("addPost메서드는")
     class DescribeAddPost {
 
+        String url = "/api/v1/posts";
+
         @Nested
         @DisplayName("제목이 없는 게시글 생성요청이 들어오면")
         class ContextWhiteSpaceTitlePostAddRequest {
 
+            PostAddRequest requestBody = new PostAddRequest("", "Hello");
+
+            MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(url)
+                    .content(toJson(requestBody))
+                    .contentType(MediaType.APPLICATION_JSON);
+
             @Test
             @DisplayName("400 bad request 응답을 반환한다.")
-            void itReturnBadRequest() {
+            void itReturnBadRequest() throws Exception {
 
+                mockMvc.perform(request)
+                        .andExpect(status().isBadRequest());
             }
         }
 
@@ -25,10 +61,18 @@ public class PostApiControllerTest {
         @DisplayName("내용이 없는 게시글 생성요청이 들어오면")
         class ContextWhiteSpaceContentPostAddRequest {
 
+            PostAddRequest requestBody = new PostAddRequest("Title", "");
+
+            MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(url)
+                    .content(toJson(requestBody))
+                    .contentType(MediaType.APPLICATION_JSON);
+
             @Test
             @DisplayName("400 bad request 응답을 반환한다.")
-            void itReturnBadRequest() {
+            void itReturnBadRequest() throws Exception {
 
+                mockMvc.perform(request)
+                        .andExpect(status().isBadRequest());
             }
         }
 
@@ -36,17 +80,37 @@ public class PostApiControllerTest {
         @DisplayName("제목과 내용이 있는 게시글 생성요청이 들어오면")
         class ContextPostAddRequest {
 
-            @Test
-            @DisplayName("200 ok 응답을 반환한다.")
-            void itReturnOk() {
+            PostAddRequest requestBody = new PostAddRequest("Title", "Hello");
 
+            MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(url)
+                    .content(toJson(requestBody))
+                    .contentType(MediaType.APPLICATION_JSON);
+
+            @Test
+            @DisplayName("201 create 응답을 반환한다.")
+            void itReturnOk() throws Exception {
+
+                mockMvc.perform(request)
+                        .andExpect(status().isCreated());
             }
 
             @Test
             @DisplayName("service의 addPost메서드를 호출한다.")
-            void CallServiceAddPost() {
+            void CallServiceAddPost() throws Exception {
 
+                mockMvc.perform(request);
+
+                verify(postService).addPost(any(Post.class));
             }
+        }
+    }
+
+    private String toJson(Object obj) {
+
+        try {
+            return objectMapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
     }
 }
