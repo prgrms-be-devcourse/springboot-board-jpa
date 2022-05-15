321741335cd18119c4e4d24dddaff75d01f64175
ndy2 <emrdbs12@gmail.com>
2022-05-15 12:09:39 +0900

    feat: 커스텀 루트 예외 - BoardRuntimeException 구현

---
+++ src/main/java/com/study/board/domain/exception/BoardRuntimeException.java
@@ -0,0 +1,24 @@
+package com.study.board.domain.exception;
+
+public class BoardRuntimeException extends RuntimeException{
+
+    public BoardRuntimeException() {
+        super();
+    }
+
+    public BoardRuntimeException(String message) {
+        super(message);
+    }
+
+    public BoardRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BoardRuntimeException(Throwable cause) {
+        super(cause);
+    }
+
+    protected BoardRuntimeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
