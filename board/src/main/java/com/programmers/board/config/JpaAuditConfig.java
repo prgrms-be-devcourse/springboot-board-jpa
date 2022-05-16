84c09a22a20b498d10024c867f33a1e585c39c3d
yeon <soccer_by@naver.com>
2022-05-17 04:04:57 +0900

    refactor: audit 매개변수 제거

--- board/src/main/java/com/programmers/board/config/JpaAuditConfig.java
+++ board/src/main/java/com/programmers/board/config/JpaAuditConfig.java
@@ -1,17 +1,20 @@
 package com.programmers.board.config;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Configuration
 @EnableJpaAuditing
 public class JpaAuditConfig {
 	@Bean
-	public AuditorAware<String> autAuditorProvider(HttpServletRequest servlet) {
-		return () -> Optional.of(servlet.getRemoteAddr());
+	public AuditorAware<String> autAuditorProvider() {
+		return () -> Optional.of(UUID.randomUUID().toString());
 	}
 }
