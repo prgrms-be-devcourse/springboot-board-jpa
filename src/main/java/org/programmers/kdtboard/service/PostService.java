6a3fce1a13d3f523dae1c9088d55d4da606c8736
HYEBPARK <hyeqls3742@gmail.com>
2022-05-17 03:05:36 +0900

    refactor : converter 선언

--- src/main/java/org/programmers/kdtboard/service/PostService.java
+++ src/main/java/org/programmers/kdtboard/service/PostService.java
@@ -1,7 +1,6 @@
 package org.programmers.kdtboard.service;
 
 import static java.text.MessageFormat.*;
-import static org.programmers.kdtboard.converter.Converter.*;
 
 import org.programmers.kdtboard.controller.response.ErrorCodeMessage;
 import org.programmers.kdtboard.converter.Converter;
@@ -10,7 +9,7 @@ import org.programmers.kdtboard.domain.post.PostRepository;
 import org.programmers.kdtboard.dto.PostDto.PostCreateRequest;
 import org.programmers.kdtboard.dto.PostDto.PostResponse;
 import org.programmers.kdtboard.dto.PostDto.PostUpdateRequest;
-import org.programmers.kdtboard.exception.NotFoundPostException;
+import org.programmers.kdtboard.exception.EntityNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -22,39 +21,44 @@ public class PostService {
 
 	private final PostRepository postRepository;
 	private final UserService userService;
+	private final Converter converter;
 
-	public PostService(PostRepository postRepository, UserService userService) {
+	public PostService(PostRepository postRepository,
+		UserService userService, Converter converter) {
 		this.postRepository = postRepository;
 		this.userService = userService;
+		this.converter = converter;
 	}
 
 	public PostResponse create(PostCreateRequest postCreateRequest) {
 		var post = Post.create(postCreateRequest.title(), postCreateRequest.content());
 		var userResponseDto = this.userService.findById(postCreateRequest.userId());
-		post.setUser(userDtoConverter(userResponseDto));
+		post.setUser(this.converter.userDtoConverter(userResponseDto));
 		this.postRepository.save(post);
 
-		return postConverter(post);
+		return this.converter.postConverter(post);
 	}
 
 	public PostResponse findById(Long id) {
 		var post = this.postRepository.findById(id)
 			.orElseThrow(
-				() -> new NotFoundPostException(format("{0}는 없는 id 입니다.", id), ErrorCodeMessage.POST_ID_NOT_FOUND));
+				() -> new EntityNotFoundException(format("post id : {0}, 없는 id 입니다.", id),
+					ErrorCodeMessage.POST_ID_NOT_FOUND));
 
-		return postConverter(post);
+		return this.converter.postConverter(post);
 	}
 
 	public Page<PostResponse> findAll(Pageable pageable) {
-		return this.postRepository.findAll(pageable).map(Converter::postConverter);
+		return this.postRepository.findAll(pageable).map(this.converter::postConverter);
 	}
 
 	public PostResponse update(Long id, PostUpdateRequest postUpdateRequest) {
 		var post = this.postRepository.findById(id)
 			.orElseThrow(() ->
-				new NotFoundPostException(format("{0}는 없는 id 입니다.", id), ErrorCodeMessage.POST_ID_NOT_FOUND));
+				new EntityNotFoundException(format("post id : {0}, 없는 id 입니다.", id),
+					ErrorCodeMessage.POST_ID_NOT_FOUND));
 		post.update(postUpdateRequest.title(), postUpdateRequest.content());
 
-		return postConverter(post);
+		return this.converter.postConverter(post);
 	}
 }
--- src/main/java/org/programmers/kdtboard/service/UserService.java
+++ src/main/java/org/programmers/kdtboard/service/UserService.java
@@ -1,14 +1,13 @@
 package org.programmers.kdtboard.service;
 
 import static java.text.MessageFormat.*;
-import static org.programmers.kdtboard.converter.Converter.*;
 
 import org.programmers.kdtboard.controller.response.ErrorCodeMessage;
 import org.programmers.kdtboard.converter.Converter;
 import org.programmers.kdtboard.domain.user.User;
 import org.programmers.kdtboard.domain.user.UserRepository;
 import org.programmers.kdtboard.dto.UserDto.UserResponseDto;
-import org.programmers.kdtboard.exception.NotFoundUserException;
+import org.programmers.kdtboard.exception.EntityNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -19,27 +18,30 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
 	private final UserRepository userRepository;
+	private final Converter converter;
 
-	public UserService(UserRepository userRepository) {
+	public UserService(UserRepository userRepository, Converter converter) {
 		this.userRepository = userRepository;
+		this.converter = converter;
 	}
 
 	public UserResponseDto create(String name, int age, String hobby) {
 		var user = User.create(name, age, hobby);
 		var save = userRepository.save(user);
 
-		return userConverter(save);
+		return this.converter.userConverter(save);
 	}
 
 	public UserResponseDto findById(Long id) {
 		var foundUser = userRepository.findById(id)
 			.orElseThrow(
-				() -> new NotFoundUserException(format("{0}는 없는 ID 입니다.", id),ErrorCodeMessage.USER_ID_NOT_FOUND));
+				() -> new EntityNotFoundException(format("user id : {0}, 없는 ID 입니다.", id),
+					ErrorCodeMessage.USER_ID_NOT_FOUND));
 
-		return userConverter(foundUser);
+		return this.converter.userConverter(foundUser);
 	}
 
 	public Page<UserResponseDto> findAll(Pageable pageable) {
-		return this.userRepository.findAll(pageable).map(Converter::userConverter);
+		return this.userRepository.findAll(pageable).map(this.converter::userConverter);
 	}
 }
