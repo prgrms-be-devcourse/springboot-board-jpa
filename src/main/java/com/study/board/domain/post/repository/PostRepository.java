b3fcde9c2b7322748adc691963ef779f59f06290
ndy2 <emrdbs12@gmail.com>
2022-05-16 23:26:08 +0900

    refactor: PostRepository 어노테이션 변경
    @Query -> @EntityGraph

--- src/main/java/com/study/board/domain/post/repository/PostRepository.java
+++ src/main/java/com/study/board/domain/post/repository/PostRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -14,6 +13,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @EntityGraph(attributePaths = "writer")
     Page<Post> findAll(Pageable pageable);
 
-    @Query("select p from Post p join fetch p.writer where p.id = :id")
+    @EntityGraph(attributePaths = "writer")
     Optional<Post> findById(Long id);
 }
