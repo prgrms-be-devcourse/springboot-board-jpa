a6aa81e396d81de798b91e1af34f19fa9f4e84a8
dbslzld15 <dbslzld15@naver.com>
2022-05-16 10:13:15 +0900

    feat: BaseEntity 구현

---
+++ src/main/java/org/prgrms/board/global/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package org.prgrms.board.global.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
