942f2da997926ff70b7110b6239f38cde473ccbb
unnokid <kimki0609@gmail.com>
2022-05-17 22:32:33 +0900

    Add Post

---
+++ src/main/java/com/example/springboardjpa/post/Post.java
@@ -0,0 +1,34 @@
+package com.example.springboardjpa.post;
+
+import com.example.springboardjpa.domain.BaseEntityCreate;
+import com.example.springboardjpa.user.User;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Table(name = "post")
+@Entity
+public class Post extends BaseEntityCreate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    @Column(name ="title", nullable = false)
+    private String title;
+
+    @Column(name ="content", nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public void setUser(User user){
+        if(Objects.nonNull(this.user)){
+            this.user.getPosts().add(this);
+        }
+    }
+}
