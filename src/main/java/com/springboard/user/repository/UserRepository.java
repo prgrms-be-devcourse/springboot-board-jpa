053278e21ea5463231f6e9c754049c8850637a48
송무송 (Kate) <real.purple.hae.s@gmail.com>
2022-05-18 01:39:12 +0900

    :sparkles: feat: User Spring Data Jpa Repository 추가

---
+++ src/main/java/com/springboard/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.springboard.user.repository;
+
+import com.springboard.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
