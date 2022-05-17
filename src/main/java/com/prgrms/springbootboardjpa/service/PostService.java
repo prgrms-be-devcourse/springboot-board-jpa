8b50bc6543e1e62f7f749164234adfe642546da7
suy2on <tnwjdrms@naver.com>
2022-05-17 21:58:11 +0900

    # feat  : post service 생성

---
+++ src/main/java/com/prgrms/springbootboardjpa/service/PostService.java
@@ -0,0 +1,57 @@
+package com.prgrms.springbootboardjpa.service;
+
+
+import com.prgrms.springbootboardjpa.dto.Converter;
+import com.prgrms.springbootboardjpa.dto.PostDto;
+import com.prgrms.springbootboardjpa.entity.Post;
+import com.prgrms.springbootboardjpa.repository.PostRepository;
+import java.util.Optional;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PostService {
+
+    PostRepository postRepository;
+
+    Converter converter;
+
+    public PostService(PostRepository postRepository, Converter converter) {
+        this.postRepository = postRepository;
+        this.converter = converter;
+    }
+
+    @Transactional
+    public Page<PostDto> findAllPosts(Pageable pageable) {
+        Page<Post> posts = postRepository.findAll(pageable);
+
+        return posts
+            .map(converter::convertToPostDto);
+    }
+
+    @Transactional
+    public PostDto findById(Long id) throws NotFoundException {
+        Optional<Post> post = postRepository.findById(id);
+        return converter.convertToPostDto(post.orElseThrow(NotFoundException::new));
+
+    }
+
+    @Transactional
+    public Long save(PostDto postDto) {
+        Post save = postRepository.save(converter.convertToPostEntity(postDto));
+        return save.getId();
+    }
+
+    @Transactional
+    public Long updateContent(Long id, String newContent) throws NotFoundException {
+        Optional<Post> post = postRepository.findById(id);
+        post.orElseThrow(NotFoundException::new).changeContent(newContent);
+        return id;
+    }
+
+
+}
+
