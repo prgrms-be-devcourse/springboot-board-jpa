91294eeceecd4b1594f53c799af56a6b648538e4
unnokid <kimki0609@gmail.com>
2022-05-17 22:32:02 +0900

    Add User

---
+++ src/main/java/com/example/springboardjpa/user/User.java
@@ -0,0 +1,34 @@
+package com.example.springboardjpa.user;
+
+import com.example.springboardjpa.domain.BaseEntityCreate;
+import com.example.springboardjpa.post.Post;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Table(name = "user")
+@Entity
+public class User extends BaseEntityCreate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    @Column(name ="name", nullable = false)
+    private String name;
+
+    @Column(name ="age", nullable = false)
+    private int age;
+
+    private String hobby;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts = new ArrayList<>();
+
+    public void addPosts(Post post){
+        post.setUser(this);
+    }
+}
