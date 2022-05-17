d40b86bbe815df65c41b1752866c370f3468a568
yuminhwan <ymh7532@icloud.com>
2022-05-17 22:25:49 +0900

    test: 테스트 케이스 추가

--- src/test/java/com/prgrms/springboard/post/domain/vo/ContentTest.java
+++ src/test/java/com/prgrms/springboard/post/domain/vo/ContentTest.java
@@ -3,6 +3,7 @@ package com.prgrms.springboard.post.domain.vo;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -23,12 +24,26 @@ class ContentTest {
         assertThat(content).isNotNull();
     }
 
-    @DisplayName("내용은 null, 공백, 200자 초과면 안 된다.")
+    @DisplayName("내용은 null, 공백이면 안 된다.")
     @ParameterizedTest
     @NullAndEmptySource
     void content_WrongContent(String content) {
         assertThatThrownBy(() -> new Content(content))
-            .isInstanceOf(InvalidValueException.class);
+            .isInstanceOf(InvalidValueException.class)
+            .hasMessage("내용은 null 이거나 공백만 있을 수 없습니다.");
+    }
+
+    @DisplayName("내용은 200자를 초과하면 안 된다.")
+    @Test
+    void content_OverMaxLength() {
+        // given
+        String content = "j".repeat(201);
+
+        // when
+        // then
+        assertThatThrownBy(() -> new Content(content))
+            .isInstanceOf(InvalidValueException.class)
+            .hasMessage("내용은 글자수가 200을 초과할 수 없습니다.");
     }
 
 }
--- src/test/resources/db/data.sql
+++ src/test/resources/db/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO user(created_at, created_by, age, hobby, name, id)
 VALUES (CURRENT_TIMESTAMP, '유민환', 26, '낚시', '유민환', 1);
 INSERT INTO post(created_at, created_by, title, content, user_id, id)
-VALUES (CURRENT_TIMESTAMP, '유민환', '제목입니다.', '내용입니다.', 1, 1)
+VALUES (CURRENT_TIMESTAMP, '유민환', '제목입니다.', '내용입니다.', 1, 10)
