7bbbac0e5aed43c0f018f8d574aaeb8e7b41f2c1
sdardew <sdardew@gmail.com>
2022-05-18 17:18:17 +0900

    Post 와의 연관관계 추가

--- src/main/java/com/sdardew/board/domain/user/User.java
+++ src/main/java/com/sdardew/board/domain/user/User.java
@@ -1,68 +1,71 @@
 package com.sdardew.board.domain.user;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.sdardew.board.domain.post.Post;
+import org.apache.tomcat.jni.Local;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "user")
 public class User {
+  @GeneratedValue(strategy = GenerationType.AUTO)
   @Id
-  @Column
   private Long id;
   private String name;
   private Integer age;
   private String hobby;
   private LocalDateTime createAt;
-  private LocalDateTime createBy;
+  @OneToMany(mappedBy = "post")
+  private List<Post> posts = new ArrayList<>();
 
   public Long getId() {
     return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
   }
 
   public String getName() {
     return name;
   }
 
-  public void setName(String name) {
-    this.name = name;
-  }
-
   public Integer getAge() {
     return age;
-  }
-
-  public void setAge(Integer age) {
-    this.age = age;
   }
 
   public String getHobby() {
     return hobby;
   }
 
-  public void setHobby(String hobby) {
-    this.hobby = hobby;
-  }
-
   public LocalDateTime getCreateAt() {
     return createAt;
+  }
+
+  public List<Post> getPosts() {
+    return posts;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public void setHobby(String hobby) {
+    this.hobby = hobby;
   }
 
   public void setCreateAt(LocalDateTime createAt) {
     this.createAt = createAt;
   }
 
-  public LocalDateTime getCreateBy() {
-    return createBy;
-  }
-
-  public void setCreateBy(LocalDateTime createBy) {
-    this.createBy = createBy;
+  public void setPosts(List<Post> posts) {
+    this.posts = posts;
   }
 }
