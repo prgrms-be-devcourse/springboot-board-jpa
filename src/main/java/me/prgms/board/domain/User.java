161a91ee0555c56089c4766dbb4f1bf61cca8315
Yanjoo <hanjub3@gmail.com>
2022-05-19 04:37:54 +0900

    User 도메인 추가

---
+++ src/main/java/me/prgms/board/domain/User.java
@@ -0,0 +1,42 @@
+package me.prgms.board.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 30)
+    private String name;
+
+    @Column(name = "age", nullable = false)
+    private int age;
+
+    @Column(name = "hobby", nullable = false, length = 50)
+    private String hobby;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+}
