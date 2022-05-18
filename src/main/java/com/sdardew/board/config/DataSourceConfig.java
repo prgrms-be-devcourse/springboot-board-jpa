2f2366536bf81d985257144bbe7ae901c8b08f5e
sdardew <sdardew@gmail.com>
2022-05-18 15:20:14 +0900

    setting: JPA 사용을 위한 설정

---
+++ src/main/java/com/sdardew/board/config/DataSourceConfig.java
@@ -0,0 +1,62 @@
+package com.sdardew.board.config;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class DataSourceConfig {
+
+  @Bean
+  public DataSource dataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName("org.h2.Driver");
+    dataSource.setUrl("jdbc:h2:~/board");
+    dataSource.setUsername("sa");
+    dataSource.setPassword("");
+
+    return dataSource;
+  }
+
+
+  @Bean
+  public JpaVendorAdapter jpaVendorAdapter(JpaProperties jpaProperties) {
+    AbstractJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+    jpaVendorAdapter.setShowSql(jpaProperties.isShowSql());
+    jpaVendorAdapter.setDatabasePlatform(jpaProperties.getDatabasePlatform());
+    jpaVendorAdapter.setGenerateDdl(jpaProperties.isGenerateDdl());
+    return jpaVendorAdapter;
+  }
+
+  @Bean
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource,
+                                                                     JpaVendorAdapter jpaVendorAdapter,
+                                                                     JpaProperties jpaProperties) {
+    LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+    em.setDataSource(dataSource);
+    em.setPackagesToScan("com.prgrms.springbootjpa.domain");
+    em.setJpaVendorAdapter(jpaVendorAdapter);
+
+    Properties properties = new Properties();
+    properties.putAll(jpaProperties.getProperties());
+    em.setJpaProperties(properties);
+
+    return em;
+  }
+
+  @Bean
+  public PlatformTransactionManager transactionManager(LocalContainerEntityManagerFactoryBean entityManagerFactory) {
+    JpaTransactionManager transactionManager = new JpaTransactionManager();
+    transactionManager.setEntityManagerFactory(entityManagerFactory.getObject());
+    return transactionManager;
+  }
+}
