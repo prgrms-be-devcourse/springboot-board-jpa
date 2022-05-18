b72de800520fbf4f9e7dcd7a8ecc7e3137d22173
sdardew <sdardew@gmail.com>
2022-05-18 17:20:11 +0900

    user 테이블에 접근하는 UserRepository 생성

--- src/main/java/com/sdardew/board/repository/UserRepository.java
+++ src/main/java/com/sdardew/board/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.sdardew.board.repository;
 
 import com.sdardew.board.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
