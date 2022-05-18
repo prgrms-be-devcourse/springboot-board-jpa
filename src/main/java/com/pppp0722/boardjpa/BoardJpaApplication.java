fa6f11b588b01bc75383beb36dede6e423d9f8a1
pppp0722 <poj0722@naver.com>
2022-05-19 00:23:06 +0900

    [add] spring boot maven 프로젝트 생성

---
+++ src/main/java/com/pppp0722/boardjpa/BoardJpaApplication.java
@@ -0,0 +1,13 @@
+package com.pppp0722.boardjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardJpaApplication.class, args);
+    }
+
+}
