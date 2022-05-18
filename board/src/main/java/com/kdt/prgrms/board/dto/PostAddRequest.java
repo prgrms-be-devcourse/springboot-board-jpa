0dc047c6c87ba2ea32577c8e8f7a71560b0a3749
UJ15 <dbwogml15@naver.com>
2022-05-18 19:28:40 +0900

    :fire: delete : @NotNull 어노테이션 제거

--- board/src/main/java/com/kdt/prgrms/board/dto/PostAddRequest.java
+++ board/src/main/java/com/kdt/prgrms/board/dto/PostAddRequest.java
@@ -1,21 +1,15 @@
 package com.kdt.prgrms.board.dto;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 public class PostAddRequest {
 
     @NotBlank
-    @NotNull
     private final String title;
 
     @NotBlank
-    @NotNull
     private final String content;
 
-    @JsonCreator
     public PostAddRequest(String title, String content) {
 
         this.title = title;
