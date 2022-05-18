4695ee2a1b18a63cd4351df6521bd563e62e1180
Gosh95 <goshk1995@gmail.com>
2022-05-18 20:21:27 +0300

    feat: update 시간 추가

--- src/main/java/prgrms/project/post/domain/BaseEntity.java
+++ src/main/java/prgrms/project/post/domain/BaseEntity.java
@@ -3,6 +3,7 @@ package prgrms.project.post.domain;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
@@ -17,8 +18,12 @@ import java.util.UUID;
 public abstract class BaseEntity {
 
     @CreatedDate
-    @Column(nullable = false, updatable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     @CreatedBy
     @Column(nullable = false, updatable = false)
