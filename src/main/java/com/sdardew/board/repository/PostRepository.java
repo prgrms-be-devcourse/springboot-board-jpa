45f657db8fbd7e460d221c0799651cac139be43b
sdardew <sdardew@gmail.com>
2022-05-18 17:19:25 +0900

    PostRespository 생성
    post 테이블에 접근할 때 사용

---
+++ src/main/java/com/sdardew/board/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.sdardew.board.repository;
+
+import com.sdardew.board.domain.post.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
