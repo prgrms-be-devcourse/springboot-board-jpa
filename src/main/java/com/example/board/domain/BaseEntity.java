57b3d7075296c2ce9887097797bbd2d31d43e417
Yoonoh <xkakak142@naver.com>
2022-05-19 01:35:01 +0900

    feat: base Entity 생성

---
+++ src/main/java/com/example/board/domain/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.example.board.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt;
+}
