ad1b2a404d8bab10c7c8f164f1263239b7c02d81
UJ15 <dbwogml15@naver.com>
2022-05-18 19:29:12 +0900

    :white_check_mark: test : test케이스 추가

--- board/src/test/java/com/kdt/prgrms/board/controller/PostApiControllerTest.java
+++ board/src/test/java/com/kdt/prgrms/board/controller/PostApiControllerTest.java
@@ -36,13 +36,89 @@ public class PostApiControllerTest {
     @DisplayName("addPost메서드는")
     class DescribeAddPost {
 
-        String url = "/api/v1/posts";
+        private final String url = "/api/v1/posts";
 
         @Nested
-        @DisplayName("제목이 없는 게시글 생성요청이 들어오면")
-        class ContextWhiteSpaceTitlePostAddRequest {
+        @DisplayName("제목이 빈 게시글 생성요청이 들어오면")
+        class ContextNotExistTitlePostAddRequest {
 
             PostAddRequest requestBody = new PostAddRequest("", "Hello");
+
+            MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(url)
+                    .content(toJson(requestBody))
+                    .contentType(MediaType.APPLICATION_JSON);
+
+            @Test
+            @DisplayName("400 bad request 응답을 반환한다.")
+            void itReturnBadRequest() throws Exception {
+
+                mockMvc.perform(request)
+                        .andExpect(status().isBadRequest());
+            }
+        }
+
+        @Nested
+        @DisplayName("제목이 Null인 게시글 생성인청이 들어오면")
+        class ContextNullTitlePostAddRequest {
+
+            PostAddRequest requestBody = new PostAddRequest(null, "Hello");
+
+            MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(url)
+                    .content(toJson(requestBody))
+                    .contentType(MediaType.APPLICATION_JSON);
+
+            @Test
+            @DisplayName("400 bad request 응답을 반환한다.")
+            void itReturnBadRequest() throws Exception {
+
+                mockMvc.perform(request)
+                        .andExpect(status().isBadRequest());
+            }
+        }
+
+        @Nested
+        @DisplayName("제목이 공백인 게시글 생성인청이 들어오면")
+        class ContextWhiteSpaceTitlePostAddRequest {
+
+            PostAddRequest requestBody = new PostAddRequest("  ", "Hello");
+
+            MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(url)
+                    .content(toJson(requestBody))
+                    .contentType(MediaType.APPLICATION_JSON);
+
+            @Test
+            @DisplayName("400 bad request 응답을 반환한다.")
+            void itReturnBadRequest() throws Exception {
+
+                mockMvc.perform(request)
+                        .andExpect(status().isBadRequest());
+            }
+        }
+
+        @Nested
+        @DisplayName("내용이 없는 게시글 생성요청이 들어오면")
+        class ContextNotExistContentPostAddRequest {
+
+            PostAddRequest requestBody = new PostAddRequest("Title", "");
+
+            MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(url)
+                    .content(toJson(requestBody))
+                    .contentType(MediaType.APPLICATION_JSON);
+
+            @Test
+            @DisplayName("400 bad request 응답을 반환한다.")
+            void itReturnBadRequest() throws Exception {
+
+                mockMvc.perform(request)
+                        .andExpect(status().isBadRequest());
+            }
+        }
+
+        @Nested
+        @DisplayName("내용이 null인 게시글 생성요청이 들어오면")
+        class ContextNullContentPostAddRequest {
+
+            PostAddRequest requestBody = new PostAddRequest("Tilte", null);
 
             MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(url)
                     .content(toJson(requestBody))
@@ -61,7 +137,7 @@ public class PostApiControllerTest {
         @DisplayName("내용이 없는 게시글 생성요청이 들어오면")
         class ContextWhiteSpaceContentPostAddRequest {
 
-            PostAddRequest requestBody = new PostAddRequest("Title", "");
+            PostAddRequest requestBody = new PostAddRequest("Title", "   ");
 
             MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(url)
                     .content(toJson(requestBody))
