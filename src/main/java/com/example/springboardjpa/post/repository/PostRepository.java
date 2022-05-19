ccd009987780ae9da27cdf52db6a9d59d8378fa7
unnokid <kimki0609@gmail.com>
2022-05-19 18:30:59 +0900

    Remove not using method

--- src/main/java/com/example/springboardjpa/post/repository/PostRepository.java
+++ src/main/java/com/example/springboardjpa/post/repository/PostRepository.java
@@ -4,5 +4,4 @@ import com.example.springboardjpa.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-
 }
