5d9f8a24948de501ace33d60a6306047ddbe36e6
beomsic <qjatjr29@khu.ac.kr>
2022-05-19 22:20:58 +0900

    [Feat] BaseEntity 기능 추가, jpa auditing 사용
    - createdBy
    - createdAt
    - updatedAt

---
+++ src/main/java/com/kdt/jpaboard/domain/board/BaseEntity.java
@@ -0,0 +1,33 @@
+package com.kdt.jpaboard.domain.board;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseEntity {
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime updatedAt;
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+}
