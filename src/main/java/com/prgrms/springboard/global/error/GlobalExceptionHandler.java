1e6463427b703a064c9b5e1ec041646e55f485ba
yuminhwan <ymh7532@icloud.com>
2022-05-19 12:15:09 +0900

    refactor: ResponseStatus 활용 및 UserNotHavePermission 예외 처리 추가

--- src/main/java/com/prgrms/springboard/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/prgrms/springboard/global/error/GlobalExceptionHandler.java
@@ -3,14 +3,15 @@ package com.prgrms.springboard.global.error;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.prgrms.springboard.global.common.ApiResponse;
 import com.prgrms.springboard.global.error.exception.InvalidValueException;
 import com.prgrms.springboard.global.error.exception.NotFoundException;
+import com.prgrms.springboard.post.exception.UserNotHavePermission;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,27 +19,34 @@ public class GlobalExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse<String>> handleNotValidException(MethodArgumentNotValidException e) {
-        return ResponseEntity.badRequest()
-            .body(ApiResponse.fail(HttpStatus.BAD_REQUEST.value(), bindingResultMessage(e)));
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResponse<String> handleNotValidException(MethodArgumentNotValidException e) {
+        return ApiResponse.fail(HttpStatus.BAD_REQUEST.value(), bindingResultMessage(e));
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ApiResponse<String>> handleNotFoundException(NotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-            .body(ApiResponse.fail(HttpStatus.NOT_FOUND.value(), e.getMessage()));
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiResponse<String> handleNotFoundException(NotFoundException e) {
+        return ApiResponse.fail(HttpStatus.NOT_FOUND.value(), e.getMessage());
+    }
+
+    @ExceptionHandler(UserNotHavePermission.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ApiResponse<String> handleNotPermissionException(UserNotHavePermission e) {
+        return ApiResponse.fail(HttpStatus.FORBIDDEN.value(), e.getMessage());
     }
 
     @ExceptionHandler(InvalidValueException.class)
-    public ResponseEntity<ApiResponse<String>> handleInvalidValueException(NotFoundException e) {
-        return ResponseEntity.badRequest().body(ApiResponse.fail(HttpStatus.NOT_FOUND.value(), e.getMessage()));
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResponse<String> handleInvalidValueException(NotFoundException e) {
+        return ApiResponse.fail(HttpStatus.BAD_REQUEST.value(), e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse<String>> handleException(Exception e) {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiResponse<String> handleException(Exception e) {
         logger.error(e.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .body(ApiResponse.fail(HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버에서 에러가 발생했습니다."));
+        return ApiResponse.fail(HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버에서 에러가 발생했습니다.");
     }
 
     private String bindingResultMessage(MethodArgumentNotValidException exception) {
