7b126e00bcf6b57d9800a5640d14a430b9934fc0
unnokid <kimki0609@gmail.com>
2022-05-19 22:15:45 +0900

    Remove not using repository

--- src/main/java/com/example/springboardjpa/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springboardjpa.user.repository;
-
-import com.example.springboardjpa.user.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
