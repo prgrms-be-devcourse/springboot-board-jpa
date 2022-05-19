783abf26358b0abaedf50bf4612afa44c5a7aeaf
ndy2 <emrdbs12@gmail.com>
2022-05-19 15:27:16 +0900

    test: Post 수정 테스트

--- src/test/java/com/study/board/domain/post/domain/PostTest.java
+++ src/test/java/com/study/board/domain/post/domain/PostTest.java
@@ -6,9 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
+import java.util.List;
+
 import static com.study.board.domain.post.AssertPost.assertPostCreation;
 import static com.study.board.domain.post.AssertPost.assertWriter;
 import static com.study.board.domain.post.domain.Post.POST_TITLE_MAX_LENGTH;
+import static com.study.board.fixture.Fixture.samplePost;
 import static com.study.board.fixture.Fixture.sampleUser;
 import static org.assertj.core.api.Assertions.*;
 
@@ -49,5 +52,40 @@ public class PostTest {
     void 작성자가_null_이면_생성_실패() {
         assertThatNullPointerException()
                 .isThrownBy(() -> new Post("제목", "내용", null));
+    }
+
+    @Test
+    void 수정_성공() {
+        Post origPost = samplePost();
+
+        Post updatedPost = origPost.update("수정제목", "수정내용");
+
+        assertWriter(updatedPost.getWriter(), origPost.getId());
+        assertThat(updatedPost.getWrittenDateTime()).isEqualTo(origPost.getWrittenDateTime());
+
+        assertThat(List.of(updatedPost.getTitle(), origPost.getTitle())).allMatch(title -> title.equals("수정제목"));
+        assertThat(List.of(updatedPost.getContent(), origPost.getContent())).allMatch(title -> title.equals("수정내용"));
+    }
+
+    @NullAndEmptySource
+    @ParameterizedTest
+    void 제목이_null_이거나_비어있으면_수정실패(String title) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> samplePost().update(title, "수정내용"));
+    }
+
+    @Test
+    void 제목의_길이가_제한_보다_크면_수정실패() {
+        String title = RandomString.make(POST_TITLE_MAX_LENGTH + 1);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> samplePost().update(title, "수정내용"));
+    }
+
+    @NullAndEmptySource
+    @ParameterizedTest
+    void 내용이_null_이거나_비어있으면_수정실패(String content) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> samplePost().update("수정제목", content));
     }
 }
--- src/test/java/com/study/board/fixture/Fixture.java
+++ src/test/java/com/study/board/fixture/Fixture.java
@@ -1,5 +1,6 @@
 package com.study.board.fixture;
 
+import com.study.board.domain.post.domain.Post;
 import com.study.board.domain.user.domain.User;
 
 public class Fixture {
@@ -8,5 +9,7 @@ public class Fixture {
         return User.create("득윤", "체스");
     }
 
-
+    public static Post samplePost() {
+        return new Post("제목", "내용", sampleUser());
+    }
 }
