aea4439d459ff6f1f8c3fc31dd718130deeca908
Yongsu Kang <dksktm15@naver.com>
2022-05-19 22:51:46 +0900

    생성 날짜 BaseEntity 작성

---
+++ spring_jpa_post/src/main/java/com/example/spring_jpa_post/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.example.spring_jpa_post;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @CreatedBy
+    private String createdBy;
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
