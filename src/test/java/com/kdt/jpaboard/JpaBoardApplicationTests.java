218858d8b360d3bd92dac74e04e1d4d07fbd5568
beomsic <qjatjr29@khu.ac.kr>
2022-05-19 22:01:33 +0900

    [Init] 초기 설정

---
+++ src/test/java/com/kdt/jpaboard/JpaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdt.jpaboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
