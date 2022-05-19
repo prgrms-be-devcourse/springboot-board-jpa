e668fe15d753cbbe329c7fff005424fa55d679be
ordilov <ordilov@gmail.com>
2022-05-20 01:15:50 +0900

    refactor: 불필요한 import를 제거하라

--- src/main/java/org/prgrms/kdt/BoardApplication.java
+++ src/main/java/org/prgrms/kdt/BoardApplication.java
@@ -2,13 +2,12 @@ package org.prgrms.kdt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class BoardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BoardApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BoardApplication.class, args);
+  }
 
 }
