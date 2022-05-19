62ef202c101c9032badde197f4bc8d9bced9acfe
San Kim <waterfog9580@gmail.com>
2022-05-19 21:05:03 +0900

    :sparkles: DefaultUserService addUser 프로덕션 코드 작성

--- src/main/java/com/waterfogsw/springbootboardjpa/user/service/DefaultUserService.java
+++ src/main/java/com/waterfogsw/springbootboardjpa/user/service/DefaultUserService.java
@@ -3,6 +3,7 @@ package com.waterfogsw.springbootboardjpa.user.service;
 import com.waterfogsw.springbootboardjpa.user.entity.User;
 import com.waterfogsw.springbootboardjpa.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 @Service
@@ -15,6 +16,7 @@ public class DefaultUserService implements UserService {
     }
 
     @Override
+    @Transactional
     public void addUser(User user) {
         Assert.notNull(user, "User should not be null");
         userRepository.save(user);
