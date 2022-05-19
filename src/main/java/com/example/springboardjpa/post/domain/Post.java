527a58c11582404bca63c4a7471d94ef80321b4c
unnokid <kimki0609@gmail.com>
2022-05-19 18:28:23 +0900

    Add Constructor and `changeTitle()` and `changeContent()`

--- src/main/java/com/example/springboardjpa/post/domain/Post.java
+++ src/main/java/com/example/springboardjpa/post/domain/Post.java
@@ -3,11 +3,14 @@ package com.example.springboardjpa.post.domain;
 import com.example.springboardjpa.domain.BaseEntityCreate;
 import com.example.springboardjpa.user.domain.User;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Getter
+@NoArgsConstructor
 @Table(name = "post")
 @Entity
 public class Post extends BaseEntityCreate {
@@ -22,13 +25,29 @@ public class Post extends BaseEntityCreate {
     @Column(name ="content", nullable = false)
     private String content;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id")
     private User user;
+
+    public Post(String createBy, LocalDateTime createdAt, String title, String content) {
+        super(createBy, createdAt);
+        this.title = title;
+        this.content = content;
+    }
 
     public void setUser(User user){
         if(Objects.nonNull(this.user)){
             this.user.getPosts().add(this);
         }
+        this.user = user;
+        user.getPosts().add(this);
+    }
+
+    public void changeTitle(String title){
+        this.title = title;
+    }
+
+    public void changeContent(String content){
+        this.content = content;
     }
 }
