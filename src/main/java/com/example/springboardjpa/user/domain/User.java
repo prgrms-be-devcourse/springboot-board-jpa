529d70fbdf1fd4afce267dd9184bae3e3d8e97b6
unnokid <kimki0609@gmail.com>
2022-05-19 18:28:00 +0900

    Add Constructor

--- src/main/java/com/example/springboardjpa/user/domain/User.java
+++ src/main/java/com/example/springboardjpa/user/domain/User.java
@@ -3,12 +3,15 @@ package com.example.springboardjpa.user.domain;
 import com.example.springboardjpa.domain.BaseEntityCreate;
 import com.example.springboardjpa.post.domain.Post;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 @Table(name = "user")
 @Entity
 public class User extends BaseEntityCreate {
@@ -27,6 +30,13 @@ public class User extends BaseEntityCreate {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Post> posts = new ArrayList<>();
+
+    public User(String createBy, LocalDateTime createdAt, String name, int age, String hobby) {
+        super(createBy, createdAt);
+        this.name = name;
+        this.age = age;
+        this.hobby = hobby;
+    }
 
     public void addPosts(Post post){
         post.setUser(this);
