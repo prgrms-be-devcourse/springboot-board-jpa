779adfdf5807129c90bb7ccbb2367a6df8639d51
jki503 <jki503@naver.com>
2022-05-20 09:04:15 +0900

    feat : 소스코드 추가

--- board/src/main/java/com/programmers/board/common/filter/ReadableRequestWrapperFilter.java
+++ board/src/main/java/com/programmers/board/common/filter/ReadableRequestWrapperFilter.java
@@ -1,2 +1,21 @@
-package com.programmers.board.common.filter;public class ReadableRequestWrapperFilter {
+package com.programmers.board.common.filter;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Order(1)
+@Component
+public class ReadableRequestWrapperFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        ContentCachingRequestWrapper wrapper = new ContentCachingRequestWrapper((HttpServletRequest) request);
+        chain.doFilter(wrapper, response);
+    }
+
 }
