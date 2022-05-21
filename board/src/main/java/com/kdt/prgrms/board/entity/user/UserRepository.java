23b051d282c08b6b4cedc6fae5addcfc43a2a7c5
UJ15 <dbwogml15@naver.com>
2022-05-21 16:19:57 +0900

    :sparkles: feat: UserService, Repository test 및 구현

---
+++ board/src/main/java/com/kdt/prgrms/board/entity/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.kdt.prgrms.board.entity.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
