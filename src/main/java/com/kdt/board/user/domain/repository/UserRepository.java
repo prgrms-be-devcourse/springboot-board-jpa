3c393ae240dc1860a8113092dbac1de8db5b67e1
ecvheo1 <ecvheo@naver.com>
2022-05-22 01:38:58 +0900

    :sparkles: User 등록 Repository 프로덕션 코드 작성

---
+++ src/main/java/com/kdt/board/user/domain/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.kdt.board.user.domain.repository;
+
+import com.kdt.board.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
