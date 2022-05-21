a414bacc4a2ffb20a9aded007660c3371609f422
ecvheo1 <ecvheo@naver.com>
2022-05-22 00:46:44 +0900

    :sparkles: User Entity & BaseEntity 생성

---
+++ src/main/java/com/kdt/board/user/domain/User.java
@@ -0,0 +1,90 @@
+package com.kdt.board.user.domain;
+
+import com.kdt.board.common.entity.BaseEntity;
+
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+@Entity
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Column(unique = true)
+    private String email;
+
+    @Min(0)
+    private int age;
+
+    private String hobby;
+
+    protected User() {
+    }
+
+    private User(Builder builder) {
+        this.name = builder.name;
+        this.email = builder.email;
+        this.age = builder.age;
+        this.hobby = builder.hobby;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    public static class Builder {
+        private String name;
+        private String email;
+        private int age;
+        private String hobby;
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder hobby(String hobby) {
+            this.hobby = hobby;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
+}
