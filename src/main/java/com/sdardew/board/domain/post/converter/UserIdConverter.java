ed9908ded70b84c37719373e405b0a33fd9628d1
sdardew <sdardew@gmail.com>
2022-05-21 14:43:08 +0900

    feat: Converter
    post 테이블의 userId를
    user 테이블의 User로 변환해주는
    컨버터

---
+++ src/main/java/com/sdardew/board/domain/post/converter/UserIdConverter.java
@@ -0,0 +1,31 @@
+package com.sdardew.board.domain.post.converter;
+
+import com.sdardew.board.domain.user.User;
+import com.sdardew.board.repository.UserRepository;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.util.NoSuchElementException;
+
+@Component
+@Converter
+public class UserIdConverter implements AttributeConverter<User, Long> {
+
+  private final UserRepository userRepository;
+
+  public UserIdConverter(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public Long convertToDatabaseColumn(User user) {
+    return user.getId();
+  }
+
+  @Override
+  public User convertToEntityAttribute(Long userId) {
+    if(userRepository.existsById(userId)) return userRepository.findById(userId).get();
+    throw new NoSuchElementException("존재하지 않는 User입니다");
+  }
+}
