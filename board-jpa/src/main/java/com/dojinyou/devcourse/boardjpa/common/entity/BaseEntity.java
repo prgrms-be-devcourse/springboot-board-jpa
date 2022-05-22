d0ef59432c6a3df24b77d8595d905234c0555144
dojinyou <djyou128@gmail.com>
2022-05-22 22:02:42 +0900

    :sparkles: feat: BaseEntity 작성

---
+++ board-jpa/src/main/java/com/dojinyou/devcourse/boardjpa/common/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.dojinyou.devcourse.boardjpa.common.entity;
+
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
