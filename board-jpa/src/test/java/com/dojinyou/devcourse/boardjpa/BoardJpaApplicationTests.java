3cef52aa3313b145ec68f7fe870d7f1a13443fca
dojinyou <djyou128@gmail.com>
2022-05-22 11:35:14 +0900

    :tada: init: project init

---
+++ board-jpa/src/test/java/com/dojinyou/devcourse/boardjpa/BoardJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojinyou.devcourse.boardjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
