02fe375d8979f067dfcd193e154c9c5322177c39
ChoiYeonho0903 <903yh@naver.com>
2022-05-22 19:20:35 +0900

    사용자 repository test 코드 구현

---
+++ kdt-jpa-board/src/test/java/com/kdt/board/user/repository/UserRepositoryTest.java
@@ -0,0 +1,79 @@
+package com.kdt.board.user.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import com.kdt.board.user.domain.User;
+import java.util.Optional;
+import javax.persistence.EntityManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    @DisplayName("저장된 User를 검색할 수 있다.")
+    void userFindTest() {
+        // Given
+        User user = new User("CHOI", 27);
+        userRepository.save(user);
+        em.flush();
+        em.clear();
+
+        // When
+        Optional<User> findUser = userRepository.findById(user.getId());
+
+        // Then
+        assertThat(findUser.isEmpty()).isFalse();
+    }
+    
+    @Test
+    @DisplayName("저장된 User 정보 업데이트, dirty checking")
+    void userUpdateTest() {
+        // Given
+        User user = new User("CHOI", 27);
+        userRepository.save(user);
+        em.flush();
+        em.clear();
+
+        // When
+        User findUser = userRepository.findById(user.getId()).get();
+        findUser.changeHobby("영화 감상");
+        em.flush();
+        em.clear();
+
+        // Then
+        User updateUser = userRepository.findById(user.getId()).get();
+        assertThat(updateUser.getHobby()).isEqualTo("영화 감상");
+    }
+
+    @Test
+    @DisplayName("저장된 User 삭제")
+    void userDeleteById() {
+        // Given
+        User user = new User("CHOI", 27);
+        userRepository.save(user);
+        em.flush();
+        em.clear();
+
+        // When
+        userRepository.deleteById(user.getId());
+        em.flush();
+
+        // Then
+        Optional<User> findUser = userRepository.findById(user.getId());
+        assertThat(findUser.isEmpty()).isTrue();
+    }
+}
