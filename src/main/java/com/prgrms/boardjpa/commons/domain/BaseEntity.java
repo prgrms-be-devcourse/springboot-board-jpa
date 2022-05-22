e07db7ae3e385e1fcdc087034fa02553db59096a
ynoolee <heerarea129@gmail.com>
2022-05-23 08:42:37 +0900

    refactor: BaseEntity 를 추상클래스로 변경하고 필드의 접근제한자를 변경

--- src/main/java/com/prgrms/boardjpa/commons/domain/BaseEntity.java
+++ src/main/java/com/prgrms/boardjpa/commons/domain/BaseEntity.java
@@ -6,27 +6,30 @@ import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseEntity {
+public abstract class BaseEntity {
 	protected BaseEntity() {
 	}
 
-	public BaseEntity(LocalDateTime createdAt, LocalDateTime updatedAt) {
+	protected BaseEntity(LocalDateTime createdAt, LocalDateTime updatedAt) {
 		this.createdAt = createdAt;
 		this.updatedAt = updatedAt;
 	}
 
+	@CreatedDate
 	@Column(nullable = false, updatable = false)
-	private LocalDateTime createdAt;
+	protected LocalDateTime createdAt;
 
 	@LastModifiedDate
-	private LocalDateTime updatedAt;
-
+	protected LocalDateTime updatedAt;
 }
