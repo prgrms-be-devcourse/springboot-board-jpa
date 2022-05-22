7063c4d39fbb82f651d80685a2919b7fd57a4240
ChoiYeonho0903 <903yh@naver.com>
2022-05-23 03:19:03 +0900

    NotFoundException 구현, controllerAdvice 구현 추가

---
+++ kdt-jpa-board/src/main/java/com/kdt/board/common/exception/advice/ExceptionControllerAdvice.java
@@ -0,0 +1,28 @@
+package com.kdt.board.common.exception.advice;
+
+import com.kdt.board.common.dto.ApiResponse;
+import com.kdt.board.common.exception.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ExceptionControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public ApiResponse<String>  notFountExceptionHandler (NotFoundException e) {
+        log.error("NotFountException", e);
+        return new ApiResponse<String>(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler
+    public ApiResponse<String> exceptionHandler (Exception e) {
+        log.error("Exception", e);
+        return new ApiResponse<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+    }
+}
