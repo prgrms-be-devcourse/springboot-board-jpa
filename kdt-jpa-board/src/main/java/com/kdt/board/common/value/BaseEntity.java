075a2a418c7b2ff29b1c7fcab7c96e3e56fbeeb7
ChoiYeonho0903 <903yh@naver.com>
2022-05-23 03:21:11 +0900

    BaseEntity 구현

---
+++ kdt-jpa-board/src/main/java/com/kdt/board/common/value/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.kdt.board.common.value;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(name = "created_by")
+    private String createdBy;
+
+}
