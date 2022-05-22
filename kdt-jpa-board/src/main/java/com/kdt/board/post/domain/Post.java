1de1493709eac6d99b00d50957beffa2b8d365e8
ChoiYeonho0903 <903yh@naver.com>
2022-05-22 19:29:11 +0900

    post entity 구현

---
+++ kdt-jpa-board/src/main/java/com/kdt/board/post/domain/Post.java
@@ -0,0 +1,67 @@
+package com.kdt.board.post.domain;
+
+import com.kdt.board.common.value.BaseEntity;
+import com.kdt.board.user.domain.User;
+import java.util.Objects;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "post")
+@Getter
+@NoArgsConstructor
+public class Post extends BaseEntity {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(name = "title", nullable = false, length = 50)
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    public Post(String title, String content, User user) {
+        this.title = title;
+        this.content = content;
+        this.user = user;
+    }
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public Post(String title) {
+        this.title = title;
+    }
+
+    public void changeTitle(String title) {
+        this.title = title;
+    }
+
+    public void changeContent(String content) {
+        this.content = content;
+    }
+
+    public void setUser(User user) {
+        if (Objects.nonNull(this.user)) {
+            this.user.getPosts().remove(this);
+        }
+        this.user = user;
+        user.getPosts().add(this);
+    }
+}
