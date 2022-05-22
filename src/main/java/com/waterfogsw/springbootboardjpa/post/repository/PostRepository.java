b921cbd579b2566a0bf333d20e937dcaaafcea8e
San Kim <waterfog9580@gmail.com>
2022-05-22 13:28:15 +0900

    :sparkles: PostRepository 프로덕션 코드 작성

--- src/main/java/com/waterfogsw/springbootboardjpa/post/repository/PostRepository.java
+++ src/main/java/com/waterfogsw/springbootboardjpa/post/repository/PostRepository.java
@@ -1,7 +1,19 @@
 package com.waterfogsw.springbootboardjpa.post.repository;
 
 import com.waterfogsw.springbootboardjpa.post.entity.Post;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query("select p from Post p join fetch p.user where p.id = :id")
+    Optional<Post> findById(@Param("id") Long id);
+
+    @Query("select p from Post p join fetch p.user")
+    List<Post> findWithPagination(Pageable pageable);
 }
