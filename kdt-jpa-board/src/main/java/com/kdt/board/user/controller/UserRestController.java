435b358b6ca48aba49c5b4f36f8bf361a86954fb
ChoiYeonho0903 <903yh@naver.com>
2022-05-22 19:27:02 +0900

    사용자 restController 구현

---
+++ kdt-jpa-board/src/main/java/com/kdt/board/user/controller/UserRestController.java
@@ -0,0 +1,33 @@
+package com.kdt.board.user.controller;
+
+import com.kdt.board.common.dto.ApiResponse;
+import com.kdt.board.user.dto.request.UserCreateRequestDto;
+import com.kdt.board.user.dto.response.UserResponseDto;
+import com.kdt.board.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserRestController {
+
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    public ApiResponse<UserResponseDto> getOne(@PathVariable("id") Long userId) {
+        UserResponseDto userResponseDto = userService.findOne(userId);
+        return ApiResponse.ok(userResponseDto);
+    }
+
+    @PostMapping
+    public ApiResponse<Long> saveUser(@RequestBody UserCreateRequestDto userCreateRequestDto) {
+        Long userId = userService.save(userCreateRequestDto);
+        return ApiResponse.ok(userId);
+    }
+}
