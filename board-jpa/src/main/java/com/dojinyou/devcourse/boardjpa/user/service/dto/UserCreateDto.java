5a153dbd3e20797c2524f78df9c3762c0c5c907b
dojinyou <djyou128@gmail.com>
2022-05-22 22:06:38 +0900

    :sparkles: User 생성용 dto 구현

---
+++ board-jpa/src/main/java/com/dojinyou/devcourse/boardjpa/user/service/dto/UserCreateDto.java
@@ -0,0 +1,53 @@
+package com.dojinyou.devcourse.boardjpa.user.service.dto;
+
+public class UserCreateDto {
+    private String name;
+    private int age;
+    private String hobby;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    private UserCreateDto(Builder builder) {
+        this.name = builder.name;
+        this.age = builder.age;
+        this.hobby = builder.hobby;
+    }
+
+    public static class Builder {
+        private String name;
+        private int age;
+        private String hobby;
+
+        public Builder() {
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder hobby(String hobby) {
+            this.hobby = hobby;
+            return this;
+        }
+
+        public UserCreateDto build() {
+            return new UserCreateDto(this);
+        }
+    }
+}
