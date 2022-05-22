36d0704d652c1c4bb14e26199db5ce178b65bd31
ChoiYeonho0903 <903yh@naver.com>
2022-05-23 03:48:34 +0900

    JpaConfiguration 리펙토리

--- kdt-jpa-board/src/main/java/com/kdt/board/configuration/JpaConfiguration.java
+++ kdt-jpa-board/src/main/java/com/kdt/board/configuration/JpaConfiguration.java
@@ -5,7 +5,9 @@ import java.util.UUID;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @Configuration
 public class JpaConfiguration {
 
