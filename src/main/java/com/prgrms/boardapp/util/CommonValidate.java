a78bbaa78f1c63a14080f8dd1705cb27b20f0d7c
taesan <pawer02023@gmail.com>
2022-05-24 02:04:41 +0900

    chore: 변수이름 변경
    - 변수이름 변경

--- src/main/java/com/prgrms/boardapp/util/CommonValidate.java
+++ src/main/java/com/prgrms/boardapp/util/CommonValidate.java
@@ -5,10 +5,11 @@ import org.springframework.util.Assert;
 public class CommonValidate {
     private static final String NOT_NULL_ERR_MSG = "null 또는 공백은 허용하지 않습니다.";
 
-    private CommonValidate() {}
+    private CommonValidate() {
+    }
 
-    public static void validateNotNullString(String name) {
-        Assert.notNull(name, NOT_NULL_ERR_MSG);
-        Assert.isTrue(!name.trim().isEmpty(), NOT_NULL_ERR_MSG);
+    public static void validateNotNullString(String str) {
+        Assert.notNull(str, NOT_NULL_ERR_MSG);
+        Assert.isTrue(!str.trim().isEmpty(), NOT_NULL_ERR_MSG);
     }
 }
