577e9a4bfdec6002d66db8021b65f11de727f7bd
NownS <sksmsx147@gmail.com>
2022-05-24 12:59:55 +0900

    Post jpa repository 구현

--- src/main/java/com/programmers/board/repository/PostRepository.java
+++ src/main/java/com/programmers/board/repository/PostRepository.java
@@ -2,10 +2,12 @@ package com.programmers.board.repository;
 
 import com.programmers.board.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     Optional<Post> findById(Long id);
     List<Post> findAll();
