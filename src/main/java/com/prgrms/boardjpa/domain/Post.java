dbfb5d73bf0ff7d7ce45f56b27fb853c8df06b02
julie0005 <julie0005@ajou.ac.kr>
2022-05-25 03:51:18 +0900

    feat : 연관관계 맵핑

---
+++ src/main/java/com/prgrms/boardjpa/domain/Post.java
@@ -0,0 +1,38 @@
+package com.prgrms.boardjpa.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "post")
+public class Post extends BaseEntity{
+    @Id
+    @Column(name = "post_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private User author;
+
+    public void setAuthor(User user){
+        if(Objects.nonNull(this.author)){
+            this.author.getPosts().remove(this);
+        }
+        this.author = user;
+        user.getPosts().add(this);
+    }
+
+}
---
+++ src/main/java/com/prgrms/boardjpa/domain/User.java
@@ -0,0 +1,43 @@
+package com.prgrms.boardjpa.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User extends BaseEntity{
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    private int age = 0;
+
+    @Column(length = 30)
+    private String hobby;
+
+    public User(){}
+
+    public User(String name, int age, String hobby){
+        this.name = name;
+        this.age = age;
+        this.hobby = hobby;
+    }
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts = new ArrayList<>();
+
+    public void addPost(Post post){
+        post.setAuthor(this);
+    }
+}
