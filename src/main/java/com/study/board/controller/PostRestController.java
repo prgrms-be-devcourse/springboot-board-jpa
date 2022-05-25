c830fb598fc52a93d0c26371d118a83cd57f224b
ndy2 <emrdbs12@gmail.com>
2022-05-26 00:17:08 +0900

    refactor: controllor 패키지 간소화

--- src/main/java/com/study/board/controller/PostRestController.java
+++ src/main/java/com/study/board/controller/PostRestController.java
@@ -1,4 +1,4 @@
-package com.study.board.controller.post;
+package com.study.board.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
