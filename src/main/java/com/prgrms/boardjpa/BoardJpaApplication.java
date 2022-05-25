dab46fae1b8690cf23b8c768f55eec8386d96b06
ynoolee <heerarea129@gmail.com>
2022-05-25 14:38:11 +0900

    refactor: JpaConfig 분리
    - MockMvc 테스트과정에서 발생하는 JPA metamodel must not be empty 에러 해결

--- src/main/java/com/prgrms/boardjpa/BoardJpaApplication.java
+++ src/main/java/com/prgrms/boardjpa/BoardJpaApplication.java
@@ -2,10 +2,8 @@ package com.prgrms.boardjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class BoardJpaApplication {
 
 	public static void main(String[] args) {
