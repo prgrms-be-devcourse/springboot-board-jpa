c619d2de12df40efd75646abe3597627532f3be5
NownS <sksmsx147@gmail.com>
2022-05-25 23:08:28 +0900

    Convertor 파일로 통합

--- src/main/java/com/programmers/board/dto/Convertor.java
+++ src/main/java/com/programmers/board/dto/Convertor.java
@@ -1,10 +1,11 @@
 package com.programmers.board.dto;
 
 import com.programmers.board.entity.Post;
+import com.programmers.board.entity.User;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-public class PostConvertor {
+public class Convertor {
     public static Post postRequestConvertor(PostRequestDto postRequestDto) {
         if (postRequestDto.getTitle() == null) {
             throw new IllegalArgumentException("제목이 없습니다.");
@@ -20,5 +21,17 @@ public class PostConvertor {
 
     public static PostResponseDto postResponseConvertor(Post post) {
         return new PostResponseDto(post.getTitle(), post.getContent(), post.getCreatedAt().toString(), post.getCreatedBy());
+    }
+    public static User userRequestConvertor(UserRequestDto userRequestDto) {
+        if (userRequestDto.getName() == null) {
+            throw new IllegalArgumentException("이름이 없습니다.");
+        }
+        if (userRequestDto.getAge() == null) {
+            throw new IllegalArgumentException("나이가 없습니다.");
+        }
+        if (userRequestDto.getHobby() == null) {
+            throw new IllegalArgumentException("취미가 없습니다.");
+        }
+        return new User(userRequestDto.getName(), userRequestDto.getAge(), userRequestDto.getHobby());
     }
 }
