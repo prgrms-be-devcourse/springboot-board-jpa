192f9b0ee1bf04636a505a5525a3d064f058ebe5
yuminhwan <ymh7532@icloud.com>
2022-05-26 00:54:36 +0900

    refactor(CreateUserRequest): int -> Integer 타입 변경
    - null 표현을 위해 변경

--- src/main/java/com/prgrms/springboard/user/dto/CreateUserRequest.java
+++ src/main/java/com/prgrms/springboard/user/dto/CreateUserRequest.java
@@ -1,6 +1,7 @@
 package com.prgrms.springboard.user.dto;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 
 import org.hibernate.validator.constraints.Length;
@@ -16,14 +17,15 @@ public class CreateUserRequest {
     @Length(max = 10, message = "이름은 {max}글자 이하여야합니다.")
     private final String name;
 
+    @NotNull(message = "나이는 비어있을 수 없습니다.")
     @PositiveOrZero(message = "나이는 양수 또는 0이여야 합니다.")
-    private final int age;
+    private final Integer age;
 
     @NotBlank(message = "취미는 비거나 공백만 있을 수 없습니다.")
     @Length(max = 15, message = "취미는 {max}글자 이하여야합니다.")
     private final String hobby;
 
-    public CreateUserRequest(String name, int age, String hobby) {
+    public CreateUserRequest(String name, Integer age, String hobby) {
         this.name = name;
         this.age = age;
         this.hobby = hobby;
