bef33703f49ac4d7fae67aaaf25a0dbb9254c2d9
NownS <sksmsx147@gmail.com>
2022-05-25 23:08:17 +0900

    PostService Dto 형식으로 변경

--- src/main/java/com/programmers/board/service/PostService.java
+++ src/main/java/com/programmers/board/service/PostService.java
@@ -1,33 +1,46 @@
 package com.programmers.board.service;
 
-import com.programmers.board.dto.PostDto;
+import com.programmers.board.dto.Convertor;
+import com.programmers.board.dto.PostRequestDto;
+import com.programmers.board.dto.PostResponseDto;
 import com.programmers.board.entity.Post;
+import com.programmers.board.entity.User;
 import com.programmers.board.exception.NotFoundException;
 import com.programmers.board.repository.PostRepository;
+import com.programmers.board.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class PostService {
-    PostRepository postRepository;
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
     @Transactional
-    public Long save(Post post){
+    public Long save(PostRequestDto postRequestDto){
+        Post post = Convertor.postRequestConvertor(postRequestDto);
+        User user = userRepository.findById(postRequestDto.getUserId())
+                .orElseThrow(() -> new NotFoundException("해당 유저가 존재하지 않습니다."));
+        post.setUser(user);
         Post entity = postRepository.save(post);
         return entity.getId();
     }
 
     @Transactional
-    public Page<Post> findAll(Pageable pageable) {
-        return postRepository.findAll(pageable);
+    public Page<PostResponseDto> findAll(Pageable pageable) {
+        return postRepository.findAll(pageable)
+                .map(Convertor::postResponseConvertor);
     }
 
     @Transactional
-    public Post find(Long id) throws NotFoundException{
-        return postRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("데이터가 존재하지 않습니다"));
+    public PostResponseDto find(Long id) throws NotFoundException{
+        Post post = postRepository.findById(id)
+            .orElseThrow(() -> new NotFoundException("데이터가 존재하지 않습니다"));
+        return Convertor.postResponseConvertor(post);
     }
 
 }
