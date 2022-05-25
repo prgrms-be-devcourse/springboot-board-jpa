0a81e552a767b0f956b6d230dd9d9b6f73f0b920
ndy2 <emrdbs12@gmail.com>
2022-05-26 01:30:56 +0900

    feat: 게시글 단건 조회

--- src/main/java/com/study/board/controller/PostRestController.java
+++ src/main/java/com/study/board/controller/PostRestController.java
@@ -5,6 +5,7 @@ import com.study.board.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,12 @@ public class PostRestController {
     private final PostService postService;
 
     @GetMapping
-    public List<PostResponse> posts(Pageable pageable){
+    public List<PostResponse> findAll(Pageable pageable){
         return postService.findAll(pageable).stream().map(PostResponse::convert).collect(Collectors.toList());
+    }
+
+    @GetMapping("/{postId}")
+    public PostResponse findById(@PathVariable Long postId){
+        return PostResponse.convert(postService.findById(postId));
     }
 }
