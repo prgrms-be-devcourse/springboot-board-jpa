e6e379c5add70be30453dddd06011d7e644e2e70
NownS <sksmsx147@gmail.com>
2022-05-25 19:54:26 +0900

    user 엔티티 primitive로 변경

--- src/main/java/com/programmers/board/entity/User.java
+++ src/main/java/com/programmers/board/entity/User.java
@@ -1,9 +1,11 @@
 package com.programmers.board.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +13,7 @@ import java.util.List;
 @Getter
 @Setter
 @Table(name = "users")
+@NoArgsConstructor
 public class User extends CreatedInfo{
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -18,12 +21,20 @@ public class User extends CreatedInfo{
     @Column(nullable = false, unique = true)
     private String name;
     @Column(nullable = false)
-    private Long age;
+    private int age;
     @Column
     private String hobby;
 
     @OneToMany(mappedBy = "user")
     private List<Post> posts = new ArrayList<>();
+
+    public User(String name, int age, String hobby) {
+        this.name = name;
+        this.age = age;
+        this.hobby = hobby;
+        this.setCreatedBy(name);
+        this.setCreatedAt(LocalDateTime.now());
+    }
 
     public void addPost(Post post) {
         post.setUser(this);
