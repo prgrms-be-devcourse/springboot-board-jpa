ed2bb4e6c08aa32016caea9c366f74fd45c446e1
julie0005 <julie0005@ajou.ac.kr>
2022-05-25 19:54:19 +0900

    feat : PostController 구현

---
+++ src/main/java/com/prgrms/boardjpa/post/controller/PostController.java
@@ -0,0 +1,44 @@
+package com.prgrms.boardjpa.post.controller;
+
+import com.prgrms.boardjpa.Response;
+import com.prgrms.boardjpa.post.dto.PostReqDto;
+import com.prgrms.boardjpa.post.dto.PostResDto;
+import com.prgrms.boardjpa.post.dto.PostUpdateDto;
+import com.prgrms.boardjpa.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor // 생성자 주입
+public class PostController {
+
+    private final PostService postService;
+
+    @ExceptionHandler
+    private Response<String> handleException(Exception exception) {
+        return Response.fail(HttpStatus.INTERNAL_SERVER_ERROR.value(), exception.getMessage());
+    }
+
+    @GetMapping("/posts/{postId}")
+    public Response<PostResDto> getOne(@PathVariable Long postId){
+        return Response.ok(postService.findOne(postId));
+    }
+
+    @GetMapping("/posts")
+    public Response<Page<PostResDto>> getAll (Pageable pageable){
+        return Response.ok(postService.findPosts(pageable));
+    }
+
+    @PostMapping("/posts")
+    public Response<Long> save (@RequestBody PostReqDto postReqDto){
+        return Response.ok(postService.save(postReqDto));
+    }
+
+    @PutMapping("/posts")
+    public Response<Long> update(@RequestBody PostUpdateDto postUpdateDto){
+        return Response.ok(postService.update(postUpdateDto));
+    }
+}
