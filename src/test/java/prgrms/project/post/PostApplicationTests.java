c4d64a45eed57860f4377c670119adc23f45977a
Gosh95 <goshk1995@gmail.com>
2022-05-26 22:08:46 +0300

    refactor: 불필요한 테스트 메서드 제거

--- src/test/java/prgrms/project/post/PostApplicationTests.java
+++ src/test/java/prgrms/project/post/PostApplicationTests.java
@@ -1,13 +1,8 @@
 package prgrms.project.post;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PostApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
