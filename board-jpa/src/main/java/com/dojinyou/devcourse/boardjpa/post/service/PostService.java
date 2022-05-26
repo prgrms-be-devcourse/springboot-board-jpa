bd40a6f92cbd119a833822b82c066e39610ce151
dojinyou <djyou128@gmail.com>
2022-05-26 11:17:56 +0900

    :sparkles: feat(PostService): findById 구현

--- board-jpa/src/main/java/com/dojinyou/devcourse/boardjpa/post/service/PostService.java
+++ board-jpa/src/main/java/com/dojinyou/devcourse/boardjpa/post/service/PostService.java
@@ -1,7 +1,10 @@
 package com.dojinyou.devcourse.boardjpa.post.service;
 
 import com.dojinyou.devcourse.boardjpa.post.service.dto.PostCreateDto;
+import com.dojinyou.devcourse.boardjpa.post.service.dto.PostResponseDto;
 
 public interface PostService {
     void create(long userId, PostCreateDto postCreateDto);
+
+    PostResponseDto findById(long id);
 }
