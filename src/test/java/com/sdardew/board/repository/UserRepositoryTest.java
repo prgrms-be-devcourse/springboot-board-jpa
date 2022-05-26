5c42d25fc7bc63c37da25f6e40bd0fa3e9db6d5e
sdardew <sdardew@gmail.com>
2022-05-26 19:27:37 +0900

    필요없는 어노테이션 제거

--- src/test/java/com/sdardew/board/repository/UserRepositoryTest.java
+++ src/test/java/com/sdardew/board/repository/UserRepositoryTest.java
@@ -5,17 +5,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(SpringExtension.class)
 @DataJpaTest
 class UserRepositoryTest {
 
