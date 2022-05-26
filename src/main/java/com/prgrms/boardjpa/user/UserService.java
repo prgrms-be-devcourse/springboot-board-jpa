cf495f04eb2eac405a53c82dbecbe109f04c3ddd
ynoolee <heerarea129@gmail.com>
2022-05-26 14:31:52 +0900

    feat: 유저서비스 클래스에 readOnly transaction 어노테이트 및 메소드 단위 트랜잭션 오버라이드 추가

--- src/main/java/com/prgrms/boardjpa/user/UserService.java
+++ src/main/java/com/prgrms/boardjpa/user/UserService.java
@@ -1,11 +1,13 @@
 package com.prgrms.boardjpa.user;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.prgrms.boardjpa.post.NotExistException;
 import com.prgrms.boardjpa.user.dto.UserDto;
 
 @Service
+@Transactional(readOnly = true)
 public class UserService {
 	private final UserRepository userRepository;
 
@@ -13,6 +15,7 @@ public class UserService {
 		this.userRepository = userRepository;
 	}
 
+	@Transactional
 	public UserDto.Info create(UserDto.CreateRequest request) {
 		User newUser = userRepository.save(
 			request.createUser()
