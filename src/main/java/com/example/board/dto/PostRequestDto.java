6e512e18decaa3b539fb2c787a667fddab686728
Yoonoh <xkakak142@naver.com>
2022-05-29 16:27:02 +0900

    feat: 사용자 입력 검증 추가

--- src/main/java/com/example/board/dto/PostRequestDto.java
+++ src/main/java/com/example/board/dto/PostRequestDto.java
@@ -1,6 +1,11 @@
 package com.example.board.dto;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+
 public record PostRequestDto (
+        @NotNull(message = "Title cannot be null")
+        @Max(value = 30, message = "Title should not be greater than 30")
         String title,
         String content,
         UserResponseDto author
--- src/main/java/com/example/board/dto/UserRequestDto.java
+++ src/main/java/com/example/board/dto/UserRequestDto.java
@@ -1,8 +1,15 @@
 package com.example.board.dto;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+
 public record UserRequestDto (
+        @NotNull(message = "Name cannot be null")
+        @Max(value = 30, message = "Name should not be greater than 30")
         String name,
+        @NotNull(message = "Age cannot be null")
         int age,
+        @Max(value = 30, message = "Hobby should not be greater than 30")
         String hobby
 ){
 
