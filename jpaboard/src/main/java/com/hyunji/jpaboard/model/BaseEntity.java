9ad55499d6a62160f6147ae9889b92744118dc54
Hyunji <rioreo2022@gmail.com>
2022-06-01 01:58:50 +0900

    feat: createdAt 포맷 변경

--- jpaboard/src/main/java/com/hyunji/jpaboard/model/BaseEntity.java
+++ jpaboard/src/main/java/com/hyunji/jpaboard/model/BaseEntity.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @Getter
 @MappedSuperclass
@@ -18,4 +19,9 @@ public abstract class BaseEntity {
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+    }
 }
