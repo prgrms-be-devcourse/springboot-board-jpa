ea076c4e0c5a6947192f043fa2f92102916d1329
Hyunji <rioreo2022@gmail.com>
2022-06-01 13:12:48 +0900

    test: DefaultUserServiceTest 구현

---
+++ jpaboard/src/test/java/com/hyunji/jpaboard/domain/user/service/DefaultUserServiceTest.java
@@ -0,0 +1,26 @@
+package com.hyunji.jpaboard.domain.user.service;
+
+import com.hyunji.jpaboard.domain.user.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class DefaultUserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void user_등록() {
+        User user = new User("user01", 30, "hobby");
+
+        userService.register(user);
+
+        assertThat(user.getId()).isNotNull();
+    }
+}
