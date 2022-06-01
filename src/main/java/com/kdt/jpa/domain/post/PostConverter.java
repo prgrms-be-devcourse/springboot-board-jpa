569178523fb9044dc2b267328113e330efe79fa8
pjh <pjh_jn@naver.com>
2022-06-01 14:42:42 +0900

    feat : DTO <-> Entity 컨버터 추가

---
+++ src/main/java/com/kdt/jpa/domain/post/PostConverter.java
@@ -0,0 +1,53 @@
+package com.kdt.jpa.domain.post;
+
+import org.springframework.stereotype.Component;
+
+import com.kdt.jpa.domain.member.MemberConverter;
+import com.kdt.jpa.domain.member.dto.MemberResponse;
+import com.kdt.jpa.domain.member.model.Member;
+import com.kdt.jpa.domain.post.dto.PostRequest;
+import com.kdt.jpa.domain.post.dto.PostResponse;
+import com.kdt.jpa.domain.post.model.Post;
+
+@Component
+public class PostConverter {
+
+	private final MemberConverter memberConverter;
+
+	public PostConverter(MemberConverter memberConverter) {
+		this.memberConverter = memberConverter;
+	}
+
+	public Post toEntity(PostRequest.UpdatePostRequest updatePostRequest) {
+		return new Post(updatePostRequest.title(), updatePostRequest.content());
+	}
+
+	public Post toEntity(PostRequest.WritePostRequest writePostRequest, MemberResponse memberResponse) {
+		Member member = this.memberConverter.toEntity(memberResponse);
+		return Post.builder()
+			.title(writePostRequest.title())
+			.content(writePostRequest.content())
+			.author(member)
+			.build();
+	}
+
+	public PostResponse toPostResponse(Post post) {
+		return new PostResponse
+			(
+				post.getId(),
+				post.getTitle(),
+				post.getContent(),
+				this.memberConverter.toMemberResponse(post.getAuthor()),
+				post.getCreatedAt()
+			);
+	}
+
+	public PostResponse.UpdatePostResponse toUpdatePostResponse(Post post) {
+		return new PostResponse.UpdatePostResponse(post.getTitle(), post.getContent());
+	}
+
+	public PostResponse.WritePostResponse toWritePostResponse(Post post) {
+		return new PostResponse.WritePostResponse(post.getId());
+	}
+
+}
