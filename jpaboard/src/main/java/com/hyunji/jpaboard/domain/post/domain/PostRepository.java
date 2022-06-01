39f52092676e9cf432273ac78923fc9f2afd9bc8
Hyunji <rioreo2022@gmail.com>
2022-06-01 12:57:53 +0900

    feat: PostRepository, PostRepositoryTest 구현

--- jpaboard/src/main/java/com/hyunji/jpaboard/domain/post/domain/PostRepository.java
+++ jpaboard/src/main/java/com/hyunji/jpaboard/domain/post/domain/PostRepository.java
@@ -1,6 +1,21 @@
 package com.hyunji.jpaboard.domain.post.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @EntityGraph(attributePaths = "user")
+    @Query(value = "select p from Post p", countQuery = "select count(p.id) from Post p")
+    Page<Post> findPageWithUser(Pageable pageable);
+
+    @EntityGraph(attributePaths = "user")
+    @Query("select p from Post p where p.id = :id")
+    Optional<Post> findPostByIdWithUser(@Param("id") Long id);
 }
--- jpaboard/src/test/java/com/hyunji/jpaboard/domain/post/domain/PostRepositoryTest.java
+++ jpaboard/src/test/java/com/hyunji/jpaboard/domain/post/domain/PostRepositoryTest.java
@@ -5,14 +5,18 @@ import com.hyunji.jpaboard.domain.user.domain.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
+@Transactional
 @SpringBootTest
 class PostRepositoryTest {
 
@@ -22,21 +26,65 @@ class PostRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    private void comparePost(Post actual, Post expected) {
+        assertThat(actual.getId()).isEqualTo(expected.getId());
+        assertThat(actual.getUser().getId()).isEqualTo(expected.getUser().getId());
+        assertThat(actual.getTitle()).isEqualTo(expected.getTitle());
+        assertThat(actual.getContent()).isEqualTo(expected.getContent());
+        assertThat(actual.getCreatedAt()).isEqualTo(expected.getCreatedAt());
+    }
+
+    private void compareUser(User actual, User expected) {
+        assertThat(actual.getId()).isEqualTo(expected.getId());
+        assertThat(actual.getName()).isEqualTo(expected.getName());
+        assertThat(actual.getAge()).isEqualTo(expected.getAge());
+        assertThat(actual.getHobby()).isEqualTo(expected.getHobby());
+        assertThat(actual.getCreatedAt()).isEqualTo(expected.getCreatedAt());
+    }
+
     @Test
     void 상태저장_테스트() {
         User user = new User("user01", 10, "hobby");
         userRepository.save(user);
         Post post = new Post("title01", "content...", user);
-
         postRepository.save(post);
 
-        Optional<Post> byId = postRepository.findById(post.getId());
-        assertThat(byId).isNotEmpty();
-        Post saved = byId.get();
-        assertThat(saved.getId()).isEqualTo(post.getId());
-        assertThat(saved.getUser().getId()).isEqualTo(user.getId());
-        assertThat(saved.getTitle()).isEqualTo(post.getTitle());
-        assertThat(saved.getContent()).isEqualTo(post.getContent());
-        assertThat(saved.getCreatedAt()).isBeforeOrEqualTo(LocalDateTime.now());
+        Optional<Post> maybePost = postRepository.findById(post.getId());
+
+        assertThat(maybePost).isPresent();
+        comparePost(maybePost.get(), post);
+    }
+
+    @Test
+    void findPostByIdWithUser() {
+        User user = new User("user01", 10, "hobby");
+        userRepository.save(user);
+        Post post = new Post("title01", "content...", user);
+        postRepository.save(post);
+
+        Optional<Post> maybePost = postRepository.findPostByIdWithUser(post.getId());
+
+        assertThat(maybePost).isPresent();
+        comparePost(maybePost.get(), post);
+        compareUser(maybePost.get().getUser(), user);
+    }
+
+    @Test
+    void 페이징_조회() {
+        IntStream.range(1, 100).forEach(i ->
+                {
+                    User user = new User("user" + i, i, "hobby");
+                    userRepository.save(user);
+                    postRepository.save(new Post("title" + i, "content..." + i, user));
+                }
+        );
+        PageRequest pageRequest = PageRequest.of(0, 10);
+
+        Page<Post> postPage = postRepository.findPageWithUser(pageRequest);
+
+        assertThat(postPage).hasSize(10);
+        assertThat(postPage.getTotalElements()).isEqualTo(99);
+        assertThat(postPage.getTotalPages()).isEqualTo(10);
+        assertThat(postPage.getNumber()).isEqualTo(0);
     }
 }
