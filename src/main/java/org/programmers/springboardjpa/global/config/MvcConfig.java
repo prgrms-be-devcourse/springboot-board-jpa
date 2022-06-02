efb5e6c0613081043cd2720652868d3e3d99e88e
ICCHOI <cicblck@gmail.com>
2022-06-02 17:37:55 +0900

    Config:애플리케이션 설정을 구현하라

---
+++ src/main/java/org/programmers/springboardjpa/global/config/MvcConfig.java
@@ -0,0 +1,15 @@
+package org.programmers.springboardjpa.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**").allowedOrigins("*");
+    }
+}
+
