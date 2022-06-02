48892410649aa4578275afc8adc16bb0df9af495
ICCHOI <cicblck@gmail.com>
2022-06-02 17:34:45 +0900

    test:테스트 코드를 작성하라

---
+++ src/test/java/org/programmers/springboardjpa/post/service/PostDefaultServiceStubTest.java
@@ -0,0 +1,100 @@
+package org.programmers.springboardjpa.post.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.programmers.springboardjpa.domain.post.dto.PostRequest;
+import org.programmers.springboardjpa.domain.post.dto.PostResponse;
+import org.programmers.springboardjpa.domain.post.repository.PostRepository;
+import org.programmers.springboardjpa.domain.post.service.PostConverter;
+import org.programmers.springboardjpa.domain.post.service.PostService;
+import org.programmers.springboardjpa.domain.user.dto.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class PostDefaultServiceStubTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    PostConverter postConverter;
+
+    @Autowired
+    PostService postService;
+
+    PostResponse.PostResponseDto savedPost;
+
+    PostRequest.PostCreateRequestDto postCreateRequest;
+
+    @BeforeEach
+    void setUp() {
+        postCreateRequest = new PostRequest.PostCreateRequestDto(
+                "아이유는", "어느집 아이유", new UserDto.UserRequest(
+                "이상혁", 15,  "LOL"));
+        savedPost = postService.savePost(postCreateRequest);
+    }
+
+    @AfterEach
+    void tearDown() {
+        postRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("id값이 자동 생성 전략에 따라 할당 되어 null이 아님을 확인 할 수 있다.")
+    void savePost() {
+        //then
+        assertThat(savedPost.id()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("저장된 게시글의 아이디 값으로 찾아온 게시글의 아이디 값을 비교하여 동일한 게시글을 가져 올 수 있다.")
+    void findPost() {
+        //given
+        var foundPost = postService.getPost(savedPost.id());
+
+        //then
+        assertThat(foundPost.id()).isEqualTo(savedPost.id());
+    }
+
+    @Test
+    @DisplayName("")
+    void getAllPost() {
+        //given
+        var post = postService.getPost(savedPost.id());
+        List<PostResponse.PostResponseDto> postResponseList = new ArrayList<>();
+        postResponseList.add(post);
+
+        //when
+        var postList = postService.getPostList(
+                Pageable.ofSize(1).withPage(1));
+
+        //then
+        assertThat(postList).hasSize(1)
+                .usingRecursiveFieldByFieldElementComparator().isEqualTo(postResponseList);
+    }
+
+    @Test
+    @DisplayName("동일한 id를 이용하여 update된 title과 content를 확인하여 update를 검증할 수 있다.")
+    void updatePost() {
+        //given
+        var requestDto = new PostRequest.PostUpdateRequestDto("아이유가", "뭐하는 아이유?");
+
+        //when
+        postService.updatePost(savedPost.id(), requestDto);
+        var updatedPost = postService.getPost(savedPost.id());
+
+        //then
+        assertThat(updatedPost.id()).isEqualTo(savedPost.id());
+        assertThat(updatedPost.title()).isEqualTo(requestDto.title());
+        assertThat(updatedPost.content()).isEqualTo(requestDto.content());
+    }
+}
---
+++ src/test/java/org/programmers/springboardjpa/post/service/PostDefaultServiceTest.java
@@ -0,0 +1,127 @@
+package org.programmers.springboardjpa.post.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.programmers.springboardjpa.domain.post.dto.PostRequest;
+import org.programmers.springboardjpa.domain.post.dto.PostRequest.PostCreateRequestDto;
+import org.programmers.springboardjpa.domain.post.dto.PostResponse.PostResponseDto;
+import org.programmers.springboardjpa.domain.post.domain.Post;
+import org.programmers.springboardjpa.domain.post.repository.PostRepository;
+import org.programmers.springboardjpa.domain.post.service.PostConverter;
+import org.programmers.springboardjpa.domain.post.service.PostDefaultService;
+import org.programmers.springboardjpa.domain.user.domain.User;
+import org.programmers.springboardjpa.domain.user.dto.UserDto;
+import org.programmers.springboardjpa.domain.user.dto.UserDto.UserResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+
+@Transactional(readOnly = true)
+@ExtendWith(MockitoExtension.class)
+class PostDefaultServiceTest {
+
+    @InjectMocks
+    private PostDefaultService postDefaultService;
+
+    @Mock
+    private PostRepository postRepository;
+
+    @Mock
+    private PostConverter postConverter;
+
+    private Post entity;
+    private PostResponseDto response;
+
+    @BeforeEach
+    void setUp() {
+        entity = Post.builder()
+                .id(1L)
+                .title("가입인사")
+                .content("냉무")
+                .user(User.builder()
+                        .id(1L)
+                        .name("익명")
+                        .age(14)
+                        .hobby("LOL")
+                        .build())
+                .build();
+        response = new PostResponseDto(1L, "안녕하세요", "반가워요",
+                new UserResponse(1L, "익명", 14, "LOL"));
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("save 행위 검증 테스트")
+    void savePost() {
+        Post post = Post.builder()
+                .title("안녕하세요")
+                .content("반가워요")
+                .user(User.builder()
+                        .name("익명")
+                        .age(14)
+                        .hobby("LOL")
+                        .build())
+                .build();
+        PostCreateRequestDto createRequest = new PostCreateRequestDto("안녕하세요", "반가워요",
+                new UserDto.UserRequest("익명", 14, "LOL"));
+        given(postConverter.convertPost(createRequest)).willReturn(post);
+        given(postRepository.save(post)).willReturn(entity);
+
+        postDefaultService.savePost(createRequest);
+
+        then(postRepository).should(times(1)).save(post);
+        then(postConverter).should(times(1)).convertPost(createRequest);
+    }
+
+    @Test
+    @DisplayName("findById 행위 검증 테스트")
+    void findById() {
+        given(postRepository.findById(1L)).willReturn(Optional.ofNullable(entity));
+        given(postConverter.convertPostDto(entity)).willReturn(response);
+
+        postDefaultService.getPost(1L);
+
+        then(postRepository).should(times(1)).findById(1L);
+        then(postConverter).should(times(1)).convertPostDto(entity);
+    }
+
+    @Test
+    @DisplayName("findAll 행위 검증 테스트")
+    void findAll() {
+        var pageable = Pageable.ofSize(1).withPage(1);
+        List<Post> postList = List.of(entity);
+        Page<Post> postPage = new PageImpl<>(postList);
+        given(postRepository.findAll(pageable)).willReturn(postPage);
+
+        postRepository.findAll(pageable);
+
+        then(postRepository).should(times(1)).findAll(pageable);
+    }
+
+    @Test
+    @DisplayName("update 행위 검증 테스트")
+    void update() {
+        var updateRequest = new PostRequest.PostUpdateRequestDto("안녕하세요", "반가워요");
+        given(postRepository.findById(1L)).willReturn(Optional.ofNullable(entity));
+        given(postConverter.convertPostDto(any(Post.class))).willReturn(response);
+
+        postDefaultService.updatePost(1L, updateRequest);
+
+        then(postRepository).should(times(1)).findById(1L);
+        then(postConverter).should(times(1)).convertPostDto(entity);
+    }
+}
