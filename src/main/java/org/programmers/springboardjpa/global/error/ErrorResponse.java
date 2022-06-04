ebef77e6d84eff4f3050e7e59f07a17990818caa
ICCHOI <cicblck@gmail.com>
2022-06-05 01:03:49 +0900

    refactor:생성자 오버로딩을 적용하라

--- src/main/java/org/programmers/springboardjpa/global/error/ErrorResponse.java
+++ src/main/java/org/programmers/springboardjpa/global/error/ErrorResponse.java
@@ -19,21 +19,15 @@ public class ErrorResponse {
     private List<FieldError> errors;
     private String code;
 
-
+    private ErrorResponse(final ErrorCode code) {
+        this(code, new ArrayList<>());
+    }
     private ErrorResponse(final ErrorCode code, final List<FieldError> errors) {
         this.message = code.getMessage();
         this.status = code.getStatus();
         this.errors = errors;
         this.code = code.getCode();
     }
-
-    private ErrorResponse(final ErrorCode code) {
-        this.message = code.getMessage();
-        this.status = code.getStatus();
-        this.code = code.getCode();
-        this.errors = new ArrayList<>();
-    }
-
 
     public static ErrorResponse of(ErrorCode code, BindingResult bindingResult) {
         return new ErrorResponse(code, FieldError.of(bindingResult));
