9b37fae23055d501881eac826304694d71ecee4c
HyoungUkJJang <guddnr1451@gmail.com>
2022-06-11 03:00:30 +0900

    refactor : Post 엔티티 id 래퍼타입 변경, 검증 메서드 네이밍 수정

--- src/main/java/com/prgrms/work/post/domain/Post.java
+++ src/main/java/com/prgrms/work/post/domain/Post.java
@@ -1,7 +1,9 @@
 package com.prgrms.work.post.domain;
 
 import com.prgrms.work.common.BaseEntity;
+import com.prgrms.work.error.EntityInvalidException;
 import com.prgrms.work.user.domain.User;
+import java.text.MessageFormat;
 import java.time.LocalDateTime;
 import java.util.Objects;
 import javax.persistence.CascadeType;
@@ -21,7 +23,7 @@ public class Post extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    long id;
+    private Long id;
 
     @Column(length = 30, nullable = false)
     private String title;
@@ -35,7 +37,7 @@ public class Post extends BaseEntity {
 
     protected Post() {}
 
-    public Post(String createdBy, String title, String content, User user) {
+    protected Post(String createdBy, String title, String content, User user) {
         super(createdBy, LocalDateTime.now());
         this.title = title;
         this.content = content;
@@ -43,12 +45,12 @@ public class Post extends BaseEntity {
     }
 
     public static Post create(String createdBy, String title, String content, User user) {
-        verifyValid(title, content);
+        verifyTitleAndContentValid(title, content);
 
         return new Post(createdBy, title, content, user);
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
@@ -65,23 +67,27 @@ public class Post extends BaseEntity {
     }
 
     public void modify(String title, String content) {
-        verifyValid(title, content);
+        verifyTitleAndContentValid(title, content);
 
         this.title = title;
         this.content = content;
     }
 
-    private static void verifyValid(String title, String content) {
+    private static void verifyTitleAndContentValid(String title, String content) {
         if (Objects.isNull(title) || Objects.isNull(content)) {
-            throw new IllegalArgumentException("제목과 내용 작성은 필수입니다.");
+            throw new EntityInvalidException("제목과 내용 작성은 필수입니다.");
         }
 
         if (title.length() < 2 || title.length() > 30) {
-            throw new IllegalArgumentException("제목의 길이는 3~30글자만 입력이 가능합니다. 입력하신 글자길이 : " + title.length());
+            throw new EntityInvalidException(
+                MessageFormat.format("제목의 길이는 3~30글자만 입력이 가능합니다. [입력하신 글자길이 : {0}]", title.length())
+            );
         }
 
         if (content.length() < 10 || content.length() > 1000) {
-            throw new IllegalArgumentException("내용 길이는 11~1000글자만 입력이 가능합니다. 입력하신 글자길이 : " + content.length());
+            throw new EntityInvalidException(
+                MessageFormat.format("내용 길이는 11~1000글자만 입력이 가능합니다. [입력하신 글자길이 : {0}]", content.length())
+            );
         }
     }
 
