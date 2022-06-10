df0259795d38657e02fc6162e008f4603ff40d19
dldydgns530@gmail.com <dldydgns530@gmail.com>
2022-06-10 19:08:39 +0900

    gitignore 다시 적용

--- src/main/java/com/kdt/board/global/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.kdt.board.global.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
