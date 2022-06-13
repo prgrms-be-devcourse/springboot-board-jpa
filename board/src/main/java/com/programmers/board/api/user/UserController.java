3e18ecb1052598792af19d7d2925cc9f5f65b876
jki503 <jki503@naver.com>
2022-06-13 17:50:35 +0900

    feat: UserController와 UserService 추가
    - 기능은 Post를 위해 save만 추가함

--- board/src/main/java/com/programmers/board/api/user/UserController.java
+++ board/src/main/java/com/programmers/board/api/user/UserController.java
@@ -1,12 +1,34 @@
 package com.programmers.board.api.user;
 
 import com.programmers.board.core.user.application.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import com.programmers.board.core.user.application.dto.UserCreateRequest;
+import com.programmers.board.core.user.application.dto.UserResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
-@RequiredArgsConstructor
+@RestControllerAdvice
+@RequestMapping("/api/v1/users")
 public class UserController {
 
     private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/join")
+    public ResponseEntity<UserResponse> join(@RequestBody UserCreateRequest userCreateRequest){
+        UserResponse joinedUser = userService.join(userCreateRequest);
+        URI location = URI.create("/api/v1/users/" + joinedUser.getId());
+        return ResponseEntity.created(location).body(joinedUser);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUser(@PathVariable Long id){
+        return ResponseEntity.ok(userService.getUser(id));
+    }
 }
--- board/src/main/java/com/programmers/board/core/user/application/UserService.java
+++ board/src/main/java/com/programmers/board/core/user/application/UserService.java
@@ -1,7 +1,34 @@
 package com.programmers.board.core.user.application;
 
+import com.programmers.board.common.exception.NotFoundException;
+import com.programmers.board.core.user.application.dto.UserCreateRequest;
+import com.programmers.board.core.user.application.dto.UserResponse;
+import com.programmers.board.core.user.domain.User;
+import com.programmers.board.core.user.domain.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public UserResponse join(UserCreateRequest userCreateRequest){
+        User user = userCreateRequest.toEntity();
+        User savedUser = userRepository.save(user);
+        return UserResponse.of(savedUser);
+    }
+
+    @Transactional(readOnly = true)
+    public UserResponse getUser(Long id){
+        return userRepository.findById(id)
+                .map(UserResponse::of)
+                .orElseThrow(NotFoundException::new);
+    }
+
 }
