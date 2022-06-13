741610db80d2607f01c152e096beccbe0b9bf602
jki503 <jki503@naver.com>
2022-06-13 17:53:25 +0900

    featPostService에서 user 검증 추가

--- board/src/main/java/com/programmers/board/common/exception/NotFoundException.java
+++ board/src/main/java/com/programmers/board/common/exception/NotFoundException.java
@@ -1,2 +1,13 @@
-package com.programmers.board.common.exception;public class NotFoundException {
+package com.programmers.board.common.exception;
+
+public class NotFoundException extends RuntimeException{
+
+    private String message;
+
+    public NotFoundException(){}
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
 }
--- board/src/main/java/com/programmers/board/core/post/application/PostService.java
+++ board/src/main/java/com/programmers/board/core/post/application/PostService.java
@@ -1,11 +1,14 @@
 package com.programmers.board.core.post.application;
 
-import com.programmers.board.core.post.application.dto.CreateRequestPost;
+import com.programmers.board.common.exception.NotFoundException;
+import com.programmers.board.core.post.application.dto.PostCreateRequest;
 import com.programmers.board.core.post.application.dto.ResponsePost;
-import com.programmers.board.core.post.application.dto.UpdateRequestPost;
+import com.programmers.board.core.post.application.dto.PostUpdateRequest;
 import com.programmers.board.core.post.domain.Post;
 import com.programmers.board.core.post.domain.repository.PostRepository;
-import lombok.RequiredArgsConstructor;
+import com.programmers.board.core.user.domain.User;
+import com.programmers.board.core.user.domain.repository.UserRepository;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -17,15 +20,19 @@ import javax.persistence.EntityNotFoundException;
 public class PostService {
 
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository, UserRepository userRepository) {
         this.postRepository = postRepository;
+        this.userRepository = userRepository;
     }
 
     @Transactional
-    public ResponsePost save(CreateRequestPost createRequestPost){
-        Post post = createRequestPost.toEntity();
-        Post savedPost = postRepository.save(post);
+    public ResponsePost save(PostCreateRequest postCreateRequest){
+        User user = userRepository.findById(postCreateRequest.getUserId())
+                .orElseThrow(NotFoundException::new);
+
+        Post post = postRepository.save(postCreateRequest.toEntity(user));
         return ResponsePost.of(post);
     }
 
@@ -43,7 +50,7 @@ public class PostService {
     }
 
     @Transactional
-    public ResponsePost update(Long id, UpdateRequestPost postDto){
+    public ResponsePost update(Long id, PostUpdateRequest postDto){
         Post retrievedPost = postRepository.findById(id)
                 .orElseThrow(()-> new EntityNotFoundException("id와 일치하는 포스팅이 없습니다."));
 
