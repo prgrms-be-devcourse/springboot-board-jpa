5f758f7bc6183a006e841eab0ccda1f84dee1b0c
jki503 <jki503@naver.com>
2022-06-15 08:46:50 +0900

    feat! : ErrorResponse 재정의
    - bindingResults로 FieldErrors 묶기
    - 롬복 제거

--- board/src/main/java/com/programmers/board/common/exception/ErrorResponse.java
+++ board/src/main/java/com/programmers/board/common/exception/ErrorResponse.java
@@ -1,23 +1,85 @@
 package com.programmers.board.common.exception;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.validation.BindingResult;
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ErrorResponse {
 
-    private int status;
+  private int status;
 
-    private String message;
+  private String message;
 
-    private ErrorResponse(ErrorCode code){
-        this.status = code.getStatus();
-        this.message = code.getMessage();
+  private String code;
+
+  private List<FieldError> errors;
+
+  private ErrorResponse() {
+  }
+
+  private ErrorResponse(ErrorCode code) {
+    this.status = code.getStatus();
+    this.message = code.getMessage();
+  }
+
+  private ErrorResponse(ErrorCode errorCode, List<FieldError> errors) {
+    this(errorCode);
+    this.errors = errors;
+  }
+
+
+  public static ErrorResponse of(ErrorCode code) {
+    return new ErrorResponse(code);
+  }
+
+  public static ErrorResponse of(ErrorCode code, BindingResult bindingResult) {
+    return new ErrorResponse(code, FieldError.of(bindingResult));
+  }
+
+  //Getter
+  public int getStatus() {
+    return status;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public List<FieldError> getErrors() {
+    return errors;
+  }
+
+  //FieldError - Inner Class
+  public static class FieldError {
+
+    private String field;
+
+    private String value;
+
+    private String reason;
+
+    private FieldError() {
     }
 
-    public static ErrorResponse of(ErrorCode code){
-        return new ErrorResponse(code);
+    private FieldError(String field, String value, String reason) {
+      this.field = field;
+      this.value = value;
+      this.reason = reason;
     }
+
+    public static List<FieldError> of(BindingResult bindingResult) {
+      List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
+      return fieldErrors.stream()
+          .map(error -> new FieldError(
+              error.getField(),
+              error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+              error.getDefaultMessage()
+          ))
+          .collect(Collectors.toList());
+    }
+  }
 }
