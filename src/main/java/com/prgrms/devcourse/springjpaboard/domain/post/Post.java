738f090e4cbbf869f7e9e22e0bc89d3298650092
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2022-12-15 04:35:25 +0900

    feat : Post, User 연관관계 매핑 추가

--- src/main/java/com/prgrms/devcourse/springjpaboard/domain/post/Post.java
+++ src/main/java/com/prgrms/devcourse/springjpaboard/domain/post/Post.java
@@ -1,11 +1,16 @@
 package com.prgrms.devcourse.springjpaboard.domain.post;
 
+import com.prgrms.devcourse.springjpaboard.domain.user.User;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -28,6 +33,11 @@ public class Post {
 	@Lob
 	@Column(name = "content", nullable = false)
 	private String content;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", referencedColumnName = "id")
+	private User user;
+
 
 	@Builder
 	public Post(String title, String content) {
