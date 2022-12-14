8a3a6cd93957912f772749678b7600b89dbf8b84
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2022-12-15 03:44:30 +0900

    feat : Post Entity 추가

---
+++ src/main/java/com/prgrms/devcourse/springjpaboard/domain/post/Post.java
@@ -0,0 +1,37 @@
+package com.prgrms.devcourse.springjpaboard.domain.post;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "post")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name = "title", nullable = false, length = 20)
+	private String title;
+
+	@Lob
+	@Column(name = "content",nullable = false)
+	private String content;
+
+	@Builder
+	public Post(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}
+}
