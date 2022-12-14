73cf0dbb6b43a693d35b04164a9e04c4bf27fcf4
Surin Lee <leesurin99@gmail.com>
2022-12-14 21:57:03 +0900

    refactor: User와 Post 연관관계 매핑

--- src/main/java/com/spring/board/springboard/post/domain/Post.java
+++ src/main/java/com/spring/board/springboard/post/domain/Post.java
@@ -1,5 +1,7 @@
 package com.spring.board.springboard.post.domain;
 
+import com.spring.board.springboard.user.domain.User;
+import com.spring.board.springboard.user.exception.AuthenticationException;
 import jakarta.persistence.*;
 
 import java.util.Objects;
@@ -20,15 +22,15 @@ public class Post {
     @Column(name = "content", nullable = false)
     private String content;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
     public Post() {
     }
 
     public Integer getId() {
         return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 
     public String getTitle() {
@@ -48,18 +50,30 @@ public class Post {
         this.content = content;
     }
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        if(Objects.nonNull(this.user)){
+            throw new AuthenticationException("작성자는 변경될 수 없습니다.");
+        }
+        this.user = user;
+        user.getPostList().add(this);
+    }
+
     public void changeTitle(String changeTitle) {
         validate(changeTitle);
         this.title = changeTitle;
     }
 
-    public void changeContent(String changeContent){
+    public void changeContent(String changeContent) {
         validate(changeContent);
         this.content = changeContent;
     }
 
-    private void validate(String input){
-        if(Objects.equals(input.length(), NOTHING)){
+    private void validate(String input) {
+        if (Objects.equals(input.length(), NOTHING)) {
             throw new IllegalArgumentException("빈 값일 수 없습니다. 반드시 입력해야합니다.");
         }
     }
