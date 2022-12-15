9a0851715e850d3f3a4fd2675ddcf3965f1becf9
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2022-12-16 00:14:31 +0900

    init: 프로젝트 생성

---
+++ src/test/java/com/programmers/kwonjoosung/board/BoardJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.kwonjoosung.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
