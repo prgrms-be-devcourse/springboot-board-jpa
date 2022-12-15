bbdd0b0d4db4cfb0387519146080cd16b04d9022
goseungwon <knmy0925@gmail.com>
2022-12-15 17:10:51 +0900

    [Chore] Create Project

---
+++ src/main/java/com/prgrms/boardjpa/BoardJpaApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.boardjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardJpaApplication.class, args);
+    }
+
+}
