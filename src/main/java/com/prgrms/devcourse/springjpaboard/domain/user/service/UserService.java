2b263bc8b77f1578d5d6f18a653b1d858361ba43
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2022-12-15 18:29:47 +0900

    feat : UserRequestService 추가

---
+++ src/main/java/com/prgrms/devcourse/springjpaboard/domain/user/service/UserService.java
@@ -0,0 +1,20 @@
+package com.prgrms.devcourse.springjpaboard.domain.user.service;
+
+import org.springframework.stereotype.Service;
+
+import com.prgrms.devcourse.springjpaboard.domain.user.User;
+import com.prgrms.devcourse.springjpaboard.domain.user.api.dto.UserRequestDto;
+import com.prgrms.devcourse.springjpaboard.domain.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+	private final UserRepository userRepository;
+
+	public void create(UserRequestDto userRequestDto) {
+		userRepository.save(userRequestDto.toUser());
+	}
+}
