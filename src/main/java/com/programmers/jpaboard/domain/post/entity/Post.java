57948f03d9a39f011af0dc8c71142620e71a9bfd
권성준 <tjdwns1865@naver.com>
2022-12-15 23:27:42 +0900

    feat: User 와 Post 1:N 연관관계 설정

--- src/main/java/com/programmers/jpaboard/domain/post/entity/Post.java
+++ src/main/java/com/programmers/jpaboard/domain/post/entity/Post.java
@@ -1,11 +1,18 @@
 package com.programmers.jpaboard.domain.post.entity;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
+
+import com.programmers.jpaboard.domain.user.entity.User;
 
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -26,4 +33,28 @@ public class Post {
 
 	@Lob
 	private String content;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", referencedColumnName = "id")
+	private User user;
+
+	public Post(String title, String content, User user) {
+		this.title = title;
+		this.content = content;
+		this.user = user;
+	}
+
+	public void setUser(User user) {
+		if (Objects.nonNull(this.user)) {
+			this.user.getPosts().remove(this);
+		}
+
+		this.user = user;
+		user.getPosts().add(this);
+	}
+
+	public void changePost(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}
 }
--- src/main/java/com/programmers/jpaboard/domain/user/entity/User.java
+++ src/main/java/com/programmers/jpaboard/domain/user/entity/User.java
@@ -1,12 +1,19 @@
 package com.programmers.jpaboard.domain.user.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
 
+import com.programmers.jpaboard.domain.post.entity.Post;
 import com.sun.istack.NotNull;
 
 import lombok.AccessLevel;
@@ -26,10 +33,33 @@ public class User {
 	@Column(nullable = false)
 	private String name;
 
+	@Email
+	@Column(nullable = false, unique = true)
+	private String email;
+
 	@NotNull
 	@Positive
 	private int age;
 
 	@Column(length = 10)
 	private String hobby;
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST)
+	List<Post> posts = new ArrayList<>();
+
+	public User(String name, String email, int age, String hobby) {
+		this.name = name;
+		this.email = email;
+		this.age = age;
+		this.hobby = hobby;
+	}
+
+	public void addPost(Post post) {
+		post.setUser(this);
+	}
+
+	public void changeUser(String email, String hobby) {
+		this.email = email;
+		this.hobby = hobby;
+	}
 }
