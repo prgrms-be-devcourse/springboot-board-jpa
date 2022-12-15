7421b33fe6ccbf4c847df769b7b9434c34641eb4
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-12-15 20:30:55 +0900

    fix: PostRepositoryTest를 수정하다.

--- src/test/java/com/prgrms/be/app/PostRepositoryTest.java
+++ src/test/java/com/prgrms/be/app/PostRepositoryTest.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 @Import(value = {JpaConfig.class})
 @DataJpaTest
 public class PostRepositoryTest {
@@ -17,12 +20,21 @@ public class PostRepositoryTest {
     @Autowired
     private PostRepository postRepository;
 
+    @PersistenceContext
+    EntityManager em;
 
+    //todo : Post Create,read Test
     @Test
-    void test() {
+    void create_read_test() {
+
+    }
+
+    //todo : Post Update test
+    @Test
+    void update_test() {
         Post post = new Post("title", "content", null);
 
-        postRepository.save(post);
     }
+
 
 }
