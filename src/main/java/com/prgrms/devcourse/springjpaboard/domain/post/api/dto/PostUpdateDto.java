4c8fe91d79acdc60b67888c2840802d5e5a8ee36
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2022-12-15 15:54:15 +0900

    feat : PostUpdateDto 추가

---
+++ src/main/java/com/prgrms/devcourse/springjpaboard/domain/post/api/dto/PostUpdateDto.java
@@ -0,0 +1,24 @@
+package com.prgrms.devcourse.springjpaboard.domain.post.api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostUpdateDto {
+
+	@NotBlank
+	private String title;
+
+	@NotBlank
+	private String content;
+
+	@Builder
+	public PostUpdateDto(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}
+}
