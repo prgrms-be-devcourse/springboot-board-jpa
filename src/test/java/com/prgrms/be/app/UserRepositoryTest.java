73171379aeaac7a34cb3132212c62e7504cdd339
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-12-15 20:31:25 +0900

    feat: UserRepositoryTest를 추가하다.

---
+++ src/test/java/com/prgrms/be/app/UserRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.prgrms.be.app;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class UserRepositoryTest {
+
+
+    //todo : User Create Test
+
+    //todo : User Read Test
+
+    //todo : User Update Test
+
+    //todo : User Delete Test
+}
