bd00920d9aba8e3cb85a4ba0a5878dd4e1e4c63a
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-12-15 13:37:05 +0900

    build: 프로젝트 환경을 구축하다.

---
+++ src/main/java/com/prgrms/be/app/JpaNoticeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.be.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaNoticeBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaNoticeBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prgrms/be/app/JpaNoticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.be.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaNoticeBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
