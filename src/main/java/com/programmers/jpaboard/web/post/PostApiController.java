962eeecb1d9c7d595ce62b477dffc4db1183a5b8
권성준 <tjdwns1865@naver.com>
2022-12-17 03:15:49 +0900

    feat: PostApiController 구현

---
+++ src/main/java/com/programmers/jpaboard/web/post/PostApiController.java
@@ -0,0 +1,54 @@
+package com.programmers.jpaboard.web.post;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.programmers.jpaboard.domain.post.service.PostService;
+import com.programmers.jpaboard.web.post.dto.PostCreateRequestDto;
+import com.programmers.jpaboard.web.post.dto.PostResponseDto;
+import com.programmers.jpaboard.web.post.dto.PostUpdateRequestDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/posts")
+public class PostApiController {
+
+	private final PostService postService;
+
+	@PostMapping
+	ResponseEntity<PostResponseDto> createPost(@RequestBody PostCreateRequestDto postCreateRequestDto) {
+		PostResponseDto createdPostDto = postService.createPost(postCreateRequestDto);
+		return ResponseEntity.ok(createdPostDto);
+	}
+
+	@GetMapping
+	ResponseEntity<Page<PostResponseDto>> getPosts(Pageable pageable) {
+		Page<PostResponseDto> posts = postService.getPosts(pageable);
+		return ResponseEntity.ok(posts);
+	}
+
+	@GetMapping("/{postId}")
+	ResponseEntity<PostResponseDto> getPostById(@PathVariable Long postId) {
+		PostResponseDto findPostDto = postService.getPostById(postId);
+		return ResponseEntity.ok(findPostDto);
+	}
+
+	@PatchMapping("/{postId}")
+	ResponseEntity<PostResponseDto> updatePost(
+		@PathVariable Long postId,
+		@RequestBody PostUpdateRequestDto postUpdateRequestDto
+	) {
+		PostResponseDto updatedPostDto = postService.updatePost(postId, postUpdateRequestDto);
+		return ResponseEntity.ok(updatedPostDto);
+	}
+}
