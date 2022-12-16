96ea500fb62203f803a269ee43d8a1159c806e5f
권성준 <tjdwns1865@naver.com>
2022-12-17 03:13:58 +0900

    refactor: Post 요청 DTO 에 생성자 추가

--- src/main/java/com/programmers/jpaboard/web/post/dto/PostCreateRequestDto.java
+++ src/main/java/com/programmers/jpaboard/web/post/dto/PostCreateRequestDto.java
@@ -5,9 +5,13 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostCreateRequestDto {
 
 	@NotBlank
--- src/main/java/com/programmers/jpaboard/web/post/dto/PostUpdateRequestDto.java
+++ src/main/java/com/programmers/jpaboard/web/post/dto/PostUpdateRequestDto.java
@@ -4,9 +4,13 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostUpdateRequestDto {
 
 	@NotBlank
