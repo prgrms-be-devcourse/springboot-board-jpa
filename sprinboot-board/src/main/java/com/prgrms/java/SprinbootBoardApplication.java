5cd4828e695c9724395430564d4537c8ea748a49
dlxortmd987 <dlxortmd987@gmail.com>
2022-12-16 13:59:56 +0900

    Feat: initial commit

---
+++ sprinboot-board/src/main/java/com/prgrms/java/SprinbootBoardApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinbootBoardApplication.class, args);
+	}
+
+}
---
+++ sprinboot-board/src/test/java/com/prgrms/java/SprinbootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbootBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
