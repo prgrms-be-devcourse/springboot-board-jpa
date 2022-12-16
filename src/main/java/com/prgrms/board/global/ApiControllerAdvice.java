8c2b40412631b9140cc46fd310d456d639cdb4ed
김기서 <5rne@naver.com>
2022-12-16 20:53:12 +0900

    feat: 예외를 처리하는 ControllerAdvice 생성

---
+++ src/main/java/com/prgrms/board/global/ApiControllerAdvice.java
@@ -0,0 +1,19 @@
+package com.prgrms.board.global;
+
+import com.prgrms.board.dto.ApiResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiControllerAdvice {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ApiResponse<String> runTimeExceptionHandler(RuntimeException e) {
+        return ApiResponse.fail(404, e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ApiResponse<String> internalServerErrorHandler (Exception e) {
+        return ApiResponse.fail(500, e.getMessage());
+    }
+}
