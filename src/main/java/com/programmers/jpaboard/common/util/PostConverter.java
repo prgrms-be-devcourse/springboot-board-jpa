91ca9350cf51a944046d7dff0e30ad7cb1473bec
권성준 <tjdwns1865@naver.com>
2022-12-16 21:57:41 +0900

    feat: PostConverter 구현
    - DTO 들과 Entity 사이의 변환 책임을 가짐.

---
+++ src/main/java/com/programmers/jpaboard/common/util/PostConverter.java
@@ -0,0 +1,27 @@
+package com.programmers.jpaboard.common.util;
+
+import org.springframework.stereotype.Component;
+
+import com.programmers.jpaboard.domain.post.entity.Post;
+import com.programmers.jpaboard.domain.user.entity.User;
+import com.programmers.jpaboard.web.post.dto.PostCreateRequestDto;
+import com.programmers.jpaboard.web.post.dto.PostResponseDto;
+
+@Component
+public class PostConverter {
+
+	public static Post toPost(PostCreateRequestDto postCreateRequestDto, User user) {
+		return new Post(postCreateRequestDto.getTitle(), postCreateRequestDto.getContent(), user);
+	}
+
+	public static PostResponseDto toPostResponseDto(Post post) {
+		return new PostResponseDto(
+			post.getId(),
+			post.getTitle(),
+			post.getContent(),
+			post.getUser().getId(),
+			post.getCreatedAt(),
+			post.getCreatedBy()
+		);
+	}
+}
