33f52b263b98eb7ca78e3d802bb391d9abbde05e
권성준 <tjdwns1865@naver.com>
2022-12-16 19:53:21 +0900

    test: Repository CRUD 테스트

---
+++ src/test/java/com/programmers/jpaboard/domain/post/repository/PostRepositoryTest.java
@@ -0,0 +1,82 @@
+package com.programmers.jpaboard.domain.post.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.programmers.jpaboard.domain.post.entity.Post;
+import com.programmers.jpaboard.domain.user.entity.User;
+import com.programmers.jpaboard.domain.user.repository.UserRepository;
+
+@DataJpaTest
+class PostRepositoryTest {
+
+	@Autowired
+	PostRepository postRepository;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@AfterEach
+	void tearDown() {
+		postRepository.deleteAll();
+	}
+
+	@Test
+	@DisplayName("정상적인 게시글 저장과 조회를 성공한다.")
+	void saveAndFindByIdPost() {
+		// given
+		User user = new User("권성준", "google@gmail.com", 100, "인터넷 서핑");
+		Post post = new Post("테스트", "테스트입니다.", user);
+
+		// when
+		userRepository.save(user);
+		Post savedPost = postRepository.save(post);
+		Post findPost = postRepository.findById(savedPost.getId()).get();
+
+		// then
+		assertThat(savedPost.getTitle()).isEqualTo(post.getTitle());
+		assertThat(savedPost.getTitle()).isEqualTo(post.getTitle());
+		assertThat(findPost).isEqualTo(savedPost);
+	}
+
+	@Test
+	@DisplayName("게시글의 수정을 성공한다.")
+	void updatePost() {
+		// given
+		User user = new User("권성준", "google@gmail.com", 100, "인터넷 서핑");
+		Post post = new Post("테스트", "테스트입니다.", user);
+		userRepository.save(user);
+		String changeTitle = "변경된 테스트";
+		String changeContent = "변경된 테스트입니다";
+
+		// when
+		post.changePost(changeTitle, changeContent);
+		Post findPost = postRepository.findById(post.getId()).get();
+
+		// then
+		assertThat(findPost).isEqualTo(post);
+	}
+
+	@Test
+	@DisplayName("게시글의 아이디로 삭제를 성공한다.")
+	void deleteByIdPost() {
+		// given
+		User user = new User("권성준", "google@gmail.com", 100, "인터넷 서핑");
+		Post post = new Post("테스트", "테스트입니다.", user);
+		userRepository.save(user);
+
+		// when
+		postRepository.deleteById(post.getId());
+		Optional<Post> optionalPost = postRepository.findById(post.getId());
+
+		// then
+		assertThat(optionalPost.isEmpty()).isTrue();
+	}
+}
---
+++ src/test/java/com/programmers/jpaboard/domain/user/repository/UserRepositoryTest.java
@@ -0,0 +1,75 @@
+package com.programmers.jpaboard.domain.user.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.programmers.jpaboard.domain.user.entity.User;
+
+@DataJpaTest
+class UserRepositoryTest {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@AfterEach
+	void tearDown() {
+		userRepository.deleteAll();
+	}
+
+	@Test
+	@DisplayName("정상적인 사용자 저장과 아이디로의 조회에 성공한다.")
+	void saveAndFindByIdTest() {
+		// given
+		User user = new User("권성준", "google@gmail.com", 100, "인터넷 서핑");
+
+		// when
+		User savedUser = userRepository.save(user);
+		User findUser = userRepository.findById(savedUser.getId()).get();
+
+		// then
+		assertThat(savedUser.getName()).isEqualTo(user.getName());
+		assertThat(savedUser.getEmail()).isEqualTo(user.getEmail());
+		assertThat(savedUser.getAge()).isEqualTo(user.getAge());
+		assertThat(savedUser.getHobby()).isEqualTo(user.getHobby());
+		assertThat(findUser).isEqualTo(savedUser);
+	}
+
+	@Test
+	@DisplayName("사용자 수정에 성공한다.")
+	void updateUser() {
+		// given
+		User user = new User("권성준", "google@gmail.com", 100, "인터넷 서핑");
+		userRepository.save(user);
+		String changeEmail = "naver@naver.com";
+		String changeHobby = "취미 없음";
+
+		// when
+		user.changeUser(changeEmail, changeHobby);
+		User findUser = userRepository.findById(user.getId()).get();
+
+		// then
+		assertThat(findUser).isEqualTo(user);
+	}
+
+	@Test
+	@DisplayName("사용자의 아이디로 삭제에 성공한다.")
+	void deleteTest() {
+		// given
+		User user = new User("권성준", "google@gmail.com", 100, "인터넷 서핑");
+		userRepository.save(user);
+
+		// when
+		userRepository.deleteById(user.getId());
+		Optional<User> optionalUser = userRepository.findById(user.getId());
+
+		// then
+		assertThat(optionalUser.isEmpty()).isTrue();
+	}
+}
