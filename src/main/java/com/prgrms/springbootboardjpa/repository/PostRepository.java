4168fc90465ca4d9c5f188bb5714e3af33d60fc6
김창규 <kimc980106@naver.com>
2022-12-17 16:22:24 +0900

    Feat: 각 Entity 별 Repository 레이어 구현

---
+++ src/main/java/com/prgrms/springbootboardjpa/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.prgrms.springbootboardjpa.repository;
+
+import com.prgrms.springbootboardjpa.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
---
+++ src/main/java/com/prgrms/springbootboardjpa/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.prgrms.springbootboardjpa.repository;
+
+import com.prgrms.springbootboardjpa.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
