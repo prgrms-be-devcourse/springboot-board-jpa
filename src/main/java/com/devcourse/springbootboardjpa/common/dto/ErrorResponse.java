299abcf037ad7b71379bf5c74488da0a23860e8b
Kong <rhdtn311@naver.com>
2022-12-18 03:38:45 +0900

    feat : Validation 에러 헨들링 코드 추가

--- src/main/java/com/devcourse/springbootboardjpa/common/dto/ErrorResponse.java
+++ src/main/java/com/devcourse/springbootboardjpa/common/dto/ErrorResponse.java
@@ -1,10 +1,26 @@
 package com.devcourse.springbootboardjpa.common.dto;
 
 import com.devcourse.springbootboardjpa.common.exception.ErrorCode;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.validation.BindException;
+
 
 public class ErrorResponse extends ResponseDTO {
 
+    private static final Integer CONFLICT_STATUS = 409;
+
     public ErrorResponse(ErrorCode errorCode) {
         super(errorCode.getStatus(), errorCode.getMessage());
+    }
+
+    public ErrorResponse(BindException bindException) {
+        super(CONFLICT_STATUS, getBindingErrorMessages(bindException));
+    }
+
+    private static String getBindingErrorMessages(BindException bindException) {
+        return bindException.getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .reduce("", (beforeErrorMessages, errorMessage) -> beforeErrorMessages + errorMessage + ", ");
     }
 }
--- src/main/java/com/devcourse/springbootboardjpa/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/devcourse/springbootboardjpa/common/exception/GlobalExceptionHandler.java
@@ -4,19 +4,29 @@ import com.devcourse.springbootboardjpa.common.dto.ErrorResponse;
 import com.devcourse.springbootboardjpa.common.dto.ResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<ResponseDTO> handlerBusinessException(BusinessException e) {
+    public ResponseEntity<ResponseDTO> handleBusinessException(BusinessException e) {
         ErrorCode errorCode = e.getErrorCode();
 
         return new ResponseEntity<>(
                 new ErrorResponse(errorCode),
                 HttpStatus.valueOf(errorCode.getStatus())
+        );
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<ErrorResponse> handleBindingException(BindException e) {
+        return new ResponseEntity<>(
+                new ErrorResponse(e),
+                HttpStatus.CONFLICT
         );
     }
 }
