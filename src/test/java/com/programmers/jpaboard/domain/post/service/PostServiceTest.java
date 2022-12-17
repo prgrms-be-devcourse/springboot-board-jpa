cad7e901be4fcf8e0dc153d8d1e25f385908f580
권성준 <tjdwns1865@naver.com>
2022-12-17 15:14:00 +0900

    test: PostService 테스트 추가

---
+++ src/test/java/com/programmers/jpaboard/domain/post/service/PostServiceTest.java
@@ -0,0 +1,115 @@
+package com.programmers.jpaboard.domain.post.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.programmers.jpaboard.common.exception.PostNotFoundException;
+import com.programmers.jpaboard.domain.post.dto.PostCreateRequestDto;
+import com.programmers.jpaboard.domain.post.dto.PostResponseDto;
+import com.programmers.jpaboard.domain.post.dto.PostUpdateRequestDto;
+import com.programmers.jpaboard.domain.post.entity.Post;
+import com.programmers.jpaboard.domain.post.repository.PostRepository;
+import com.programmers.jpaboard.domain.user.entity.User;
+import com.programmers.jpaboard.domain.user.repository.UserRepository;
+
+@ExtendWith(MockitoExtension.class)
+class PostServiceTest {
+
+	@InjectMocks
+	PostService postService;
+
+	@Mock
+	PostRepository postRepository;
+
+	@Mock
+	UserRepository userRepository;
+
+	private User user = new User(1L, "권성준", "naver@naver.com", 26, "취미");
+	private Post post = new Post(1L, "제목", "내용입니다");
+
+	@Test
+	@DisplayName("게시물 생성에 성공한다.")
+	void successCreatePost() {
+		// given
+		post.setUser(user);
+		PostCreateRequestDto postCreateRequestDto = new PostCreateRequestDto("제목", "내용입니다.", user.getId());
+		when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+		when(postRepository.save(any(Post.class))).thenReturn(post);
+
+		// when
+		PostResponseDto savedPostDto = postService.createPost(postCreateRequestDto);
+
+		// then
+		assertThat(savedPostDto.getId()).isEqualTo(post.getId());
+		assertThat(savedPostDto.getTitle()).isEqualTo(post.getTitle());
+		assertThat(savedPostDto.getContent()).isEqualTo(post.getContent());
+		assertThat(savedPostDto.getUserId()).isEqualTo(post.getUser().getId());
+		assertThat(savedPostDto.getCreatedAt()).isEqualTo(post.getCreatedAt());
+		assertThat(savedPostDto.getLastModifiedAt()).isEqualTo(post.getLastModifiedAt());
+		verify(userRepository, times(1)).findById(user.getId());
+		verify(postRepository, times(1)).save(any(Post.class));
+	}
+
+	@Test
+	@DisplayName("게시물 아이디로 조회에 성공한다.")
+	void getPostById() {
+		// given
+		post.setUser(user);
+		when(postRepository.findById(post.getId())).thenReturn(Optional.of(post));
+
+		// when
+		PostResponseDto findPostDto = postService.getPostById(post.getId());
+
+		// then
+		assertThat(findPostDto.getId()).isEqualTo(post.getId());
+		assertThat(findPostDto.getTitle()).isEqualTo(post.getTitle());
+		assertThat(findPostDto.getContent()).isEqualTo(post.getContent());
+		assertThat(findPostDto.getUserId()).isEqualTo(post.getUser().getId());
+		verify(postRepository, times(1)).findById(post.getId());
+	}
+
+	@Test
+	@DisplayName("게시물 아이디로 조회에 실패한다.")
+	void failGetPostById() {
+		// given
+		Post noIdPost = new Post("제목", "내용입니다.");
+		when(postRepository.findById(noIdPost.getId())).thenReturn(Optional.empty());
+
+		// when, then
+		assertThatThrownBy(() -> postService.getPostById(noIdPost.getId()))
+			.isInstanceOf(PostNotFoundException.class);
+		verify(postRepository, times(1)).findById(noIdPost.getId());
+	}
+
+	@Test
+	@DisplayName("게시물 수정에 성공한다.")
+	void updatePost() {
+		// given
+		post.setUser(user);
+		String updateTitle = "수정된 제목";
+		String updateContent = "수정된 내용입니다.";
+		PostUpdateRequestDto postUpdateRequestDto = new PostUpdateRequestDto(updateTitle, updateContent);
+		when(postRepository.findById(post.getId())).thenReturn(Optional.of(post));
+
+		// when
+		PostResponseDto updatedPostDto = postService.updatePost(post.getId(), postUpdateRequestDto);
+
+		// then
+		assertThat(updatedPostDto.getId()).isEqualTo(post.getId());
+		assertThat(updatedPostDto.getTitle()).isEqualTo(updateTitle);
+		assertThat(updatedPostDto.getContent()).isEqualTo(updateContent);
+		assertThat(updatedPostDto.getUserId()).isEqualTo(post.getUser().getId());
+		assertThat(post.getTitle()).isEqualTo(updateTitle);
+		assertThat(post.getContent()).isEqualTo(updateContent);
+		verify(postRepository, times(1)).findById(post.getId());
+	}
+}
