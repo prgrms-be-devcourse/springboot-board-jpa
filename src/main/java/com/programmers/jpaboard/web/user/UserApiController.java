6e7cce959e492154b78f5a27a2e8a705b5648cc5
권성준 <tjdwns1865@naver.com>
2022-12-18 03:05:25 +0900

    feat: UserApiController 구현

---
+++ src/main/java/com/programmers/jpaboard/web/user/UserApiController.java
@@ -0,0 +1,29 @@
+package com.programmers.jpaboard.web.user;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.programmers.jpaboard.domain.user.dto.UserCreateRequestDto;
+import com.programmers.jpaboard.domain.user.dto.UserResponseDto;
+import com.programmers.jpaboard.domain.user.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+public class UserApiController {
+
+	private final UserService userService;
+
+	@PostMapping
+	ResponseEntity<UserResponseDto> createUser(@RequestBody @Valid UserCreateRequestDto userCreateRequestDto) {
+		UserResponseDto createdUser = userService.createUser(userCreateRequestDto);
+		return ResponseEntity.ok(createdUser);
+	}
+}
