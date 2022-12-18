6f156bcd43cea3ad80f75fb0bdb010bb190065cb
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2022-12-18 22:11:21 +0900

    test : findAll 테스트 추가

--- src/test/java/com/prgrms/devcourse/springjpaboard/domain/post/api/PostControllerTest.java
+++ src/test/java/com/prgrms/devcourse/springjpaboard/domain/post/api/PostControllerTest.java
@@ -1,10 +1,10 @@
 package com.prgrms.devcourse.springjpaboard.domain.post.api;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -15,7 +15,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.prgrms.devcourse.springjpaboard.domain.post.service.dto.PostRequestDto;
 import com.prgrms.devcourse.springjpaboard.domain.post.service.dto.PostResponseDto;
@@ -50,7 +49,7 @@ class PostControllerTest {
 
 		String json = objectMapper.writeValueAsString(postSaveDto);
 
-		Mockito.doNothing().when(postFacade).create(postSaveDto);
+		Mockito.doNothing().when(postFacade).create(ArgumentMatchers.any());
 
 		mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/posts")
 				.contentType(MediaType.APPLICATION_JSON)
@@ -58,6 +57,7 @@ class PostControllerTest {
 			.andExpect(MockMvcResultMatchers.status().isOk())
 			.andDo(MockMvcResultHandlers.print());
 
+		Mockito.verify(postFacade).create(ArgumentMatchers.any());
 	}
 
 	@Test
@@ -85,11 +85,13 @@ class PostControllerTest {
 			.andExpect(MockMvcResultMatchers.jsonPath("$.content").value(postResponseDto.getContent()))
 			.andDo(MockMvcResultHandlers.print());
 
+		Mockito.verify(postFacade).findById(postId);
+
 	}
 
 	@Test
 	@DisplayName("Post 수정 정상요청")
-	void editTest() throws Exception {
+	void updateTest() throws Exception {
 
 		Long postId = 1L;
 
@@ -100,7 +102,7 @@ class PostControllerTest {
 
 		String json = objectMapper.writeValueAsString(postUpdateDto);
 
-		Mockito.doNothing().when(postFacade).update(postId, postUpdateDto);
+		Mockito.doNothing().when(postFacade).update(ArgumentMatchers.any(), ArgumentMatchers.any());
 
 		mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/posts/{id}", postId)
 				.content(json)
@@ -108,54 +110,58 @@ class PostControllerTest {
 			.andExpect(MockMvcResultMatchers.status().isOk())
 			.andDo(MockMvcResultHandlers.print());
 
+		Mockito.verify(postFacade).update(ArgumentMatchers.any(), ArgumentMatchers.any());
+
 	}
 
-	// @Test
-	// @DisplayName("전체 Post 조회 정상요청")
-	// void findAll() throws Exception {
-	//
-	// 	Long cursorId = 2L;
-	// 	Integer size = 1;
-	//
-	// 	PostRequestDto postRequestDto = PostRequestDto.builder()
-	// 		.cursorId(cursorId)
-	// 		.size(size)
-	// 		.build();
-	//
-	// 	Long postId = 1L;
-	//
-	// 	PostResponseDto postResponseDto = PostResponseDto.builder()
-	// 		.id(postId)
-	// 		.title("hello")
-	// 		.content("hi")
-	// 		.build();
-	//
-	// 	List<PostResponseDto> postResponseDtoList = List.of(postResponseDto);
-	//
-	// 	PostResponseDtos postResponseDtos = PostResponseDtos.builder()
-	// 		.postResponseDtoList(postResponseDtoList)
-	// 		.cursorId(cursorId)
-	// 		.hasNext(true)
-	// 		.build();
-	//
-	//
-	// 	String requestJson = objectMapper.writeValueAsString(postRequestDto);
-	// 	String responseJson = objectMapper.writeValueAsString(postResponseDtos);
-	//
-	// 	System.out.println(responseJson);
-	//
-	// 	Mockito.when(postFacade.findAll(postRequestDto)).thenReturn(postResponseDtos);
-	//
-	// 	mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/posts")
-	// 			.contentType(MediaType.APPLICATION_JSON)
-	// 			.content(requestJson))
-	// 		.andExpect(MockMvcResultMatchers.status().isOk())
-	// 		.andExpect(MockMvcResultMatchers.content().json(requestJson))
-	// 		.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-	// 		.andExpect(MockMvcResultMatchers.jsonPath("$.hasNext").value(postResponseDtos.getHasNext()))
-	// 		.andExpect(MockMvcResultMatchers.jsonPath("$.cursorId").value(postResponseDtos.getCursorId()))
-	// 		.andDo(MockMvcResultHandlers.print());
-	//
-	//
-	// }
+	@Test
+	@DisplayName("전체 Post 조회 정상요청")
+	void findAll() throws Exception {
+
+		Long cursorId = 2L;
+		Integer size = 1;
+
+		PostRequestDto postRequestDto = PostRequestDto.builder()
+			.cursorId(cursorId)
+			.size(size)
+			.build();
+
+		Long postId = 1L;
+
+		PostResponseDto postResponseDto = PostResponseDto.builder()
+			.id(postId)
+			.title("hello")
+			.content("hi")
+			.build();
+
+		List<PostResponseDto> postResponseDtoList = List.of(postResponseDto);
+
+		PostResponseDtos postResponseDtos = PostResponseDtos.builder()
+			.postResponseDtoList(postResponseDtoList)
+			.cursorId(cursorId)
+			.hasNext(true)
+			.build();
+
+		String requestJson = objectMapper.writeValueAsString(postRequestDto);
+		String responseJson = objectMapper.writeValueAsString(postResponseDtos);
+
+		Mockito.when(postFacade.findAll(ArgumentMatchers.any())).thenReturn(postResponseDtos);
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/posts")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(requestJson))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.content().json(responseJson))
+			.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.postResponseDtoList.[0].id").value(postId))
+			.andExpect(
+				MockMvcResultMatchers.jsonPath("$.postResponseDtoList.[0].title").value(postResponseDto.getTitle()))
+			.andExpect(
+				MockMvcResultMatchers.jsonPath("$.postResponseDtoList.[0].content").value(postResponseDto.getContent()))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.cursorId").value(postResponseDtos.getCursorId()))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.hasNext").value(postResponseDtos.getHasNext()))
+			.andDo(MockMvcResultHandlers.print());
+
+		Mockito.verify(postFacade).findAll(ArgumentMatchers.any());
+	}
 }
