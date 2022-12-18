39d2dae5b815e4dc2c1865fb3e76764936fa14ee
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2022-12-19 04:04:30 +0900

    feat : JpaAuditing configuration 추가

--- src/main/java/com/prgrms/devcourse/springjpaboard/domain/base/BaseEntity.java
+++ src/main/java/com/prgrms/devcourse/springjpaboard/domain/base/BaseEntity.java
@@ -3,17 +3,25 @@ package com.prgrms.devcourse.springjpaboard.domain.base;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.Getter;
 
 @Getter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
+	@CreatedDate
 	@Column(name = "created_at")
 	public LocalDateTime createdAt;
 
+	@CreatedBy
 	@Column(name = "created_by")
 	public String createdBy;
 }
---
+++ src/main/java/com/prgrms/devcourse/springjpaboard/global/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.prgrms.devcourse.springjpaboard.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
