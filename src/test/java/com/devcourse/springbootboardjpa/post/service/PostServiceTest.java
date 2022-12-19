01721ec5a6369aaeb6217060e92cfcd2925c3d86
Kong <rhdtn311@naver.com>
2022-12-19 12:49:16 +0900

    cache clear

--- src/test/java/com/devcourse/springbootboardjpa/post/service/PostServiceTest.java
+++ src/test/java/com/devcourse/springbootboardjpa/post/service/PostServiceTest.java
@@ -16,7 +16,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.ArrayList;
