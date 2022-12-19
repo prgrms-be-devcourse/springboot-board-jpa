b4523898cd748e2c895ae537e4cf358891d52801
ybkim-dev <beanybigggy@gmail.com>
2022-12-19 17:31:26 +0900

    feat : createdAt, createdBy BaseEntity 추가

--- src/main/java/com/prgrms/boardjpa/domain/BaseEntity.java
+++ src/main/java/com/prgrms/boardjpa/domain/BaseEntity.java
@@ -1,13 +1,22 @@
 package com.prgrms.boardjpa.domain;
 
 import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
+@Setter
 public abstract class BaseEntity {
+    @Column(name="created_at", columnDefinition = "TIMESTAMP")
     private LocalDateTime createdAt;
+    @Column(name = "created_by")
     private String createdBy;
+
+    public BaseEntity(){}
+    public BaseEntity(LocalDateTime createdAt, String createdBy) {
+    }
 }
