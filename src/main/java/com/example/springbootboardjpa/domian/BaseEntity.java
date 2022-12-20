6fef13a3fafdf9a11f2b18bcb0ad0ebce92bbab7
youngji <youngji.jang804@gmail.com>
2022-12-20 15:26:04 +0900

    feat: baseEntity Auditing 추가

--- src/main/java/com/example/springbootboardjpa/domian/BaseEntity.java
+++ src/main/java/com/example/springbootboardjpa/domian/BaseEntity.java
@@ -1,18 +1,22 @@
 package com.example.springbootboardjpa.domian;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Setter
 @Getter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     @Column(name = "created_by")
     private String createdBy;
-    @Column(name = "created_at", columnDefinition = "TIMESTAMP")
+    @CreatedDate
     private LocalDateTime cratedAt;
 }
