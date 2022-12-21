89dcbe9bdd69e9c57a0e7ca5e855d9856e5e729e
bufflect <bndgc@naver.com>
2022-12-22 00:03:18 +0900

    add : database setting

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,55 @@
+insert into MEMBER(age, hobby, name)
+values (10, 'hobby1', 'name1');
+
+insert into MEMBER(age, hobby, name)
+values (20, 'hobby2', 'name2');
+
+insert into MEMBER(age, hobby, name)
+values (30, 'hobby3', 'name3');
+
+
+-- post
+insert into post(title, content)
+values ('title1', 'content1');
+
+insert into post(title, content)
+values ('title2', 'content2');
+
+insert into post(title, content)
+values ('title3', 'content3');
+
+insert into post(title, content)
+values ('title4', 'content4');
+
+insert into post(title, content)
+values ('title5', 'content5');
+
+insert into post(title, content)
+values ('title6', 'content6');
+
+insert into post(title, content)
+values ('title7', 'content7');
+
+insert into post(title, content)
+values ('title8', 'content8');
+
+insert into post(title, content)
+values ('title9', 'content9');
+
+insert into post(title, content)
+values ('title10', 'content10');
+
+insert into post(title, content)
+values ('title11', 'content11');
+
+insert into post(title, content)
+values ('title12', 'content12');
+
+insert into post(title, content)
+values ('title13', 'content13');
+
+insert into post(title, content)
+values ('title14', 'content14');
+
+insert into post(title, content)
+values ('title15', 'content15');
