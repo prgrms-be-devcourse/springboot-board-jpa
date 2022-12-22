ff19866c7aa3c6230a9870f0e0107120c572bc9b
hyunseo <goatsfish@gmail.com>
2022-12-22 15:48:27 +0900

    feat : 게시글 저장, 수정, 조회 요청, 응답 dto 추가

---
+++ src/main/java/com/example/board/domain/post/dto/PostDto.java
@@ -0,0 +1,31 @@
+package com.example.board.domain.post.dto;
+
+import com.example.board.domain.post.entity.Post;
+import com.example.board.domain.user.dto.UserDto;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
+public class PostDto {
+
+    public record CreatePostRequest(@NotNull(message = "{exception.entity.user.id}") Long userId,
+                                    @NotNull(message = "{exception.entity.post.title.null}") @Length(min = 1, message = "{exception.entity.post.title.length}") String title,
+                                    @NotNull(message = "{exception.entity.post.content.null}") @Length(min = 1, message = "{exception.entity.post.content.length}") String content) {
+    }
+
+    public record UpdatePostRequest(@NotNull(message = "{exception.entity.post.title.null}") @Length(min = 1, message = "{exception.entity.post.title.length}") String title,
+                                    @NotNull(message = "{exception.entity.post.content.null}") @Length(min = 1, message = "{exception.entity.post.content.length}") String content) {
+    }
+
+    public record SinglePostResponse(@NotNull(message = "{exception.entity.post.id}") Long postId,
+                                     @NotNull(message = "{exception.entity.post.title.null}") @Length(min = 1, message ="{exception.entity.post.title.length}") String title,
+                                     @NotNull(message = "{exception.entity.post.content.null}") @Length(min = 1, message = "{exception.entity.post.content.length}") String content,
+                                     @NotNull(message = "{exception.entity.user.null}") UserDto.SingleUserSimpleResponse user) {
+
+        public static SinglePostResponse toResponse(Post post) {
+            return new SinglePostResponse(post.getId(),
+                    post.getTitle(),
+                    post.getContent(),
+                    UserDto.SingleUserSimpleResponse.toResponse(post.getUser()));
+        }
+    }
+}
