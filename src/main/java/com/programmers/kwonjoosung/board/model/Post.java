4fc9e3327e31b6685820198240915916f1277d3c
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2022-12-23 04:38:38 +0900

    feat: 엔티티 설계하기
    * User: 작성자
    * Post: 게시글
    * Base Entity 설정: 생성 시간, 최종 수정 시간
    * 메서드로 Validation Check 하였음

---
+++ src/main/java/com/programmers/kwonjoosung/board/model/Post.java
@@ -0,0 +1,66 @@
+package com.programmers.kwonjoosung.board.model;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import org.springframework.util.Assert;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Post extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String title;
+
+    @Column(nullable = false, length = 100)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    public Post(String title, String content) {
+        validateTitle(title);
+        validateContent(content);
+        this.title = title;
+        this.content = content;
+    }
+
+    private void validateContent(String content) {
+        Assert.isTrue(content.length() <= 100, "게시글은 최대 100글자 입니다.");
+    }
+
+    private void validateTitle(String title) {
+        Assert.isTrue(title.length() <= 30, "제목은 최대 30글자 입니다.");
+    }
+
+    public void setUser(User user) { // 연관관계 편의 메소드
+        if(Objects.nonNull(this.user)) {
+            this.user.getPosts().remove(this);
+        }
+        this.user = user;
+        user.getPosts().add(this);
+    }
+
+
+    public void changeTitle(String title) {
+        validateTitle(title);
+        this.title = title;
+    }
+
+    public void changeContent(String content) {
+        validateContent(content);
+        this.content = content;
+    }
+
+}
---
+++ src/main/java/com/programmers/kwonjoosung/board/model/User.java
@@ -0,0 +1,79 @@
+package com.programmers.kwonjoosung.board.model;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import org.springframework.util.Assert;
+
+import javax.persistence.*;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "user")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,length = 20, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    @Column(length = 20)
+    private String hobby;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts;
+
+    public User(String name, Integer age, String hobby) {
+        validateName(name);
+        validateAge(age);
+        validateHobby(hobby);
+        this.name = name;
+        this.age = age;
+        this.hobby = hobby;
+    }
+
+    private void validateHobby(String hobby) {
+        if(hobby == null) {
+            return;
+        }
+        Assert.isTrue(hobby.length() <= 20, "취미는 최대 20글자 입니다.");
+    }
+
+    private void validateAge(Integer age) {
+        Assert.isTrue(age >= 0, "나이는 0살 이상이어야 합니다.");
+    }
+
+    private void validateName(String name) {
+        Assert.notNull(name, "이름은 필수 값입니다.");
+        Assert.isTrue(name.length() > 0 && name.length() <= 20,
+                "이름은 최소 1글자 이상, 최대 20글자 이내 입니다.");
+
+    }
+
+    public void addPost(Post post) {
+        post.setUser(this);
+    }
+
+    public void changeName(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    public void changeAge(Integer age) {
+        validateAge(age);
+        this.age = age;
+    }
+
+    public void changeHobby(String hobby) {
+        validateHobby(hobby);
+        this.hobby = hobby;
+    }
+}
