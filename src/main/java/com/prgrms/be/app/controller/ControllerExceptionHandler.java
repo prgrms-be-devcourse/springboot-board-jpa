9297a9757bdd6aac8c622d053f4561d20cd3060c
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-12-22 20:40:25 +0900

    feat: Exception을 처리하기 위한 클래스를 추가하다.

---
+++ src/main/java/com/prgrms/be/app/controller/ControllerExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.prgrms.be.app.controller;
+
+import com.prgrms.be.app.domain.dto.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.EntityNotFoundException;
+
+@Slf4j
+@RestControllerAdvice
+public class ControllerExceptionHandler<T> extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ApiResponse<String> notFoundErrorHandler(EntityNotFoundException exception) {
+        log.warn("EntityNotFoundException : ", exception);
+        return ApiResponse.fail(404, exception.getMessage(), ResponseMessage.ERRORED);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    protected ApiResponse<String> invalidParameterErrorHandler(IllegalArgumentException exception) {
+        log.warn("IllegalArgumentException : ", exception);
+        return ApiResponse.fail(400, exception.getMessage(), ResponseMessage.ERRORED);
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ApiResponse<String> internalServerErrorHandler(Exception exception) {
+        log.error("IntervalServerError : ", exception);
+        return ApiResponse.fail(500, exception.getMessage(), ResponseMessage.ERRORED);
+    }
+
+}
