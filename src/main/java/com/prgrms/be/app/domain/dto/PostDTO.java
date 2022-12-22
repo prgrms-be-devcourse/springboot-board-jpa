0e32f096acc48119d715443668d4e04e3643bab3
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-12-22 20:35:33 +0900

    feat: 필요하다고 느껴지는 속성과 validation을 추가하다.

--- src/main/java/com/prgrms/be/app/domain/dto/PostDTO.java
+++ src/main/java/com/prgrms/be/app/domain/dto/PostDTO.java
@@ -3,9 +3,10 @@ package com.prgrms.be.app.domain.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import javax.validation.constraints.Max;
+import javax.persistence.Column;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 public class PostDTO {
@@ -14,10 +15,11 @@ public class PostDTO {
     @AllArgsConstructor
     public static class CreateRequest {
         @NotBlank(message = "게시글의 제목을 기입해야 합니다.")
-        @Max(value = 20)
+        @Size(min = 1, max = 20)
         private String title;
 
         @NotBlank(message = "게시글의 내용을 기입해야 합니다.")
+        @Column(columnDefinition = "TEXT")
         private String content;
 
         @NotNull
@@ -28,18 +30,27 @@ public class PostDTO {
     @AllArgsConstructor
     public static class UpdateRequest {
         @NotBlank(message = "게시글의 제목을 빈 상태로 수정할 수 없습니다.")
-        @Max(value = 20)
+        @Size(min = 1, max = 20)
         private String title;
 
         @NotBlank(message = "게시글의 내용을 빈 상태로 수정할 수 없습니다.")
+        @Column(columnDefinition = "TEXT")
         private String content;
     }
 
-    public record PostsResponse(String title, Long postId, LocalDateTime createdAt) {
+    public record PostsResponse(
+            String title,
+            Long postId,
+            LocalDateTime createdAt) {
     }
 
-    public record PostDetailResponse(String title, String content, Long postId, LocalDateTime createdAt, Long userId,
-                                     String userName) {
+    public record PostDetailResponse(
+            String title,
+            String content,
+            Long postId,
+            LocalDateTime createdAt,
+            Long userId,
+            String userName) {
     }
 
 
