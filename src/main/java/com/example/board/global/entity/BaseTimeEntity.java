abba09c06aafa865ad084e2d62988071ff1716c7
hyunseo <goatsfish@gmail.com>
2022-12-22 15:38:12 +0900

    feat : 생성 시간, 수정 시간 베이스 엔티티 추가

---
+++ src/main/java/com/example/board/global/entity/BaseTimeEntity.java
@@ -0,0 +1,31 @@
+package com.example.board.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity implements Serializable {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @CreatedBy
+    @Column(name = "created_by")
+    private Long createdBy;
+}
