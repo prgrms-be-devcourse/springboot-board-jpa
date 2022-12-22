857157427829a69b0941ba4f1b198c066a5bce0a
hyunseo <goatsfish@gmail.com>
2022-12-22 15:42:22 +0900

    feat : 메시지 소스 변환 유틸리티 추가

---
+++ src/main/java/com/example/board/util/MessageUtil.java
@@ -0,0 +1,31 @@
+package com.example.board.util;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Resource;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.FieldError;
+
+import java.util.Locale;
+
+@Component
+public class MessageUtil {
+
+    @Resource
+    private MessageSource source;
+
+    static MessageSource messageSource;
+
+    @PostConstruct
+    private void init() {
+        messageSource = source;
+    }
+
+    public static String getMessage(FieldError fieldError) {
+        return messageSource.getMessage(fieldError, Locale.KOREA);
+    }
+
+    public static String getMessage(String message) {
+        return messageSource.getMessage(message, null, "오류 메시지를 찾지 못했습니다.", Locale.KOREA);
+    }
+}
