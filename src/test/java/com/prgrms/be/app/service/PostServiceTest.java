2b2d69cef46be3378578809f5a6e9861d7136050
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-12-22 20:36:03 +0900

    feat: PostService에 대한 테스트를 추가하다.

--- src/test/java/com/prgrms/be/app/service/PostServiceTest.java
+++ src/test/java/com/prgrms/be/app/service/PostServiceTest.java
@@ -1,9 +1,10 @@
-package com.prgrms.be.app.domain;
+package com.prgrms.be.app.service;
 
+import com.prgrms.be.app.domain.Post;
+import com.prgrms.be.app.domain.User;
 import com.prgrms.be.app.domain.dto.PostDTO;
 import com.prgrms.be.app.repository.PostRepository;
 import com.prgrms.be.app.repository.UserRepository;
-import com.prgrms.be.app.service.PostService;
 import com.prgrms.be.app.util.PostConverter;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
@@ -13,11 +14,13 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @Slf4j
@@ -27,10 +30,21 @@ class PostServiceTest {
     private final UserRepository userRepository = mock(UserRepository.class);
     private final PostConverter postConverter = spy(PostConverter.class);
     private final PostService postService = new PostService(postRepository, userRepository, postConverter);
-    //todo : 게시글 작성 테스트
 
     @Test
-    @DisplayName("Post를 생성할 수 있다.")
+    @DisplayName("게시글 생성 시 존재하지 않는 유저 id가 들어올 경우 예외가 발생한다.")
+    void wrongUserIdTest() {
+        // given
+        PostDTO.CreateRequest postCreateDTO = new PostDTO.CreateRequest("title", "content", 1L);
+        // when
+        when(userRepository.findById(anyLong()))
+                .thenReturn(Optional.empty());
+        // then
+        assertThrows(EntityNotFoundException.class, () -> postService.createPost(postCreateDTO));
+    }
+
+    @Test
+    @DisplayName("게시글을 생성할 수 있다.")
     void shouldCreatePost() {
         // given
         PostDTO.CreateRequest postCreateDTO = new PostDTO.CreateRequest("title", "content", 1L);
@@ -75,7 +89,16 @@ class PostServiceTest {
         }
     }
 
-    //todo : 게시글 단건조회 테스트
+    @Test
+    @DisplayName("잘못된 게시글 id로 단건 조회 시 예외가 발생한다.")
+    void wrongPostIdTest() {
+        // given, when
+        when(postRepository.findById(anyLong()))
+                .thenReturn(Optional.empty());
+        // then
+        assertThrows(EntityNotFoundException.class, () -> postService.findById(1L));
+    }
+
     @Test
     @DisplayName("게시글 id로 단건 조회 할 수 있다.")
     void shouldFindPostById() {
@@ -101,7 +124,19 @@ class PostServiceTest {
         );
     }
 
-    //todo : 게시글 수정 테스트
+    @Test
+    @DisplayName("잘못된 게시글 id로 수정 시 예외가 발생한다.")
+    void wrongPostIdUpdateTest() {
+        // given
+        PostDTO.UpdateRequest postUpdateRequest = new PostDTO.UpdateRequest("change title", "change content");
+        // when
+        when(postRepository.findById(anyLong()))
+                .thenReturn(Optional.empty());
+        // then
+        assertThrows(EntityNotFoundException.class, () -> postService.updatePost(1L, postUpdateRequest));
+    }
+
+
     @Test
     @DisplayName("게시글 수정을 할 수 있다.")
     void shouldUpdatePost() {
