56955bd8c8a380eb0a140c2fdd28a2b121a1d916
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2022-12-23 05:04:25 +0900

    add: 전역 예외 처리를 위한 ControllerAdvice 도입

---
+++ src/main/java/com/programmers/kwonjoosung/board/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.programmers.kwonjoosung.board.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler { // 오류 메시지를 어떻게 일관성을 가지고 처리할 것인가?
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> illegalArgumentExceptionHandle(IllegalArgumentException e) {
+        log.error("IllegalArgumentException: {}", e.getMessage(),e);
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> exceptionHandle(Exception e) {
+        log.error("Exception: {}", e.getMessage(),e);
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+}
