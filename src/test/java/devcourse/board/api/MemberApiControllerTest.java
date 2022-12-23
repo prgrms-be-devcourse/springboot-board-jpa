9fdde0470cfd17c617648961cbb1f143f66286f3
Seonho Kim <sunhowold@naver.com>
2022-12-24 04:16:22 +0900

    Feat: REST Docs 테스트 코드 추가

--- src/test/java/devcourse/board/api/MemberApiControllerTest.java
+++ src/test/java/devcourse/board/api/MemberApiControllerTest.java
@@ -5,17 +5,23 @@ import devcourse.board.domain.member.model.MemberRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.JsonFieldType.NUMBER;
+import static org.springframework.restdocs.payload.JsonFieldType.STRING;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 @SpringBootTest
 @Transactional
 class MemberApiControllerTest {
@@ -37,6 +43,16 @@ class MemberApiControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(joinDto)))
                 .andExpect(status().isOk())
-                .andDo(print());
+                .andDo(print())
+                .andDo(document("member-join",
+                        requestFields(
+                                fieldWithPath("name").type(STRING).description("이름"),
+                                fieldWithPath("age").type(NUMBER).description("나이").optional(),
+                                fieldWithPath("hobby").type(STRING).description("취미").optional()
+                        ),
+                        responseFields(
+                                fieldWithPath("description").type(STRING).description("회원 생성 결과"),
+                                fieldWithPath("data").type(NUMBER).description("회원 아이디")
+                        )));
     }
 }
--- src/test/java/devcourse/board/api/PostApiControllerTest.java
+++ src/test/java/devcourse/board/api/PostApiControllerTest.java
@@ -8,16 +8,22 @@ import devcourse.board.domain.post.model.PostRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.JsonFieldType.STRING;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@AutoConfigureRestDocs
 @AutoConfigureMockMvc
 @SpringBootTest
 @Transactional
@@ -48,7 +54,17 @@ class PostApiControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(creationDto)))
                 .andExpect(status().isOk())
-                .andDo(print());
+                .andDo(print())
+                .andDo(document("post-create",
+                        requestFields(
+                                fieldWithPath("memberId").description("회원 식별자"),
+                                fieldWithPath("title").description("게시글 제목"),
+                                fieldWithPath("content").description("게시글 내용")
+                        ),
+                        responseFields(
+                                fieldWithPath("description").description("게시글 생성 결과"),
+                                fieldWithPath("data").description("게시글 식별자")
+                        )));
     }
 
     @Test
@@ -61,7 +77,21 @@ class PostApiControllerTest {
         mockMvc.perform(get("/posts/{postId}", createdPostId)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andDo(print());
+                .andDo(print())
+                .andDo(document("post-get-one",
+                        pathParameters(
+                                parameterWithName("postId").description("게시글 식별자")
+                        ),
+                        responseFields(
+                                fieldWithPath("description").description("게시글 단건 조회 결과"),
+
+                                fieldWithPath("data").description("게시글 정보"),
+                                fieldWithPath("data.postId").description("게시글 식별자"),
+                                fieldWithPath("data.title").description("게시글 제목"),
+                                fieldWithPath("data.content").description("게시글 내용"),
+                                fieldWithPath("data.createdAt").description("게시글 아이디"),
+                                fieldWithPath("data.createdBy").description("게시글 작성자 이름")
+                        )));
     }
 
     @Test
@@ -76,7 +106,22 @@ class PostApiControllerTest {
                         .param("size", String.valueOf(10))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andDo(print());
+                .andDo(print())
+                .andDo(document("post-get-all",
+                        requestParameters(
+                                parameterWithName("page").description("시작 페이지"),
+                                parameterWithName("size").description("조회할 게시글 개수")
+                        ),
+                        responseFields(
+                                fieldWithPath("description").type(STRING).description("게시글 전체 조회 결과"),
+
+                                fieldWithPath("data").description("게시글 정보"),
+                                fieldWithPath("data[].postId").description("게시글 식별자"),
+                                fieldWithPath("data[].title").description("게시글 제목"),
+                                fieldWithPath("data[].content").description("게시글 내용"),
+                                fieldWithPath("data[].createdAt").description("게시글 아이디"),
+                                fieldWithPath("data[].createdBy").description("게시글 작성자 이름")
+                        )));
     }
 
     @Test
@@ -91,7 +136,25 @@ class PostApiControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(updateDto)))
                 .andExpect(status().isOk())
-                .andDo(print());
+                .andDo(print())
+                .andDo(document("post-update",
+                        pathParameters(
+                                parameterWithName("postId").description("게시글 식별자")
+                        ),
+                        requestFields(
+                                fieldWithPath("title").type(STRING).description("게시글 제목"),
+                                fieldWithPath("content").type(STRING).description("게시글 내용")
+                        ),
+                        responseFields(
+                                fieldWithPath("description").description("전체 게시글 조회 결과"),
+
+                                fieldWithPath("data").description("게시글 정보"),
+                                fieldWithPath("data.postId").description("게시글 식별자"),
+                                fieldWithPath("data.title").description("게시글 제목"),
+                                fieldWithPath("data.content").description("게시글 내용"),
+                                fieldWithPath("data.createdAt").description("게시글 아이디"),
+                                fieldWithPath("data.createdBy").description("게시글 작성자 이름")
+                        )));
     }
 
     private Long saveDummyMember(String name) {
