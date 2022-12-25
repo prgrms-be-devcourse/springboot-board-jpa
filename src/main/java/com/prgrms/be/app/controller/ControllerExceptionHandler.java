310d595fdf501a50108383c16fa8e4a3fd22a1d6
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-12-25 18:44:15 +0900

    remove: 예외 상황에서도 동일한 데이터 형식을 반환하기에 필요없는 상속을 삭제하다.

--- src/main/java/com/prgrms/be/app/controller/ControllerExceptionHandler.java
+++ src/main/java/com/prgrms/be/app/controller/ControllerExceptionHandler.java
@@ -4,13 +4,12 @@ import com.prgrms.be.app.domain.dto.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.persistence.EntityNotFoundException;
 
 @Slf4j
 @RestControllerAdvice
-public class ControllerExceptionHandler<T> extends ResponseEntityExceptionHandler {
+public class ControllerExceptionHandler<T> {
 
     @ExceptionHandler(EntityNotFoundException.class)
     protected ApiResponse<String> notFoundErrorHandler(EntityNotFoundException exception) {
