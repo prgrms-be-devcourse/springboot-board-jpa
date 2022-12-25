f01dff6a98cf4ab7ef1b7a74bde93f1e69efb54b
Fortune00 <sinyoungbok99@gmail.com>
2022-12-25 14:22:46 +0900

    refactor: 사용하지 않는 코드 지우기
    - ResponseEntity를 통해 이미 만들어주고 있음

--- src/main/java/kdt/springbootboardjpa/exception/ExceptionResolver.java
+++ src/main/java/kdt/springbootboardjpa/exception/ExceptionResolver.java
@@ -6,12 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 @ControllerAdvice
 public class ExceptionResolver {
     @ExceptionHandler({IllegalArgumentException.class})
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<ErrorResponse> handleForbiddenException(HttpServletRequest request, RuntimeException e){
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), HttpStatus.NOT_FOUND.toString(), request.getRequestURI());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
