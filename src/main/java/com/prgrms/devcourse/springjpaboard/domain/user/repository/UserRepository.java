d33e88ad61ac6f7d93780b424aeccd5f1b1d42b1
GeonWoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2022-12-28 05:19:28 +0900

    Revert "[3기-C 이건우] JPA 게시판 구현 미션 제출합니다."

--- src/main/java/com/prgrms/devcourse/springjpaboard/domain/user/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.prgrms.devcourse.springjpaboard.domain.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.prgrms.devcourse.springjpaboard.domain.user.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- src/main/java/com/prgrms/devcourse/springjpaboard/global/error/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.prgrms.devcourse.springjpaboard.global.error;
-
-public class EntityNotFoundException extends RuntimeException {
-	public EntityNotFoundException(String message) {
-		super(message);
-	}
-}
