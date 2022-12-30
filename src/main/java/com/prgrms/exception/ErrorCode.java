16b8a13a15d1eea36eea54d54fa70a0099715c3f
yewon.seo <dodnjs1241@naver.com>
2022-12-30 17:44:38 +0900

    refactor: 에러 표시 문구 수정

--- src/main/java/com/prgrms/exception/ErrorCode.java
+++ src/main/java/com/prgrms/exception/ErrorCode.java
@@ -6,8 +6,8 @@ public enum ErrorCode {
 
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "파라미터 값을 확인해주세요."),
 
-    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 게시물 ID 입니다."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 유저 ID 입니다."),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 게시물 입니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 유저 입니다."),
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러입니다.");
 
