4cf29aba02c88d727fdb5b8c56d108e867c1268d
kwonjoosung <kwonjoosung@gmail.com>
2023-01-03 05:39:55 +0900

    refactor: 피드백 반영
    * ControllerAdvice -> RestControllerAdvice
    * server 설정 추가

--- src/main/java/com/programmers/kwonjoosung/board/exception/GlobalExceptionHandler.java
+++ src/main/java/com/programmers/kwonjoosung/board/exception/GlobalExceptionHandler.java
@@ -2,11 +2,11 @@ package com.programmers.kwonjoosung.board.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler { // 오류 메시지를 어떻게 일관성을 가지고 처리할 것인가?
 
     @ExceptionHandler(IllegalArgumentException.class)
