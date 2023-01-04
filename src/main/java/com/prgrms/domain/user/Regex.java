f3af4ed47843ff5f70f87ba6aa02655310203068
yewon.seo <dodnjs1241@naver.com>
2023-01-04 22:35:12 +0900

    refactor: Pattern미리 생성

--- src/main/java/com/prgrms/domain/user/Regex.java
+++ src/main/java/com/prgrms/domain/user/Regex.java
@@ -8,15 +8,17 @@ public enum Regex {
     EMAIL_REGEX("[0-9a-zA-Z]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$"),
     PASSWORD_REGEX("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*\\W).{8,20}$");
 
-    private final String rgx;
+    private final Pattern pattern;
 
     Regex(String rgx) {
-        this.rgx = rgx;
+
+        this.pattern = Pattern.compile(rgx);
     }
-    
+
     public boolean match(String subject) {
-        Pattern p = Pattern.compile(rgx);
-        Matcher m = p.matcher(subject);
+
+        Matcher m = pattern.matcher(subject);
         return m.matches();
     }
+
 }
