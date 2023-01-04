ebe1b42aa55dc91f29b5a10c040a12d07baba35f
Seonho Kim <sunhowold@naver.com>
2023-01-05 00:56:04 +0900

    Feat: 회원 API ver2 구현 (세션 사용)

--- src/main/java/devcourse/board/web/api/v2/MemberApiV2.java
+++ src/main/java/devcourse/board/web/api/v2/MemberApiV2.java
@@ -1,4 +1,49 @@
 package devcourse.board.web.api.v2;
 
+
+import devcourse.board.domain.member.MemberService;
+import devcourse.board.domain.member.model.MemberJoinRequest;
+import devcourse.board.domain.member.model.MemberResponse;
+import devcourse.board.web.authentication.SessionManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v2/members")
 public class MemberApiV2 {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    private final SessionManager sessionManager;
+
+    private final MemberService memberService;
+
+    public MemberApiV2(SessionManager sessionManager, MemberService memberService) {
+        this.sessionManager = sessionManager;
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/my-page")
+    public ResponseEntity<MemberResponse> getMember(
+            HttpServletRequest request
+    ) {
+        Long loginMemberId = sessionManager.getLoginMemberIdentifier(request);
+        return ResponseEntity.ok()
+                .body(memberService.getMember(loginMemberId));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> join(
+            @Valid @RequestBody MemberJoinRequest joinRequest
+    ) {
+        memberService.join(joinRequest);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .build();
+    }
 }
---
+++ src/test/java/devcourse/board/web/api/v2/MemberApiV2Test.java
@@ -0,0 +1,82 @@
+package devcourse.board.web.api.v2;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import devcourse.board.domain.member.MemberRepository;
+import devcourse.board.domain.member.model.Member;
+import devcourse.board.domain.member.model.MemberJoinRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.JsonFieldType.NUMBER;
+import static org.springframework.restdocs.payload.JsonFieldType.STRING;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureRestDocs
+@AutoConfigureMockMvc
+@SpringBootTest
+@Transactional
+class MemberApiV2Test {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("회원 가입 성공")
+    void join() throws Exception {
+        // given
+        MemberJoinRequest joinRequest =
+                new MemberJoinRequest("email@email.com", "password", "name");
+
+        // when & then
+        mockMvc.perform(post("/api/v2/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(joinRequest)))
+                .andExpect(status().isCreated())
+                .andDo(print())
+                .andDo(document("member-join-v2",
+                        requestFields(
+                                fieldWithPath("email").type(STRING).description("이메일"),
+                                fieldWithPath("password").type(STRING).description("비밀번호"),
+                                fieldWithPath("name").type(STRING).description("이름"),
+                                fieldWithPath("age").type(NUMBER).description("나이").optional(),
+                                fieldWithPath("hobby").type(STRING).description("취미").optional()
+                        )));
+    }
+
+    @Test
+    @DisplayName("로그인 하지 않은 사용자는 자신의 정보를 조회할 수 없다.")
+    void should_return_unauthorized_when_non_login_member_access_own_info() throws Exception {
+        // given
+        Member member = Member.create("email@email.com", "password", "name");
+        memberRepository.save(member);
+
+        // when & then
+        mockMvc.perform(get("/api/v2/members/my-page", member.getId()))
+                .andExpect(status().isUnauthorized())
+                .andDo(print())
+                .andDo(document("member-my-page-v2",
+                        responseFields(
+                                fieldWithPath("errorMessage").type(STRING).description("회원 정보 조회 불가 사유")
+                        )
+                ));
+    }
+}
