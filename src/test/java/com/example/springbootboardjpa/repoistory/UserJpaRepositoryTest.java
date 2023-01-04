fa94a497c7856559f67cbfaba95b78d572c74d77
youngji <youngji.jang804@gmail.com>
2023-01-05 01:43:19 +0900

    style: 주석 삭제

--- src/test/java/com/example/springbootboardjpa/repoistory/UserJpaRepositoryTest.java
+++ src/test/java/com/example/springbootboardjpa/repoistory/UserJpaRepositoryTest.java
@@ -8,12 +8,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
-// @Import(JpaAuditConfig.class)
 @DataJpaTest
 @Slf4j
 class UserJpaRepositoryTest {
