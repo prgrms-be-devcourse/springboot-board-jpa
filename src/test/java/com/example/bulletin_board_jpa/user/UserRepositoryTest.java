e861bd1b5dbc66498659b71071d4ae27822c19aa
Albatross3 <superstring77@gmail.com>
2023-01-05 00:34:26 +0900

    gitignore 추가

---
+++ src/test/java/com/example/bulletin_board_jpa/user/UserRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.example.bulletin_board_jpa.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    void insert() {
+        User user1 = new User();
+        user1.setName("이동준");
+        user1.setAge(28);
+        user1.setHobby("기타 치기");
+
+        userRepository.save(user1);
+
+        User user2 = new User();
+        user2.setName("이유진");
+        user2.setAge(25);
+        user2.setHobby("놀기");
+
+        userRepository.save(user2);
+
+        List<User> list = userRepository.findAll();
+        for (User user : list) {
+            System.out.println(user.getId());
+        }
+    }
+}
