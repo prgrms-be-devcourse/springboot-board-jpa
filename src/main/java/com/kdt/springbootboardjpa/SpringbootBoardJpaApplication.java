786b66833931aaf6dd7dc201fe4fd3040b590fc6
hikarigin99 <hikarigin99@gmail.com>
2023-01-06 02:25:19 +0900

    add: auditing

--- src/main/java/com/kdt/springbootboardjpa/SpringbootBoardJpaApplication.java
+++ src/main/java/com/kdt/springbootboardjpa/SpringbootBoardJpaApplication.java
@@ -2,12 +2,12 @@ package com.kdt.springbootboardjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringbootBoardJpaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringbootBoardJpaApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/kdt/springbootboardjpa/global/common/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.kdt.springbootboardjpa.global.common;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(name = "created_by")
+    private Long createdBy;  // 0
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
