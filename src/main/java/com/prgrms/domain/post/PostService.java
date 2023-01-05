3b5b4b962997aa754708ca7d4df5060763b2d68e
yewon.seo <dodnjs1241@naver.com>
2023-01-06 00:22:09 +0900

    refactor: 순환참조 문제 dto로 해결

--- src/main/java/com/prgrms/domain/post/PostService.java
+++ src/main/java/com/prgrms/domain/post/PostService.java
@@ -1,12 +1,14 @@
 package com.prgrms.domain.post;
 
+import static com.prgrms.dto.PostDto.Request;
+import static com.prgrms.dto.PostDto.Update;
+
 import com.prgrms.domain.user.User;
 import com.prgrms.domain.user.UserService;
-import com.prgrms.dto.PostDto;
 import com.prgrms.dto.PostDto.Response;
 import com.prgrms.dto.PostDto.ResponsePostDtos;
-import com.prgrms.exception.ErrorCode;
-import com.prgrms.exception.customException.PostNotFoundException;
+import com.prgrms.global.error.ErrorCode;
+import com.prgrms.global.exception.PostNotFoundException;
 import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
@@ -32,39 +34,44 @@ public class PostService {
     public Response findPostById(Long id) {
 
         return postRepository.findById(id)
-            .map(Response::new)
+            .map(Response::from)
             .orElseThrow(() -> new PostNotFoundException("id: " + id, ErrorCode.POST_NOT_FOUND));
     }
 
     @Transactional
-    public Response insertPost(@Valid PostDto.Request postDto) {
+    public Response insertPost(Long userId, @Valid Request postDto) {
 
-        User user = userService.findUserById(postDto.userId()).toUser();
+        User user = userService.findUserById(userId).toUser();
         Post saved = postRepository.save(postDto.toPost(user));
 
-        return new Response(saved);
+        return Response.from(saved);
 
     }
 
     @Transactional
-    public Response updatePost(Long id, @Valid PostDto.Update postDto) {
+    public Response updatePost(Long id, @Valid Update postDto) {
 
         Post post = postRepository.findById(id)
             .orElseThrow(() -> new PostNotFoundException("id: " + id, ErrorCode.POST_NOT_FOUND));
 
         post.update(postDto.content(), postDto.title());
 
-        return new Response(post);
+        return Response.from(post);
     }
 
     @Transactional(readOnly = true)
     public ResponsePostDtos getPostsByPage(Pageable pageable) {
 
         List<Response> responses = postRepository.findAll(pageable)
-            .map(Response::new)
+            .map(Response::from)
             .stream().toList();
 
         return new ResponsePostDtos(responses);
+    }
+
+    @Transactional
+    public void deletePost(Long id) {
+        postRepository.deleteById(id);
     }
 
 }
--- src/main/java/com/prgrms/dto/PostDto.java
+++ src/main/java/com/prgrms/dto/PostDto.java
@@ -3,22 +3,26 @@ package com.prgrms.dto;
 import com.prgrms.domain.post.Post;
 import com.prgrms.domain.user.User;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 public class PostDto {
 
-    public record Request(@NotBlank String title, @NotBlank String content, @NotNull Long userId) {
+    public record Request(@NotBlank String title, @NotBlank String content) {
 
         public Post toPost(User user) {
             return new Post(title, content, user);
         }
     }
 
-    public record Response(Post post) {
+    public record Response(Long id, String title, String content, Long userId, String userName) {
+
+        public static Response from(Post post) {
+            return new Response(post.getId(), post.getTitle(), post.getContent(),
+                post.getUser().getId(), post.getUser().getName());
+        }
 
         public long getPostId() {
-            return post.getId();
+            return id;
         }
     }
 
