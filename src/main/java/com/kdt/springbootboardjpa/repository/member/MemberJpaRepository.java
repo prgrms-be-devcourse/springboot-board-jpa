a27e13e6040863d6f26b52e6db61174b8593b83b
hikarigin99 <hikarigin99@gmail.com>
2023-01-06 02:41:07 +0900

    add: all Commit

--- src/main/java/com/kdt/springbootboardjpa/repository/member/MemberJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kdt.springbootboardjpa.repository.member;
-
-import com.kdt.springbootboardjpa.domain.member.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberJpaRepository extends JpaRepository<Member, Long> {
-}
--- src/main/java/com/kdt/springbootboardjpa/repository/post/PostJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kdt.springbootboardjpa.repository.post;
-
-import com.kdt.springbootboardjpa.domain.post.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostJpaRepository extends JpaRepository<Post, Long> {
-}
