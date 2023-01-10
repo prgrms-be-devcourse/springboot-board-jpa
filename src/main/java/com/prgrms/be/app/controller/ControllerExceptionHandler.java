272e7bcf6ba41a9cee924d63c3d9e0ab724b39f9
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-01-10 10:32:44 +0900

    fix: Http 상태 코드를 스프링에 존재하는 상수를 이용하게 수정하다.

--- src/main/java/com/prgrms/be/app/controller/ControllerExceptionHandler.java
+++ src/main/java/com/prgrms/be/app/controller/ControllerExceptionHandler.java
@@ -2,6 +2,7 @@ package com.prgrms.be.app.controller;
 
 import com.prgrms.be.app.domain.dto.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,19 +15,18 @@ public class ControllerExceptionHandler<T> {
     @ExceptionHandler(EntityNotFoundException.class)
     protected ApiResponse<String> notFoundErrorHandler(EntityNotFoundException exception) {
         log.warn("EntityNotFoundException : ", exception);
-        return ApiResponse.fail(404, exception.getMessage(), ResponseMessage.ERRORED);
+        return ApiResponse.fail(HttpStatus.NOT_FOUND.value(), exception.getMessage(), ResponseMessage.ERRORED);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     protected ApiResponse<String> invalidParameterErrorHandler(IllegalArgumentException exception) {
         log.warn("IllegalArgumentException : ", exception);
-        return ApiResponse.fail(400, exception.getMessage(), ResponseMessage.ERRORED);
+        return ApiResponse.fail(HttpStatus.BAD_REQUEST.value(), exception.getMessage(), ResponseMessage.ERRORED);
     }
 
     @ExceptionHandler(Exception.class)
     protected ApiResponse<String> internalServerErrorHandler(Exception exception) {
         log.error("IntervalServerError : ", exception);
-        return ApiResponse.fail(500, exception.getMessage(), ResponseMessage.ERRORED);
+        return ApiResponse.fail(HttpStatus.INTERNAL_SERVER_ERROR.value(), exception.getMessage(), ResponseMessage.ERRORED);
     }
-
 }
