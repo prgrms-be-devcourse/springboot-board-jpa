c47a2c366dca415ebb406226ece59027a0840550
goseungwon <knmy0925@gmail.com>
2023-01-11 20:45:16 +0900

    [Style] @Service어노테이션으로 변경

--- src/main/java/com/prgrms/boardjpa/posts/facade/PostFacade.java
+++ src/main/java/com/prgrms/boardjpa/posts/facade/PostFacade.java
@@ -1,12 +1,13 @@
-package com.prgrms.boardjpa.posts.service;
+package com.prgrms.boardjpa.posts.facade;
 
 import com.prgrms.boardjpa.posts.dto.PostDto;
 import com.prgrms.boardjpa.posts.dto.PostRequest;
+import com.prgrms.boardjpa.posts.service.PostService;
 import com.prgrms.boardjpa.users.service.UserService;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Component
+@Service
 public class PostFacade {
 
   private final PostService postService;
@@ -18,6 +19,7 @@ public class PostFacade {
     this.userService = userService;
   }
 
+  //user를 조회하고 하는 것은 정석적인 방법 and 검증의 기회
   @Transactional
   public PostDto createPost(PostRequest postRequest) {
     userService.getUser(postRequest.getUserDto().getUserId());
@@ -27,7 +29,7 @@ public class PostFacade {
   @Transactional
   public PostDto updatePost(Long postId, PostRequest postRequest) {
     userService.getUser(postRequest.getUserDto().getUserId());
-    return postService.createPost(postRequest);
+    return postService.updatePost(postId, postRequest);
   }
 
 }
