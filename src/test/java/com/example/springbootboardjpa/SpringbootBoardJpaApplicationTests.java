1744975c0acaa6ada2721da9dc23c3a50172b296
cloudwi <cloudwi@naver.com>
2023-01-11 18:29:48 +0900

    실행을 위한 파일 업로드

---
+++ src/test/java/com/example/springbootboardjpa/SpringbootBoardJpaApplicationTests.java
@@ -0,0 +1,8 @@
+package com.example.springbootboardjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBoardJpaApplicationTests {
+}
