d644c638d2b52531cdb1450e63a335c76cd1e700
cloudwi <cloudwi@naver.com>
2023-01-11 18:19:54 +0900

    log를 모든 매서드마다 확인하기 위해서 aop를 적용하요 log를 찍음

---
+++ src/main/java/com/example/springbootboardjpa/global/aop/CommonPointcut.java
@@ -0,0 +1,8 @@
+package com.example.springbootboardjpa.global.aop;
+
+import org.aspectj.lang.annotation.Pointcut;
+
+public class CommonPointcut {
+    @Pointcut("execution(public * com.example.springbootboardjpa..*.*(..))")
+    public void packagePublicMethodPointcut() {}
+}
