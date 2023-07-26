8ed8874bd7f02db1e84778132e7693ccda131dee
Hongseob Lee <sjun2918@naver.com>
2023-07-26 20:21:25 +0900

    feat : 생성,수정일을 공통으로 제공하는 BaseEntity 구현

--- src/main/java/com/prgrms/boardjpa/BoardJpaApplication.java
+++ src/main/java/com/prgrms/boardjpa/BoardJpaApplication.java
@@ -2,12 +2,14 @@ package com.prgrms.boardjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BoardJpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BoardJpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BoardJpaApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/prgrms/boardjpa/global/domain/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.prgrms.boardjpa.global.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
