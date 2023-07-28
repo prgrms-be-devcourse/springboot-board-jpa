8dcdc691502457eed8bb1a6ed9e3d21d37e59287
Bombo <bombo96@chungwoon.ac.kr>
2023-07-28 22:34:54 +0900

    refactor : 반환 값에 생성 시간 추가

--- src/main/java/com/prgms/jpaBoard/domain/user/application/dto/UserResponse.java
+++ src/main/java/com/prgms/jpaBoard/domain/user/application/dto/UserResponse.java
@@ -3,14 +3,17 @@ package com.prgms.jpaBoard.domain.user.application.dto;
 import com.prgms.jpaBoard.domain.user.HobbyType;
 import com.prgms.jpaBoard.domain.user.User;
 
+import java.time.ZonedDateTime;
+
 public record UserResponse(
         Long id,
         String name,
         int age,
-        HobbyType hobby
+        HobbyType hobby,
+        ZonedDateTime createdAt
 ) {
 
     public UserResponse(User user) {
-        this(user.getId(), user.getName(), user.getAge(), user.getHobby());
+        this(user.getId(), user.getName(), user.getAge(), user.getHobby(), user.getCreatedAt());
     }
 }
