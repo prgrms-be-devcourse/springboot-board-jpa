8883323600b11c8b08a498ba9633ed1b7eec4087
BeommoKoo-dev <95630007+BeommoKoo-dev@users.noreply.github.com>
2023-07-29 20:31:43 +0900

    feat : BaseEntity 구현

---
+++ MyBoard/src/main/java/org/prgrms/myboard/domain/BaseEntity.java
@@ -0,0 +1,21 @@
+package org.prgrms.myboard.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
