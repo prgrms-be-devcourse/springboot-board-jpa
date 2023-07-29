3bfb5c240c38485bf5136f7e6d5644511bbbe22c
BeommoKoo-dev <95630007+BeommoKoo-dev@users.noreply.github.com>
2023-07-29 21:52:18 +0900

    feat : 작성자 기반 find메소드 구현

--- MyBoard/src/main/java/org/prgrms/myboard/repository/PostRepository.java
+++ MyBoard/src/main/java/org/prgrms/myboard/repository/PostRepository.java
@@ -4,6 +4,9 @@ import org.prgrms.myboard.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByCreatedBy(String author);
 }
