08c4a9426f79470aa5a788eb28bd71f56120f513
Bombo <bombo96@chungwoon.ac.kr>
2023-07-29 23:09:21 +0900

    feat : Spring Rest Docs 적용

--- README.md
+++ README.md
@@ -1,15 +1,46 @@
-# [미션] Spring Boot JPA로 게시판 구현
+요구 사항
+# **요구 사항**
 
-## 미션 소개 😎
-Spring Boot JPA - Rest API를 강의를 듣고, 게시판 구현 미션을 수행해봅시다.
+### **SpringDataJPA 를 설정한다.**
 
-## 이곳은 공개 Repo입니다.
-1. 여러분의 포트폴리오로 사용하셔도 됩니다.
-2. 때문에 이 repo를 fork한 뒤
-3. 여러분의 개인 Repo에 작업하며 
-4. 이 Repo에 PR을 보내어 멘토의 코드 리뷰와 피드백을 받으세요.
+- datasource : h2 or mysql
 
-## Branch 명명 규칙
-1.  여러분 repo는 알아서 해주시고 😀(본인 레포니 main으로 하셔두 되져)
-2.  prgrms-be-devcourse/spring-board 레포로 PR시 branch는 본인 username을 적어주세요 :)  
-base repo : `여기repo` base : `username` ← head repo : `여러분repo` compare : `main`
+### **엔티티를 구성한다**
+- [X] 회원(User)
+    - id (PK) (auto increment)
+    - name
+    - age
+    - hobby
+    - **created_at**
+    - **created_by**
+- [X] 게시글(Post)
+    - id (PK) (auto increment)
+    - title
+    - content
+    - **created_at**
+    - **created_by**
+- [X] 회원과 게시글에 대한 연관관계를 설정한다.
+    - 회원과 게시글은 1:N 관계이다.
+- [X] 게시글 Repository를 구현한다. (PostRepository)
+
+### **API를 구현한다.**
+
+- [X] 게시글 조회
+    - 페이징 조회 (GET "/posts")
+    - 단건 조회 (GET "/posts/{id}")
+- [X] 게시글 작성 (POST "/posts")
+- [X] 게시글 수정 (POST "/posts/{id}")
+
+### **REST-DOCS를 이용해서 문서화한다.**
+- [X] 게시글 조회
+    - 페이징 조회 (GET "/posts")
+    - 단건 조회 (GET "/posts/{id}")
+- [X] 게시글 작성 (POST "/posts")
+- [X] 게시글 수정 (POST "/posts/{id}")
+
+
+---
+### 테스트 컨벤션
+테스트 할 기능만 작성하자.
+테스트 메서드를 식별하는 방법으로 주석을 사용하고 있기 때문에, 테스트 할 기능만 간단하게 쓰는 것이 더 낫다는 의견이 많다. 
+또한 코드의 악취를 방지하고, 문서화 된 형태의 유닛 테스트를 수행하므로 권장되는 방법이다.
---
+++ src/test/java/com/prgms/jpaBoard/domain/post/presentation/PostControllerRestDocsTest.java
@@ -0,0 +1,241 @@
+package com.prgms.jpaBoard.domain.post.presentation;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.prgms.jpaBoard.domain.post.application.PostService;
+import com.prgms.jpaBoard.domain.post.application.dto.PostResponse;
+import com.prgms.jpaBoard.domain.post.application.dto.PostResponses;
+import com.prgms.jpaBoard.domain.post.presentation.dto.PostSaveRequest;
+import com.prgms.jpaBoard.domain.post.presentation.dto.PostUpdateRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@WebMvcTest(PostController.class)
+@ExtendWith(RestDocumentationExtension.class)
+public class PostControllerRestDocsTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PostService postService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setup(
+            WebApplicationContext webApplicationContext,
+            RestDocumentationContextProvider restDocumentationContextProvider
+    ) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .apply(documentationConfiguration(restDocumentationContextProvider)
+                        .operationPreprocessors()
+                        .withRequestDefaults(modifyUris().host("localhost").port(8080), prettyPrint())
+                        .withResponseDefaults(modifyUris().host("localhost").port(8080), prettyPrint()))
+                .build();
+    }
+
+    @Test
+    @DisplayName("게시글을 생성 할 수 있다.")
+    void createPost() throws Exception {
+        // Given
+        PostSaveRequest postSaveRequest = new PostSaveRequest("제목", "내용", 1L);
+
+        given(postService.post(postSaveRequest)).willReturn(1L);
+
+        // When
+
+        // Then
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(postSaveRequest))
+                        .characterEncoding("UTF-8"))
+                .andDo(print())
+                .andDo(document("create-post",
+                        requestFields(
+                                fieldWithPath("title").type(JsonFieldType.STRING).description("제목"),
+                                fieldWithPath("content").type(JsonFieldType.STRING).description("내용"),
+                                fieldWithPath("userId").type(JsonFieldType.NUMBER).description("회원 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("statusCode").type(JsonFieldType.NUMBER).description("상태코드"),
+                                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("게시글 id"),
+                                fieldWithPath("serverDateTime").type(JsonFieldType.STRING).description("서버 시간")
+                        )));
+    }
+
+    @Test
+    @DisplayName("게시글 하나를 조회할 수 있다.")
+    void readPost() throws Exception {
+        // Given
+        PostResponse postResponse = new PostResponse(
+                1L,
+                "title",
+                "content",
+                ZonedDateTime.now(ZoneId.of("Asia/Seoul")),
+                "Writer");
+
+        given(postService.readPost(1L)).willReturn(postResponse);
+
+        // When
+
+
+        // Then
+        mockMvc.perform(get("/api/v1/posts/{id}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8"))
+                .andDo(print())
+                .andDo(document("read-post",
+                        responseFields(
+                                fieldWithPath("statusCode").type(JsonFieldType.NUMBER).description("상태코드"),
+                                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("게시글 ID"),
+                                fieldWithPath("data.title").type(JsonFieldType.STRING).description("게시글 제목"),
+                                fieldWithPath("data.content").type(JsonFieldType.STRING).description("게시글 내용"),
+                                fieldWithPath("data.createdAt").type(JsonFieldType.STRING).description("작성 시간"),
+                                fieldWithPath("data.createdBy").type(JsonFieldType.STRING).description("작성자 이름"),
+                                fieldWithPath("serverDateTime").type(JsonFieldType.STRING).description("서버 시간")
+                        )));
+    }
+
+    @Test
+    @DisplayName("게시글 전체를 조회할 수 있다.")
+    void readPosts() throws Exception {
+        // Given
+        PostResponse postResponseA = new PostResponse(
+                1L,
+                "titleA",
+                "contentA",
+                ZonedDateTime.now(ZoneId.of("Asia/Seoul")),
+                "Writer");
+
+        PostResponse postResponseB = new PostResponse(
+                2L,
+                "titleB",
+                "contentB",
+                ZonedDateTime.now(ZoneId.of("Asia/Seoul")),
+                "Writer");
+
+        PostResponse postResponseC = new PostResponse(
+                3L,
+                "titleC",
+                "contentC",
+                ZonedDateTime.now(ZoneId.of("Asia/Seoul")),
+                "Writer");
+
+        PostResponse postResponseD = new PostResponse(
+                4L,
+                "titleD",
+                "contentD",
+                ZonedDateTime.now(ZoneId.of("Asia/Seoul")),
+                "Writer");
+
+        PageRequest pageRequest = PageRequest.of(0, 20, Sort.by(Sort.Direction.DESC, "id"));
+
+        given(postService.readAllPost(pageRequest))
+                .willReturn(new PostResponses(List.of(postResponseA, postResponseB, postResponseC, postResponseD)));
+
+        // When
+
+
+        // Then
+        mockMvc.perform(get("/api/v1/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")
+                        .param("page", "0")
+                        .param("size", "20")
+                        .param("sort", "id,desc"))
+                .andDo(print())
+                .andDo(document("read-posts",
+                        responseFields(
+                                fieldWithPath("statusCode").type(JsonFieldType.NUMBER)
+                                        .description("상태코드"),
+                                fieldWithPath("data.postResponses[].id").type(JsonFieldType.NUMBER)
+                                        .description("게시글 ID"),
+                                fieldWithPath("data.postResponses[].title").type(JsonFieldType.STRING)
+                                        .description("게시글 제목"),
+                                fieldWithPath("data.postResponses[].content").type(JsonFieldType.STRING)
+                                        .description("게시글 내용"),
+                                fieldWithPath("data.postResponses[].createdAt").type(JsonFieldType.STRING)
+                                        .description("작성 시간"),
+                                fieldWithPath("data.postResponses[].createdBy").type(JsonFieldType.STRING)
+                                        .description("작성자 이름"),
+                                fieldWithPath("serverDateTime").type(JsonFieldType.STRING)
+                                        .description("서버 시간")
+                        )));
+    }
+
+    @Test
+    @DisplayName("게시글을 수정할 수 있다.")
+    void updatePost() throws Exception {
+        // Given
+        PostUpdateRequest postUpdateRequest = new PostUpdateRequest("ModifiedTitle", "ModifiedContent");
+
+        PostResponse postResponse = new PostResponse(
+                1L,
+                "ModifiedTitle",
+                "ModifiedContent",
+                ZonedDateTime.now(ZoneId.of("Asia/Seoul")),
+                "Writer"
+        );
+
+        given(postService.updatePost(1L, postUpdateRequest)).willReturn(postResponse);
+
+        // When
+
+        // Then
+        mockMvc.perform(post("/api/v1/posts/{id}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(postUpdateRequest))
+                        .characterEncoding("UTF-8"))
+                .andDo(print())
+                .andDo(document("update-post",
+                        requestFields(
+                                fieldWithPath("title").type(JsonFieldType.STRING).description("게시글 제목"),
+                                fieldWithPath("content").type(JsonFieldType.STRING).description("게시글 내용")
+                        ),
+                        responseFields(
+                                fieldWithPath("statusCode").type(JsonFieldType.NUMBER).description("상태코드"),
+                                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("게시글 ID"),
+                                fieldWithPath("data.title").type(JsonFieldType.STRING).description("게시글 제목"),
+                                fieldWithPath("data.content").type(JsonFieldType.STRING).description("게시글 내용"),
+                                fieldWithPath("data.createdAt").type(JsonFieldType.STRING).description("작성 시간"),
+                                fieldWithPath("data.createdBy").type(JsonFieldType.STRING).description("작성자 이름"),
+                                fieldWithPath("serverDateTime").type(JsonFieldType.STRING).description("서버 시간")
+                        )));
+    }
+}
