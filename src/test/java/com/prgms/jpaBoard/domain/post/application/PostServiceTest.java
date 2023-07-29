da1d8c7a2db41d5866a2e161b2d13a6767e8de37
Bombo <bombo96@chungwoon.ac.kr>
2023-07-29 23:00:04 +0900

    fix : 테스트 메서드에 트랜잭션을 걸지 않아 롤백되지 않는 문제 해결

--- src/test/java/com/prgms/jpaBoard/domain/post/application/PostServiceTest.java
+++ src/test/java/com/prgms/jpaBoard/domain/post/application/PostServiceTest.java
@@ -13,10 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class PostServiceTest {
 
     @Autowired
