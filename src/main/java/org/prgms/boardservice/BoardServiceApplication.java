0b89f5d61926ab6ee72248190fc528aad1397127
hyeon-z <phj8221@naver.com>
2023-07-30 15:59:10 +0900

    feat: 프로젝트 세팅

---
+++ src/main/java/org/prgms/boardservice/BoardServiceApplication.java
@@ -0,0 +1,13 @@
+package org.prgms.boardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardServiceApplication.class, args);
+    }
+
+}
