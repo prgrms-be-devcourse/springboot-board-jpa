732add59014cf27c73bfb3471f518898f7de7cb9
박은지 <qkrdmswl1018@naver.com>
2023-07-30 15:18:51 +0900

    feat: email, password로 User 조회하는 기능 추가

--- src/main/java/dev/jpaboard/user/repository/UserRepository.java
+++ src/main/java/dev/jpaboard/user/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package dev.jpaboard.user.repository;
 
-import org.apache.catalina.User;
+import dev.jpaboard.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByEmailAndPassword(String email, String password);
 }
