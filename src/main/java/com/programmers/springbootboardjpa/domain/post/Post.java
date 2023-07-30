9185d30339e58d343965765b47abc25e185c347e
changhyeon <changhyeon.h@kakao.com>
2023-07-30 17:44:02 +0900

    feat: Post도메인과 User도메인 연관관계 설정

--- src/main/java/com/programmers/springbootboardjpa/domain/post/Post.java
+++ src/main/java/com/programmers/springbootboardjpa/domain/post/Post.java
@@ -1,11 +1,15 @@
 package com.programmers.springbootboardjpa.domain.post;
 
 import com.programmers.springbootboardjpa.domain.BaseEntity;
+import com.programmers.springbootboardjpa.domain.user.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,5 +30,9 @@ public class Post extends BaseEntity {
 
     @Column(name = "content", columnDefinition = "TEXT")
     private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(columnDefinition = "user_id", referencedColumnName = "id")
+    private User user;
 
 }
