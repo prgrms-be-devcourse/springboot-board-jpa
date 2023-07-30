107821e9432fdf9e10b55c8d9ae30f0cf931092b
changhyeon <changhyeon.h@kakao.com>
2023-07-30 17:57:56 +0900

    feat: Post, User Repository 구현

---
+++ src/main/java/com/programmers/springbootboardjpa/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.programmers.springbootboardjpa.repository;
+
+import com.programmers.springbootboardjpa.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
