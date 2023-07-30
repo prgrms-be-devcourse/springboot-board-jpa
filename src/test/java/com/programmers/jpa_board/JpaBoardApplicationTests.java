83e877f4a1b4dc5a42660e11f4594bb9a10079fa
beombu <beombu13@gmail.com>
2023-07-30 21:02:29 +0900

    :tada: 프로젝트 생성

---
+++ src/test/java/com/programmers/jpa_board/JpaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.jpa_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
