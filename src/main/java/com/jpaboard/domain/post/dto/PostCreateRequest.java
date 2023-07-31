50f0b39da9bb499a1ce1fd45e8f3776d7b290eca
weonest <geonhee33@naver.com>
2023-07-31 22:49:21 +0900

    refactor: Long 타입에 대한 Valid 어노테이션 수정

--- src/main/java/com/jpaboard/domain/post/dto/PostCreateRequest.java
+++ src/main/java/com/jpaboard/domain/post/dto/PostCreateRequest.java
@@ -1,11 +1,10 @@
 package com.jpaboard.domain.post.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record PostCreateRequest(
-        @NotBlank(message = "비회원은 게시글 작성 기능을 이용할 수 없습니다.")
+        @NotNull(message = "비회원은 게시글 작성 기능을 이용할 수 없습니다.")
         Long userId,
 
         @Size(min = 1, message = "제목은 한 글자 이상입니다.")
