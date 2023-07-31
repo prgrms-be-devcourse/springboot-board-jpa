1ef517bce4129b832ce33a7b3d8b51e8bdbf47f4
weonest <geonhee33@naver.com>
2023-07-31 22:09:51 +0900

    refactor: PostServiceImplTest 리팩토링

--- src/test/java/com/jpaboard/domain/post/application/PostServiceImplTest.java
+++ src/test/java/com/jpaboard/domain/post/application/PostServiceImplTest.java
@@ -8,15 +8,14 @@ import com.jpaboard.domain.user.dto.request.UserCreationRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest
 class PostServiceImplTest {
