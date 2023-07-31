d9519aad0160137e007c3add1a4056516c6d1c47
kmebin <blowhuiee@gmail.com>
2023-07-31 18:23:43 +0900

    feat: 전역 예외 처리

--- src/main/java/com/prgrms/board/global/common/BaseResponse.java
+++ src/main/java/com/prgrms/board/global/common/BaseResponse.java
@@ -3,31 +3,39 @@ package com.prgrms.board.global.common;
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.*;
 import static org.springframework.http.HttpStatus.*;
 
+import org.springframework.http.HttpStatus;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class BaseResponse<T> {
 
-	private final int status;
-	private final String message;
+    private final int status;
+    private final String message;
 
-	@JsonInclude(NON_NULL)
-	private T data;
+    @JsonInclude(NON_NULL)
+    private T data;
 
-	public static BaseResponse<Object> ok(SuccessMessage message) {
-		return new BaseResponse<>(OK.value(), message.getValue());
-	}
+    public static <T> BaseResponse<T> ok(SuccessMessage message, T data) {
+        return new BaseResponse<>(OK.value(), message.getValue(), data);
+    }
 
-	public static <T> BaseResponse<T> ok(SuccessMessage message, T data) {
-		return new BaseResponse<>(OK.value(), message.getValue(), data);
-	}
+    public static <T> BaseResponse<T> created(SuccessMessage message, T data) {
+        return new BaseResponse<>(CREATED.value(), message.getValue(), data);
+    }
 
-	public static <T> BaseResponse<T> created(SuccessMessage message, T data) {
-		return new BaseResponse<>(CREATED.value(), message.getValue(), data);
-	}
+    public static BaseResponse<Object> error(ErrorCode error) {
+        return new BaseResponse<>(error.getStatusCode(), error.getMessage());
+    }
+
+    public static BaseResponse<Object> error(HttpStatus status, String message) {
+        return new BaseResponse<>(status.value(), message);
+    }
 }
--- src/main/java/com/prgrms/board/global/exception/CustomException.java
+++ src/main/java/com/prgrms/board/global/exception/CustomException.java
@@ -2,6 +2,9 @@ package com.prgrms.board.global.exception;
 
 import com.prgrms.board.global.common.ErrorCode;
 
+import lombok.Getter;
+
+@Getter
 public class CustomException extends RuntimeException {
 
 	private final ErrorCode error;
