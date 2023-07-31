74ea22b2c0e98c7611b8a4043a84c2842c3c6379
changhyeon <changhyeon.h@kakao.com>
2023-07-31 20:26:22 +0900

    feat: UserService와 User관련 dto 구현

---
+++ src/main/java/com/programmers/springbootboardjpa/service/UserService.java
@@ -0,0 +1,64 @@
+package com.programmers.springbootboardjpa.service;
+
+import com.programmers.springbootboardjpa.domain.user.User;
+import com.programmers.springbootboardjpa.dto.UserCreateRequest;
+import com.programmers.springbootboardjpa.dto.UserResponse;
+import com.programmers.springbootboardjpa.dto.UserUpdateRequest;
+import com.programmers.springbootboardjpa.mapper.user.UserMapper;
+import com.programmers.springbootboardjpa.repository.UserRepository;
+import java.util.List;
+import java.util.NoSuchElementException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Transactional
+    public void save(UserCreateRequest userCreateRequest) {
+        User user = userMapper.toUser(userCreateRequest);
+        user.setCreatedBy(user.getName());
+
+        userRepository.save(user);
+    }
+
+    public UserResponse findById(Long id) {
+        User user = findById(id, "찾으시려는 아이디가 없습니다.");
+
+        return userMapper.toUserResponse(user);
+    }
+
+    public List<UserResponse> findAll(Pageable pageable) {
+        return userRepository.findAllByOrderByNameAsc(pageable)
+                .map(userMapper::toUserResponse)
+                .toList();
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        findById(id, "삭제하려는 아이디가 없습니다.");
+
+        userRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void updateById(Long id, UserUpdateRequest userUpdateRequest) {
+        User user = findById(id, "업데이트하려는 아이디가 없습니다.");
+
+        user.changeName(userUpdateRequest.getName());
+        user.changeHobby(userUpdateRequest.getHobby());
+    }
+
+    private User findById(Long id, String exceptionMessage) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException(exceptionMessage));
+    }
+
+}
