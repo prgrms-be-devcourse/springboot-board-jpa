860421f8f8a5dbae95f194792fff0e444f910ae7
BeommoKoo-dev <95630007+BeommoKoo-dev@users.noreply.github.com>
2023-07-31 11:52:57 +0900

    feat : 기존 restcontroller api 메소드 수정 및 delete메소드 추가

--- MyBoard/src/main/java/org/prgrms/myboard/controller/PostRestController.java
+++ MyBoard/src/main/java/org/prgrms/myboard/controller/PostRestController.java
@@ -1,8 +1,8 @@
 package org.prgrms.myboard.controller;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.prgrms.myboard.domain.CursorResult;
 import org.prgrms.myboard.domain.OffsetResult;
 import org.prgrms.myboard.dto.PostCreateRequestDto;
@@ -11,11 +11,12 @@ import org.prgrms.myboard.dto.PostResponseDto;
 import org.prgrms.myboard.dto.PostUpdateRequestDto;
 import org.prgrms.myboard.service.PostService;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("api/v1/posts")
+@RequestMapping("/posts")
 @RestController
 @RequiredArgsConstructor
 @Validated
@@ -23,31 +24,40 @@ public class PostRestController {
     private static final int PAGE_BASE_OFFSET = 1;
     private final PostService postService;
 
-    @PostMapping("/new")
+    @PostMapping
     public ResponseEntity<PostResponseDto> createPost(@Valid @RequestBody PostCreateRequestDto postCreateRequestDto) {
         return ResponseEntity.ok(postService.createPost(postCreateRequestDto));
     }
 
-    @GetMapping
+    @GetMapping("/cursor")
     public ResponseEntity<CursorResult<PostResponseDto>> getPostsByCursorId(@Valid @RequestBody
         PostCursorRequestDto postCursorRequestDto) {
         return ResponseEntity.ok(postService.findPostsByCursorPagination(postCursorRequestDto));
     }
 
-    @PutMapping("/{postId}")
+    @PostMapping("/{postId}")
     public ResponseEntity<PostResponseDto> updatePostById(@PathVariable("postId") Long postId,
         @RequestBody PostUpdateRequestDto postUpdateRequestDto) {
         return ResponseEntity.ok(postService.updateById(postId, postUpdateRequestDto));
     }
 
+    @GetMapping("/{postId}")
+    public ResponseEntity<PostResponseDto> getPostById(@PathVariable("postId") Long postId) {
+        return ResponseEntity.ok(postService.findById(postId));
+    }
+
     // page가 totalpage보다 더 크게 들어오면 어떡하지?
-    @GetMapping("/{page}")
+    @GetMapping
     public ResponseEntity<OffsetResult<PostResponseDto>> getPostsByOffsetPagination(
-        @PathVariable("page") @Min(value = 1, message = "page offset은 최소 1이여야 합니다.")
-        int page,
-        @RequestParam("size") @Min(value = 1, message = "page Size는 최소 1이여야 합니다.")
-        int pageSize) {
+        @RequestParam(value = "page", defaultValue = "1") int page,
+        @RequestParam(value = "size", defaultValue = "10") int pageSize) {
         return ResponseEntity.ok(postService.findPostsByOffsetPagination(
             PageRequest.of(page - PAGE_BASE_OFFSET, pageSize)));
+    }
+
+    @DeleteMapping("{postId}")
+    public ResponseEntity deletePostById(@PathVariable("postId") Long postId) {
+        postService.deleteById(postId);
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
