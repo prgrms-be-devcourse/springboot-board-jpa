372e511626cc6d1390ce02e52e6272e4ca8e9b2f
junho <ssmm0205@naver.com>
2023-07-31 14:04:58 +0900

    feat: BaseTimeEntity 추가

---
+++ src/main/java/com/ray/springbootboard/domain/BaseTimeEntity.java
@@ -0,0 +1,16 @@
+package com.ray.springbootboard.domain;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
