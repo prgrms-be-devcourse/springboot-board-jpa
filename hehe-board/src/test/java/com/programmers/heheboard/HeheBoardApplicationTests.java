ecd61ec001c980f2b4327184288f4ea97206368b
Curry4182 <kangbk4182@naver.com>
2023-08-01 01:14:13 +0900

    init commit

---
+++ hehe-board/src/test/java/com/programmers/heheboard/HeheBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.heheboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeheBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
