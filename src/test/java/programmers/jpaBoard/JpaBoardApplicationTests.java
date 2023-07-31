2add4643b0cb7b8a253f286fe76d8968d9ab167a
kys0411 <32180846@dankook.ac.kr>
2023-07-31 14:39:02 +0900

    🎉 프로젝트 초기화

---
+++ src/test/java/programmers/jpaBoard/JpaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package programmers.jpaBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
