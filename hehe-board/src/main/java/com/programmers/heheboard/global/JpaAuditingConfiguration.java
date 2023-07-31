a82193909388bc130416e4f0771c464945725da7
Curry4182 <kangbk4182@naver.com>
2023-08-01 01:15:36 +0900

    feat: BaseEntity 및 JpaAuditingConfiguration 설정 파일 추가

---
+++ hehe-board/src/main/java/com/programmers/heheboard/global/JpaAuditingConfiguration.java
@@ -0,0 +1,10 @@
+package com.programmers.heheboard.global;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfiguration {
+
+}
