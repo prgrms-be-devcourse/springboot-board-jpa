12887db9a6959132d2737cf5ecb69fa6571245d1
hyeon-z <phj8221@naver.com>
2023-07-31 14:21:42 +0900

    feat: 엔티티 validation 구현

--- src/main/java/org/prgms/boardservice/domain/post/Post.java
+++ src/main/java/org/prgms/boardservice/domain/post/Post.java
@@ -2,6 +2,7 @@ package org.prgms.boardservice.domain.post;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.prgms.boardservice.domain.BaseTime;
@@ -27,4 +28,24 @@ public class Post extends BaseTime {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    public Post(String title, String content) {
+        validateTitleLength(title);
+        validateContentLength(content);
+
+        this.title = title;
+        this.content = content;
+    }
+
+    private void validateTitleLength(String title) {
+        if (title.length() > 20) {
+            throw new IllegalArgumentException("제목은 20자 이내여야 합니다.");
+        }
+    }
+
+    private void validateContentLength(String content) {
+        if (content.length() > 500) {
+            throw new IllegalArgumentException("내용은 500자 이내여야 합니다.");
+        }
+    }
 }
--- src/main/java/org/prgms/boardservice/domain/user/User.java
+++ src/main/java/org/prgms/boardservice/domain/user/User.java
@@ -2,12 +2,14 @@ package org.prgms.boardservice.domain.user;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.prgms.boardservice.domain.BaseTime;
 import org.prgms.boardservice.domain.post.Post;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -32,4 +34,36 @@ public class User extends BaseTime {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST)
     private List<Post> posts = new ArrayList<>();
+
+    public User(String email, String password, String nickname) {
+        validateEmailPattern(email);
+        validatePasswordPattern(password);
+        validateNicknameLength(nickname);
+
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
+
+    private void validateEmailPattern(String email) {
+        String regex = "\\\\b[\\\\w\\\\.-]+@[\\\\w\\\\.-]+\\\\.\\\\w{2,4}\\\\b";
+
+        if (!Pattern.matches(regex, email)) {
+            throw new IllegalArgumentException("이메일 형식에 맞지 않습니다.");
+        }
+    }
+
+    private void validatePasswordPattern(String password) {
+        String regex = "/^.*(?=^.{8,15}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/";
+
+        if (!Pattern.matches(regex, password)) {
+            throw new IllegalArgumentException("비밀번호 형식에 맞지 않습니다.");
+        }
+    }
+
+    private void validateNicknameLength(String nickname) {
+        if (nickname.length() < 2 || nickname.length() > 10) {
+            throw new IllegalArgumentException("닉네임 길이는 2이상 10이하여야 합니다.");
+        }
+    }
 }
