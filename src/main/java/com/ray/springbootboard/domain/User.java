9b2dce91dad38e04f03f19373ce92ebb8d90f30b
JHS <wnghdtjr129@naver.com>
2023-08-01 14:33:00 +0900

    feat: 검증 로직 추가

--- src/main/java/com/ray/springbootboard/domain/User.java
+++ src/main/java/com/ray/springbootboard/domain/User.java
@@ -1,27 +1,36 @@
 package com.ray.springbootboard.domain;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
 import static lombok.AccessLevel.PROTECTED;
 
 @Getter
 @Entity
 @NoArgsConstructor(access = PROTECTED)
-@AllArgsConstructor
-@Table(name = "users")
 public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue
     private Long id;
 
-    @Column(nullable = false)
+    @NotBlank
+    @Size(max = 20)
+    @Column(nullable = false, length = 20)
     private String name;
 
-    private int age;
+    @Range(min = 1, max = 120)
+    private Integer age;
 
+    @Size(max = 20)
+    @Column(length = 20)
     private String hobby;
+
 }
