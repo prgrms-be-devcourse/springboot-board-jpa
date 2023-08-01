101bf2940160fdba312b669c3d795904c03860de
jay-so <merry9504@naver.com>
2023-08-01 18:24:28 +0900

    feat: Hobby enum 구현

---
+++ src/main/java/com/example/springbootboardjpa/enums/Hobby.java
@@ -0,0 +1,16 @@
+package com.example.springbootboardjpa.enums;
+
+public enum Hobby {
+    EXERCISE("운동"),
+    GAME("게임"),
+    READING("독서"),
+    SINGING("노래"),
+
+    COOKING("요리");
+
+    private final String hobbyName;
+
+    Hobby(String hobby) {
+        this.hobbyName = hobby;
+    }
+}
