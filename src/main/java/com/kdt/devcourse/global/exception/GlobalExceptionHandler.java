d2a2dbf1e541d0d25afcc353f3211bbd9e3421d9
Hejow <gmlwh124@naver.com>
2023-08-01 23:37:29 +0900

    :sparkles: 예외 메시지 처리기 구현

--- src/main/java/com/kdt/devcourse/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kdt/devcourse/global/exception/GlobalExceptionHandler.java
@@ -6,15 +6,25 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private static final String INTERNAL_ERROR_MESSAGE = "서버 내부 오류가 발생했습니다.";
+
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(NOT_FOUND)
     public ErrorResponse handleEntityNotFoundException(EntityNotFoundException e) {
         log.warn("EntityNotFoundException Occurs : {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleUnexpectedException(RuntimeException e) {
+        log.warn("UnexpectedException Occures : {}", e.getMessage());
+        return new ErrorResponse(INTERNAL_ERROR_MESSAGE);
     }
 }
