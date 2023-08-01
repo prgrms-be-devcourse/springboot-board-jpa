4dbba7d9f3e8ab74373dc892999207bf11a0cd1f
baejunil <bjo6300@naver.com>
2023-08-01 21:51:03 +0900

    feat : BaseEntity 생성

---
+++ board/src/main/java/com/example/board/global/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.example.board.global;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+  @CreatedDate
+  @Column(name = "created_at")
+  private LocalDateTime createdAt;
+
+  @Setter
+  @Column(name = "created_by")
+  private String createdBy;
+}
