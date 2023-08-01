3f44e2114230c2e4a3c98413061ec4507e8530ea
kylekim2123 <kyle2123@naver.com>
2023-08-01 12:40:44 +0900

    feat: User - Post 1:N 연관관계 매핑

--- src/main/java/com/programmers/jpaboard/domain/Post.java
+++ src/main/java/com/programmers/jpaboard/domain/Post.java
@@ -2,18 +2,20 @@ package com.programmers.jpaboard.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.util.Objects;
 import lombok.AccessLevel;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "post")
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post extends BaseEntity {
 
@@ -26,4 +28,17 @@ public class Post extends BaseEntity {
 
     @Lob
     private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    public void updateUser(User user) {
+        if (Objects.nonNull(this.user)) {
+            this.user.removePost(this);
+        }
+
+        this.user = user;
+        user.addPost(this);
+    }
 }
--- src/main/java/com/programmers/jpaboard/domain/User.java
+++ src/main/java/com/programmers/jpaboard/domain/User.java
@@ -1,18 +1,19 @@
 package com.programmers.jpaboard.domain;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
 import lombok.AccessLevel;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "user")
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
 
@@ -28,4 +29,15 @@ public class User extends BaseEntity {
 
     @Column(nullable = false, length = 50)
     private String hobby;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts;
+
+    public void addPost(Post post) {
+        posts.add(post);
+    }
+
+    public void removePost(Post post) {
+        posts.remove(post);
+    }
 }
