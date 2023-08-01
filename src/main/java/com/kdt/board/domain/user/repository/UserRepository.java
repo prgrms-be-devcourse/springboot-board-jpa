cd171cd3b640bc228fec46b41cbd35e590eaeb35
Choi Seongwon <vlflalektzja@naver.com>
2023-08-01 15:27:25 +0900

    chore: 마지막 개행 추가

--- src/main/java/com/kdt/board/domain/user/repository/UserRepository.java
+++ src/main/java/com/kdt/board/domain/user/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.kdt.board.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    
 }
