8e41a1e37b82a335946a88a55338b563d7c48ebf
JHS <wnghdtjr129@naver.com>
2023-08-01 14:33:17 +0900

    feat: 검증 로직 추가 및 createdBy 분리

--- src/main/java/com/ray/springbootboard/domain/Post.java
+++ src/main/java/com/ray/springbootboard/domain/Post.java
@@ -1,25 +1,29 @@
 package com.ray.springbootboard.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
 
 import static jakarta.persistence.FetchType.LAZY;
-import static lombok.AccessLevel.*;
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor
-@Table(name = "post")
-public class Post extends BaseEntity {
+public class Post extends BaseTimeEntity {
 
     @Id
     @GeneratedValue
     private Long id;
 
-    @Column(nullable = false)
+    @NotBlank
+    @Size(max = 50)
+    @Column(nullable = false, length = 50)
     private String title;
 
     @Lob
@@ -28,4 +32,7 @@ public class Post extends BaseEntity {
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    @CreatedBy
+    private String createdBy;
 }
