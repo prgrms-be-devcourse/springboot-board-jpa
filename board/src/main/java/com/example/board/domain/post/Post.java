c84604e2532f17cd05db82693bf9734239fcdee8
baejunil <bjo6300@naver.com>
2023-08-02 00:17:58 +0900

    feat : post entity, repository 구현

---
+++ board/src/main/java/com/example/board/domain/post/Post.java
@@ -0,0 +1,75 @@
+package com.example.board.domain.post;
+
+import static java.util.Objects.nonNull;
+
+import com.example.board.domain.user.User;
+import com.example.board.global.BaseEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "post")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseEntity {
+
+  private static final int TITLE_MAX = 30;
+  private static final int CONTENT_MAX = 100;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank
+  @Size(max = TITLE_MAX)
+  @Column(nullable = false, length = TITLE_MAX)
+  private String title;
+
+  @NotBlank
+  @Size(max = CONTENT_MAX)
+  @Column(nullable = false, length = CONTENT_MAX)
+  private String content;
+
+  @JoinColumn(name = "user_id")
+  @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+  private User user;
+
+  public void updateUser(User user) {
+    if (nonNull(user)) {
+      this.user = user;
+    }
+  }
+
+  public void updateTitle(String title) {
+    if (nonNull(title)) {
+      this.title = title;
+    }
+  }
+
+  public void updateContent(String content) {
+    if (nonNull(content)) {
+      this.content = content;
+    }
+  }
+
+  public Post(String title, String content, User user) {
+    if (nonNull(title) && nonNull(content) && nonNull(user)) {
+      this.title = title;
+      this.content = content;
+      this.user = user;
+    }
+  }
+}
