8c31cbe6ad260eb2793a8eca3bcf65193d576a35
hyee0715 <hyee0715@naver.com>
2023-08-01 23:39:41 +0900

    test: PostController 테스트 코드 작성

---
+++ src/test/java/com/programmers/springbootboardjpa/domain/post/controller/PostControllerTest.java
@@ -0,0 +1,197 @@
+package com.programmers.springbootboardjpa.domain.post.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.programmers.springbootboardjpa.domain.post.dto.PostCreateRequestDto;
+import com.programmers.springbootboardjpa.domain.post.dto.PostResponseDto;
+import com.programmers.springbootboardjpa.domain.post.dto.PostUpdateRequestDto;
+import com.programmers.springbootboardjpa.domain.post.service.PostService;
+import com.programmers.springbootboardjpa.domain.user.dto.UserRequestDto;
+import com.programmers.springbootboardjpa.domain.user.dto.UserResponseDto;
+import com.programmers.springbootboardjpa.domain.user.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@Transactional
+@SpringBootTest
+class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private UserService userService;
+
+    private PostCreateRequestDto postCreateRequestDto;
+
+    @BeforeEach
+    void setUp() {
+        UserRequestDto userRequestDto = UserRequestDto.builder()
+                .name("김이름")
+                .age(27)
+                .hobby("산책")
+                .build();
+
+        UserResponseDto userResponseDto = userService.create(userRequestDto);
+        Long userId = userResponseDto.id();
+
+        postCreateRequestDto = PostCreateRequestDto.builder()
+                .title("등록용 제목")
+                .content("등록용 내용")
+                .userId(userId)
+                .build();
+    }
+
+    @DisplayName("게시글을 저장한다")
+    @Test
+    void create() throws Exception {
+        //given
+        //when
+        //then
+        mockMvc.perform(post("/api/v1/posts")
+                        .content(objectMapper.writeValueAsString(postCreateRequestDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("post-create",
+                        requestFields(
+                                fieldWithPath("title").type(JsonFieldType.STRING).description("title"),
+                                fieldWithPath("content").type(JsonFieldType.STRING).description("content"),
+                                fieldWithPath("userId").type(JsonFieldType.NUMBER).description("userId")
+                        ), responseFields(
+                                fieldWithPath("httpStatus").type(JsonFieldType.STRING).description("httpStatus"),
+                                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("id"),
+                                fieldWithPath("data.title").type(JsonFieldType.STRING).description("title"),
+                                fieldWithPath("data.content").type(JsonFieldType.STRING).description("content"),
+                                fieldWithPath("data.userId").type(JsonFieldType.NUMBER).description("userId"),
+                                fieldWithPath("data.createdBy").type(JsonFieldType.NULL).description("createdBy"),
+                                fieldWithPath("data.createdAt").type(JsonFieldType.STRING).description("createdAt")
+                        )
+                ));
+    }
+
+    @DisplayName("저장된 게시글들을 페이징 조회한다")
+    @Test
+    void findAll() throws Exception {
+        //given
+        postService.create(postCreateRequestDto);
+
+        //when
+        //then
+        mockMvc.perform(get("/api/v1/posts")
+                        .param("page", String.valueOf(0))
+                        .param("size", String.valueOf(10))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("post-get-all",
+                        responseFields(
+                                fieldWithPath("httpStatus").type(JsonFieldType.STRING).description("httpStatus"),
+                                fieldWithPath("data.content[].id").type(JsonFieldType.NUMBER).description("id"),
+                                fieldWithPath("data.content[].title").type(JsonFieldType.STRING).description("title"),
+                                fieldWithPath("data.content[].content").type(JsonFieldType.STRING).description("content"),
+                                fieldWithPath("data.content[].userId").type(JsonFieldType.NUMBER).description("userId"),
+                                fieldWithPath("data.content[].createdBy").type(JsonFieldType.NULL).description("createdBy"),
+                                fieldWithPath("data.content[].createdAt").type(JsonFieldType.STRING).description("createdAt"),
+                                fieldWithPath("data.pageable.sort.empty").type(JsonFieldType.BOOLEAN).description("data.pageable.sort.empty").ignored(),
+                                fieldWithPath("data.pageable.sort.sorted").type(JsonFieldType.BOOLEAN).description("data.pageable.sort.sorted").ignored(),
+                                fieldWithPath("data.pageable.sort.unsorted").type(JsonFieldType.BOOLEAN).description("data.pageable.sort.unsorted").ignored(),
+                                fieldWithPath("data.pageable.offset").type(JsonFieldType.NUMBER).description("data.pageable.offset").ignored(),
+                                fieldWithPath("data.pageable.pageSize").type(JsonFieldType.NUMBER).description("data.pageable.pageSize"),
+                                fieldWithPath("data.pageable.pageNumber").type(JsonFieldType.NUMBER).description("data.pageable.pageNumber"),
+                                fieldWithPath("data.pageable.paged").type(JsonFieldType.BOOLEAN).description("data.pageable.paged").ignored(),
+                                fieldWithPath("data.pageable.unpaged").type(JsonFieldType.BOOLEAN).description("data.pageable.unpaged").ignored(),
+                                fieldWithPath("data.last").type(JsonFieldType.BOOLEAN).description("data.last").ignored(),
+                                fieldWithPath("data.totalElements").type(JsonFieldType.NUMBER).description("data.totalElements"),
+                                fieldWithPath("data.totalPages").type(JsonFieldType.NUMBER).description("data.totalPages"),
+                                fieldWithPath("data.size").type(JsonFieldType.NUMBER).description("data.size").ignored(),
+                                fieldWithPath("data.number").type(JsonFieldType.NUMBER).description("data.number").ignored(),
+                                fieldWithPath("data.sort.empty").type(JsonFieldType.BOOLEAN).description("data.sort.empty").ignored(),
+                                fieldWithPath("data.sort.sorted").type(JsonFieldType.BOOLEAN).description("data.sort.sorted").ignored(),
+                                fieldWithPath("data.sort.unsorted").type(JsonFieldType.BOOLEAN).description("data.sort.unsorted").ignored(),
+                                fieldWithPath("data.first").type(JsonFieldType.BOOLEAN).description("data.first").ignored(),
+                                fieldWithPath("data.numberOfElements").type(JsonFieldType.NUMBER).description("data.numberOfElements").ignored(),
+                                fieldWithPath("data.empty").type(JsonFieldType.BOOLEAN).description("data.empty").ignored()
+                        )
+                ));
+    }
+
+    @DisplayName("id로 게시글을 단건 조회한다")
+    @Test
+    void findById() throws Exception {
+        //given
+        PostResponseDto postResponseDto = postService.create(postCreateRequestDto);
+        Long savedPostId = postResponseDto.id();
+
+        //when
+        //then
+        mockMvc.perform(get("/api/v1/posts/{id}", savedPostId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("post-get-one",
+                        responseFields(
+                                fieldWithPath("httpStatus").type(JsonFieldType.STRING).description("httpStatus"),
+                                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("id"),
+                                fieldWithPath("data.title").type(JsonFieldType.STRING).description("title"),
+                                fieldWithPath("data.content").type(JsonFieldType.STRING).description("content"),
+                                fieldWithPath("data.userId").type(JsonFieldType.NUMBER).description("userId"),
+                                fieldWithPath("data.createdBy").type(JsonFieldType.NULL).description("createdBy"),
+                                fieldWithPath("data.createdAt").type(JsonFieldType.STRING).description("createdAt")
+                        )
+                ));
+    }
+
+    @DisplayName("게시글을 수정한다")
+    @Test
+    void update() throws Exception {
+        //given
+        PostResponseDto postResponseDto = postService.create(postCreateRequestDto);
+        Long savedPostId = postResponseDto.id();
+
+        PostUpdateRequestDto postUpdateRequestDto = PostUpdateRequestDto.builder()
+                .title("수정용 제목")
+                .content("수정용 내용")
+                .build();
+
+        //when
+        //then
+        mockMvc.perform(put("/api/v1/posts/{id}", savedPostId)
+                        .content(objectMapper.writeValueAsString(postUpdateRequestDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("post-update",
+                        requestFields(
+                                fieldWithPath("title").type(JsonFieldType.STRING).description("title"),
+                                fieldWithPath("content").type(JsonFieldType.STRING).description("content")
+                        ),
+                        responseFields(
+                                fieldWithPath("httpStatus").type(JsonFieldType.STRING).description("httpStatus"),
+                                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("id"),
+                                fieldWithPath("data.title").type(JsonFieldType.STRING).description("title"),
+                                fieldWithPath("data.content").type(JsonFieldType.STRING).description("content"),
+                                fieldWithPath("data.userId").type(JsonFieldType.NUMBER).description("userId"),
+                                fieldWithPath("data.createdBy").type(JsonFieldType.NULL).description("createdBy"),
+                                fieldWithPath("data.createdAt").type(JsonFieldType.STRING).description("createdAt")
+                        )
+                ));
+    }
+}
