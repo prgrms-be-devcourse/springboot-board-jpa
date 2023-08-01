bce719bd5d56d723514e7bbfbfbcad509e5ebb13
hseong3243 <hseong3243@gmail.com>
2023-08-02 00:32:44 +0900

    refactor: UsersGetRequest가 PageRequest를 상속하도록 변경

--- src/main/java/com/programmers/board/dto/request/UsersGetRequest.java
+++ src/main/java/com/programmers/board/dto/request/UsersGetRequest.java
@@ -1,19 +1,10 @@
 package com.programmers.board.dto.request;
 
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 
 @Getter
-public class UsersGetRequest {
-    @PositiveOrZero(message = "페이지 번호는 0 이상이어야 합니다")
-    private final int page;
-
-    @Positive(message = "페이지 사이즈는 0보다 커야합니다")
-    private final int size;
-
-    public UsersGetRequest(int page, int size) {
-        this.page = page;
-        this.size = size;
+public class UsersGetRequest extends PageRequest {
+    protected UsersGetRequest(int page, int size) {
+        super(page, size);
     }
 }
