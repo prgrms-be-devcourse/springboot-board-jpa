97a0fae3aff81f285d06adbc4920b073fd0a736c
ysng_ysng <yeasung67@gmail.com>
2023-08-01 11:01:55 +0900

    feat: springboot-board-jpa setup
    springboot-board-jpa setup

---
+++ src/test/java/prgrms/board/SpringbootBoardJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package prgrms.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBoardJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
