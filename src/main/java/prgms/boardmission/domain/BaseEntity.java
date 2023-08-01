0fb28045d0880d3b58c9c37b006c826b31c40f47
2jie0516 <2jie0516>
2023-08-01 16:53:02 +0900

    :sparkles: member 엔티티 필드 설정 및 생성

--- src/main/java/prgms/boardmission/domain/BaseEntity.java
+++ src/main/java/prgms/boardmission/domain/BaseEntity.java
@@ -1,4 +1,30 @@
 package prgms.boardmission.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
 public class BaseEntity {
+    @Column(name = "created_by")
+    private String createdBy;
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime cratedAt;
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCratedAt() {
+        return cratedAt;
+    }
+
+    public void setCratedAt(LocalDateTime cratedAt) {
+        this.cratedAt = cratedAt;
+    }
 }
--- src/main/java/prgms/boardmission/domain/Member.java
+++ src/main/java/prgms/boardmission/domain/Member.java
@@ -1,4 +1,55 @@
 package prgms.boardmission.domain;
 
-public class Member {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+
+    @Column(nullable = false,length = 30)
+    private String name;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Column(name = "hobby",nullable = true)
+    private String hobby;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
 }
