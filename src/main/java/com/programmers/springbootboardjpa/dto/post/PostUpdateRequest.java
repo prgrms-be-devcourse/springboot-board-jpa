9cd0bdbde250dbf4a8581cbc4f16b953c6d79599
changhyeon <changhyeon.h@kakao.com>
2023-08-02 17:30:17 +0900

    refactor: request dto에 NoArgsConstructor와 Builder추가

--- src/main/java/com/programmers/springbootboardjpa/dto/post/PostUpdateRequest.java
+++ src/main/java/com/programmers/springbootboardjpa/dto/post/PostUpdateRequest.java
@@ -4,19 +4,24 @@ import com.programmers.springbootboardjpa.global.validate.ValidationGroups.NotBl
 import com.programmers.springbootboardjpa.global.validate.ValidationGroups.SizeCheckGroup;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostUpdateRequest {
 
     @NotBlank(message = "제목은 Null이거나 공백이거나 값이 없을 수 없습니다.", groups = NotBlankGroup.class)
     @Size(min = 3, max = 100, message = "제목은 3글자 이상, 100글자 이하여야합니다.", groups = SizeCheckGroup.class)
-    private final String title;
+    private String title;
 
     @NotBlank(message = "내용은 Null이거나 공백이거나 값이 없을 수 없습니다.", groups = NotBlankGroup.class)
     @Size(min = 3, message = "내용은 3글자 이상이어야합니다.", groups = SizeCheckGroup.class)
-    private final String content;
+    private String content;
 
+    @Builder
     public PostUpdateRequest(String title, String content) {
         this.title = title;
         this.content = content;
--- src/main/java/com/programmers/springbootboardjpa/dto/user/UserCreateRequest.java
+++ src/main/java/com/programmers/springbootboardjpa/dto/user/UserCreateRequest.java
@@ -9,22 +9,25 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserCreateRequest {
 
     @NotBlank(message = "사용자 이름은 Null이거나, 공백 또는 값이 없을 수 없습니다.", groups = NotBlankGroup.class)
     @Size(min = 2, max = 30, message = "사용자 이름은 2글자 이상, 30글자 미만이어야합니다.", groups = SizeCheckGroup.class)
-    private final String name;
+    private String name;
 
     @NotNull(message = "나이는 값이 Null일 수 없습니다", groups = NotNullGroup.class)
     @Positive(message = "나이는 숫자여야하며 양수여야합니다.", groups = PositiveGroup.class)
-    private final Integer age;
+    private Integer age;
 
     @NotNull(message = "취미는 값이 Null일 수 없으며 목록에 있는 값이어야합니다.")
-    private final Hobby hobby;
+    private Hobby hobby;
 
     @Builder
     public UserCreateRequest(String name, Integer age, Hobby hobby) {
