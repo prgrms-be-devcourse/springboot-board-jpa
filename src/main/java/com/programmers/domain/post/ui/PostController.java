3f1b1e46ddc9a2ea04019ebf5394da4225eb0ad3
yumyeonghan <mgtmh991013@gmail.com>
2023-08-02 21:45:57 +0900

    feat: PostService 인터페이스 사용

--- src/main/java/com/programmers/domain/post/ui/PostController.java
+++ src/main/java/com/programmers/domain/post/ui/PostController.java
@@ -1,7 +1,7 @@
 package com.programmers.domain.post.ui;
 
 import com.programmers.common.dto.ApiResponse;
-import com.programmers.domain.post.application.PostServiceImpl;
+import com.programmers.domain.post.application.PostService;
 import com.programmers.domain.post.ui.dto.PostDto;
 import com.programmers.domain.post.ui.dto.PostUpdateDto;
 import lombok.RequiredArgsConstructor;
@@ -18,26 +18,26 @@ import java.util.List;
 @RequestMapping("/api/posts")
 public class PostController {
 
-    private final PostServiceImpl postServiceImpl;
+    private final PostService postService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public ApiResponse<Long> createPost(@RequestBody PostDto postDto) {
-        return new ApiResponse<>(postServiceImpl.createPost(postDto));
+        return new ApiResponse<>(postService.createPost(postDto));
     }
 
     @GetMapping
     public List<PostDto> findPostList(@PageableDefault(size = 20, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
-        return postServiceImpl.findAll(pageable);
+        return postService.findAll(pageable);
     }
 
     @GetMapping("/{id}")
     public PostDto findPost(@PathVariable Long id) {
-        return postServiceImpl.findPost(id);
+        return postService.findPost(id);
     }
 
     @PatchMapping("/{id}")
     public PostDto updatePost(@PathVariable Long id, @RequestBody PostUpdateDto postUpdateDto) {
-        return postServiceImpl.updatePost(postUpdateDto, id);
+        return postService.updatePost(postUpdateDto, id);
     }
 }
