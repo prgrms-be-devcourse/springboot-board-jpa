15acab5cf874f169783932105a076daf9cb6f599
changhyeon <changhyeon.h@kakao.com>
2023-08-02 17:27:43 +0900

    refactor: ApiResponse를 ResponseEntity로 묶어서 코드 반환할 수 있도록 변경

--- src/main/java/com/programmers/springbootboardjpa/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/programmers/springbootboardjpa/global/exception/GlobalExceptionHandler.java
@@ -2,41 +2,49 @@ package com.programmers.springbootboardjpa.global.exception;
 
 import com.programmers.springbootboardjpa.global.ApiResponse;
 import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ApiResponse<ErrorResponse> handlerValidationException(MethodArgumentNotValidException methodArgumentNotValidException) {
+    public ResponseEntity<ApiResponse<ErrorResponse>> handlerValidationException(MethodArgumentNotValidException methodArgumentNotValidException) {
         ErrorResponse errorResponse = createValidationErrorResponse(methodArgumentNotValidException);
-        
-        return ApiResponse.fail(errorResponse);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.fail(errorResponse));
     }
 
     @ExceptionHandler(UserException.class)
-    public ApiResponse<ErrorResponse> handlerUserException(UserException userException) {
+    public ResponseEntity<ApiResponse<ErrorResponse>> handlerUserException(UserException userException) {
         ErrorResponse errorResponse = createErrorResponse(userException);
 
-        return ApiResponse.fail(errorResponse);
+        return ResponseEntity.status(userException.getStatus())
+                .body(ApiResponse.fail(errorResponse));
     }
 
     @ExceptionHandler(PostException.class)
-    public ApiResponse<ErrorResponse> handlerPostException(PostException postException) {
+    public ResponseEntity<ApiResponse<ErrorResponse>> handlerPostException(PostException postException) {
         ErrorResponse errorResponse = createErrorResponse(postException);
 
-        return ApiResponse.fail(errorResponse);
+        return ResponseEntity.status(postException.getStatus())
+                .body(ApiResponse.fail(errorResponse));
     }
 
     @ExceptionHandler(Exception.class)
-    public ApiResponse<ErrorResponse> handlerException(Exception exception) {
+    public ResponseEntity<ApiResponse<ErrorResponse>> handlerException(Exception exception) {
         ErrorResponse errorResponse = createTopLevelErrorResponse(exception);
 
-        return ApiResponse.fail(errorResponse);
+        log.info(" : ", exception);
+
+        return ResponseEntity.status(500).body(ApiResponse.fail(errorResponse));
     }
 
     private ErrorResponse createErrorResponse(BusinessException businessException) {
@@ -70,7 +78,7 @@ public class GlobalExceptionHandler {
         return ErrorResponse.builder()
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .statusCode(500)
-                .errorMessage("서버에서 알 수 없는 오류가 발생했습니다.")
+                .errorMessage("서버에서 알 수 없는 에러가 발생했습니다. 관리자에게 문의해주세요.")
                 .build();
     }
 
