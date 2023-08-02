88ba28d84de3266d093e1998efdfc0b509a12e1d
KimMinheee <99alsgmlalsl@naver.com>
2023-08-02 21:27:48 +0900

    [Feat] : 프로젝트 세팅

---
+++ src/main/java/com/blackdog/springbootBoardJpa/SpringbootBoardJpaApplication.java
@@ -0,0 +1,13 @@
+package com.blackdog.springbootBoardJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBoardJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBoardJpaApplication.class, args);
+	}
+
+}
