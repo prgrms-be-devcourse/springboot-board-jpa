2473ee6627f0d53dee9e7c7e6457e8bc86b70544
jay-so <merry9504@naver.com>
2023-08-03 14:14:58 +0900

    refactor: User클래스 테이블명 리팩토링

--- src/main/java/com/example/springbootboardjpa/entity/User.java
+++ src/main/java/com/example/springbootboardjpa/entity/User.java
@@ -2,36 +2,40 @@ package com.example.springbootboardjpa.entity;
 
 import com.example.springbootboardjpa.enums.Hobby;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "user")
-@Data
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
 public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @Column(name = "name", nullable = false,length = 30)
+    @Column(name = "name", nullable = false, length = 30)
     private String name;
 
-    @Column(name = "age",nullable = false,length = 30)
+    @Column(name = "age", nullable = false)
     private int age;
 
     @Enumerated(EnumType.STRING)
     private Hobby hobby;
 
-    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,orphanRemoval = true)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     List<Post> posts = new ArrayList<>();
 
-    public void addPost(Post post){
+    public void addPost(Post post) {
         posts.add(post);
     }
 
-    public void removePost(Post post){
+    public void removePost(Post post) {
         posts.remove(post);
     }
 }
