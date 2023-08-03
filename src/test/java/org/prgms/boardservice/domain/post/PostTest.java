92dd2e19794bf97b5cff912cbe443dd2de497317
hyeon-z <phj8221@naver.com>
2023-08-04 02:15:27 +0900

    test: 게시글 생성자 테스트 변경

--- src/test/java/org/prgms/boardservice/domain/post/PostTest.java
+++ src/test/java/org/prgms/boardservice/domain/post/PostTest.java
@@ -13,34 +13,38 @@ import static org.prgms.boardservice.util.ErrorMessage.INVALID_POST_TITLE;
 
 class PostTest {
 
+    private final Long userId = 1L;
+    private final String titleMoreThenTwentyLength = "TitleMoreThenTwentyLength";
+    private final String contentMoreThanFiveHundredLength = "sit amet cursus sit amet dictum sit amet justo donec enim diam vulputate ut pharetra sit amet aliquam id diam maecenas ultricies mi eget mauris pharetra et ultrices neque ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna neque viverra justo nec ultrices dui sapien eget mi proin sed libero enim sed faucibus turpis in eu mi bibendum neque egestas congue quisque egestas diam in arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat sed cras ornare arcu dui vivamus arcu felis bibendum ut tristique et egestas";
+
     @ParameterizedTest(name = "test with value [{0}]")
     @NullAndEmptySource
-    @ValueSource(strings = {"    "})
-    @DisplayName("제목이 null, 빈 문자열 또는 공백일 경우 에러가 발생한다.")
-    void should_ThrowException_When_EmptyTitle(String title) {
+    @ValueSource(strings = {"    ", titleMoreThenTwentyLength})
+    @DisplayName("생성자에서 제목이 유효하지 않은 경우 에러가 발생한다.")
+    void fail_Post_Constructor_With_Invalid_Title(String title) {
         String content = "content";
 
-        assertThatThrownBy(() -> new Post(title, content))
+        assertThatThrownBy(() -> new Post(title, content, userId))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(INVALID_POST_TITLE.getMessage());
     }
 
     @ParameterizedTest(name = "test with value [{0}]")
     @NullAndEmptySource
-    @ValueSource(strings = {"    "})
-    @DisplayName("내용이 null, 빈 문자열 또는 공백일 경우 에러가 발생한다.")
-    void should_ThrowException_When_EmptyContent(String content) {
+    @ValueSource(strings = {"    ", contentMoreThanFiveHundredLength})
+    @DisplayName("생성자에서 내용이 유효하지 않은 경우 에러가 발생한다.")
+    void fail_Post_Constructor_With_Invalid_Content(String content) {
         String title = "title";
 
-        assertThatThrownBy(() -> new Post(title, content))
+        assertThatThrownBy(() -> new Post(title, content, userId))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(INVALID_POST_CONTENT.getMessage());
     }
 
     @Test
     @DisplayName("게시글이 성공적으로 생성된다.")
-    void should_Success_When_NewPost() {
-        Post post = new Post("title", "content");
+    void success_New_Post() {
+        Post post = new Post("title", "content", userId);
 
         assertThat(post).isNotNull();
     }
