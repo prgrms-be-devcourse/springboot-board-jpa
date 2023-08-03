33819a02179da9b0d54de34cda4e114235795858
2jie0516 <2jie0516>
2023-08-03 13:49:40 +0900

    :sparkles: post 엔티티 필드 설정 및 생성

--- src/main/java/prgms/boardmission/domain/Post.java
+++ src/main/java/prgms/boardmission/domain/Post.java
@@ -1,4 +1,59 @@
 package prgms.boardmission.domain;
 
-public class Post {
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Post extends BaseEntity{
+    @Id
+    private long id;
+
+    @Column(nullable = false,length = 30)
+    private String title;
+
+    @Lob
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "member_id", referencedColumnName = "userId")
+    private Member member;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setMember(Member postMember){
+        member = postMember;
+        this.setCreatedBy(postMember.getName());
+    }
 }
