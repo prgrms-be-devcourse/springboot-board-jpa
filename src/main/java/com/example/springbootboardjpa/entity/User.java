14353c1c473f7472f99518abff2a439bc91d8fd4
jay-so <merry9504@naver.com>
2023-08-03 14:58:14 +0900

    refactor: User 엔티티 리팩토링

--- src/main/java/com/example/springbootboardjpa/entity/User.java
+++ src/main/java/com/example/springbootboardjpa/entity/User.java
@@ -1,35 +1,38 @@
 package com.example.springbootboardjpa.entity;
 
 import com.example.springbootboardjpa.enums.Hobby;
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
 @NoArgsConstructor
 @Getter
-@Setter
 public class User extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
-
     @Column(name = "name", nullable = false, length = 30)
     private String name;
-
     @Column(name = "age", nullable = false)
     private int age;
-
     @Enumerated(EnumType.STRING)
     private Hobby hobby;
-
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    List<Post> posts = new ArrayList<>();
+    private List<Post> posts = new ArrayList<>();
 
     public void addPost(Post post) {
         posts.add(post);
