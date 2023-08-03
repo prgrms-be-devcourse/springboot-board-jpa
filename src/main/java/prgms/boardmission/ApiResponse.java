324ad386a73bc484062042d35a7b213410f7fb20
2jie0516 <2jie0516>
2023-08-03 13:51:25 +0900

    :sparkles: ApiResponse 응답값 설정

--- src/main/java/prgms/boardmission/ApiResponse.java
+++ src/main/java/prgms/boardmission/ApiResponse.java
@@ -1,4 +1,28 @@
 package prgms.boardmission;
 
-public class ApiResponse {
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public class ApiResponse<T> {
+    @JsonFormat
+    private int statusCode;
+    @JsonFormat
+    private T data;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime serverDatetime;
+
+    public ApiResponse(int statusCode, T data) {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.serverDatetime = LocalDateTime.now();
+    }
+
+    public static <T> ApiResponse<T> ok(T data) {
+        return new ApiResponse<>(200, data);
+    }
+
+    public static <T> ApiResponse<T> fail(int statusCode, T data) {
+        return new ApiResponse<>(statusCode, data);
+    }
 }
