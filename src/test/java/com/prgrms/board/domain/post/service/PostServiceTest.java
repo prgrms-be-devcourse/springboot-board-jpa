70e06c0d3996880c3fc87449cbebceaf11754e0d
kmebin <blowhuiee@gmail.com>
2023-08-03 15:00:50 +0900

    feat: 전체 게시물 조회 서비스 테스트

--- src/test/java/com/prgrms/board/domain/post/service/PostServiceTest.java
+++ src/test/java/com/prgrms/board/domain/post/service/PostServiceTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +13,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import com.prgrms.board.domain.post.dto.request.PostCreateRequest;
 import com.prgrms.board.domain.post.dto.response.PostResponse;
@@ -18,6 +24,7 @@ import com.prgrms.board.domain.post.entity.Post;
 import com.prgrms.board.domain.post.repository.PostRepository;
 import com.prgrms.board.domain.user.entity.User;
 import com.prgrms.board.domain.user.service.UserService;
+import com.prgrms.board.global.common.PageResponse;
 import com.prgrms.board.support.PostFixture;
 import com.prgrms.board.support.UserFixture;
 
@@ -53,5 +60,26 @@ class PostServiceTest {
 
         // then
         assertThat(result.title()).isEqualTo("제목");
+    }
+
+    @Test
+    @DisplayName("전체 게시물 조회에 성공한다.")
+    void get_posts_success() {
+        // given
+        Post post1 = PostFixture.create(user, "제목1", "내용1");
+        Post post2 = PostFixture.create(user, "제목2", "내용2");
+        Post post3 = PostFixture.create(user, "제목3", "내용3");
+        List<Post> posts = List.of(post1, post2, post3);
+
+        Pageable pageable = PageRequest.of(0, 2);
+        Page<Post> page = new PageImpl<>(posts.subList(0, 2), pageable, posts.size());
+        given(postRepository.findAll(any(Pageable.class))).willReturn(page);
+
+        // when
+        PageResponse<Post> result = postService.getPosts(pageable);
+
+        // then
+        assertThat(result.totalCount()).isEqualTo(3);
+        assertThat(result.items()).hasSize(2);
     }
 }
