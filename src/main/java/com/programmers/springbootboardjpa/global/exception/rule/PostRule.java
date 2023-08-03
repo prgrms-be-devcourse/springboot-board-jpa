2823e93fcaf06d294167c632a9d35fd266d7313c
changhyeon <changhyeon.h@kakao.com>
2023-08-04 00:17:23 +0900

    refactor: http status notfound로 변경

--- src/main/java/com/programmers/springbootboardjpa/global/exception/rule/PostRule.java
+++ src/main/java/com/programmers/springbootboardjpa/global/exception/rule/PostRule.java
@@ -8,9 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum PostRule implements BusinessRule {
 
-    POST_NOT_FOUND_FOR_FIND(HttpStatus.BAD_REQUEST, "찾는 게시글이 없습니다."),
-    POST_NOT_FOUND_FOR_DELETE(HttpStatus.BAD_REQUEST, "삭제하려는 게시글이 없습니다."),
-    POST_NOT_FOUND_FOR_UPDATE(HttpStatus.BAD_REQUEST, "업데이트하려는 게시글이 없습니다.");
+    POST_NOT_FOUND_FOR_FIND(HttpStatus.NOT_FOUND, "찾는 게시글이 없습니다."),
+    POST_NOT_FOUND_FOR_DELETE(HttpStatus.NOT_FOUND, "삭제하려는 게시글이 없습니다."),
+    POST_NOT_FOUND_FOR_UPDATE(HttpStatus.NOT_FOUND, "업데이트하려는 게시글이 없습니다.");
 
     private final HttpStatus status;
     private final String message;
--- src/main/java/com/programmers/springbootboardjpa/global/exception/rule/UserRule.java
+++ src/main/java/com/programmers/springbootboardjpa/global/exception/rule/UserRule.java
@@ -8,9 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum UserRule implements BusinessRule {
 
-    USER_NOT_FOUND_FOR_FIND(HttpStatus.BAD_REQUEST, "찾는 유저가 없습니다."),
-    USER_NOT_FOUND_FOR_DELETE(HttpStatus.BAD_REQUEST, "삭제하려는 유저가 없습니다."),
-    USER_NOT_FOUND_FOR_UPDATE(HttpStatus.BAD_REQUEST, "업데이트하려는 유저가 없습니다.");
+    USER_NOT_FOUND_FOR_FIND(HttpStatus.NOT_FOUND, "찾는 유저가 없습니다."),
+    USER_NOT_FOUND_FOR_DELETE(HttpStatus.NOT_FOUND, "삭제하려는 유저가 없습니다."),
+    USER_NOT_FOUND_FOR_UPDATE(HttpStatus.NOT_FOUND, "업데이트하려는 유저가 없습니다.");
 
     private final HttpStatus status;
     private final String message;
