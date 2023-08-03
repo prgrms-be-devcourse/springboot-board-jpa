34d56f7505dd5b899b840539f5972476a81d96a3
2jie0516 <2jie0516>
2023-08-03 15:01:43 +0900

    :sparkles: 사용자 예외(발견된 게시글 없음)  추가

---
+++ src/main/java/prgms/boardmission/post/exception/NoFoundPostException.java
@@ -0,0 +1,12 @@
+package prgms.boardmission.post.exception;
+
+public class NoFoundPostException extends RuntimeException{
+
+    public NoFoundPostException() {
+        super();
+    }
+
+    public NoFoundPostException(String message) {
+        super(message);
+    }
+}
