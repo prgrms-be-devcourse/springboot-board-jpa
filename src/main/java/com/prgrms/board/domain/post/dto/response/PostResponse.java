2c15cdac45d5e4e3207d3d56ec9c132ad89bb4c8
kmebin <blowhuiee@gmail.com>
2023-08-03 16:10:10 +0900

    feat: PostResponse 빌더 패턴 적용

--- src/main/java/com/prgrms/board/domain/post/dto/response/PostResponse.java
+++ src/main/java/com/prgrms/board/domain/post/dto/response/PostResponse.java
@@ -2,12 +2,19 @@ package com.prgrms.board.domain.post.dto.response;
 
 import com.prgrms.board.domain.post.entity.Post;
 
+import lombok.Builder;
+
+@Builder
 public record PostResponse(
     Long postId,
     String title,
     String content
 ) {
     public static PostResponse from(Post post) {
-        return new PostResponse(post.getId(), post.getTitle(), post.getContent());
+        return PostResponse.builder()
+            .postId(post.getId())
+            .title(post.getTitle())
+            .content(post.getContent())
+            .build();
     }
 }
