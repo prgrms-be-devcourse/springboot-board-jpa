7d5155f0aefe31fd952b812a49173247d19652f0
jay-so <merry9504@naver.com>
2023-08-03 17:23:36 +0900

    feat: PostCreateRequest 구현

---
+++ src/main/java/com/example/springbootboardjpa/dto/request/PostCreateRequest.java
@@ -0,0 +1,20 @@
+package com.example.springbootboardjpa.dto.request;
+
+import com.example.springbootboardjpa.entity.Post;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostCreateRequest {
+
+    private String title;
+    private String content;
+
+    public Post toEntity() {
+        return Post.builder()
+                .title(title)
+                .content(content)
+                .build();
+    }
+}
