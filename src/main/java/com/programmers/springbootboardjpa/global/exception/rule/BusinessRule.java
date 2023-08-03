ff4f0fec2fc879e404b0269b64abf069c2db2317
changhyeon <changhyeon.h@kakao.com>
2023-08-04 00:14:46 +0900

    refactor: 불필요한 필드값 제거

--- src/main/java/com/programmers/springbootboardjpa/global/exception/rule/BusinessRule.java
+++ src/main/java/com/programmers/springbootboardjpa/global/exception/rule/BusinessRule.java
@@ -3,10 +3,7 @@ package com.programmers.springbootboardjpa.global.exception.rule;
 import org.springframework.http.HttpStatus;
 
 public interface BusinessRule {
-
-    String message = null;
-    HttpStatus status = null;
-
+    
     String getMessage();
 
     HttpStatus getStatus();
