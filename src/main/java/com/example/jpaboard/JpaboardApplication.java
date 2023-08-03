53a20063a84ab90164e6f3ab745cb62a70983530
byeolhaha <j35635aa@gmail.com>
2023-08-03 21:38:17 +0900

    git init

---
+++ src/main/java/com/example/jpaboard/JpaboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpaboard/JpaboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
