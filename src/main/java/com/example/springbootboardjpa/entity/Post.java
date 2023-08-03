9ab5b332d415a92acd08a2fd9c57a12a06c689f0
jay-so <merry9504@naver.com>
2023-08-03 14:57:45 +0900

    refactor: Post 엔티티 리팩토링

--- src/main/java/com/example/springbootboardjpa/entity/Post.java
+++ src/main/java/com/example/springbootboardjpa/entity/Post.java
@@ -1,31 +1,40 @@
 package com.example.springbootboardjpa.entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import java.util.Objects;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "post")
-@Data
+@NoArgsConstructor
+@Getter
 public class Post extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @Column(name = "title",nullable = false,length = 100)
+    @Column(name = "title", nullable = false, length = 100)
     private String title;
 
-    @Column(name = "content",nullable = false)
+    @Column(name = "content", nullable = false)
     private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(columnDefinition = "user_id",referencedColumnName = "id")
+    @JoinColumn(columnDefinition = "user_id", referencedColumnName = "id")
     private User user;
 
-    public void updateUser(User user){
-        if(Objects.nonNull(this.user)){
+    public void updateUser(User user) {
+        if (Objects.nonNull(this.user)) {
             this.user.removePost(this);
         }
         this.user = user;
