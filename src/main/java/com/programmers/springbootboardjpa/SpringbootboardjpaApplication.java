019bd2e56573340ff02d9e5b990a18f2537c76d7
charlesuu <tiger0192004@gmail.com>
2023-08-03 17:54:04 +0900

    chore: init project

---
+++ src/main/java/com/programmers/springbootboardjpa/SpringbootboardjpaApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.springbootboardjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootboardjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootboardjpaApplication.class, args);
+	}
+
+}
