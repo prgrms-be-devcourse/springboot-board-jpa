9c4d03163e5f915ce23d44224baaca74100ac2aa
Choi Seongwon <vlflalektzja@naver.com>
2023-08-04 13:11:40 +0900

    feat: PostService 기능 추가

--- src/main/java/com/kdt/board/domain/post/service/PostService.java
+++ src/main/java/com/kdt/board/domain/post/service/PostService.java
@@ -1,13 +1,12 @@
 package com.kdt.board.domain.post.service;
 
-import com.kdt.board.domain.post.dto.PostRequestDto;
 import com.kdt.board.domain.post.entity.Post;
 import com.kdt.board.domain.post.repository.PostRepository;
-import com.kdt.board.domain.user.entity.User;
-import com.kdt.board.domain.user.repository.UserRepository;
 import com.kdt.board.global.exception.BaseException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,12 +15,26 @@ import org.springframework.stereotype.Service;
 public class PostService {
 
     private final PostRepository postRepository;
-    private final UserRepository userRepository;
 
-    public Post save(PostRequestDto postRequestDto) {
-        User user = userRepository.findById(postRequestDto.getUserId())
-                .orElseThrow(() -> new BaseException("등록되지 않은 유저입니다"));
-        Post post = PostRequestDto.from(postRequestDto, user);
+    public Post save(Post post) {
+        return postRepository.save(post);
+    }
+
+    public Page<Post> findAll(Pageable pageable) {
+        return postRepository.findAll(pageable);
+    }
+
+    public Post findById(Long id) {
+        return postRepository.findById(id).orElseThrow(
+                () -> new BaseException("해당 게시글이 없습니다")
+        );
+    }
+
+    public Post updatePost(Long id, Post post) {
+        if (!postRepository.existsById(id)) {
+            throw new IllegalArgumentException("해당 게시글이 없습니다");
+        }
+
         return postRepository.save(post);
     }
 }
