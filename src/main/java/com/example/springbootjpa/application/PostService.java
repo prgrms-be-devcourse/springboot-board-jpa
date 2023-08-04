26b857432de111f733330f1ad91da4786ef2f75b
PHJ <legowww@gmail.com>
2023-08-04 13:22:22 +0900

    refactor: 서비스 클래스 메서드 명 에러 메세지 구체화

--- src/main/java/com/example/springbootjpa/application/PostService.java
+++ src/main/java/com/example/springbootjpa/application/PostService.java
@@ -7,7 +7,6 @@ import com.example.springbootjpa.domain.user.User;
 import com.example.springbootjpa.domain.user.UserRepository;
 import com.example.springbootjpa.ui.dto.post.PostFindResponse;
 import jakarta.persistence.EntityNotFoundException;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,13 +14,17 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class PostService {
 
     private final PostRepository postRepository;
 
     private final UserRepository userRepository;
+
+    public PostService(PostRepository postRepository, UserRepository userRepository) {
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+    }
 
     public List<PostFindResponse> findAllPosts(Pageable pageable) {
         return postRepository.findAll(pageable)
@@ -30,7 +33,7 @@ public class PostService {
                 .toList();
     }
 
-    public PostFindResponse findPost(Long postId) {
+    public PostFindResponse find(Long postId) {
         Post post = findByPostId(postId);
 
         return PostFindResponse.from(post);
@@ -47,7 +50,7 @@ public class PostService {
     private Post findByPostId(Long postId) {
         return postRepository.findById(postId)
                 .orElseThrow(
-                        () -> new EntityNotFoundException("post doesn't exist")
+                        () -> new EntityNotFoundException("Invalid post id!! please check post id again")
                 );
     }
 
@@ -55,12 +58,12 @@ public class PostService {
     public long createPost(long userId, String title, String content) {
         User user = userRepository.findById(userId)
                 .orElseThrow(
-                        () -> new EntityNotFoundException("user doesn't exist")
+                        () -> new EntityNotFoundException("Invalid user id!! please check user id again")
                 );
 
         Post post = new Post(title, content, user);
-        postRepository.save(post);
+        Post savedPost = postRepository.save(post);
 
-        return post.getId();
+        return savedPost.getId();
     }
 }
--- src/main/java/com/example/springbootjpa/application/UserService.java
+++ src/main/java/com/example/springbootjpa/application/UserService.java
@@ -6,25 +6,27 @@ import com.example.springbootjpa.domain.user.User;
 import com.example.springbootjpa.domain.user.UserRepository;
 import com.example.springbootjpa.ui.dto.user.UserFindResponse;
 import jakarta.persistence.EntityNotFoundException;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class UserService {
 
     private final UserRepository userRepository;
 
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Transactional
     public long createUser(String name, int age, Hobby hobby) {
         User user = new User(name, age, hobby);
-        userRepository.save(user);
+        User savedUser = userRepository.save(user);
 
-        return user.getId();
+        return savedUser.getId();
     }
 
     public List<UserFindResponse> findAllUsers() {
@@ -34,9 +36,9 @@ public class UserService {
                 .toList();
     }
 
-    public UserFindResponse findById(Long id) {
+    public UserFindResponse find(Long id) {
         User user = userRepository.findById(id).orElseThrow(
-                () -> new EntityNotFoundException("member doesn't exist")
+                () -> new EntityNotFoundException("Invalid user id!! please check user id again")
         );
 
         return UserFindResponse.from(user);
