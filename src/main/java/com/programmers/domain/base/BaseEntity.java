8cb1a9d00c89a8707b40293c73d0ca5e70b3536e
yumyeonghan <mgtmh991013@gmail.com>
2023-08-04 13:41:07 +0900

    refactor: BaseEntity 추상 클래스로 정의

--- src/main/java/com/programmers/domain/base/BaseEntity.java
+++ src/main/java/com/programmers/domain/base/BaseEntity.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
-public class BaseEntity {
+public abstract class BaseEntity {
 
     @CreatedBy
     @Column(name = "created_by", updatable = false)
