00daaba35ea2f33834237981730e886a48e1b1af
junho <ssmm0205@naver.com>
2023-08-04 15:59:26 +0900

    feat: UserServiceImpl에 transactional 추가

--- src/main/java/com/ray/springbootboard/service/UserServiceImpl.java
+++ src/main/java/com/ray/springbootboard/service/UserServiceImpl.java
@@ -4,8 +4,10 @@ import com.ray.springbootboard.domain.User;
 import com.ray.springbootboard.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
