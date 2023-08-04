5624111595939080fdda63c12667aa4a149dc729
jay-so <merry9504@naver.com>
2023-08-04 17:15:17 +0900

    style: User 클래스 개행 추가

--- src/main/java/com/example/springbootboardjpa/entity/User.java
+++ src/main/java/com/example/springbootboardjpa/entity/User.java
@@ -1,20 +1,12 @@
 package com.example.springbootboardjpa.entity;
 
 import com.example.springbootboardjpa.enums.Hobby;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -25,12 +17,16 @@ public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
     @Column(name = "name", nullable = false, length = 30)
     private String name;
+
     @Column(name = "age", nullable = false)
     private int age;
+
     @Enumerated(EnumType.STRING)
     private Hobby hobby;
+
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Post> posts = new ArrayList<>();
 
