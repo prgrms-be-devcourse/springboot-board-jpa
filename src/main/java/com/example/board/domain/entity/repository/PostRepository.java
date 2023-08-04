f1973f6662f9f9c957169cb8893065a3b84278db
seongHyun-Min <steve6133@naver.com>
2023-08-04 19:38:12 +0900

    feat : PostRepository 추가

--- src/main/java/com/example/board/domain/entity/repository/PostRepository.java
+++ src/main/java/com/example/board/domain/entity/repository/PostRepository.java
@@ -1,7 +1,25 @@
 package com.example.board.domain.entity.repository;
 
 import com.example.board.domain.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    // 유저와 함께 조회(fetch Join)
+    @Query("select p from Post p join fetch p.user where p.id = :postId")
+    Optional<Post> findByIdWithUser(@Param("postId") Long postId);
+
+    // 댓글과 함께 조회
+    @Query("select p from Post p join fetch p.user left join fetch p.comments where p.id = :postId")
+    Optional<Post> findByIdWithUserAndComments(@Param("postId") Long postId);
+
+    // 페이징된 게시글만 조회
+    // @Query("select p from Post p join fetch p.user u") fetch Join을 사용하면 에러가 발생하여 일반적으로 가져오는 경우
+    // N+1문제 발생...
+    Page<Post> findAll(Pageable pageable);
 }
