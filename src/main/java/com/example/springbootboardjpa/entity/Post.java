96b89f097482a2c662bfe2f2bac400071a53d5d5
jay-so <merry9504@naver.com>
2023-08-04 17:26:57 +0900

    style: Post 클래스 불필요한 개행 삭제

--- src/main/java/com/example/springbootboardjpa/entity/Post.java
+++ src/main/java/com/example/springbootboardjpa/entity/Post.java
@@ -1,18 +1,11 @@
 package com.example.springbootboardjpa.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import java.util.Objects;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.Objects;
 
 @Entity
 @Table(name = "post")
@@ -39,7 +32,6 @@ public class Post extends BaseEntity {
         this.title = title;
         this.content = content;
     }
-
 
     public void updateUser(User user) {
         if (Objects.nonNull(this.user)) {
