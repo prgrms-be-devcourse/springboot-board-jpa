1952f9f3793953de18bb72b96faac4cfbe448ec2
junho <ssmm0205@naver.com>
2023-08-04 16:02:01 +0900

    feat: post update 기능 추가 및 content 제약 조건 추가

--- src/main/java/com/ray/springbootboard/domain/Post.java
+++ src/main/java/com/ray/springbootboard/domain/Post.java
@@ -6,13 +6,16 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.data.annotation.CreatedBy;
+import org.springframework.util.StringUtils;
 
 import static jakarta.persistence.FetchType.LAZY;
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Getter
+@DynamicUpdate
 @NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor
 public class Post extends BaseTimeEntity {
@@ -27,6 +30,8 @@ public class Post extends BaseTimeEntity {
     private String title;
 
     @Lob
+    @NotBlank
+    @Column(nullable = false)
     private String content;
 
     @ManyToOne(fetch = LAZY)
@@ -39,5 +44,30 @@ public class Post extends BaseTimeEntity {
     public Post(String title, String content) {
         this.title = title;
         this.content = content;
+    }
+
+    public void allocateWriter(User user) {
+        this.user = user;
+    }
+
+    public void update(String title, String content) {
+        validate(title);
+        validate(content);
+        this.title = test(this.title, title);
+        this.content = test(this.content, content);
+    }
+
+    private void validate(String target) {
+        if (!StringUtils.hasText(target)) {
+            throw new IllegalArgumentException("빈 값일 수 없습니다");
+        }
+    }
+
+    public String test(String origin, String target) {
+        if (origin.equals(target)) {
+            return origin;
+        }
+
+        return target;
     }
 }
