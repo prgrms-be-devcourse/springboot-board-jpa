a8eb54dba746d46ebd74bf3819de625808594d1e
baejunil <bjo6300@naver.com>
2023-08-04 16:21:44 +0900

    refactor : postResponse에 userResponse 추가

--- board/src/main/java/com/example/board/domain/post/dto/PostResponse.java
+++ board/src/main/java/com/example/board/domain/post/dto/PostResponse.java
@@ -1,12 +1,17 @@
 package com.example.board.domain.post.dto;
 
 import com.example.board.domain.post.Post;
+import com.example.board.domain.user.dto.UserResponse;
+import java.time.LocalDateTime;
 
-public record PostResponse(Long id, String title, String content, String createdAt,
+public record PostResponse(Long id, String title, String content, UserResponse userResponse,
+                           LocalDateTime createdAt,
                            String createdBy) {
 
   public static PostResponse from(Post post) {
-    return new PostResponse(post.getId(), post.getTitle(), post.getContent(),
-        post.getCreatedAt().toString(), post.getCreatedBy());
+    UserResponse userResponse = UserResponse.from(post.getUser());
+
+    return new PostResponse(post.getId(), post.getTitle(), post.getContent(), userResponse,
+        post.getCreatedAt(), post.getCreatedBy());
   }
 }
--- board/src/main/java/com/example/board/domain/user/dto/UserResponse.java
+++ board/src/main/java/com/example/board/domain/user/dto/UserResponse.java
@@ -1,13 +1,13 @@
 package com.example.board.domain.user.dto;
 
 import com.example.board.domain.user.User;
+import java.time.LocalDateTime;
 
-public record UserResponse(Long id, String name, Integer age, String hobby, String createdAt,
+public record UserResponse(Long id, String name, Integer age, String hobby, LocalDateTime createdAt,
                            String createdBy) {
 
   public static UserResponse from(User user) {
     return new UserResponse(user.getId(), user.getName(), user.getAge(), user.getHobby(),
-        user.getCreatedAt().toString(),
-        user.getCreatedBy());
+        user.getCreatedAt(), user.getCreatedBy());
   }
 }
