0aa91a1811c402bd8d96e63a32630afc536e708a
2jie0516 <2jie0516>
2023-08-05 18:25:59 +0900

    :recycle: api응답에서 localdatetime 변경

--- src/main/java/prgms/boardmission/ApiResponse.java
+++ src/main/java/prgms/boardmission/ApiResponse.java
@@ -7,13 +7,10 @@ import java.time.LocalDateTime;
 public class ApiResponse<T> {
     private int statusCode;
     private T data;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
-    private LocalDateTime serverDatetime;
 
     public ApiResponse(int statusCode, T data) {
         this.statusCode = statusCode;
         this.data = data;
-        this.serverDatetime = LocalDateTime.now();
     }
 
     public static <T> ApiResponse<T> ok(T data) {
