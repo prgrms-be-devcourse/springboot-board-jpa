3bfc47b1591e70263ed95f00b9789e0b6e2b1a24
hseong3243 <hseong3243@gmail.com>
2023-08-05 17:06:35 +0900

    fix: loginService `@Transactional` 추가

--- src/main/java/com/programmers/board/service/LoginService.java
+++ src/main/java/com/programmers/board/service/LoginService.java
@@ -4,6 +4,7 @@ import com.programmers.board.domain.User;
 import com.programmers.board.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.NoSuchElementException;
 
@@ -12,6 +13,7 @@ import java.util.NoSuchElementException;
 public class LoginService {
     private final UserRepository userRepository;
 
+    @Transactional(readOnly = true)
     public Long login(String name) {
         User user = userRepository.findByName(name)
                 .orElseThrow(() -> new NoSuchElementException("존재하지 않는 유저입니다"));
