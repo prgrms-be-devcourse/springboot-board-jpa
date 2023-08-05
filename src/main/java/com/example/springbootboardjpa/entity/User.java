ccc5b9e42c4c609498daf185a773d78296eb4923
jay-so <merry9504@naver.com>
2023-08-05 20:59:15 +0900

    User 클래스 빌더 적용

--- src/main/java/com/example/springbootboardjpa/entity/User.java
+++ src/main/java/com/example/springbootboardjpa/entity/User.java
@@ -2,6 +2,7 @@ package com.example.springbootboardjpa.entity;
 
 import com.example.springbootboardjpa.enums.Hobby;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -29,6 +30,26 @@ public class User extends BaseEntity {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Post> posts = new ArrayList<>();
+
+    @Builder
+    private User(String name, int age, Hobby hobby) {
+        this.name = name;
+        this.age = age;
+        this.hobby = hobby;
+    }
+
+
+    public void updateName(String name) {
+        this.name = name;
+    }
+
+    public void updateAge(int age) {
+        this.age = age;
+    }
+
+    public void updateHobby(Hobby hobby) {
+        this.hobby = hobby;
+    }
 
     public void addPost(Post post) {
         posts.add(post);
