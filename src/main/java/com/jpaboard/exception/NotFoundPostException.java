f635189b8512b9112013ae813ee6804ffd044d97
sujjangOvO <ssu7038@yu.ac.kr>
2023-08-06 00:43:06 +0900

    :recycle: 사용자 정의 예외 상수 메시지를 필드에서 가지고 있도로 변경

--- src/main/java/com/jpaboard/exception/NotFoundPostException.java
+++ src/main/java/com/jpaboard/exception/NotFoundPostException.java
@@ -1,7 +1,10 @@
 package com.jpaboard.exception;
 
 public class NotFoundPostException extends RuntimeException{
-    public NotFoundPostException(String message) {
-        super(message);
+
+    private final static String NOT_FOUND_POST_MESSAGE = "게시글 정보를 찾을 수 없습니다.";
+
+    public NotFoundPostException() {
+        super(NOT_FOUND_POST_MESSAGE);
     }
 }
--- src/main/java/com/jpaboard/exception/NotFoundUserException.java
+++ src/main/java/com/jpaboard/exception/NotFoundUserException.java
@@ -1,7 +1,10 @@
 package com.jpaboard.exception;
 
 public class NotFoundUserException extends RuntimeException{
-    public NotFoundUserException(String message) {
-        super(message);
+
+    private static final String NOT_FOUND_USER_MESSAGE = "회원 정보를 찾을 수 없습니다.";
+
+    public NotFoundUserException() {
+        super(NOT_FOUND_USER_MESSAGE);
     }
 }
