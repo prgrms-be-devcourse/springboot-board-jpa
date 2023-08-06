05fef14a82dd02b2010ed3a968a36ff76536966b
BeommoKoo-dev <95630007+BeommoKoo-dev@users.noreply.github.com>
2023-08-06 21:29:27 +0900

    style : postMapping 주소 수정

--- MyBoard/src/main/java/org/prgrms/myboard/controller/UserRestController.java
+++ MyBoard/src/main/java/org/prgrms/myboard/controller/UserRestController.java
@@ -6,10 +6,7 @@ import org.prgrms.myboard.dto.UserCreateRequestDto;
 import org.prgrms.myboard.dto.UserResponseDto;
 import org.prgrms.myboard.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/users")
 @RestController
@@ -17,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserRestController {
     private final UserService userService;
 
-    @PostMapping("/new")
+    @PostMapping
     public ResponseEntity<UserResponseDto> createUser(@Valid @RequestBody UserCreateRequestDto userCreateRequestDto) {
         return ResponseEntity.ok(userService.createUser(userCreateRequestDto));
     }
