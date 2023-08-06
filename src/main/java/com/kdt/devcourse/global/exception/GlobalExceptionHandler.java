85f62ea5fcb7af583c38e058e6cdb7180e3cc4be
Hejow <gmlwh124@naver.com>
2023-08-06 18:33:15 +0900

    :art: 내부 오류 메시지 코드 에러 메시지로 이동

--- src/main/java/com/kdt/devcourse/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kdt/devcourse/global/exception/GlobalExceptionHandler.java
@@ -7,13 +7,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import static com.kdt.devcourse.global.exception.ErrorCode.INTERNAL_ERROR_MESSAGE;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
-    private static final String INTERNAL_ERROR_MESSAGE = "서버 내부 오류가 발생했습니다.";
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handleEntityNotFoundException(CustomException e) {
@@ -26,6 +25,6 @@ public class GlobalExceptionHandler {
     @ResponseStatus(INTERNAL_SERVER_ERROR)
     public ErrorResponse handleUnexpectedException(RuntimeException e) {
         log.warn("UnexpectedException Occures : {}", e.getMessage());
-        return new ErrorResponse(INTERNAL_ERROR_MESSAGE);
+        return new ErrorResponse(INTERNAL_ERROR_MESSAGE.getMessage());
     }
 }
