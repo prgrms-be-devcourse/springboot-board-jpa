1ff3ef521d33031752f8969757ad4b5e2011cde3
Hejow <gmlwh124@naver.com>
2023-08-06 17:55:12 +0900

    :recycle: 게시글 페이징 처리에 필요한 인자만 받도록 수정

--- src/main/java/com/kdt/devcourse/module/post/presentation/PostController.java
+++ src/main/java/com/kdt/devcourse/module/post/presentation/PostController.java
@@ -5,14 +5,14 @@ import com.kdt.devcourse.module.post.application.PostService;
 import com.kdt.devcourse.module.post.presentation.dto.PostDto;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,8 +34,10 @@ public class PostController {
 
     @GetMapping
     @ResponseStatus(OK)
-    public ApiResponse<Page<PostDto.DefaultResponse>> findAll(@PageableDefault(sort = "id") Pageable pageable) {
-        Page<PostDto.DefaultResponse> responses = postService.findAll(pageable);
+    public ApiResponse<Page<PostDto.DefaultResponse>> findAll(@RequestParam(defaultValue = "0") int page,
+                                                              @RequestParam(defaultValue = "10") int size) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        Page<PostDto.DefaultResponse> responses = postService.findAll(pageRequest);
         return new ApiResponse<>(responses);
     }
 
