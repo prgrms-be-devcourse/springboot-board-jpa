c23a4f41d52be3c98854fb573570fc6e633190af
hseong3243 <hseong3243@gmail.com>
2023-08-06 17:32:11 +0900

    fix: 예외 메시지 오타 수정

--- src/main/java/com/programmers/board/dto/request/PostCreateRequest.java
+++ src/main/java/com/programmers/board/dto/request/PostCreateRequest.java
@@ -9,7 +9,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public class PostCreateRequest {
     private static final String TITLE_VALIDATE = "게시글 제목은 1자 이상, 100자 이하여야 합니다";
-    private static final String CONTENT_VALIDATE = "게시글 본문은 1자 이사이어야 합니다";
+    private static final String CONTENT_VALIDATE = "게시글 본문은 1자 이상이어야 합니다";
     private static final String TITLE_MANDATORY = "게시글 제목은 필수입니다";
     private static final String CONTENT_MANDATORY = "게시글 본문은 필수입니다";
 
