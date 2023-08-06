af4e8da1824c8156c53818466936d00ddd9ea847
jay-so <merry9504@naver.com>
2023-08-06 21:36:33 +0900

    feat: RestDocsConfiguratoin 구현

---
+++ src/test/java/com/example/springbootboardjpa/RestDocs/RestDocsConfiguration.java
@@ -0,0 +1,20 @@
+package com.example.springbootboardjpa.RestDocs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
+
+@Configuration
+public class RestDocsConfiguration {
+
+    @Bean
+    public RestDocumentationResultHandler write() {
+        return MockMvcRestDocumentation.document(
+                "{class-name}/{method-name}",
+                Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+                Preprocessors.preprocessResponse(Preprocessors.prettyPrint())
+        );
+    }
+}
