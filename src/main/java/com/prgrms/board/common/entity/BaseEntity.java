2da43d74bcbf5b46ab4bedfe35f773684a26c9b2
Jaeyoung Ahn <quaki@naver.com>
2023-08-06 21:45:19 +0900

    feat : Post 및 User 엔티티 추가

--- src/main/java/com/prgrms/board/common/entity/BaseEntity.java
+++ src/main/java/com/prgrms/board/common/entity/BaseEntity.java
@@ -4,23 +4,15 @@ package com.prgrms.board.common.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
-
     @Column(nullable = false, updatable = false)
     @CreatedDate
     private LocalDateTime createdDate;
@@ -28,4 +20,15 @@ public abstract class BaseEntity {
     @Column(updatable = false)
     @CreatedBy
     private String createdBy;
+
+    protected BaseEntity() {
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
 }
