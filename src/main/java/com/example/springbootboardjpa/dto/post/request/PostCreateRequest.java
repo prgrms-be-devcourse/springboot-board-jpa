9bab7b963bbea416da5896bfdbfe799dbf839f73
jay-so <merry9504@naver.com>
2023-08-06 21:38:45 +0900

    refactor: Post Dto 리팩토링

--- src/main/java/com/example/springbootboardjpa/dto/post/request/PostCreateRequest.java
+++ src/main/java/com/example/springbootboardjpa/dto/post/request/PostCreateRequest.java
@@ -1,15 +1,18 @@
 package com.example.springbootboardjpa.dto.post.request;
 
 import com.example.springbootboardjpa.entity.Post;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class PostCreateRequest {
 
     private String title;
     private String content;
+    private Long userId;
 
     public Post toEntity() {
         return Post.builder()
--- src/main/java/com/example/springbootboardjpa/dto/post/request/PostUpdateRequest.java
+++ src/main/java/com/example/springbootboardjpa/dto/post/request/PostUpdateRequest.java
@@ -1,10 +1,12 @@
 package com.example.springbootboardjpa.dto.post.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class PostUpdateRequest {
 
     private String title;
