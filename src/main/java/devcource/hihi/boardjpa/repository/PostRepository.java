fb30983e28f292365d59582f2cf173ff7e5432af
bona <ra_ari@naver.com>
2023-08-07 00:49:30 -0700

    [refactor] query 상수로 빼기

--- src/main/java/devcource/hihi/boardjpa/repository/PostRepository.java
+++ src/main/java/devcource/hihi/boardjpa/repository/PostRepository.java
@@ -8,7 +8,9 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    @Query(value = "SELECT * FROM post WHERE post_id > :cursor ORDER BY post_id ASC LIMIT :pageSize", nativeQuery = true)
+
+    String CursorQuery = "SELECT * FROM post WHERE post_id > :cursor ORDER BY post_id ASC LIMIT :pageSize";
+    @Query(value = CursorQuery, nativeQuery = true)
     List<Post> findByCursor(@Param("cursor") Long cursor, @Param("pageSize") int pageSize);
 
 }
