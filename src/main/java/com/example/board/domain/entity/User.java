21cc3e658d87fc184eeee6e3c0cdc6fa8771a882
seongHyun-Min <steve6133@naver.com>
2023-08-07 11:37:33 +0900

    refactor : UserUpdateDto validation 수정 NotBlank -> @Pattern 정규식 동적으로 유저 update 가능

--- src/main/java/com/example/board/domain/entity/User.java
+++ src/main/java/com/example/board/domain/entity/User.java
@@ -48,10 +48,6 @@ public class User extends BaseTime {
 
     //업데이트 dirty checking
     public void update(UserUpdateRequestDto requestDto) {
-        //질문 : updateRequestDto에 validation으로 값을 검증하는 로직을 작성하면 null값이 들어오지 못해 동적으로 엔티티의 변경이 불가능합니다.
-        //그렇다고 valdation으로 검증 로직을 제거 하면 원하지 않은 값이 들어와 엔티티에 문제가 생깁니다.
-        //이전에는 생성자로 새로 생성하고 save 하는 머지 방식을 선택하여 업데이트를 하였을 때는 위와같은 문제가 생기지 않았으나
-        //dirty chekcing으로 엔티티를 변경할 때는 이러한 문제를 어떻게 해결하는지 궁굼합니다.!!
         this.name = (requestDto.getName() != null) ? requestDto.getName() : this.name;
         this.email = (requestDto.getEmail() != null) ? requestDto.getEmail() : this.email;
         this.password = (requestDto.getPassword() != null) ? requestDto.getPassword() : this.password;
--- src/main/java/com/example/board/dto/user/UserUpdateRequestDto.java
+++ src/main/java/com/example/board/dto/user/UserUpdateRequestDto.java
@@ -7,21 +7,21 @@ import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserUpdateRequestDto {
-    @NotBlank(message = "이름에는 공백을 사용할 수 없습니다")
+    private static final String NON_BLANK_PATTERN = "^(?!\\s*$).+";
+
+    @Pattern(regexp = NON_BLANK_PATTERN, message = "공백은 허용되지 않습니다")
     private String name;
 
-    @NotBlank(message = "이메일에는 공백을 사용할 수 없습니다")
     @Email(message = "올바른 이메일 형식을 입력해주세요")
     private String email;
 
-    @NotBlank(message = "비밀번호에는 공백을 사용할 수 없습니다")
     @Size(min = 8, message = "비밀번호는 최소 8자리 이상이여야 합니다")
     private String password;
 
