d1809821950d906d1126bad1258c63dee75a3679
2jie0516 <2jie0516>
2023-08-07 20:10:32 +0900

    :recycle: API url에 공통적인 부분 매핑 처리

--- src/main/java/prgms/boardmission/post/controller/PostController.java
+++ src/main/java/prgms/boardmission/post/controller/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import prgms.boardmission.ApiResponse;
 import prgms.boardmission.post.dto.PostDto;
@@ -17,6 +18,7 @@ import prgms.boardmission.post.service.PostService;
 
 import java.util.NoSuchElementException;
 
+@RequestMapping("/posts")
 @RestController
 public class PostController {
     private final PostService postService;
@@ -30,25 +32,25 @@ public class PostController {
         this.postService = postService;
     }
 
-    @PostMapping("/posts")
+    @PostMapping("/")
     public ApiResponse<Long> save(@RequestBody PostDto postDto) {
         Long id = postService.save(postDto);
         return ApiResponse.ok(id);
     }
 
-    @GetMapping("/posts")
+    @GetMapping("/")
     public ApiResponse<Page<PostDto>> findAll(Pageable pageable) {
         Page<PostDto> postDtoPage = postService.findAll(pageable);
         return ApiResponse.ok(postDtoPage);
     }
 
-    @GetMapping("/posts/{postId}")
+    @GetMapping("/{postId}")
     public ApiResponse<PostDto> findById(@PathVariable long postId) {
         PostDto postDto = postService.findById(postId);
         return ApiResponse.ok(postDto);
     }
 
-    @PatchMapping("/posts/{postId}")
+    @PatchMapping("/{postId}")
     public ApiResponse<Long> update(@PathVariable long postId, PostUpdateDto postUpdateDto) {
         Long updateId = postService.updatePost(postId, postUpdateDto);
         return ApiResponse.ok(updateId);
