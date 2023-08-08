952e6aca079de42e63dba6aa5b427062bd571519
KimMinheee <99alsgmlalsl@naver.com>
2023-08-08 15:51:55 +0900

    [Feat] : ErrorResponse 구현

---
+++ src/main/java/com/blackdog/springbootBoardJpa/global/response/ErrorResponse.java
@@ -0,0 +1,31 @@
+package com.blackdog.springbootBoardJpa.global.response;
+
+public class ErrorResponse {
+
+    private final String code;
+    private final String message;
+
+    public ErrorResponse(
+            final String code,
+            final String message
+    ) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public static ErrorResponse of(ErrorCode errorCode) {
+        return new ErrorResponse(
+                errorCode.getCode(),
+                errorCode.getMessage()
+        );
+    }
+
+    @Override
+    public String toString() {
+        return "ErrorResponse{" +
+                "code='" + code + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+
+}
