c64bc78ae760425d34d49b429a4625a018c34885
hyee0715 <hyee0715@naver.com>
2023-08-08 09:58:08 +0900

    refactor: GlobalExceptionHandler log 제거

--- src/main/java/com/programmers/springbootboardjpa/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/programmers/springbootboardjpa/global/error/GlobalExceptionHandler.java
@@ -2,31 +2,26 @@ package com.programmers.springbootboardjpa.global.error;
 
 import com.programmers.springbootboardjpa.global.error.exception.InvalidEntityValueException;
 import com.programmers.springbootboardjpa.global.error.exception.NotFoundException;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(InvalidEntityValueException.class)
     private ErrorResponse invalidEntityValueExceptionHandler(InvalidEntityValueException exception) {
-        log.error("handle invalidEntityValueException: {}", exception.getMessage());
 
         return new ErrorResponse(exception.getErrorCode());
     }
 
     @ExceptionHandler(NotFoundException.class)
     private ErrorResponse notFoundExceptionHandler(NotFoundException exception) {
-        log.error("handle notFoundException: {}", exception.getMessage());
 
         return new ErrorResponse(exception.getErrorCode());
     }
 
     @ExceptionHandler(Exception.class)
     private ErrorResponse exceptionHandler(Exception exception) {
-        log.error("handle exception: {}", exception.getMessage());
 
         return new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR, exception.getMessage());
     }
