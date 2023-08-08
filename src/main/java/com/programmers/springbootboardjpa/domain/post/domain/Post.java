43bdb2273184cae945597c97711f94d305f1616a
hyee0715 <hyee0715@naver.com>
2023-08-08 13:49:11 +0900

    refactor: Post의 user cascade 설정 제거

--- src/main/java/com/programmers/springbootboardjpa/domain/post/domain/Post.java
+++ src/main/java/com/programmers/springbootboardjpa/domain/post/domain/Post.java
@@ -28,7 +28,7 @@ public class Post extends BaseEntity {
     @Column(nullable = false)
     private String content;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
