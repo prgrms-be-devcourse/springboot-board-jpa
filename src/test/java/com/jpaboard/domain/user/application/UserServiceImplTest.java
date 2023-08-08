e8b4da2cffd48ee1353ff827958b5ba4aba7664c
weonest <geonhee33@naver.com>
2023-08-08 17:29:54 +0900

    refactor: 서비스 레이어 테스트 Mock & Instancio 적용

--- src/test/java/com/jpaboard/domain/user/application/UserServiceImplTest.java
+++ src/test/java/com/jpaboard/domain/user/application/UserServiceImplTest.java
@@ -1,65 +1,109 @@
 package com.jpaboard.domain.user.application;
 
+import com.jpaboard.domain.user.User;
+import com.jpaboard.domain.user.UserConverter;
 import com.jpaboard.domain.user.dto.request.UserCreationRequest;
 import com.jpaboard.domain.user.dto.request.UserUpdateRequest;
 import com.jpaboard.domain.user.dto.response.UserResponse;
-import jakarta.persistence.EntityNotFoundException;
-import org.assertj.core.api.ObjectAssert;
-import org.junit.jupiter.api.BeforeEach;
+import com.jpaboard.domain.user.infrastructure.UserRepository;
+import org.instancio.Instancio;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.instancio.Select.all;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
-@DataJpaTest
-@Import(UserServiceImpl.class)
+@ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
-    @Autowired
-    private UserService userService;
+    @InjectMocks
+    private UserServiceImpl userServiceImpl;
 
-    private Long userId;
+    @Mock
+    private UserRepository userRepository;
 
-    @BeforeEach
-    void setup() {
-        userId = userService.createUser(new UserCreationRequest("이름", 20, "취미"));
+    @Test
+    @DisplayName("유저를 생성할 수 있다.")
+    void testCreateUser() {
+        // given
+        UserCreationRequest request = Instancio.create(UserCreationRequest.class);
+        User user = createUserByCreationRequest(request);
+        given(userRepository.save(any(User.class))).willReturn(user);
+
+        // when
+        Long createdUserId = userServiceImpl.createUser(request);
+
+        // then
+        assertThat(createdUserId).isEqualTo(user.getId());
     }
 
     @Test
     @DisplayName("유저를 조회할 수 있다.")
     void testFindUserById() {
         // given
+        User user = createUser();
+        given(userRepository.findById(user.getId())).willReturn(Optional.of(user));
+        UserResponse expected = createUserResponse(user);
+
         // when
-        UserResponse userResponse = userService.findUserById(userId);
+        UserResponse response = userServiceImpl.findUserById(user.getId());
+
         // then
-        assertThat(userResponse).isNotNull();
+        assertThat(response).isEqualTo(expected);
     }
 
     @Test
     @DisplayName("유저를 수정할 수 있다.")
-    void testUpdate() {
+    void testUpdateUser() {
         // given
-        UserUpdateRequest request = new UserUpdateRequest("수정된 이름", 22, "수정된 취미");
+        User user = mock(User.class);
+        UserUpdateRequest request = Instancio.create(UserUpdateRequest.class);
+        given(userRepository.findById(user.getId())).willReturn(Optional.of(user));
+
         // when
-        userService.updateUser(userId, request);
-        UserResponse userResponse = userService.findUserById(userId);
+        userServiceImpl.updateUser(user.getId(), request);
+
         // then
-        assertThat(userResponse).isNotNull()
-                .extracting(UserResponse::name, UserResponse::age, UserResponse::hobby)
-                .containsExactly(request.name(), request.age(), request.hobby());
+        verify(userRepository, atMostOnce()).findById(user.getId());
+        verify(user, atMostOnce()).update(request.name(), request.age(), request.hobby());
     }
 
     @Test
     @DisplayName("유저를 삭제할 수 있다.")
-    void testDelete() {
+    void testDeleteUser() {
         // given
+        User user = mock(User.class);
+        Long userId = user.getId();
+        doNothing().when(userRepository).deleteById(userId);
+
         // when
-        userService.deleteUser(userId);
+        userServiceImpl.deleteUser(userId);
+
         // then
-        assertThatThrownBy(() -> userService.findUserById(userId))
-                .isInstanceOf(EntityNotFoundException.class);
+        verify(userRepository, only()).deleteById(userId);
+    }
+
+    private User createUser() {
+        return Instancio.create(User.class);
+    }
+
+    private UserResponse createUserResponse(User user) {
+        return Instancio.of(UserResponse.class)
+                .supply(all(UserResponse.class), gen -> UserConverter.convertEntityToResponse(user))
+                .create();
+    }
+
+    private User createUserByCreationRequest(UserCreationRequest request) {
+        return Instancio.of(User.class)
+                .supply(all(User.class), gen -> UserConverter.convertRequestToEntity(request))
+                .create();
     }
 }
