cdb0a8e0f16a636689e3348838828b4ade144bd1
hseong3243 <hseong3243@gmail.com>
2023-08-09 10:37:02 +0900

    refactor: util 클래스 final로 변경

--- src/main/java/com/programmers/board/constant/SessionConst.java
+++ src/main/java/com/programmers/board/constant/SessionConst.java
@@ -4,6 +4,6 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public abstract class SessionConst {
+public final class SessionConst {
     public static final String LOGIN_USER_ID = "userId";
 }
