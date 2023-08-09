8c4b0ae9741dc28eb9e289ec46c276b3884cadf2
JHS <wnghdtjr129@naver.com>
2023-08-09 16:51:23 +0900

    refactor: post 생성 시점에 user 조회 후 주입

--- src/main/java/com/ray/springbootboard/controller/dto/PostSaveRequest.java
+++ src/main/java/com/ray/springbootboard/controller/dto/PostSaveRequest.java
@@ -4,7 +4,7 @@ import com.ray.springbootboard.domain.Post;
 import com.ray.springbootboard.domain.User;
 
 public record PostSaveRequest(String title, String content) {
-    public Post toEntity(Long userId) {
-        return new Post(title, content, User.withOnlyId(userId));
+    public Post toEntity(User user) {
+        return new Post(title, content, user);
     }
 }
