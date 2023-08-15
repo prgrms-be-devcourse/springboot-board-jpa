13d1b2f582895ef862d02d6a62594788f00d7d6f
sujjangOvO <ssu7038@yu.ac.kr>
2023-08-15 14:20:48 +0900

    :fire: ApiResponse 제거 및 데이터 반환 방식 수정

--- src/main/java/com/jpaboard/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jpaboard/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.jpaboard.exception;
 
-import com.jpaboard.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,19 +10,19 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiResponse<String> invalidHandle(IllegalArgumentException exception) {
-        return ApiResponse.fail(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
+    public String invalidHandle(IllegalArgumentException exception) {
+        return exception.getMessage();
     }
 
     @ExceptionHandler(NotFoundPostException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ApiResponse<String> notFoundUserHandle(NotFoundPostException notFoundPostException) {
-        return ApiResponse.fail(HttpStatus.NOT_FOUND.value(), notFoundPostException.getMessage());
+    public String notFoundUserHandle(NotFoundPostException notFoundPostException) {
+        return notFoundPostException.getMessage();
     }
 
     @ExceptionHandler(NotFoundUserException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ApiResponse<String> notFoundUserHandle(NotFoundUserException notFoundUserException) {
-        return ApiResponse.fail(HttpStatus.NOT_FOUND.value(), notFoundUserException.getMessage());
+    public String notFoundUserHandle(NotFoundUserException notFoundUserException) {
+        return notFoundUserException.getMessage();
     }
 }
--- src/main/java/com/jpaboard/post/ui/PostRestController.java
+++ src/main/java/com/jpaboard/post/ui/PostRestController.java
@@ -1,13 +1,11 @@
 package com.jpaboard.post.ui;
 
-import com.jpaboard.ApiResponse;
 import com.jpaboard.post.application.PostService;
+import com.jpaboard.post.ui.dto.PageParam;
 import com.jpaboard.post.ui.dto.PostDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 
 @RestController
@@ -20,28 +18,28 @@ public class PostRestController {
     }
 
     @GetMapping
-    public ApiResponse<Page<PostDto.Response>> findPostAll(@RequestBody Map<String, Integer> param) {
-        int page = param.get("page");
-        int size = param.get("size");
-        return ApiResponse.ok(postService.findPostAll(PageRequest.of(page, size)));
+    public Page<PostDto.Response> findPostAll(@RequestBody PageParam param) {
+        int page = param.page();
+        int size = param.size();
+        return postService.findPostAll(PageRequest.of(page, size));
     }
 
     @GetMapping("/{id}")
-    public ApiResponse<PostDto.Response> findPost(@PathVariable long id) {
+    public PostDto.Response findPost(@PathVariable long id) {
         PostDto.Response response = postService.findPost(id);
-        return ApiResponse.ok(response);
+        return response;
     }
 
     @PostMapping
-    public ApiResponse<Long> createPost(@RequestBody PostDto.Request request) {
+    public Long createPost(@RequestBody PostDto.Request request) {
         Long postId = postService.createPost(request);
-        return ApiResponse.ok(postId);
+        return postId;
     }
 
     @PatchMapping("/{id}")
-    public ApiResponse<Long> updatePost(@PathVariable long id,
-                                        @RequestBody PostDto.PostUpdateRequest postUpdateRequest) {
+    public Long updatePost(@PathVariable long id,
+                           @RequestBody PostDto.PostUpdateRequest postUpdateRequest) {
         Long postId = postService.updatePost(id, postUpdateRequest);
-        return ApiResponse.ok(postId);
+        return postId;
     }
 }
