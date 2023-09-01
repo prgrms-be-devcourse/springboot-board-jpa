2977f68c40934da2848bcb59d4f3407d61edaef1
born-A <ra_ari@naver.com>
2023-09-01 22:39:25 +0900

    [feat] post service - cursor based pagenation

--- src/main/java/devcource/hihi/boardjpa/repository/PostRepository.java
+++ src/main/java/devcource/hihi/boardjpa/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package devcource.hihi.boardjpa.repository;
 
 import devcource.hihi.boardjpa.domain.Post;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,8 +10,9 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    String CursorQuery = "SELECT * FROM post WHERE post_id > :cursor ORDER BY post_id ASC LIMIT :pageSize";
-    @Query(value = CursorQuery, nativeQuery = true)
-    List<Post> findByCursor(@Param("cursor") Long cursor, @Param("pageSize") int pageSize);
+    List<Post> findAllByOrderByIdDesc(Pageable page);
 
+    List<Post> findByIdLessThanOrderByIdDesc(Long id, Pageable page);
+
+    Boolean existsByIdLessThan(Long id);
 }
--- src/main/java/devcource/hihi/boardjpa/service/PostService.java
+++ src/main/java/devcource/hihi/boardjpa/service/PostService.java
@@ -2,12 +2,14 @@ package devcource.hihi.boardjpa.service;
 
 import devcource.hihi.boardjpa.domain.Post;
 import devcource.hihi.boardjpa.domain.User;
+import devcource.hihi.boardjpa.dto.CursorResult;
 import devcource.hihi.boardjpa.dto.post.CreateRequestDto;
 import devcource.hihi.boardjpa.dto.post.ResponsePostDto;
 import devcource.hihi.boardjpa.dto.post.SearchResponseDto;
 import devcource.hihi.boardjpa.dto.post.UpdateRequestDto;
 import devcource.hihi.boardjpa.repository.PostRepository;
 import devcource.hihi.boardjpa.repository.UserRepository;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,13 +27,14 @@ public class PostService {
     }
 
     @Transactional
-    public ResponsePostDto createPost(CreateRequestDto postDto) {
+    public ResponsePostDto createPost(Long userId, CreateRequestDto postDto) {
         Post post = postDto.toEntity();
 
-        User user = userRepository.findById(postDto.user().getId()).orElseThrow(() -> new RuntimeException("작성자는 null이 될 수 없습니다"));
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("작성자는 null이 될 수 없습니다"));
         post.allocateUser(user);
+        Post save = postRepository.save(post);
 
-        return Post.toResponseDto(post);
+        return Post.toResponseDto(save);
     }
     public ResponsePostDto findPost(Long id) {
         Post post = postRepository.findById(id).orElseThrow(() -> new RuntimeException("id에 해당하는 post가 없습니다."));
@@ -51,23 +54,23 @@ public class PostService {
         postRepository.deleteById(id);
     }
 
-    public SearchResponseDto<Post> getPostsByCursor(Long cursor, int limit) {
-        List<Post> posts = postRepository.findByCursor(cursor, limit + 1);
-        Long prevCursor = null;
-        Long nextCursor = null;
+    public CursorResult<Post> get(Long cursorId, Pageable page) {
+        final List<Post> posts = getPosts(cursorId, page);
+        final Long lastIdOfList = posts.isEmpty() ?
+                null : posts.get(posts.size() - 1).getId();
 
-        if (!posts.isEmpty()) {
-            if (posts.size() > limit) {
-                posts.remove(limit);
-                nextCursor = posts.get(limit - 1).getId();
-            }
+        return new CursorResult<>(posts, hasNext(lastIdOfList));
+    }
 
-            if (cursor != null) {
-                prevCursor = cursor;
-            }
-        }
+    public List<Post> getPosts(Long id, Pageable page) {
+        return id == null ?
+                this.postRepository.findAllByOrderByIdDesc(page) :
+                this.postRepository.findByIdLessThanOrderByIdDesc(id, page);
+    }
 
-        return new SearchResponseDto<>(posts, prevCursor, nextCursor);
+    public Boolean hasNext(Long id) {
+        if (id == null) return false;
+        return this.postRepository.existsByIdLessThan(id);
     }
 
 
