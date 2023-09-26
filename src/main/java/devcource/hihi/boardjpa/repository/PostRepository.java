3fe65ebfb7948169aef719817af3a57260e22f57
born-A <ra_ari@naver.com>
2023-09-26 19:46:58 +0900

    [refactor]  PostRepository

--- src/main/java/devcource/hihi/boardjpa/repository/PostRepository.java
+++ src/main/java/devcource/hihi/boardjpa/repository/PostRepository.java
@@ -4,15 +4,16 @@ import devcource.hihi.boardjpa.domain.Post;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    List<Post> findAllByOrderByIdDesc(Pageable page);
+    @Query(value = "select * from Post p limit :size", nativeQuery = true)
+    List<Post> findFirstPage(int size);
 
-    List<Post> findByIdLessThanOrderByIdDesc(Long id, Pageable page);
+    @Query(value = "select * from Post p where p.id >: cursorId limit :size", nativeQuery = true)
+    List<Post> findByIdLessThanOrderByIdDesc(Long cursorId, int size);
 
     Boolean existsByIdLessThan(Long id);
 }
