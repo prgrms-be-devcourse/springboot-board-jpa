66fa377295403b4abdfaaf258c53ca912b88f438
KimMinhee <99alsgmlalsl@naver.com>
2023-09-29 19:29:42 +0900

    [Refactor] : Response Dto 클래스 내 @Valid 검증 어노테이션 삭제

--- src/main/java/com/blackdog/springbootBoardJpa/domain/post/service/dto/PostResponse.java
+++ src/main/java/com/blackdog/springbootBoardJpa/domain/post/service/dto/PostResponse.java
@@ -8,24 +8,17 @@ import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 public record PostResponse(
-        @NotNull
         Long id,
 
-        @NotBlank
         String title,
 
-        @NotNull
-        @Size(max = 255)
         String content,
 
-        @NotBlank
         String name,
 
-        @NotNull
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
         LocalDateTime createdAt,
 
-        @NotNull
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
         LocalDateTime updatedAt
 ) {
--- src/main/java/com/blackdog/springbootBoardJpa/domain/user/service/dto/UserResponse.java
+++ src/main/java/com/blackdog/springbootBoardJpa/domain/user/service/dto/UserResponse.java
@@ -1,29 +1,21 @@
 package com.blackdog.springbootBoardJpa.domain.user.service.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
 public record UserResponse(
-        @NotNull
         Long id,
 
-        @NotBlank
         String name,
 
-        @NotNull
         int age,
 
-        @NotBlank
         String hobby,
 
-        @NotNull
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
         LocalDateTime createdAt,
 
-        @NotNull
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
         LocalDateTime updatedAt
 ) {
