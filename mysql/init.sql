77a14e25f399e770e339401fcca87ab42ecadf31
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-16 21:30:47 +0900

    chore: test table 작성 및 검증 의존성 추가

--- mysql/init.sql
+++ mysql/init.sql
@@ -26,3 +26,32 @@ create table posts
 );
 
 insert into users (name, age, hobby, created_at, updated_at) values ('test', 20, 'coding', now(), now());
+
+CREATE DATABASE IF NOT EXISTS board_test;
+
+USE board_test;
+
+create table users
+(
+    user_id bigint primary key auto_increment,
+    name varchar(20) not null ,
+    age int not null,
+    hobby varchar(20) not null ,
+    created_at datetime not null ,
+    updated_at datetime not null
+);
+
+create table posts
+(
+    post_id bigint primary key auto_increment,
+    user_id bigint not null ,
+    title varchar(30) not null,
+    contents blob not null ,
+    created_at datetime not null ,
+    updated_at datetime not null,
+    created_by varchar(20) not null,
+
+    constraint post_user_fk FOREIGN key (user_id) references users(user_id)
+);
+
+insert into users (name, age, hobby, created_at, updated_at) values ('test', 20, 'coding', now(), now());
