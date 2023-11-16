254b96165a3a10c05a83539c9080c30caa701cba
EomYerim <djadpfa1002@naver.com>
2023-11-16 16:16:10 +0900

    chore: 프로젝트 생성

---
+++ src/test/java/org/prgms/springbootboardjpayu/SpringbootBoardJpaYuApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prgms.springbootboardjpayu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBoardJpaYuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
