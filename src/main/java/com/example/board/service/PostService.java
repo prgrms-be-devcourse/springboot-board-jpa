68f1e21b9a8631e47e7d42cf7913219e7f9e94af
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-17 17:35:07 +0900

    #5 feat: Post 페이지 조회 서비스 구현

--- src/main/java/com/example/board/service/PostService.java
+++ src/main/java/com/example/board/service/PostService.java
@@ -1,6 +1,7 @@
 package com.example.board.service;
 
 import com.example.board.dto.PostDto;
+import com.example.board.dto.PostResponseDto;
 import com.example.board.exception.BaseException;
 import com.example.board.exception.ErrorMessage;
 import com.example.board.model.Post;
@@ -8,6 +9,8 @@ import com.example.board.model.User;
 import com.example.board.repository.PostRepository;
 import com.example.board.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,5 +26,9 @@ public class PostService {
                 new BaseException(ErrorMessage.USER_NOT_FOUND)
         );
         return postRepository.save(Post.from(user, postDto)).getId();
+    }
+
+    public Page<PostResponseDto> readAllPost(Pageable pageable) {
+        return postRepository.findAll(pageable).map(PostResponseDto::from);
     }
 }
