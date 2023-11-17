973c558d1d8a25d62b39f60cd8151375c002b0b8
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-17 17:34:45 +0900

    #5 feat: Post 조회용 dto 구현

---
+++ src/main/java/com/example/board/dto/PostResponseDto.java
@@ -0,0 +1,13 @@
+package com.example.board.dto;
+
+import com.example.board.model.Post;
+
+public record PostResponseDto(
+        String userName,
+        String title
+) {
+
+    public static PostResponseDto from(Post post) {
+        return new PostResponseDto(post.getTitle(), post.getUser().getName());
+    }
+}
