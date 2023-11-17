e3603d7408143eb117d46c24362270996977728e
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-17 17:35:14 +0900

    #5 feat: Post 페이지 조회 api 구현

--- src/main/java/com/example/board/controller/PostController.java
+++ src/main/java/com/example/board/controller/PostController.java
@@ -1,24 +1,18 @@
 package com.example.board.controller;
 
 import com.example.board.dto.PostDto;
-import com.example.board.exception.BaseException;
+import com.example.board.dto.PostResponseDto;
 import com.example.board.exception.BindingException;
-import com.example.board.exception.ErrorMessage;
 import com.example.board.response.Response;
 import com.example.board.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/posts")
@@ -30,6 +24,13 @@ public class PostController {
     public Response<Long> save(@RequestBody @Validated PostDto postDto, BindingResult bindingResult) {
         bindChecking(bindingResult);
         return Response.success(postService.save(postDto));
+    }
+
+    @GetMapping
+    public Response<Page<PostResponseDto>> readAllPost(
+            @PageableDefault(page = 0, size = 10) Pageable pageable
+    ){
+        return Response.success(postService.readAllPost(pageable));
     }
 
     private static void bindChecking(BindingResult bindingResult) {
