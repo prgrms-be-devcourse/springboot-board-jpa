4ddf132764ae3322e84d1223b365f6aa71165ff2
EomYerim <djadpfa1002@naver.com>
2023-11-18 11:25:37 +0900

    test: 게시글 수정 테스트 수행

--- src/test/java/org/prgms/springbootboardjpayu/controller/PostControllerTest.java
+++ src/test/java/org/prgms/springbootboardjpayu/controller/PostControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.prgms.springbootboardjpayu.dto.request.CreatePostRequest;
+import org.prgms.springbootboardjpayu.dto.request.UpdatePostRequest;
 import org.prgms.springbootboardjpayu.dto.response.PostResponse;
 import org.prgms.springbootboardjpayu.dto.response.UserProfile;
 import org.prgms.springbootboardjpayu.service.PostService;
@@ -20,6 +21,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -83,6 +85,43 @@ class PostControllerTest {
         mockMvc.perform(
                 post("/api/v1/posts")
                         .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isBadRequest());
+    }
+
+    @DisplayName("게시글 수정에 성공한다.")
+    @Test
+    void updatePost() throws Exception {
+        // given
+        Long id = 1L;
+        UpdatePostRequest updateRequest = new UpdatePostRequest("제목 수정", "내용 수정");
+
+        PostResponse response = createPostResponse();
+        given(postService.updatePost(id, updateRequest)).willReturn(response);
+
+        // when then
+        mockMvc.perform(
+                patch("/api/v1/posts/{id}", id)
+                        .content(objectMapper.writeValueAsString(updateRequest))
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk());
+    }
+
+    @DisplayName("수정된 제목이 1 ~ 30자 범위를 초과로 게시글 수정에 실패한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "  ", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"})
+    void updatePostWithOutOfRangeTitle(String title) throws Exception {
+        // given
+        Long id = 1L;
+        UpdatePostRequest updateRequest = new UpdatePostRequest(title, "내용 수정");
+
+        PostResponse response = createPostResponse();
+        given(postService.updatePost(id, updateRequest)).willReturn(response);
+
+        // when then
+        mockMvc.perform(
+                patch("/api/v1/posts/{id}", id)
+                        .content(objectMapper.writeValueAsString(updateRequest))
                         .contentType(MediaType.APPLICATION_JSON)
         ).andExpect(status().isBadRequest());
     }
--- src/test/java/org/prgms/springbootboardjpayu/service/PostServiceTest.java
+++ src/test/java/org/prgms/springbootboardjpayu/service/PostServiceTest.java
@@ -1,5 +1,6 @@
 package org.prgms.springbootboardjpayu.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.prgms.springbootboardjpayu.domain.User;
 import org.prgms.springbootboardjpayu.dto.request.CreatePostRequest;
+import org.prgms.springbootboardjpayu.dto.request.UpdatePostRequest;
 import org.prgms.springbootboardjpayu.dto.response.PostResponse;
 import org.prgms.springbootboardjpayu.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -54,7 +56,7 @@ class PostServiceTest {
 
         // when then
         assertThatThrownBy(() -> postService.createPost(request))
-                .isInstanceOf(IllegalArgumentException.class)
+                .isInstanceOf(EntityNotFoundException.class)
                 .hasMessage("존재하지 않는 유저입니다.");
     }
 
@@ -79,6 +81,55 @@ class PostServiceTest {
 
         // when then
         assertThatThrownBy(() -> postService.createPost(request))
+                .isInstanceOf(ConstraintViolationException.class);
+    }
+
+    @DisplayName("게시글 수정에 성공한다.")
+    @Test
+    void updatePost() {
+        // given
+        User user = createUser("예림");
+        CreatePostRequest request = new CreatePostRequest("제목", "내용", user.getId());
+        PostResponse savedPost = postService.createPost(request);
+
+        String newTitle = "제목 수정";
+        String newContent = "내용 수정";
+        UpdatePostRequest updateRequest = new UpdatePostRequest(newTitle, newContent);
+
+        // when
+        PostResponse updatedPost = postService.updatePost(savedPost.id(), updateRequest);
+
+        // then
+        assertThat(updatedPost).extracting("title", "content")
+                .containsExactly(newTitle, newContent);
+    }
+
+    @DisplayName("게시글이 없으면 게시글 수정에 실패한다.")
+    @Test
+    void updatePostWithNonExistPost() {
+        // given
+        Long invalidPostId = 999L;
+        UpdatePostRequest updateRequest = new UpdatePostRequest("제목 수정", "내용 수정");
+
+        // when then
+        assertThatThrownBy(() -> postService.updatePost(invalidPostId, updateRequest))
+                .isInstanceOf(EntityNotFoundException.class)
+                .hasMessage("존재하지 않는 게시글입니다.");
+    }
+
+    @DisplayName("수정된 제목이 1 ~ 30자 범위를 초과로 게시글 수정에 실패한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "  ", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"})
+    void updatePostWithOutOfRangeTitle(String title) {
+        // given
+        User user = createUser("예림");
+        CreatePostRequest request = new CreatePostRequest("제목", "내용", user.getId());
+        PostResponse savedPost = postService.createPost(request);
+
+        UpdatePostRequest updateRequest = new UpdatePostRequest(title, "내용 수정");
+
+        // when then
+        assertThatThrownBy(() -> postService.updatePost(savedPost.id(), updateRequest))
                 .isInstanceOf(ConstraintViolationException.class);
     }
 
