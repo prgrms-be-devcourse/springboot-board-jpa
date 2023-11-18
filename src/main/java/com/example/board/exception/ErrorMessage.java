a7a7ecab8557563c0af8f53c1e5c3e7230a82819
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-18 15:26:02 +0900

    #6 feat: 단건 조회 시 게시글이 존재하지 않는 경우의 예외 메시지 추가

--- src/main/java/com/example/board/exception/ErrorMessage.java
+++ src/main/java/com/example/board/exception/ErrorMessage.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorMessage {
     INTERNAL_SERVER_ERROR(500, HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부에서 장애가 발생했습니다."),
     // 1xxx user exception
-    USER_NOT_FOUND(1000, HttpStatus.NOT_FOUND, "찾으시는 사용자가 없습니다.");
+    USER_NOT_FOUND(1000, HttpStatus.NOT_FOUND, "찾으시는 사용자가 없습니다."),
+
+    //2xxx post exception
+    POST_NOT_FOUND(2000, HttpStatus.NOT_FOUND, "찾으시는 게시글이 없습니다.");
 
     private int code;
     private HttpStatus httpStatus;
