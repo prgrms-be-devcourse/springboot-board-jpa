ba4f41a8358f0d2b78b5a602436db4e6822a2722
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-18 16:23:12 +0900

    #6 feat: 게시글 수정 api 구현

--- src/main/java/com/example/board/controller/PostController.java
+++ src/main/java/com/example/board/controller/PostController.java
@@ -3,6 +3,7 @@ package com.example.board.controller;
 import com.example.board.dto.PostDetailResponseDto;
 import com.example.board.dto.PostDto;
 import com.example.board.dto.PostResponseDto;
+import com.example.board.dto.PostUpdateDto;
 import com.example.board.exception.BindingException;
 import com.example.board.response.Response;
 import com.example.board.service.PostService;
@@ -20,24 +21,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class PostController {
     private final PostService postService;
-
-    @PostMapping
-    public Response<Long> save(@RequestBody @Validated PostDto postDto, BindingResult bindingResult) {
-        bindChecking(bindingResult);
-        return Response.success(postService.save(postDto));
-    }
-
-    @GetMapping
-    public Response<Page<PostResponseDto>> readAllPost(
-            @PageableDefault Pageable pageable
-    ){
-        return Response.success(postService.readAllPost(pageable));
-    }
-
-    @GetMapping("/{postId}")
-    public Response<PostDetailResponseDto> readDetailPost(@PathVariable Long postId){
-        return Response.success(postService.readPostDetail(postId));
-    }
 
     private static void bindChecking(BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
@@ -59,4 +42,30 @@ public class PostController {
         }
     }
 
+    @PostMapping
+    public Response<Long> save(@RequestBody @Validated PostDto postDto, BindingResult bindingResult) {
+        bindChecking(bindingResult);
+        return Response.success(postService.save(postDto));
+    }
+
+    @GetMapping
+    public Response<Page<PostResponseDto>> readAllPost(
+            @PageableDefault Pageable pageable
+    ) {
+        return Response.success(postService.readAllPost(pageable));
+    }
+
+    @GetMapping("/{postId}")
+    public Response<PostDetailResponseDto> readDetailPost(@PathVariable Long postId) {
+        return Response.success(postService.readPostDetail(postId));
+    }
+
+    // TODO: 이후 로그인 정보로 유저 일치 확인하기(변경 예정)
+    @PatchMapping("/{postId}")
+    public Response<Long> update(
+            @PathVariable Long postId,
+            @RequestBody @Validated PostUpdateDto postUpdateDto
+    ) {
+        return Response.success(postService.update(postId, postUpdateDto, postUpdateDto.userId()));
+    }
 }
