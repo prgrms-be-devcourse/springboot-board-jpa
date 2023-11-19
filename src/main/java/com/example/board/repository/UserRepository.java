13dd2707587cc8d4b889cf24471b3a5e250091c3
Hanna <hanna@trdst.com>
2023-11-19 22:00:20 +0900

    feat: existsByName 추가

--- src/main/java/com/example/board/repository/UserRepository.java
+++ src/main/java/com/example/board/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.example.board.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByName(String name);
 }
