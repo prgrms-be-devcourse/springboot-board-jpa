0e99e74f7f51bef8cf4599a5cd33e641f7644a15
dlswns2480 <dlswns2480@naver.com>
2023-11-19 14:24:32 +0900

    feat : created_by 필드 -> updated_at 필드로 변경

--- src/main/java/com/kdt/simpleboard/common/BaseEntity.java
+++ src/main/java/com/kdt/simpleboard/common/BaseEntity.java
@@ -2,8 +2,8 @@ package com.kdt.simpleboard.common;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
