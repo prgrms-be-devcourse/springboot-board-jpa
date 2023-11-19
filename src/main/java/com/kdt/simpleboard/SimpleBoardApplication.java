64c1e09f7877202f424def9124d9d25a483517d4
dlswns2480 <dlswns2480@naver.com>
2023-11-19 12:13:53 +0900

    init : 프로젝트 생성

---
+++ src/main/java/com/kdt/simpleboard/SimpleBoardApplication.java
@@ -0,0 +1,12 @@
+package com.kdt.simpleboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBoardApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBoardApplication.class, args);
+	}
+
+}
