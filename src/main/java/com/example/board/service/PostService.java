f1836f1c2bba033a8799afb1c50617b008d21fb8
Hanna <hanna@trdst.com>
2023-11-19 21:56:44 +0900

    feat: service 작성

---
+++ src/main/java/com/example/board/service/PostService.java
@@ -0,0 +1,48 @@
+package com.example.board.service;
+
+import com.example.board.converter.PostConverter;
+import com.example.board.domain.Post;
+import com.example.board.domain.User;
+import com.example.board.dto.request.CreatePostRequest;
+import com.example.board.dto.request.UpdatePostRequest;
+import com.example.board.dto.response.PostResponse;
+import com.example.board.repository.PostRepository;
+import com.example.board.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    public Long createPost(CreatePostRequest requestDto) {
+        final User user = userRepository.findById(requestDto.authorId()).orElseThrow(() -> new NoSuchElementException("유저가 없음"));
+        final Post post = postRepository.save(PostConverter.toPost(requestDto, user));
+        return post.getId();
+    }
+
+    //TODO: getPosts
+
+    @Transactional(readOnly = true)
+    public PostResponse getPost(Long id) {
+        final Post post = postRepository.findById(id).orElseThrow(() -> new NoSuchElementException("게시글이 없음"));
+        return PostConverter.toPostResponse(post);
+    }
+
+    public void updatePost(Long id, UpdatePostRequest requestDto) {
+        final Post post = postRepository.findById(id).orElseThrow(() -> new NoSuchElementException("게시글이 없음"));
+        post.update(requestDto.title(), requestDto.content());
+    }
+
+    public void deletePost(Long id) {
+        final Post post = postRepository.findById(id).orElseThrow(() -> new NoSuchElementException("게시글이 없음"));
+        postRepository.delete(post);
+    }
+}
---
+++ src/main/java/com/example/board/service/UserService.java
@@ -0,0 +1,46 @@
+package com.example.board.service;
+
+import com.example.board.converter.UserConverter;
+import com.example.board.domain.User;
+import com.example.board.dto.request.CreateUserRequest;
+import com.example.board.dto.request.UpdateUserRequest;
+import com.example.board.dto.response.UserResponse;
+import com.example.board.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+
+@Service
+@Transactional
+@AllArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public Long createUser(CreateUserRequest requestDto) {
+        if (userRepository.existsByName(requestDto.name())) {
+            throw new IllegalArgumentException("이미 존재하는 이름입니다.");
+        }
+        return userRepository.save(UserConverter.toUser(requestDto)).getId();
+    }
+
+    //TODO: getUsers
+
+    @Transactional(readOnly = true)
+    public UserResponse getUser(Long id) {
+        final User user = userRepository.findById(id).orElseThrow(() -> new NoSuchElementException("유저가 존재하지 않습니다."));
+        return UserConverter.toUserResponse(user);
+    }
+
+    public void updateUser(Long id, UpdateUserRequest requestDto) {
+        final User user = userRepository.findById(id).orElseThrow(() -> new NoSuchElementException("유저가 존재하지 않습니다."));
+        user.update(requestDto.name(), requestDto.age(), requestDto.hobby());
+    }
+
+    public void deleteUser(Long id) {
+        final User user = userRepository.findById(id).orElseThrow(() -> new NoSuchElementException("유저가 존재하지 않습니다."));
+        userRepository.delete(user);
+    }
+}
