6c6a8b2e5f65193050e4f6feed0f15df9e203748
ebcho <joeunbi514@naver.com>
2023-11-20 21:37:00 +0900

    BaseEntity auditing 구현

---
+++ src/main/java/com/programmers/springboard/entity/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.programmers.springboard.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+	@CreatedDate
+	private LocalDateTime createdAt;
+	@CreatedBy
+	private String createdBy;
+
+}
