5d1b20ca6c200d553a26e4a004e9e792ecc8b10a
eugene <81062608+eugene225@users.noreply.github.com>
2023-11-20 22:19:02 +0900

    init commit

---
+++ src/main/java/com/prgrms/dev/springbootboardjpa/SpringbootBoardJpaApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.dev.springbootboardjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBoardJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBoardJpaApplication.class, args);
+    }
+
+}
