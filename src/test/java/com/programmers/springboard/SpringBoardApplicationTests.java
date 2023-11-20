c7c0c2626b3b1325f68203bf7767cf4e3fef3db8
ebcho <joeunbi514@naver.com>
2023-11-20 15:14:43 +0900

    init project

---
+++ src/test/java/com/programmers/springboard/SpringBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.springboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
