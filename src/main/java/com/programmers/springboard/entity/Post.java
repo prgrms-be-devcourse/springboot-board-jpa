6003ff2c6ef71fa49e4bde1fc982bb17ff3bdd02
ebcho <joeunbi514@naver.com>
2023-11-20 21:37:31 +0900

    Post validation 추가

--- src/main/java/com/programmers/springboard/entity/Post.java
+++ src/main/java/com/programmers/springboard/entity/Post.java
@@ -1,11 +1,15 @@
 package com.programmers.springboard.entity;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -23,13 +27,16 @@ public class Post extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
-	@Column(nullable = false)
+	@Column(nullable = false, length = 60)
 	private String title;
 
+	@Column(nullable = false)
+	@Lob
 	private String content;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	private Member member;
+	// todo : @OnDelete 찾아보기
+ 	private Member member;
 
 	public void changePost(String title, String content) {
 		this.title = title;
