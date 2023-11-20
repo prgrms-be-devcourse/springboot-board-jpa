a62da51be90a3e2ad0fc5bf5896bd1576f42af37
ebcho <joeunbi514@naver.com>
2023-11-20 17:01:10 +0900

    member 엔티티 생성

---
+++ src/main/java/com/programmers/springboard/entity/Member.java
@@ -0,0 +1,31 @@
+package com.programmers.springboard.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Member extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = false, length = 30)
+	private String name;
+
+	@Column(nullable = false)
+	private int age;
+
+	private String hobby;
+}
