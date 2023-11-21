46557fc0ae8a8272fb415374e9d490578669b92f
shoeone96 <dlwnddnjs96@gmail.com>
2023-11-21 16:55:54 +0900

    chore: 통일 안된 Entity 및 Dto 에서의 ID 변수 타입 통일 필요

--- src/main/java/com/example/board/dto/PostUpdateDto.java
+++ src/main/java/com/example/board/dto/PostUpdateDto.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+//TODO: userId 타입 통일
 public record PostUpdateDto(
         @NotNull Long userId,
         @Size(max = 20) String title,
