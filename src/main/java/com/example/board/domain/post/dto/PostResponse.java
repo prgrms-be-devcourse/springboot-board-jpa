fb24be484f796c5c1db32679f491a723c3f359fb
park0jae <okvv26@naver.com>
2023-11-21 16:39:58 +0900

    feat : 페이징 조건 검색 관련 클래스 생성

--- src/main/java/com/example/board/domain/post/dto/PostResponse.java
+++ src/main/java/com/example/board/domain/post/dto/PostResponse.java
@@ -1,13 +1,16 @@
 package com.example.board.domain.post.dto;
 
 import com.example.board.domain.post.entity.Post;
+import java.time.format.DateTimeFormatter;
 
 public record PostResponse(
     Long id,
     String title,
     String content,
     int view,
-    String name
+    String name,
+    String createdAt,
+    String updatedAt
 ) {
     public static PostResponse from(Post post) {
         return new PostResponse(
@@ -15,7 +18,9 @@ public record PostResponse(
             post.getTitle(),
             post.getContent(),
             post.getView(),
-            post.getMember().getName()
+            post.getMember().getName(),
+            post.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")),
+            post.getUpdatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
         );
     }
 }
