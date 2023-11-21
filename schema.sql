9bc80e2241a1775bf55c1d0268b07491e6bb6f36
Jay <injaesong0@gmail.com>
2023-11-21 15:19:39 +0900

    chore: configure the datasource

---
+++ schema.sql
@@ -0,0 +1,22 @@
+create table posts
+(
+    id         bigint       not null auto_increment primary key,
+    created_at datetime(6) not null,
+    updated_at datetime(6) not null,
+    created_by varchar(255) not null,
+    title      varchar(255) not null,
+    updated_by varchar(255) not null,
+    content    tinytext     not null,
+);
+
+create table users
+(
+    id         bigint       not null auto_increment primary key,
+    created_at datetime(6) not null,
+    updated_at datetime(6) not null,
+    age        integer      not null,
+    created_by varchar(255) not null,
+    hobby      varchar(255),
+    name       varchar(255) not null,
+    updated_by varchar(255) not null,
+);
