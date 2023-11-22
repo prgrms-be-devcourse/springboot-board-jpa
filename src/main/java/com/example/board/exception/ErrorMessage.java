66ec7699953dc4b3d1c7d9096ab13b4207a8dcca
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-22 15:52:13 +0900

    #11 feat: 유효성 검사 관련 ErrorMessage 추가

--- src/main/java/com/example/board/exception/ErrorMessage.java
+++ src/main/java/com/example/board/exception/ErrorMessage.java
@@ -10,10 +10,13 @@ public enum ErrorMessage {
     INTERNAL_SERVER_ERROR(500, HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부에서 장애가 발생했습니다."),
     // 1xxx user exception
     USER_NOT_FOUND(1000, HttpStatus.NOT_FOUND, "찾으시는 사용자가 없습니다."),
-
+    WRONG_USER_NAME(1001, HttpStatus.SERVICE_UNAVAILABLE, "사용자 이름이 존재하지 않습니다."),
+    WRONG_USER_ID(1002, HttpStatus.SERVICE_UNAVAILABLE, "사용자 id 값이 존재하지 않습니다."),
     //2xxx post exception
     POST_NOT_FOUND(2000, HttpStatus.NOT_FOUND, "찾으시는 게시글이 없습니다."),
-    WRITER_NOT_MATCHED(2001, HttpStatus.BAD_REQUEST, "작성자가 일치하지 않습니다.");
+    WRITER_NOT_MATCHED(2001, HttpStatus.BAD_REQUEST, "작성자가 일치하지 않습니다."),
+    WRONG_TITLE_VALUE(2002, HttpStatus.BAD_REQUEST, "게시글 제목은 1자~20자 이내로 작성해주십시오."),
+    WRONG_CONTENTS_VALUE(2003, HttpStatus.BAD_REQUEST, "게시글 내용을 작성해주십시오.");
 
     private int code;
     private HttpStatus httpStatus;
