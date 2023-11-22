e962e4442875fc3dd0c7a2c883b8fedd5b944a04
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-22 15:48:20 +0900

    #11 feat: PostResponseDto 유효성 검사 로직 구현

--- src/main/java/com/example/board/dto/PostResponseDto.java
+++ src/main/java/com/example/board/dto/PostResponseDto.java
@@ -1,15 +1,31 @@
 package com.example.board.dto;
 
+import com.example.board.exception.BaseException;
+import com.example.board.exception.ErrorMessage;
 import com.example.board.model.Post;
 
 public record PostResponseDto(
+        Long postId,
         String userName,
         String title
 ) {
-
     public static PostResponseDto from(Post post) {
-        return new PostResponseDto(post.getTitle(), post.getUser().getName());
+        postResponseValidate(post);
+        return new PostResponseDto(post.getId(), post.getTitle(), post.getUser().getName());
     }
 
-
+    private static void postResponseValidate(Post post) {
+        if (post.getId() == null) {
+            throw new BaseException(ErrorMessage.POST_NOT_FOUND);
+        }
+        if (post.getTitle().isBlank() || (post.getTitle().length() > 20)) {
+            throw new BaseException(ErrorMessage.WRONG_TITLE_VALUE);
+        }
+        if (post.getUser() == null) {
+            throw new BaseException(ErrorMessage.USER_NOT_FOUND);
+        }
+        if (post.getUser().getName().isBlank()) {
+            throw new BaseException(ErrorMessage.WRONG_USER_NAME);
+        }
+    }
 }
