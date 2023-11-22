8d7546ac7eaa3d852b199d37b9590d75e5e6f5b2
dlswns2480 <dlswns2480@naver.com>
2023-11-22 22:29:02 +0900

    feat : BoardController swagger operation 추가
    Co-authored-by: hyun2371 <77sghyun@gmail.com>

--- src/main/java/com/kdt/simpleboard/board/controller/BoardController.java
+++ src/main/java/com/kdt/simpleboard/board/controller/BoardController.java
@@ -2,7 +2,13 @@ package com.kdt.simpleboard.board.controller;
 
 import com.kdt.simpleboard.board.service.BoardService;
 import com.kdt.simpleboard.common.dto.PageResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -19,24 +25,40 @@ import static com.kdt.simpleboard.board.dto.BoardResponse.FindBoardRes;
 public class BoardController {
     private final BoardService boardService;
 
+    @Operation(summary = "보드 생성")
     @PostMapping
     public ResponseEntity<CreateBoardRes> createBoard(@Valid @RequestBody CreateBoardRequest request){
         CreateBoardRes response = boardService.createBoard(request);
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "보드 업데이트")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "회원 생성 성공",
+                    content = @Content(mediaType = "application/json"))
+    })
     @PostMapping("/{id}")
     public ResponseEntity<FindBoardRes> updateBoard(@PathVariable("id") Long boardId, @Valid @RequestBody ModifyBoard request) {
         FindBoardRes response = boardService.updateBoard(boardId, request);
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "보드 단건 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "회원 생성 성공",
+                    content = @Content(mediaType = "application/json"))
+    })
     @GetMapping("/{id}")
     public ResponseEntity<FindBoardRes> findBoard(@PathVariable("id") Long boardId){
         FindBoardRes response = boardService.findById(boardId);
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "보드 모두 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "회원 생성 성공",
+                    content = @Content(mediaType = "application/json"))
+    })
     @GetMapping
     public ResponseEntity<PageResponse<FindBoardRes>> findAll(Pageable pageable){
         PageResponse<FindBoardRes> response = boardService.findAll(pageable);
