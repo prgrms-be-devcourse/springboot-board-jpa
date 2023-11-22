fb94cd29a0b3e7ad0186fb64696efa4ce835b912
dlswns2480 <dlswns2480@naver.com>
2023-11-22 22:29:27 +0900

    feat : BaseIntegrationTest class 생성
    Co-authored-by: hyun2371 <77sghyun@gmail.com>

---
+++ src/test/java/com/kdt/simpleboard/BaseIntegrationTest.java
@@ -0,0 +1,23 @@
+package com.kdt.simpleboard;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+public class BaseIntegrationTest {
+    @Autowired
+    public MockMvc mvc;
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
