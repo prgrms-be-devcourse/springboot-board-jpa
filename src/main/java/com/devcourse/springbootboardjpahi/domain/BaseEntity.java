c154bf740694005160323a69af7ec9f87a1d8384
ASak1104 <khw56184@gmail.com>
2023-11-23 04:40:26 +0900

    feat: add getter

--- src/main/java/com/devcourse/springbootboardjpahi/domain/BaseEntity.java
+++ src/main/java/com/devcourse/springbootboardjpahi/domain/BaseEntity.java
@@ -3,9 +3,11 @@ package com.devcourse.springbootboardjpahi.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+@Getter
 @MappedSuperclass
 public class BaseEntity {
 
