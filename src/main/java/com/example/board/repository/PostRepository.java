aef02ed1ea48f372948128a1c6799ac0b6aa301e
marooo326 <ktnqkr01@gmail.com>
2023-11-22 21:37:55 +0900

    refactor: 패키지 정리

--- src/main/java/com/example/board/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.board.repository;
-
-import com.example.board.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long>, PostQuerydslRepository {
-}
--- src/main/java/com/example/board/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.board.repository;
-
-import com.example.board.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    boolean existsByName(String name);
-}
