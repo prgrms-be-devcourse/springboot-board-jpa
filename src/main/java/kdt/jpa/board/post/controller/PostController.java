4a0b783dc4fbf26ede0bfa3c7061c40b0650b9d6
ParkJuHan94 <1004sunye@naver.com>
2023-11-22 12:03:00 +0900

    feat: 셋팅 추가

--- src/main/java/kdt/jpa/board/post/controller/PostController.java
+++ src/main/java/kdt/jpa/board/post/controller/PostController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/posts")
+@RequestMapping("/api/posts")
 public class PostController {
 
     private final PostService postService;
