0d402c2ebab649fbf6954a583e2e71c925c3da22
dlswns2480 <dlswns2480@naver.com>
2023-11-22 22:30:31 +0900

    feat : BoardController Test 완료
    Co-authored-by: hyun2371 <77sghyun@gmail.com>

---
+++ src/test/java/com/kdt/simpleboard/board/controller/BoardControllerTest.java
@@ -0,0 +1,102 @@
+package com.kdt.simpleboard.board.controller;
+
+import com.kdt.simpleboard.BaseIntegrationTest;
+import com.kdt.simpleboard.board.domain.Board;
+import com.kdt.simpleboard.board.dto.BoardRequest;
+import com.kdt.simpleboard.board.dto.BoardResponse;
+import com.kdt.simpleboard.board.repository.BoardRepository;
+import com.kdt.simpleboard.data.BoardData;
+import com.kdt.simpleboard.user.UserData;
+import com.kdt.simpleboard.user.domain.User;
+import com.kdt.simpleboard.user.repository.UserRepository;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.List;
+
+import static com.kdt.simpleboard.board.dto.BoardRequest.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class BoardControllerTest extends BaseIntegrationTest {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    @DisplayName("게시물 생성 api 호출에 성공한다")
+    void createBoard() throws Exception {
+        User user = UserData.user();
+        userRepository.save(user);
+        mvc.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(BoardData.createBoardRequest())))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.createdId").value(1L));
+    }
+
+    @Test
+    @DisplayName("게시물 업데이트 api 호출에 성공한다")
+    void updateBoard() throws Exception {
+        User user = UserData.user();
+        Board board = BoardData.board();
+
+        boardRepository.save(board);
+        userRepository.save(user);
+
+        ModifyBoard modifyBoardRequest = BoardData.modifyBoardRequest();
+        mvc.perform(post("/posts/{id}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(modifyBoardRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value(modifyBoardRequest.title()))
+                .andExpect(jsonPath("$.content").value(modifyBoardRequest.content()))
+        ;
+    }
+
+    @Test
+    @DisplayName("게시물 단건 조회 api 호출에 성공한다")
+    void findBoardById() throws Exception{
+        Board board = BoardData.board();
+        boardRepository.save(board);
+        mvc.perform(get("/posts/{id}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value(board.getTitle()))
+                .andExpect(jsonPath("$.content").value(board.getContent()))
+        ;
+    }
+
+    @Test
+    @DisplayName("전체 게시물을 조회할 수 있다.")
+    void findAll() throws Exception{
+        User user = UserData.user();
+        userRepository.save(user);
+
+        List<Board> boards = BoardData.getBoards();
+        ReflectionTestUtils.setField(boards.get(0), "user", user);
+        ReflectionTestUtils.setField(boards.get(1), "user", user);
+        boardRepository.saveAll(boards);
+
+
+        mvc.perform(get("/posts")
+                        .param("page","0")
+                        .param("size", "10")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.totalItems", is(boards.size())))
+        ;
+    }
+}
