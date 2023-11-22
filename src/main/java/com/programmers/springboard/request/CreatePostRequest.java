815acf7d6176f0a9ccef8480861e3f83bbc3f09c
ebcho <joeunbi514@naver.com>
2023-11-22 17:19:59 +0900

    dto 메시지 추가

--- src/main/java/com/programmers/springboard/request/CreatePostRequest.java
+++ src/main/java/com/programmers/springboard/request/CreatePostRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record CreatePostRequest(@NotBlank (message = "제목을 입력해주세요")
-								@Size(max = 60)
+								@Size(max = 60, message = "제목은 60자 미만입니다")
 								String title,
 								@NotBlank (message = "내용을 입력해주세요")
 								String content,
