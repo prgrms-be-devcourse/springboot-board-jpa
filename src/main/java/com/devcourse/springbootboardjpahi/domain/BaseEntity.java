00c26367920e614b7b500c02a7d677e9f2a101cb
Jay <injaesong0@gmail.com>
2023-11-23 02:53:00 +0900

    fix: fix auditing strategy use that of hibernate instead of jpa in order to avoid unexpected exception regarding of OS

--- src/main/java/com/devcourse/springbootboardjpahi/domain/BaseEntity.java
+++ src/main/java/com/devcourse/springbootboardjpahi/domain/BaseEntity.java
@@ -1,30 +1,23 @@
 package com.devcourse.springbootboardjpahi.domain;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import java.time.LocalDateTime;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     @Column(name = "created_at", nullable = false)
-    @CreatedDate
+    @CreationTimestamp
     private LocalDateTime createdAt;
     @Column(name = "updated_at", nullable = false)
-    @LastModifiedDate
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
     @Column(name = "created_by")
-    @CreatedBy
     private String createdBy;
     @Column(name = "updated_by")
-    @LastModifiedBy
     private String updatedBy;
+
 }
