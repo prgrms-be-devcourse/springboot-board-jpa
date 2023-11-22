e2e231ba8e9b0b64bfa31e91933b9550f1ec4223
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-11-23 04:40:16 +0900

    feat: Entity가 생성 시, 생성/수정 시간 자동 설정
    Co-Authored-By: hyoguoo <113650170+hyoguoo@users.noreply.github.com>

--- src/main/java/jehs/springbootboardjpa/entity/BaseEntity.java
+++ src/main/java/jehs/springbootboardjpa/entity/BaseEntity.java
@@ -1,17 +1,26 @@
 package jehs.springbootboardjpa.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Builder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
-public class BaseEntity {
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
 
+    @Builder.Default
     @Column(name = "created_at", columnDefinition = "datetime")
-    private LocalDateTime createdAt;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    @Builder.Default
+    @LastModifiedDate
     @Column(name = "updated_at", columnDefinition = "datetime")
-    private LocalDateTime updatedAt;
+    private LocalDateTime updatedAt = LocalDateTime.now();
     @Column(name = "deleted_at", columnDefinition = "datetime")
     private LocalDateTime deletedAt;
 }
