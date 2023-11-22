8561607c9d561c69ce11da07e33ae3752e349ac7
park0jae <okvv26@naver.com>
2023-11-22 18:00:20 +0900

    test : PostController 테스트 코드 수정

--- src/test/java/com/example/board/domain/post/controller/PostControllerTest.java
+++ src/test/java/com/example/board/domain/post/controller/PostControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.board.domain.post.controller;
 
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
@@ -126,11 +127,6 @@ class PostControllerTest {
     @Test
     void 게시글_전체_조회_호출_테스트() throws Exception {
         // Given
-        PostPageCondition condition = PostPageCondition.builder()
-            .page(1)
-            .size(10)
-            .build();
-
         List<PostResponse> posts = List.of(new PostResponse(
             1L,
             "제목1",
@@ -142,13 +138,10 @@ class PostControllerTest {
         ));
         PageRequest pageable = PageRequest.of(0, 10);
         Page<PostResponse> pageResponse = PageableExecutionUtils.getPage(posts, pageable, () -> posts.size());
-        given(postService.findPostsByCondition(condition)).willReturn(pageResponse);
-
+        given(postService.findPostsByCondition(any(PostPageCondition.class))).willReturn(pageResponse);
 
         // When & Then
         mvc.perform(get("/api/v1/posts")
-            .param("page", String.valueOf(condition.getPage()))
-            .param("size", String.valueOf(condition.getSize()))
             .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andDo(print())
@@ -160,7 +153,26 @@ class PostControllerTest {
                     fieldWithPath("content[].view").type(JsonFieldType.NUMBER).description("게시글 조회수"),
                     fieldWithPath("content[].name").type(JsonFieldType.STRING).description("게시글 작성자"),
                     fieldWithPath("content[].createdAt").type(JsonFieldType.STRING).description("게시글 생성 시간"),
-                    fieldWithPath("content[].updatedAt").type(JsonFieldType.STRING).description("게시글 수정 시간")
+                    fieldWithPath("content[].updatedAt").type(JsonFieldType.STRING).description("게시글 수정 시간"),
+                    fieldWithPath("pageable.pageNumber").type(JsonFieldType.NUMBER).description("페이지 번호"),
+                    fieldWithPath("pageable.pageSize").type(JsonFieldType.NUMBER).description("페이지 당 개수"),
+                    fieldWithPath("pageable.sort.empty").type(JsonFieldType.BOOLEAN).description("정렬 필드가 비어 있는지 여부"),
+                    fieldWithPath("pageable.sort.sorted").type(JsonFieldType.BOOLEAN).description("정렬이 적용된 상태인지 여부"),
+                    fieldWithPath("pageable.sort.unsorted").type(JsonFieldType.BOOLEAN).description("정렬이 적용되지 않은 상태인지 여부"),
+                    fieldWithPath("pageable.offset").type(JsonFieldType.NUMBER).description("페이지 오프셋 (시작 인덱스)"),
+                    fieldWithPath("pageable.paged").type(JsonFieldType.BOOLEAN).description("페이징이 적용된 상태인지 여부"),
+                    fieldWithPath("pageable.unpaged").type(JsonFieldType.BOOLEAN).description("페이징이 적용되지 않은 상태인지 여부"),
+                    fieldWithPath("last").type(JsonFieldType.BOOLEAN).description("마지막 페이지 여부"),
+                    fieldWithPath("totalPages").type(JsonFieldType.NUMBER).description("전체 페이지 수"),
+                    fieldWithPath("totalElements").type(JsonFieldType.NUMBER).description("전체 데이터 개수"),
+                    fieldWithPath("first").type(JsonFieldType.BOOLEAN).description("첫 페이지 여부"),
+                    fieldWithPath("size").type(JsonFieldType.NUMBER).description("페이지 크기"),
+                    fieldWithPath("number").type(JsonFieldType.NUMBER).description("현재 페이지 번호"),
+                    fieldWithPath("sort.empty").type(JsonFieldType.BOOLEAN).description("정렬 필드가 비어 있는지 여부"),
+                    fieldWithPath("sort.sorted").type(JsonFieldType.BOOLEAN).description("현재 정렬된 상태인지 여부"),
+                    fieldWithPath("sort.unsorted").type(JsonFieldType.BOOLEAN).description("현재 정렬되지 않은 상태인지 여부"),
+                    fieldWithPath("numberOfElements").type(JsonFieldType.NUMBER).description("현재 페이지의 데이터 개수"),
+                    fieldWithPath("empty").type(JsonFieldType.BOOLEAN).description("응답이 비어 있는지 여부")
                 )
             ));
     }
