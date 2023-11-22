33b67e29fb6d365181b8ab660355e26771dfac81
eugene <81062608+eugene225@users.noreply.github.com>
2023-11-22 11:52:28 +0900

    Feat : GlobalExceptionHandler 구현

---
+++ src/main/java/com/prgrms/dev/springbootboardjpa/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.prgrms.dev.springbootboardjpa.exception;
+
+import com.prgrms.dev.springbootboardjpa.ApiResponse;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final int NOT_FOUND = 404;
+    private static final int INTERNAL_SERVER_ERROR = 404;
+
+    @ExceptionHandler(ChangeSetPersister.NotFoundException.class)
+    public ApiResponse<String> notFoundHandler(ChangeSetPersister.NotFoundException e) {
+        return ApiResponse.fail(NOT_FOUND, e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ApiResponse<String> internalServerErrorHandler(Exception e) {
+        return ApiResponse.fail(INTERNAL_SERVER_ERROR, e.getMessage());
+    }
+}
