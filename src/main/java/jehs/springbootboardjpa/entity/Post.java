8fd8a61788dac452b0f57f2e4e37ff94de78b575
hyoguoo <hyoguoo@gmail.com>
2023-11-23 00:25:11 +0900

    feat: Post Entity 추가

---
+++ src/main/java/jehs/springbootboardjpa/entity/Post.java
@@ -0,0 +1,33 @@
+package jehs.springbootboardjpa.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "posts")
+public class Post extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", length = 59)
+    private String title;
+
+    @Column(name = "content", length = 1000)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private PostType postType;
+
+    enum PostType {
+        FREE, INFO, MARKET, EMPLOYMENT
+    }
+}
