51da029dae9411ccda86f6eeb28f06889af896f8
hyoguoo <hyoguoo@gmail.com>
2023-11-23 15:23:01 +0900

    feat: BaseEntity Getter 추가
    Co-authored-by: JeongeunChoi <77786996+jeongeunchoi@users.noreply.github.com>

--- src/main/java/jehs/springbootboardjpa/entity/BaseEntity.java
+++ src/main/java/jehs/springbootboardjpa/entity/BaseEntity.java
@@ -4,12 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Builder;
-import org.springframework.data.annotation.CreatedDate;
+import lombok.Getter;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
