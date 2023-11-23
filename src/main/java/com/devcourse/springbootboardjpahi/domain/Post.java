28813b259b2965bfe2c09f30abd4ba6b5251cfd6
ASak1104 <khw56184@gmail.com>
2023-11-23 21:11:40 +0900

    feat: add update post method

--- src/main/java/com/devcourse/springbootboardjpahi/domain/Post.java
+++ src/main/java/com/devcourse/springbootboardjpahi/domain/Post.java
@@ -37,4 +37,12 @@ public class Post extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
+
+    public void updateTitle(String title) {
+        this.title = title;
+    }
+
+    public void updateContent(String content) {
+        this.content = content;
+    }
 }
