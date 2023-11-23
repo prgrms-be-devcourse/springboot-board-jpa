dd359785bfa0b50ea78bd7fb9fe10ea5299489bf
ASak1104 <khw56184@gmail.com>
2023-11-24 00:17:00 +0900

    fix: allow id to zero

--- src/main/java/com/devcourse/springbootboardjpahi/dto/CreatePostRequest.java
+++ src/main/java/com/devcourse/springbootboardjpahi/dto/CreatePostRequest.java
@@ -2,14 +2,14 @@ package com.devcourse.springbootboardjpahi.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 
 public record CreatePostRequest(
         @NotBlank(message = "제목은 공백일 수 없습니다.")
         String title,
         @NotNull(message = "내용이 존재하지 않습니다.")
         String content,
-        @Positive(message = "유효하지 않은 유저 아이디입니다.")
+        @PositiveOrZero(message = "유효하지 않은 유저 아이디입니다.")
         Long userId
 ) {
 }
