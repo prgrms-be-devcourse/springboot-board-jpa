8fcf90203e0e6bd2132fccb6e1680197ae76cc76
letskuku <yujung9288@naver.com>
2023-11-23 20:17:46 +0900

    init: 프로젝트 환경설정

---
+++ src/test/java/com/programmers/boardjpa/BoardJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.boardjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
