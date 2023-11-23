db7f465754776593a59d51f3f117b7b76900d6a6
letskuku <yujung9288@naver.com>
2023-11-23 20:20:06 +0900

    feat: 생성 일시, 수정 일시 관리하는 BaseEntity 구현

---
+++ src/main/java/com/programmers/boardjpa/global/common/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.programmers.boardjpa.global.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
