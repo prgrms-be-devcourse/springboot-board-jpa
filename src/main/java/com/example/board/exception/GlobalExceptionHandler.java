478bca4699eaea3202d842851a1a7081f42600a4
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-11-24 11:26:32 +0900

    refactor: 불필요한 예외 핸들러 삭제

--- src/main/java/com/example/board/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/board/exception/GlobalExceptionHandler.java
@@ -1,14 +1,12 @@
 package com.example.board.exception;
 
 import com.example.board.response.Response;
-import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-// TODO: httpstatus 상태 코드도 함께 전달하는 방법 찾기
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -35,11 +33,5 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Response<?>> exceptionHandle(Exception e) {
         log.info("error: {}", e.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Response.fail(e));
-    }
-
-    @ExceptionHandler(MismatchedInputException.class)
-    public ResponseEntity<Response<?>> customExceptionHandle(MismatchedInputException e) {
-        log.info("error: {}", e.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Response.fail(e));
     }
 }
