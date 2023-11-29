2493f52c8b02c086b2c895cc4b7bf9235315dd8f
Hanna <hanna@trdst.com>
2023-11-29 16:40:00 +0900

    fix: validation max 값 추가 지정

--- src/main/java/com/example/board/dto/request/post/UpdatePostRequest.java
+++ src/main/java/com/example/board/dto/request/post/UpdatePostRequest.java
@@ -7,11 +7,11 @@ import jakarta.validation.constraints.Size;
 public record UpdatePostRequest(
 
         @NotBlank(message = "제목을 입력해주세요.")
-        @Size(min = 3, message = "제목은 3자 이상 입력해주세요.")
+        @Size(min = 3, max = 100, message = "제목은 3글자 이상, 최대 100글자 이하로 입력해주세요.")
         String title,
 
         @NotBlank(message = "내용을 입력해주세요.")
-        @Size(min = 3, message = "내용안 3자 이상 입력해주세요.")
+        @Size(min = 3, message = "내용은 3글자 이상 입력해주세요.")
         String content,
 
         @NotNull(message = "작성자 아이디가 필요합니다.")
--- src/main/java/com/example/board/dto/request/user/CreateUserRequest.java
+++ src/main/java/com/example/board/dto/request/user/CreateUserRequest.java
@@ -1,20 +1,19 @@
 package com.example.board.dto.request.user;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public record CreateUserRequest(
 
         @NotBlank(message = "이름을 입력해 주세요.")
-        @Size(min = 2, message = "이름은 최소 2글자 이상 입력해주세요.")
+        @Size(min = 2, max = 20, message = "이름은 최소 2글자 이상, 최대 20글자 이하로 입력해주세요.")
         String name,
 
         @NotNull(message = "나이를 입력해주세요.")
-        @Min(value = 1, message = "나이는 0살 이상 입력해주세요.")
+        @Min(value = 1, message = "나이는 1 이상으로 입력해주세요.")
+        @Max(value = 200, message = "나이는 200 이하로 입력해주세요.")
         Integer age,
 
+        @Size(min = 1, max = 20, message = "취미는 최소 1글자 이상, 최대 20글자 이하로 입력해주세요.")
         String hobby
 ) {
 }
