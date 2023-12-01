85f75ee68f850071f70b6940f7bf02e07b9e6b27
shoeone96 <dlwnddnjs96@gmail.com>
2023-12-01 11:47:36 +0900

    refactor: Util 클래스 생성자 제한

--- src/main/java/com/example/board/util/Validation.java
+++ src/main/java/com/example/board/util/Validation.java
@@ -1,12 +1,15 @@
 package com.example.board.util;
 
 import com.example.board.exception.BindingException;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.springframework.validation.BindingResult;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Validation {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Validation {
     public static void bindChecking(BindingResult bindingResult) {
         List<String> messages = new ArrayList<>();
         if (bindingResult.hasErrors()) {
