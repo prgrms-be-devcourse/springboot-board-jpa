d5160dde8d9ccbdc8b155a4ef40385c7faa06dc3
letskuku <yujung9288@naver.com>
2023-12-01 17:30:20 +0900

    refactor: PostInsertRequestDto에서 필요없는 필드 삭제

--- src/main/java/com/programmers/boardjpa/post/dto/PostInsertRequestDto.java
+++ src/main/java/com/programmers/boardjpa/post/dto/PostInsertRequestDto.java
@@ -1,4 +1,4 @@
 package com.programmers.boardjpa.post.dto;
 
-public record PostInsertRequestDto (Long postId, String title, String content, Long userId) {
+public record PostInsertRequestDto (String title, String content, Long userId) {
 }
