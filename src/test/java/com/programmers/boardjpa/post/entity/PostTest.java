75fd522e6867e1bc6fc79cfc8030f4b3af5b8e1d
letskuku <yujung9288@naver.com>
2023-12-01 17:59:29 +0900

    test: Post 필드 검증 로직 테스트 코드 작성

---
+++ src/test/java/com/programmers/boardjpa/post/entity/PostTest.java
@@ -0,0 +1,25 @@
+package com.programmers.boardjpa.post.entity;
+
+import com.programmers.boardjpa.post.exception.PostException;
+import com.programmers.boardjpa.user.entity.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class PostTest {
+
+    @Test
+    @DisplayName("Post 생성 시 잘못된 제목 길이는 예외를 발생시킨다.")
+    void testInvalidTitleErrorException() {
+        // given
+        String title = "asdhiqwhiufhweifhweuifhewuifheuwifhewuifhewuifhewuifhyuawfhuyrbvudsyfbvudsyfbvhgudfysghfuydghfdsuyhgydfshgufydhgfduighfduighfdusghusfidhguisdfhuifdshhdfsuighdfusihgfdhguifdhsguihdfiughfduihgufdsighfdsuhguisehrguheuyghrseuighuisdhguifdhguidfhgufidhgduhdfgughdufa";
+        String content = "content for post";
+        User user = new User("김뫄뫄", 10, "NETFLIX");
+
+        // when - then
+        assertThatThrownBy(() -> new Post(title, content, user))
+                .isInstanceOf(PostException.class)
+                .hasMessageContaining("title 길이가 올바르지 않습니다.");
+    }
+}
