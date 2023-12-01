ee96cc00aa36c16679be820ccb9e64a4a4f91211
letskuku <yujung9288@naver.com>
2023-12-01 16:42:29 +0900

    feat: 게시글 제목 길이 검증 로직 추가

--- src/main/java/com/programmers/boardjpa/post/entity/Post.java
+++ src/main/java/com/programmers/boardjpa/post/entity/Post.java
@@ -1,6 +1,8 @@
 package com.programmers.boardjpa.post.entity;
 
 import com.programmers.boardjpa.global.common.BaseEntity;
+import com.programmers.boardjpa.post.exception.PostErrorCode;
+import com.programmers.boardjpa.post.exception.PostException;
 import com.programmers.boardjpa.user.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
@@ -9,6 +11,8 @@ import lombok.*;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post extends BaseEntity {
+    private static final int MIN_TITLE_LEN = 0;
+    private static final int MAX_TITLE_LEN = 255;
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -35,5 +39,13 @@ public class Post extends BaseEntity {
     public void changeTitleAndContent(String title, String content) {
         this.title = title;
         this.content = content;
+    }
+
+    public void validateTitle(String title) {
+        int titleLen = title.length();
+
+        if (titleLen <= MIN_TITLE_LEN || titleLen > MAX_TITLE_LEN) {
+            throw new PostException(PostErrorCode.INVALID_TITLE_ERROR);
+        }
     }
 }
--- src/main/java/com/programmers/boardjpa/post/exception/PostErrorCode.java
+++ src/main/java/com/programmers/boardjpa/post/exception/PostErrorCode.java
@@ -5,7 +5,8 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum PostErrorCode {
-    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 Post를 찾을 수 없습니다.");
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 Post를 찾을 수 없습니다."),
+    INVALID_TITLE_ERROR(HttpStatus.BAD_REQUEST, "title 길이가 올바르지 않습니다.");
 
     private final HttpStatus errorCode;
     private final String errorMessage;
