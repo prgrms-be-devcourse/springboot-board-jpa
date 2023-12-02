c91d55f8c764aaeba020382a355386679bdef364
marooo326 <ktnqkr01@gmail.com>
2023-12-02 14:44:42 +0900

    fix: 불필요한 어노테이션 제거

--- src/main/java/com/example/board/controller/RootController.java
+++ src/main/java/com/example/board/controller/RootController.java
@@ -1,10 +1,8 @@
 package com.example.board.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
 @RestController
 public class RootController {
     @GetMapping("/health")
