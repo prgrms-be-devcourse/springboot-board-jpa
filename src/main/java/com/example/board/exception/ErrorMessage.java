6eaf5b2bc50be4a475f0a09d8912c7aa6513ab5e
shoeone96 <dlwnddnjs96@gmail.com>
2023-12-02 21:34:38 +0900

    #16 feat: 글 작성 로직에서 상황 별 글 작성 제한 로직에 따라 발생할 Exception 추가

--- src/main/java/com/example/board/exception/ErrorMessage.java
+++ src/main/java/com/example/board/exception/ErrorMessage.java
@@ -16,7 +16,9 @@ public enum ErrorMessage {
     POST_NOT_FOUND(2000, HttpStatus.NOT_FOUND, "찾으시는 게시글이 없습니다."),
     WRITER_NOT_MATCHED(2001, HttpStatus.BAD_REQUEST, "작성자가 일치하지 않습니다."),
     WRONG_TITLE_VALUE(2002, HttpStatus.BAD_REQUEST, "게시글 제목은 1자~20자 이내로 작성해주십시오."),
-    WRONG_CONTENTS_VALUE(2003, HttpStatus.BAD_REQUEST, "게시글 내용을 작성해주십시오.");
+    WRONG_CONTENTS_VALUE(2003, HttpStatus.BAD_REQUEST, "게시글 내용을 작성해주십시오."),
+    OVER_MAX_POST_PER_DAY(2004, HttpStatus.BAD_REQUEST, "하루에 작성할 수 있는 최대 게시글 개수를 초과하였습니다."),
+    OVER_MAX_POST_PER_MINUTE(2005, HttpStatus.BAD_REQUEST, "1분 간 5개 이상의 글을 작성할 수 없습니다.");
 
     private final int code;
     private final HttpStatus httpStatus;
