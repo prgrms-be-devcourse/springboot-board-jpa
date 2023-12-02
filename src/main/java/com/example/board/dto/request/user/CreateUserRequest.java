c29f60ead23e747809db5270110e0eae7335100a
marooo326 <ktnqkr01@gmail.com>
2023-12-02 17:43:32 +0900

    feat: Dto max validation 추가

--- src/main/java/com/example/board/dto/request/user/CreateUserRequest.java
+++ src/main/java/com/example/board/dto/request/user/CreateUserRequest.java
@@ -1,20 +1,19 @@
 package com.example.board.dto.request.user;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public record CreateUserRequest(
 
-        @NotBlank(message = "이름을 입력해 주세요.")
-        @Size(min = 2, message = "이름은 최소 2글자 이상 입력해주세요.")
+        @NotBlank(message = "이름은 필수 입력값입니다.")
+        @Size(min = 2, max = 10, message = "이름은 2 ~ 10자 사이로 입력해 주세요.")
         String name,
 
-        @NotNull(message = "나이를 입력해주세요.")
-        @Min(value = 1, message = "나이는 0살 이상 입력해주세요.")
+        @NotNull(message = "나이는 필수 입력값입니다.")
+        @Min(value = 1, message = "나이는 1살 이상으로 입력해 주세요.")
+        @Max(value = 100, message = "나이는 100살 이하로 입력해 주세요.")
         Integer age,
 
+        @Size(min = 2, max = 20, message = "취미는 2 ~ 20자 사이로 입력해 주세요.")
         String hobby
 ) {
 }
