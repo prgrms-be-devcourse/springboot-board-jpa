3a39ad059e7f602d4c287bfaa64e0949ffa19404
marooo326 <ktnqkr01@gmail.com>
2023-12-04 01:20:32 +0900

    chore: swagger jwt 사용 설정

--- src/main/java/com/example/board/annotation/AuthUser.java
+++ src/main/java/com/example/board/annotation/AuthUser.java
@@ -1,6 +1,8 @@
 package com.example.board.annotation;
 
 
+import io.swagger.v3.oas.annotations.Parameter;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,5 +10,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
+@Parameter(hidden = true)
 public @interface AuthUser {
 }
