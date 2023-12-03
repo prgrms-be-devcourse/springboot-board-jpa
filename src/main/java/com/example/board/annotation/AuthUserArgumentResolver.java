690d04b1799e75173c148f9b8638a517c752390e
marooo326 <ktnqkr01@gmail.com>
2023-12-03 19:09:36 +0900

    refactor: 커스텀 어노테이션 로직 간소화

--- src/main/java/com/example/board/annotation/AuthUserArgumentResolver.java
+++ src/main/java/com/example/board/annotation/AuthUserArgumentResolver.java
@@ -5,6 +5,7 @@ import com.example.board.exception.CustomException;
 import org.springframework.core.MethodParameter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -24,13 +25,9 @@ public class AuthUserArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        Object principal = null;
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        if (authentication != null) {
-            principal = authentication.getPrincipal();
-        }
-        if (principal == null || principal.getClass() == String.class) {
+        if (!(authentication.getPrincipal() instanceof UserDetails)) {
             throw new CustomException(ResponseStatus.USER_NOT_FOUND);
         }
 
