6232a38c3cacf27ad34381cb5428b893b26b60a6
hyoguoo <hyoguoo@gmail.com>
2023-12-04 14:46:41 +0900

    mod: Post 리스트 조회 시 Pageable 객체 대신 필요한 값만 받도록 변경 및 Post 리스트 조회 Response 적용
    Co-authored-by: JeongeunChoi <77786996+jeongeunchoi@users.noreply.github.com>

---
+++ src/main/java/jehs/springbootboardjpa/dto/PostsResponse.java
@@ -0,0 +1,21 @@
+package jehs.springbootboardjpa.dto;
+
+import jehs.springbootboardjpa.entity.Post;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Getter
+public class PostsResponse {
+    
+    private final List<PostResponse> content;
+    private final int totalPages;
+    private final long totalElements;
+
+    public PostsResponse(Page<Post> postsPage) {
+        this.content = postsPage.getContent().stream().map(PostResponse::new).toList();
+        this.totalPages = postsPage.getTotalPages();
+        this.totalElements = postsPage.getTotalElements();
+    }
+}
--- src/main/java/jehs/springbootboardjpa/service/PostService.java
+++ src/main/java/jehs/springbootboardjpa/service/PostService.java
@@ -3,15 +3,14 @@ package jehs.springbootboardjpa.service;
 import jehs.springbootboardjpa.dto.PostCreateRequest;
 import jehs.springbootboardjpa.dto.PostResponse;
 import jehs.springbootboardjpa.dto.PostUpdateRequest;
+import jehs.springbootboardjpa.dto.PostsResponse;
 import jehs.springbootboardjpa.entity.Post;
 import jehs.springbootboardjpa.entity.User;
 import jehs.springbootboardjpa.exception.PostErrorMessage;
 import jehs.springbootboardjpa.exception.PostException;
 import jehs.springbootboardjpa.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -47,14 +46,15 @@ public class PostService {
     }
 
     @Transactional(readOnly = true)
-    public Page<PostResponse> getAllPostsWithUser(Pageable pageable) {
-        return postRepository.findAllWithUser(pageable)
-                .map(PostResponse::new);
+    public PostsResponse getAllPostsWithUser(int page, int size) {
+        return new PostsResponse(
+                postRepository.findAllWithUser(PageRequest.of(page, size, Sort.by("id").descending()))
+        );
     }
 
     @Transactional(readOnly = true)
-    public Slice<PostResponse> getAllPostsWithUserByCursor(Pageable pageable, Long cursorId) {
-        return postRepository.findAllWithUserByCursor(cursorId, pageable)
+    public Slice<PostResponse> getAllPostsWithUserByCursor(int size, Long cursorId) {
+        return postRepository.findAllWithUserByCursor(cursorId, PageRequest.of(0, size))
                 .map(PostResponse::new);
     }
 }
