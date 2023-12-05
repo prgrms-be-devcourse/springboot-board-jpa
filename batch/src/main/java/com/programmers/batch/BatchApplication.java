e78c49f70b1fc579479d90cb3ecbf924f86949fd
KarmaPol <kkhkr98@gmail.com>
2023-12-05 23:43:58 +0900

    feat : add spring batch invalidMemberConfig

--- batch/src/main/java/com/programmers/batch/BatchApplication.java
+++ batch/src/main/java/com/programmers/batch/BatchApplication.java
@@ -1,11 +1,9 @@
 package com.programmers.batch;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableBatchProcessing
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.programmers"})
 public class BatchApplication {
 
     public static void main(String[] args) {
--- batch/src/main/java/com/programmers/batch/jobs/invalidMemberConfig.java
+++ batch/src/main/java/com/programmers/batch/jobs/invalidMemberConfig.java
@@ -1,13 +1,78 @@
 package com.programmers.batch.jobs;
 
-import lombok.AllArgsConstructor;
+import com.programmers.springboard.entity.Member;
+import com.programmers.springboard.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Configuration
 public class invalidMemberConfig {
 
-//    private MemberRepository
+    private final MemberRepository memberRepository;
+
+    @Bean
+    public Job invalidMemberJob(JobRepository jobRepository, Step invalidMemberStep){
+        return new JobBuilder("invalidMemberJob", jobRepository)
+                .start(invalidMemberStep)
+                .build();
+    }
+
+    @Bean
+    public Step invalidMemberStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager){
+        log.info(">>>>> invalidMemberJobStep");
+
+        return new StepBuilder("invalidMemberStep", jobRepository)
+                .<Member, Member>chunk(10, platformTransactionManager)
+                .reader(itemReader())
+                .processor(itemProcessor())
+                .writer(itemWriter())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public ItemWriter<Member> itemWriter() {
+        log.info(">>>>> ListItemWriter");
+
+        return memberRepository::saveAll;
+    }
+
+    @Bean
+    @StepScope
+    public ItemProcessor<Member, Member> itemProcessor() {
+        log.info(">>>>> ListItemProcessor");
+
+        return member ->{
+            member.changeMemberStatusInActive();
+            return member;
+        };
+    }
+
+    @Bean
+    @StepScope
+    public ItemReader<Member> itemReader() {
+        log.info(">>>>> ListItemReader");
+        List<Member> members = memberRepository.findAllByLoginAtBefore(LocalDateTime.now().minusYears(1L));
+        log.info(">>>>> InActive Member size {}", members.size());
+        return new ListItemReader<>(members);
+    }
+
 }
