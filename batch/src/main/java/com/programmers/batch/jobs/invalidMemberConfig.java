908ac3b002c49b25775b1dc82b7fbc3c001b8494
KarmaPol <kkhkr98@gmail.com>
2023-12-05 14:09:18 +0900

    build : 멀티 모듈 설정 완료

---
+++ batch/src/main/java/com/programmers/batch/jobs/invalidMemberConfig.java
@@ -0,0 +1,13 @@
+package com.programmers.batch.jobs;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@AllArgsConstructor
+@Configuration
+public class invalidMemberConfig {
+
+//    private MemberRepository
+}
