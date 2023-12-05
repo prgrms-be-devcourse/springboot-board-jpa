0f91afa6e83e4bd0023f3ec75a305eb19604445f
kimnamgyu <lethargic.hood@gmail.com>
2023-12-05 17:05:08 +0900

    Refactor : ApiResponse -> ResponseEntity 변경

--- src/main/java/com/prgrms/dev/springbootboardjpa/controller/PostController.java
+++ src/main/java/com/prgrms/dev/springbootboardjpa/controller/PostController.java
@@ -1,48 +1,49 @@
 package com.prgrms.dev.springbootboardjpa.controller;
 
-import com.prgrms.dev.springbootboardjpa.ApiResponse;
 import com.prgrms.dev.springbootboardjpa.dto.PostCreateRequestDto;
 import com.prgrms.dev.springbootboardjpa.dto.PostDto;
 import com.prgrms.dev.springbootboardjpa.dto.PostUpdateRequestDto;
 import com.prgrms.dev.springbootboardjpa.service.PostService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@AllArgsConstructor // 생성자 lombok ...
-@RequestMapping("/api/v1/posts") // 새로운 요구사항 -> native에서는 하위 버전을 고려하기 위해, 에러 발생 시 mapping되는 범위
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/posts")
 public class PostController {
 
     private final PostService postService;
 
     //단건 조회 -> find
     @GetMapping("/{id}")
-    public ApiResponse<PostDto> findById(@PathVariable Long id) {
+    public ResponseEntity<PostDto> findById(@PathVariable Long id) {
         PostDto postDto = postService.findById(id);
-        return ApiResponse.ok(postDto);
+        return ResponseEntity.status(HttpStatus.OK).body(postDto);
     }
 
     //페이지 조회 -> get
     @GetMapping
-    public ApiResponse<Page<PostDto>> getAll(Pageable pageable) {
+    public ResponseEntity<Page<PostDto>> getAll(Pageable pageable) {
         Page<PostDto> postDto = postService.getAll(pageable);
-        return ApiResponse.ok(postDto);
+        return ResponseEntity.status(HttpStatus.OK).body(postDto);
     }
 
     //생성
     @PostMapping
-    public ApiResponse<PostDto> create(@RequestBody PostCreateRequestDto postCreateRequestDto) {
+    public ResponseEntity<PostDto> create(@RequestBody PostCreateRequestDto postCreateRequestDto) {
         PostDto postDto = postService.create(postCreateRequestDto);
-        return ApiResponse.ok(postDto);
+        return ResponseEntity.status(HttpStatus.OK).body(postDto);
     }
 
     //수정
-    @PutMapping("/{id}")
-    public ApiResponse<PostDto> update(@RequestBody PostUpdateRequestDto postUpdateRequestDto, @PathVariable Long id) {
+    @PatchMapping("/{id}")
+    public ResponseEntity<PostDto> update(@RequestBody PostUpdateRequestDto postUpdateRequestDto, @PathVariable Long id) {
         PostDto update = postService.update(postUpdateRequestDto, id);
-        return ApiResponse.ok(update);
+        return ResponseEntity.status(HttpStatus.OK).body(update);
     }
 
     // 서로 다른 리턴 값을 줄 때 컴파일 시점에서 잡아줄 수 있는 에러 ...
--- src/main/java/com/prgrms/dev/springbootboardjpa/exception/GlobalExceptionHandler.java
+++ src/main/java/com/prgrms/dev/springbootboardjpa/exception/GlobalExceptionHandler.java
@@ -1,23 +1,25 @@
 package com.prgrms.dev.springbootboardjpa.exception;
 
-import com.prgrms.dev.springbootboardjpa.ApiResponse;
+import com.prgrms.dev.springbootboardjpa.controller.PostController;
 import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@RestControllerAdvice
+@RestControllerAdvice(basePackageClasses = PostController.class)
 public class GlobalExceptionHandler {
 
     private static final int NOT_FOUND = 404;
     private static final int INTERNAL_SERVER_ERROR = 500;
 
     @ExceptionHandler(ChangeSetPersister.NotFoundException.class)
-    public ApiResponse<String> notFoundHandler(ChangeSetPersister.NotFoundException e) {
-        return ApiResponse.fail(NOT_FOUND, e.getMessage());
+    public ResponseEntity<String> notFoundHandler(ChangeSetPersister.NotFoundException e) {
+        return ResponseEntity.status(NOT_FOUND).body(e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
-    public ApiResponse<String> internalServerErrorHandler(Exception e) {
-        return ApiResponse.fail(INTERNAL_SERVER_ERROR, e.getMessage());
-    } // 다음 타자 .................
+    public ResponseEntity<String> internalServerErrorHandler(Exception e) {
+        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
 }
