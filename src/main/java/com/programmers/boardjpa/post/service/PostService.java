e2017c6beb0622aeb37198dc42e59a0e55cf7ca6
letskuku <yujung9288@naver.com>
2023-12-09 16:00:43 +0900

    chore: Slf4j 어노테이션 추가

--- src/main/java/com/programmers/boardjpa/post/service/PostService.java
+++ src/main/java/com/programmers/boardjpa/post/service/PostService.java
@@ -14,11 +14,13 @@ import com.programmers.boardjpa.user.exception.UserErrorCode;
 import com.programmers.boardjpa.user.exception.UserException;
 import com.programmers.boardjpa.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
