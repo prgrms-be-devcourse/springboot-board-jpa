3689b053f16fcf2dd48906e909c9720e5d0c831f
park0jae <okvv26@naver.com>
2023-12-17 17:43:13 +0900

    comment : schema.sql 파일 테이블에 대한 코멘트 작성

--- board-api/src/main/resources/sql/schema.sql
+++ board-api/src/main/resources/sql/schema.sql
@@ -1,53 +1,58 @@
+-- 회원의 정보를 저장하는 테이블
 CREATE TABLE IF NOT EXISTS member (
-    member_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(25) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    name VARCHAR(15) NOT NULL,
-    age INT NOT NULL,
-    hobby VARCHAR(255) NOT NULL,
-    is_deleted BIT(1) NOT NULL DEFAULT FALSE,
-    last_updated_password TIMESTAMP(6),
-    created_at TIMESTAMP(6),
-    updated_at TIMESTAMP(6),
-    updated_by VARCHAR(25)
+    member_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, # 각 회원의 고유 식별자
+    email VARCHAR(25) NOT NULL UNIQUE, # 회원 이메일 주소 (고유)
+    password VARCHAR(255) NOT NULL, # 회원 비밀번호
+    name VARCHAR(15) NOT NULL, # 회원 이름
+    age INT NOT NULL, # 회원 나이
+    hobby VARCHAR(255) NOT NULL, # 회원 취미
+    is_deleted BIT(1) NOT NULL DEFAULT FALSE, # 회원 삭제 여부
+    last_updated_password TIMESTAMP(6), # 회원의 마지막 비밀번호 변경 시점
+    created_at TIMESTAMP(6), # 회원 생성 시각
+    updated_at TIMESTAMP(6), # 회원 수정 시각
+    updated_by VARCHAR(25) # 회원을 마지막으로 수정한 수정자
 );
 
+-- 게시물 정보를 저장하는 테이블
 CREATE TABLE IF NOT EXISTS post (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(50) NOT NULL,
-    content VARCHAR(255) NOT NULL,
-    view INT DEFAULT 0,
-    created_at TIMESTAMP(6),
-    updated_at TIMESTAMP(6),
-    updated_by VARCHAR(25),
-    member_id INT UNSIGNED NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY, # 각 게시물 고유 식별자
+    title VARCHAR(50) NOT NULL, # 게시물 제목
+    content VARCHAR(255) NOT NULL, # 게시물 내용
+    view INT DEFAULT 0, # 게시물 조회수
+    created_at TIMESTAMP(6), # 게시물 생성 시각
+    updated_at TIMESTAMP(6), # 게시물 수정 시각
+    updated_by VARCHAR(25), # 게시물을 마지막으로 수정한 수정자
+    member_id INT UNSIGNED NOT NULL # 회원을 참조하는 외래 키
 );
 
+-- 역할 정보를 저장하는 테이블
 CREATE TABLE IF NOT EXISTS role (
-    role_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    role_type VARCHAR(10) NOT NULL
+    role_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, # 각 역할의 고유 식별자
+    role_type VARCHAR(10) NOT NULL # 역할의 유형 (USER , ADMIN ..)
 );
 
+-- 회원과 역할 간의 관계를 저장하는 테이블
 CREATE TABLE IF NOT EXISTS member_role (
-    member_role_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    member_id INT UNSIGNED,
-    role_id INT UNSIGNED
+    member_role_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, # 각 회원-역할 관계의 고유 식별자
+    member_id INT UNSIGNED, # 회원을 참조하는 외래 키
+    role_id INT UNSIGNED # 역할을 참조하는 외래 키
 );
 
+-- 댓글 정보를 저장하는 테이블
 CREATE TABLE IF NOT EXISTS comment (
-     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-     content VARCHAR(255) NOT NULL,
-     member_id INT,
-     post_id INT,
-     parent_id INT,
-     created_at TIMESTAMP(6),
-     updated_at TIMESTAMP(6),
-     updated_by VARCHAR(25)
+     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, # 각 댓글의 고유 식별자
+     content VARCHAR(255) NOT NULL, # 댓글 내용
+     member_id INT, # 회원을 참조하는 외래 키
+     post_id INT, # 게시물을 참조하는 외래 키
+     parent_id INT, # 부모 댓글을 참조하는 외래 키
+     created_at TIMESTAMP(6), # 댓글 생성 시각
+     updated_at TIMESTAMP(6), # 댓글 수정 시각
+     updated_by VARCHAR(25) # 댓글을 마지막으로 업데이트한 수정자
 );
 
+-- 이메일 인증 정보를 저장하는 테이블
 CREATE TABLE IF NOT EXISTS email_auth (
-    auth_key VARCHAR(255) NOT NULL,
-    email VARCHAR(25) NOT NULL,
-    purpose VARCHAR(15) NOT NULL,
-    PRIMARY KEY (auth_key)
+    auth_key VARCHAR(255) NOT NULL PRIMARY KEY , # 이메일 인증 키
+    email VARCHAR(25) NOT NULL, # 이메일 주소
+    purpose VARCHAR(15) NOT NULL # 인증 목적
 );
