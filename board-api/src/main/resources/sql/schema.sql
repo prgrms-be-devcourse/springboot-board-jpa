f3b13fec89a19a742617dfb564eca6e75731e630
park0jae <okvv26@naver.com>
2023-12-20 14:42:15 +0900

    test : MemberServiceTest 수정

--- board-api/src/main/resources/sql/schema.sql
+++ board-api/src/main/resources/sql/schema.sql
@@ -1,62 +1,62 @@
 -- 회원의 정보를 저장하는 테이블
 CREATE TABLE IF NOT EXISTS member (
-    member_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, # 각 회원의 고유 식별자
-    email VARCHAR(25) NOT NULL UNIQUE, # 회원 이메일 주소 (고유)
-    password VARCHAR(255) NOT NULL, # 회원 비밀번호
-    name VARCHAR(15) NOT NULL, # 회원 이름
-    age INT NOT NULL, # 회원 나이
-    hobby VARCHAR(255) NOT NULL, # 회원 취미
-    is_deleted BIT(1) NOT NULL DEFAULT FALSE, # 회원 삭제 여부
-    last_updated_password TIMESTAMP(6), # 회원의 마지막 비밀번호 변경 시점
-    created_at TIMESTAMP(6), # 회원 생성 시각
-    updated_at TIMESTAMP(6), # 회원 수정 시각
-    updated_by VARCHAR(25), # 회원을 마지막으로 수정한 수정자
-    INDEX idx_email (email) # 이메일을 참조하는 인덱스 추가
+    member_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- 각 회원의 고유 식별자
+    email VARCHAR(25) NOT NULL UNIQUE, -- 회원 이메일 주소 (고유)
+    password VARCHAR(255) NOT NULL, -- 회원 비밀번호
+    name VARCHAR(15) NOT NULL, -- 회원 이름
+    age INT NOT NULL, -- 회원 나이
+    hobby VARCHAR(255) NOT NULL, -- 회원 취미
+    is_deleted BIT(1) NOT NULL DEFAULT FALSE, -- 회원 삭제 여부
+    last_updated_password TIMESTAMP(6), -- 회원의 마지막 비밀번호 변경 시점
+    created_at TIMESTAMP(6), -- 회원 생성 시각
+    updated_at TIMESTAMP(6), -- 회원 수정 시각
+    updated_by VARCHAR(25), -- 회원을 마지막으로 수정한 수정자
+    INDEX idx_email (email) -- 이메일을 참조하는 인덱스 추가
 );
 
 -- 게시물 정보를 저장하는 테이블
 CREATE TABLE IF NOT EXISTS post (
-    id INT AUTO_INCREMENT PRIMARY KEY, # 각 게시물 고유 식별자
-    title VARCHAR(50) NOT NULL, # 게시물 제목
-    content VARCHAR(255) NOT NULL, # 게시물 내용
-    view INT DEFAULT 0, # 게시물 조회수
-    created_at TIMESTAMP(6), # 게시물 생성 시각
-    updated_at TIMESTAMP(6), # 게시물 수정 시각
-    updated_by VARCHAR(25), # 게시물을 마지막으로 수정한 수정자
-    member_id INT UNSIGNED NOT NULL, # 회원을 참조하는 외래 키
-    INDEX idx_member_id (member_id), # 회원을 참조하는 외래 키에 대한 인덱스 추가
-    INDEX idx_title (title) # 제목을 참조하는 인덱스 추가
+    id INT AUTO_INCREMENT PRIMARY KEY, -- 각 게시물 고유 식별자
+    title VARCHAR(50) NOT NULL, -- 게시물 제목
+    content VARCHAR(255) NOT NULL, -- 게시물 내용
+    view INT DEFAULT 0, -- 게시물 조회수
+    created_at TIMESTAMP(6), -- 게시물 생성 시각
+    updated_at TIMESTAMP(6), -- 게시물 수정 시각
+    updated_by VARCHAR(25), -- 게시물을 마지막으로 수정한 수정자
+    member_id INT UNSIGNED NOT NULL, -- 회원을 참조하는 외래 키
+    INDEX idx_member_id (member_id), -- 회원을 참조하는 외래 키에 대한 인덱스 추가
+    INDEX idx_title (title) -- 제목을 참조하는 인덱스 추가
 );
 
 -- 역할 정보를 저장하는 테이블
 CREATE TABLE IF NOT EXISTS role (
-    role_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, # 각 역할의 고유 식별자
-    role_type VARCHAR(10) NOT NULL # 역할의 유형 (USER , ADMIN ..)
+    role_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- 각 역할의 고유 식별자
+    role_type VARCHAR(10) NOT NULL -- 역할의 유형 (USER , ADMIN ..)
 );
 
 -- 회원과 역할 간의 관계를 저장하는 테이블
 CREATE TABLE IF NOT EXISTS member_role (
-    member_role_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, # 각 회원-역할 관계의 고유 식별자
-    member_id INT UNSIGNED, # 회원을 참조하는 외래 키
-    role_id INT UNSIGNED # 역할을 참조하는 외래 키
+    member_role_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- 각 회원-역할 관계의 고유 식별자
+    member_id INT UNSIGNED, -- 회원을 참조하는 외래 키
+    role_id INT UNSIGNED -- 역할을 참조하는 외래 키
 );
 
 -- 댓글 정보를 저장하는 테이블
 CREATE TABLE IF NOT EXISTS comment (
-     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, # 각 댓글의 고유 식별자
-     content VARCHAR(255) NOT NULL, # 댓글 내용
-     member_id INT, # 회원을 참조하는 외래 키
-     post_id INT, # 게시물을 참조하는 외래 키
-     parent_id INT, # 부모 댓글을 참조하는 외래 키
-     created_at TIMESTAMP(6), # 댓글 생성 시각
-     updated_at TIMESTAMP(6), # 댓글 수정 시각
-     updated_by VARCHAR(25), # 댓글을 마지막으로 업데이트한 수정자
-     INDEX idx_post_id (post_id) # 게시물을 참조하는 외래 키에 대한 인덱스 추가
+     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, -- 각 댓글의 고유 식별자
+     content VARCHAR(255) NOT NULL, -- 댓글 내용
+     member_id INT, -- 회원을 참조하는 외래 키
+     post_id INT, -- 게시물을 참조하는 외래 키
+     parent_id INT, -- 부모 댓글을 참조하는 외래 키
+     created_at TIMESTAMP(6), -- 댓글 생성 시각
+     updated_at TIMESTAMP(6), -- 댓글 수정 시각
+     updated_by VARCHAR(25), -- 댓글을 마지막으로 업데이트한 수정자
+     INDEX idx_post_id (post_id) -- 게시물을 참조하는 외래 키에 대한 인덱스 추가
 );
 
 -- 이메일 인증 정보를 저장하는 테이블
 CREATE TABLE IF NOT EXISTS email_auth (
-    auth_key VARCHAR(255) NOT NULL PRIMARY KEY , # 이메일 인증 키
-    email VARCHAR(25) NOT NULL, # 이메일 주소
-    purpose VARCHAR(15) NOT NULL # 인증 목적
+    auth_key VARCHAR(255) NOT NULL PRIMARY KEY , -- 이메일 인증 키
+    email VARCHAR(25) NOT NULL, -- 이메일 주소
+    purpose VARCHAR(15) NOT NULL -- 인증 목적
 );
--- board-api/src/test/java/com/example/board/domain/member/service/MemberServiceTest.java
+++ board-api/src/test/java/com/example/board/domain/member/service/MemberServiceTest.java
@@ -1,7 +1,18 @@
 package com.example.board.domain.member.service;
 
-import com.example.board.domain.email.entity.EmailAuth;
-import com.example.board.domain.email.repository.EmailAuthRepository;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+
 import com.example.board.domain.member.dto.LoginRequest;
 import com.example.board.domain.member.dto.LoginResponse;
 import com.example.board.domain.member.dto.MemberCreateRequest;
@@ -10,203 +21,187 @@ import com.example.board.domain.member.dto.MemberUpdateRequest;
 import com.example.board.domain.member.dto.PasswordResetRequest;
 import com.example.board.domain.member.entity.Member;
 import com.example.board.domain.member.repository.MemberRepository;
-import com.example.board.domain.role.entity.Role;
-import com.example.board.domain.role.entity.RoleType;
 import com.example.board.domain.role.repository.RoleRepository;
 import com.example.board.global.exception.CustomException;
 import com.example.board.global.security.jwt.provider.JwtTokenProvider;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.List;
-import java.util.Optional;
-import org.springframework.test.context.ActiveProfiles;
-
-import static com.example.board.factory.member.MemberFactory.createMemberWithRoleUser;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
-import static org.mockito.BDDMockito.given;
+import com.example.board.global.util.RedisService;
 
 @SpringBootTest
 @ActiveProfiles("dev")
 class MemberServiceTest {
 
-    @Autowired
-    private MemberService memberService;
+	@Autowired
+	private MemberService memberService;
 
-    @Autowired
-    private MemberRepository memberRepository;
+	@Autowired
+	private MemberRepository memberRepository;
 
-    @Autowired
-    private RoleRepository roleRepository;
+	@Autowired
+	private RoleRepository roleRepository;
 
-    @Autowired
-    private EmailAuthRepository emailAuthRepository;
+	@Autowired
+	private RedisService redisService;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private AuthenticationManagerBuilder authenticationManager;
+	@Autowired
+	private AuthenticationManagerBuilder authenticationManager;
 
-    @Autowired
-    private JwtTokenProvider jwtTokenProvider;
+	@Autowired
+	private JwtTokenProvider jwtTokenProvider;
 
-    private Member member;
+	private Member member;
 
-    @BeforeEach
-    void setUp() {
-        member = memberRepository.findById(2L).get();
-        emailAuthRepository.save(new EmailAuth("123456", "test@naver.com", "SIGN-UP"));
-        emailAuthRepository.save(new EmailAuth("234567", "user123@naver.com", "RESET-PASSWORD"));
-    }
+	@BeforeEach
+	void setUp() {
+		member = memberRepository.findById(2L).get();
+		redisService.setEmail("test@naver.com", "123456", 60000L, "SIGN-UP");
+		redisService.setEmail("user123@naver.com", "234567", 60000L, "RESET-PASSWORD");
+	}
 
-    @Test
-    void 회원가입_테스트() {
-        // Given
-        MemberCreateRequest request = new MemberCreateRequest(
-            "test@naver.com",
-            "test123!",
-            "tester2",
-            25,
-            "축구",
-            "123456"
-        );
+	@AfterEach
+	void tearDown() {
+		redisService.deleteEmail("test@naver.com", "SIGN-UP");
+		redisService.deleteEmail("user123@naver.com", "RESET-PASSWORD");
+	}
 
-        // When
-        MemberResponse signUpMember = memberService.createMember(request);
+	@Test
+	void 회원가입_테스트() {
+		// Given
+		MemberCreateRequest request = new MemberCreateRequest(
+			"test@naver.com",
+			"test123!",
+			"tester2",
+			25,
+			"축구",
+			"123456"
+		);
 
-        // Then
-        assertThat(signUpMember.email()).isEqualTo(request.email());
-    }
+		// When
+		MemberResponse signUpMember = memberService.createMember(request);
 
-    @Test
-    void 중복_이메일_회원가입_실패_테스트() {
-        // Given
-        MemberCreateRequest request = new MemberCreateRequest(
-            "user123@naver.com",
-            "test123!",
-            "tester2",
-            25,
-            "축구",
-            "345678"
-        );
+		// Then
+		assertThat(signUpMember.email()).isEqualTo(request.email());
+	}
 
-        // When & Then
-        assertThatThrownBy(() -> memberService.createMember(request))
-            .isInstanceOf(CustomException.class);
-    }
+	@Test
+	void 중복_이메일_회원가입_실패_테스트() {
+		// Given
+		MemberCreateRequest request = new MemberCreateRequest(
+			"user123@naver.com",
+			"test123!",
+			"tester2",
+			25,
+			"축구",
+			"345678"
+		);
 
-    @Test
-    void 이메일_인증키_불일치_회원가입_실패_테스트() {
-        // Given
-        String notExistAuthKey = "000000";
-        MemberCreateRequest memberCreateRequest = new MemberCreateRequest(
-            "newUser123@naver.com",
-            "test123!",
-            "tester2",
-            25,
-            "축구",
-            notExistAuthKey
-        );
+		// When & Then
+		assertThatThrownBy(() -> memberService.createMember(request))
+			.isInstanceOf(CustomException.class);
+	}
 
-        // When & Then
-        assertThatThrownBy(() -> memberService.createMember(memberCreateRequest))
-            .isInstanceOf(CustomException.class);
-    }
+	@Test
+	void 이메일_인증키_불일치_회원가입_실패_테스트() {
+		// Given
+		String notExistAuthKey = "000000";
+		MemberCreateRequest memberCreateRequest = new MemberCreateRequest(
+			"newUser123@naver.com",
+			"test123!",
+			"tester2",
+			25,
+			"축구",
+			notExistAuthKey
+		);
 
-    @Test
-    void 로그인_테스트() {
-        // Given
-        LoginRequest loginRequest = new LoginRequest(
-            "admin123@naver.com",
-            "admin123!"
-        );
+		// When & Then
+		assertThatThrownBy(() -> memberService.createMember(memberCreateRequest))
+			.isInstanceOf(CustomException.class);
+	}
 
-        // When
-        LoginResponse loginMember = memberService.login(loginRequest);
+	@Test
+	void 로그인_테스트() {
+		// Given
+		LoginRequest loginRequest = new LoginRequest(
+			"admin123@naver.com",
+			"admin123!"
+		);
 
-        // Then
-        assertThat(loginMember.username()).isEqualTo(loginRequest.email());
-    }
+		// When
+		LoginResponse loginMember = memberService.login(loginRequest);
 
-    @Test
-    void 비밀번호_불일치_로그인_실패_테스트() {
-        // Given
-        String notExistPassword = "notExistPassword123!";
-        LoginRequest loginRequest = new LoginRequest(
-            member.getEmail(),
-            notExistPassword
-        );
+		// Then
+		assertThat(loginMember.username()).isEqualTo(loginRequest.email());
+	}
 
-        // When
-        assertThatThrownBy(() -> memberService.login(loginRequest))
-            .isInstanceOf(CustomException.class);
-    }
+	@Test
+	void 비밀번호_불일치_로그인_실패_테스트() {
+		// Given
+		String notExistPassword = "notExistPassword123!";
+		LoginRequest loginRequest = new LoginRequest(
+			member.getEmail(),
+			notExistPassword
+		);
 
-    @Test
-    void 비밀번호_재설정_테스트() {
-        // Given
-        PasswordResetRequest request = new PasswordResetRequest(
-            member.getEmail(),
-            "234567",
-            "newPassword123!",
-            "newPassword123!"
-        );
+		// When
+		assertThatThrownBy(() -> memberService.login(loginRequest))
+			.isInstanceOf(CustomException.class);
+	}
 
-        // When
-        Throwable thrown = catchThrowable(() -> memberService.resetPassword(request));
+	@Test
+	void 비밀번호_재설정_테스트() {
+		// Given
+		PasswordResetRequest request = new PasswordResetRequest(
+			member.getEmail(),
+			"234567",
+			"newPassword123!",
+			"newPassword123!"
+		);
 
-        // Then
-        assertThat(thrown).isNull();
-    }
-    
-    @Test
-    void 회원_아이디로_조회_테스트() {
-        // Given
-        Long memberId = 2L;
+		// When
+		Throwable thrown = catchThrowable(() -> memberService.resetPassword(request));
 
-        // When
-        MemberResponse findMember = memberService.findMemberById(memberId);
+		// Then
+		assertThat(thrown).isNull();
+	}
 
-        // Then
-        assertThat(findMember.id()).isEqualTo(memberId);
-    }
+	@Test
+	void 회원_아이디로_조회_테스트() {
+		// Given
+		Long memberId = 2L;
 
-    @Test
-    void 회원_아이디로_조회_실패_테스트() {
-        // Given
-        Long notExistId = 5L;
+		// When
+		MemberResponse findMember = memberService.findMemberById(memberId);
 
-        // When & Then
-        assertThatThrownBy(() -> memberService.findMemberById(notExistId))
-            .isInstanceOf(CustomException.class);
-    }
+		// Then
+		assertThat(findMember.id()).isEqualTo(memberId);
+	}
 
-    @Test
-    void 회원_수정_테스트() {
-        // Given
-        Long memberId = member.getId();
-        MemberUpdateRequest request = new MemberUpdateRequest(
-            "수정된 일반 유저",
-            "수정된 취미"
-        );
+	@Test
+	void 회원_아이디로_조회_실패_테스트() {
+		// Given
+		Long notExistId = 5L;
 
-        // When
-        MemberResponse updatedMember = memberService.updateMember(memberId, request);
+		// When & Then
+		assertThatThrownBy(() -> memberService.findMemberById(notExistId))
+			.isInstanceOf(CustomException.class);
+	}
 
-        // Then
-        assertThat(updatedMember.name()).isEqualTo(request.name());
-        assertThat(updatedMember.hobby()).isEqualTo(request.hobby());
-    }
+	@Test
+	void 회원_수정_테스트() {
+		// Given
+		Long memberId = member.getId();
+		MemberUpdateRequest request = new MemberUpdateRequest(
+			"수정된 일반 유저",
+			"수정된 취미"
+		);
+
+		// When
+		MemberResponse updatedMember = memberService.updateMember(memberId, request);
+
+		// Then
+		assertThat(updatedMember.name()).isEqualTo(request.name());
+		assertThat(updatedMember.hobby()).isEqualTo(request.hobby());
+	}
 }
