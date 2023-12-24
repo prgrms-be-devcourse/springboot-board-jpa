e0d36f049bbba9e2650fc06754dc7bacd9ecbe21
ebcho <joeunbi514@naver.com>
2023-12-24 10:37:58 +0900

    style: 롬복으로 수정

--- src/main/java/com/programmers/springboard/global/config/JwtConfig.java
+++ src/main/java/com/programmers/springboard/global/config/JwtConfig.java
@@ -3,48 +3,17 @@ package com.programmers.springboard.global.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Component
 @ConfigurationProperties(prefix = "jwt")
 public class JwtConfig {
 
 	private String header;
-
 	private String issuer;
-
 	private String clientSecret;
-
 	private int expirySeconds;
-
-	public String getHeader() {
-		return header;
-	}
-
-	public void setHeader(String header) {
-		this.header = header;
-	}
-
-	public String getIssuer() {
-		return issuer;
-	}
-
-	public void setIssuer(String issuer) {
-		this.issuer = issuer;
-	}
-
-	public String getClientSecret() {
-		return clientSecret;
-	}
-
-	public void setClientSecret(String clientSecret) {
-		this.clientSecret = clientSecret;
-	}
-
-	public int getExpirySeconds() {
-		return expirySeconds;
-	}
-
-	public void setExpirySeconds(int expirySeconds) {
-		this.expirySeconds = expirySeconds;
-	}
-
 }
